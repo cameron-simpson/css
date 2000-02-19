23b0f85e586816ea9f92f8d1f1dac9ef59ac14c8
Cameron Simpson <cs@zip.com.au>
2000-02-19 04:01:09 +0000

    Initial commit for new CVS repository. [CVS rev 1.1]

---
+++ cs/PhoneDB.pm
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+#
+# Phone database stuff.
+#	- Cameron Simpson <cs@zip.com.au> 10dec97
+#
+
+use strict qw(vars);
+
+BEGIN { use cs::DEBUG; cs::DEBUG::using(__FILE__); }
+
+use cs::Persist;
+
+package cs::PhoneDB;
+
+sub db	{ cs::Persist
+
+1;
