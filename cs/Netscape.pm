ee2908d3f2bf4c1225b4cb420bfd3a4c36351fde
Cameron Simpson <cs@zip.com.au>
2000-02-19 04:01:08 +0000

    Initial commit for new CVS repository. [CVS rev 1.1]

---
+++ cs/Netscape.pm
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+#
+# Do things with netscape stuff.
+#	- Cameron Simpson <cs@zip.com.au> 10aug99
+#
+
+use strict qw(vars);
+
+BEGIN { use cs::DEBUG; cs::DEBUG::using(__FILE__); }
+
+package cs::Netscape;
+
+@cs::Netscape::ISA=qw();
+
+# read the cookies file and return a list of cookie specs
+sub cookies
+{ my($file)=@_;
+  $file="$ENV{HOME}/private/netscape/cookies" if ! defined $file;
+
+  return () if ! open(COOKIES,"< $file\0");
+
+  my @cookies=();
+
+  COOKIE:
+  while (<COOKIES>)
+  { next COOKIE unless /^([^#\s]\S*)\s+(TRUE|FALSE)\s+(\/\S*)\s+(TRUE|FALSE)\s+(\d+)\s+(\S+)\s+(\S+)$/;
+
+    push(@cookies, { DOMAIN => lc($1),
+		     DOMAINWIDE => $2 eq TRUE,
+		     PATH => $3,
+		     SECURE => $4 eq TRUE,
+		     EXPIRES => $5+0,
+		     NAME => $6,
+		     VALUE => $7,
+		   });
+  }
+
+  @cookies;
+}
+
+1;
