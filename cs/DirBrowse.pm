4c35a4969235afc18c074347e82627350a2677e9
Cameron Simpson <cs@zip.com.au>
2000-02-19 04:01:06 +0000

    Initial commit for new CVS repository. [CVS rev 1.1]

---
+++ cs/DirBrowse.pm
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+#
+
+use strict qw(vars);
+
+BEGIN { use cs::DEBUG; cs::DEBUG::using(__FILE__); }
+
+package DirBrowse;
+
+@cs::DirBrowse::ISA=qw();
+
+sub new
+	{ my($class,$type)=(shift,shift);
+
+	  if ($type eq 
+	}
+
+1;
