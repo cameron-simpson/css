5cda440f126f6dc7db0d03b5112f41fde791b77d
Cameron Simpson <cs@zip.com.au>
2000-02-19 04:01:06 +0000

    Initial commit for new CVS repository. [CVS rev 1.1]

---
+++ cs/Encode.pm
@@ -0,0 +1,59 @@
+#!/usr/bin/perl
+#
+# This is a front end for a Sink.
+#	- Cameron Simpson <cs@zip.com.au> 23may96
+#
+# new cs::Encode Sink, \&encoder[, args-to-encoder]
+#   The encoder routine takes a data string and the args-to-encoder
+#   and returns encoded data. It returns undef if there isn't enough
+#   data to encode (in which case it is expected to save the unprocessed
+#   data in some state area probably referenced by the args-to-encoder
+#   for later processing). It is called with undef as data at the end
+#   as a sentinel to flush any such unprocessed data.
+#
+
+use strict qw(vars);
+
+BEGIN { use cs::DEBUG; cs::DEBUG::using(__FILE__); }
+
+use cs::Sink;
+
+package cs::Encode;
+
+@cs::Encode::ISA=qw(cs::Sink);
+
+sub new
+	{ my($class,$s,$encoder)=(shift,shift,shift);
+	  my($this);
+	  $this=new cs::Sink Sink, $s;
+	  $this->{ENCODE}=$encoder;
+	  $this->{ENCODE_ARGS}=[ @_ ];
+
+	  bless $this, $class;
+	}
+
+sub DESTROY
+	{ my($this)=shift;
+
+	  # print STDERR "DESTROY(",Hier::h2a($this),")\n";
+
+	  $this->Write(undef);
+	  $this->SUPER::DESTROY();
+	}
+
+sub Write
+	{ my($this,$data)=@_;
+	  my($edata);
+
+	  $edata=&{$this->{ENCODE}}($data,@{$this->{ENCODE_ARGS}});
+
+	  return undef if ! defined $edata;
+
+	  # print STDERR "Encode::Write: this=",Hier::h2a($this),"\n";
+	  # print STDERR "\tPut($edata)\n";
+	  $this->{DS}->Put($edata);
+
+	  length $data;
+	}
+
+1;
