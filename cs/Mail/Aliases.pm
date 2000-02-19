c8da4e9fd3fcba185fb84ad45e77a0f01ff8da3f
Cameron Simpson <cs@zip.com.au>
2000-02-19 04:01:43 +0000

    Initial commit for new CVS repository. [CVS rev 1.1]

---
+++ cs/Mail/Aliases.pm
@@ -0,0 +1,247 @@
+#!/usr/bin/perl
+#
+# Handle mail aliases.
+#	- Cameron Simpson <cs@zip.com.au> 
+#
+
+use strict qw(vars);
+
+BEGIN { use cs::DEBUG; cs::DEBUG::using(__FILE__); }
+
+package cs::Mail::Aliases;
+
+@cs::Mail::Aliases::ISA=qw();
+
+sub db
+{ my($path,$rw)=@_;
+  $rw=0 if ! defined $rw;
+
+  my($db)=cs::Persist::db($path,$rw);
+
+  return undef if ! defined $db;
+
+  my($adb)=new cs::Mail::Aliases;
+
+  $adb->AddDB($db);
+
+  $adb;
+}
+
+sub new
+{ my($class)=@_;
+  die if @_ != 1;
+
+  my($this)={ ALIASES => {},
+	      DBS     => [],
+	    };
+
+  bless $this, $class;
+}
+
+sub Value
+{ my($this,$key)=@_;
+
+  my($a);
+
+  return undef if ! defined ($a=$this->Alias($key));
+
+  $a->{VALUE};
+}
+
+sub Context
+{ my($this)=@_;
+
+  return "" if ! exists $this->{CONTEXT};
+  $this->{CONTEXT};
+}
+
+sub newEntry
+{ my($value,$context)=@_;
+
+  my($this)={ VALUE => $value };
+
+  $this->{CONTEXT}=$context if defined $context && length $context;
+
+  bless $this, cs::Mail::Aliases;
+}
+
+sub AddDB
+{ my($this,$db)=(shift,shift);
+
+  my($pushdb)=$db;
+
+  if (@_)
+	{ $pushdb={};
+	  for (@_)
+		{ $pushdb->{$_}=$db->{$_};
+		}
+	}
+
+  push(@{$this->{DBS}},$pushdb);
+}
+
+sub Add
+{ my($this,$key,$value,$force,$context)=@_;
+  $force=0 if ! defined $force;
+
+  if ($force || ! exists $this->{ALIASES}->{$key})
+	{ $this->{ALIASES}->{$key}=newEntry($value,$context);
+	}
+}
+
+sub implAliases
+{ my($key,$entry)=@_;
+  ## warn "e=$entry\n";
+
+  return {} if ! exists $entry->{EMAIL};
+
+  my $impl = {};
+
+  my($addrs)=$entry->{EMAIL};
+  my(@addrs)=keys %$addrs;
+  ## warn "$key: [@addrs]\n";
+  ## warn "addrs=".cs::Hier::h2a($addrs,1)."\n";
+  ## warn cs::Hier::h2a($entry,1)."\n";
+
+  my($aval,@sfx,$addrtext);
+
+  my $n = 0;
+
+  for my $addr (sort @addrs)
+  { $aval=$addrs->{$addr};
+    if (! ref $aval)
+    { @sfx=$aval;
+    }
+    elsif (exists $aval->{TAGS})
+    { @sfx=@{$aval->{TAGS}};
+    }
+    else
+    { @sfx=();
+    }
+
+    @sfx=grep(! /OLD/ && ! /BOGUS/, @sfx);
+    if (! @sfx)
+    { $n++;
+      @sfx=".$n";	# ($n > 1 ? $n : '');
+    }
+
+    $addrtext=$entry->AddrText($addr);
+
+    ## warn "$key => $addrtext\n";
+    for my $sfx (@sfx)
+    {
+      ## warn "$key$sfx => $addrtext\n";
+      if (! exists $impl->{$key.$sfx})
+      { $impl->{$key.$sfx}=$addrtext;
+      }
+
+      if (! exists $impl->{$key}
+       && $sfx !~ /^\.\d+$/)
+      { $impl->{$key}=$addrtext;
+      }
+    }
+  }
+
+  $impl;
+}
+
+sub Expand
+{ my($this,$text)=@_;
+  $this->_Expand($text,{});
+}
+
+sub _Expand
+{ my($this,$text,$live)=@_;
+  my($ndx)=cs::RFC822::addrSet($text);
+
+  my($useaddr)=new cs::Set;
+  my($exp);
+  my($purge);
+
+  for my $addr ($ndx->KEYS())
+	{ if ($purge=($addr =~ /^!\s*/))
+		{ $addr=$';
+		}
+
+	  $exp=$this->_ExpAlias($addr,$live);
+	  if (! $exp)
+		# $addr isn't an alias name - use as-is
+		{ ($exp=new cs::Set)->STORE($addr,$ndx->FETCH($addr));
+		}
+
+	  # array of expanded addresses
+	  if ($purge)
+		{ for my $subaddr ($exp->KEYS())
+			{ $useaddr->DELETE($subaddr);
+			}
+		}
+	  else	{ for my $subaddr ($exp->KEYS())
+			{ $useaddr->STORE($subaddr,
+					  $exp->FETCH($subaddr));
+			}
+		}
+	}
+
+  $useaddr;
+}
+
+sub _ExpAlias
+{ my($this,$addr,$live)=@_;
+
+  # loop detection
+  return 0 if exists $live->{$addr};
+
+  my($a)=$this->Alias($addr);
+
+  return 0 if ! defined $a;
+
+  $live->{$addr}=1;
+
+  my($exp)=$this->_Expand($a->{VALUE},$live);
+
+  delete $live->{$addr};
+
+  $exp;
+}
+
+sub Aliases
+{ my($this)=@_;
+
+  my(%hash);
+
+  map($hash{$_}=1, keys %{$this->{ALIASES}});
+  my($db);
+  for $db (@{$this->{DBS}})
+	{ map($hash{$_}=1, keys %$db);
+	}
+
+  keys %hash;
+}
+
+# get alias record, or undef
+sub Alias
+{ my($this,$alias)=@_;
+
+  my($A)=$this->{ALIASES};
+
+  if (exists $A->{$alias})
+	{ return undef if ! defined $A->{$alias};
+	  return $A->{$alias};
+	}
+
+  DB:
+    for (@{$this->{DBS}})
+	{
+	  next DB if ! exists $_->{$alias};
+
+	  # cache it and return
+	  my($v)=$_->{$alias};
+	  $v={VALUE => $v} if ! ref $v;
+	  $A->{$alias}=bless $v, cs::Mail::Aliases;
+	  return $A->{$alias};
+	}
+
+  return undef;
+}
+
+1;
