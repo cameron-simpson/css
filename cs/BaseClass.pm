30d8cf66c495ea79f0184270682e0f4aef868284
Cameron Simpson <cs@zip.com.au>
2000-02-19 04:01:05 +0000

    Initial commit for new CVS repository. [CVS rev 1.1]

---
+++ cs/BaseClass.pm
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+#
+# Basic routines to supply the META component of a base class.
+#	- Cameron Simpson <cs@zip.com.au> 17may96
+#
+
+use strict qw(vars);
+
+BEGIN { use cs::DEBUG; cs::DEBUG::using(__FILE__); }
+
+use cs::Object;
+
+package cs::BaseClass;
+
+@cs::BaseClass::ISA=(cs::Object);
+
+sub DESTROY	{ SUPER::DESTROY(@_); }
+
+# the meta data for the immediate superclass
+sub _Meta
+	{ my($this)=@_;
+	  die if @_ != 1;
+	  $this->{META}={} if ! exists $this->{META};
+	  $this->{META};
+	}
+
+# the meta data for the supers of the immediate super
+sub Meta
+	{ my($_meta)=_Meta(@_);
+	  $_meta->{META}={} if ! exists $_meta->{META};
+	  $_meta->{META};
+	}
+
+1;
