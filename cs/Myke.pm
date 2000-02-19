46ab9ab19089ddbe0178832bfa7694292e9334a7
Cameron Simpson <cs@zip.com.au>
2000-02-19 04:01:08 +0000

    Initial commit for new CVS repository. [CVS rev 1.1]

---
+++ cs/Myke.pm
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+#
+# Myke - my make program
+#
+#	- Cameron Simpson <cs@zip.com.au> 02nov97
+#
+
+use strict qw(vars);
+
+BEGIN { use cs::DEBUG; cs::DEBUG::using(__FILE__); }
+
+use cs::ALL;
+
+package cs::Myke;	cs::ALL::useAll();
+
+sub new
+	{ my($class)=@_;
+
+	  bless { MACROS	=> {},
+		  LEVEL		=> 0,
+		  DEPS		=> {},
+		  RULES		=> [],	# first to last
+		}, $class;
+	}
+
+sub FindMacro
+	{ my($this,$macro,$dp)=@_;
+	  
+	  return undef if ! exists $this->{MACROS}->{$macro};
+
+	  $this->{MACROS}->{$macro};
+	}
+
+sub SetMacro
+	{ my($this,$context,$level,$macro,$args,$mvalue)=@_;
+	  $this->{MACROS}->{$macro}=new cs::Myke::Macro ($context,
+							 $level,
+							 $macro,
+							 $args,
+							 $mvalue);
+	}
+
+1;
