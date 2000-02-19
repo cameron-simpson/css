a4d89ff5b3775ee48d56de231ee16778c2662f02
Cameron Simpson <cs@zip.com.au>
2000-02-19 04:01:08 +0000

    Initial commit for new CVS repository. [CVS rev 1.1]

---
+++ cs/MsgidIndex.pm
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+#
+# Index data keyed on RFC822 Message-IDs.
+#	- Cameron Simpson <cs@zip.com.au> 06jun1997
+#
+
+use strict qw(vars);
+
+BEGIN { use cs::DEBUG; cs::DEBUG::using(__FILE__); }
+
+use cs::DeepIndex;
+
+package cs::MsgidIndex;
+
+@cs::MsgidIndex::ISA=(cs::DeepIndex);
+
+sub _KeyChain
+	{ my($this,$rawkey)=@_;
+	  my($okey)=$rawkey;
+
+	  $rawkey =~ s/^<//;
+	  $rawkey =~ s/>$//;
+	  $rawkey =~ s/.*\@//;
+	  $rawkey =~ s/\s+//g;
+
+	  my(@s)=split(/\.+/,$rawkey);
+	  warn "s=[@s]";
+	  my(@keychain)=(map(lc($_),grep(length,@s)),$okey);
+	  warn "bad keys in chain: [@keychain]"
+		if grep($_ eq '.',@keychain);
+
+	  @keychain;
+	}
+
+1;
