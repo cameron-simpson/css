70663ae6af4a3fc48c90c6b156830485e51f26fc
Cameron Simpson <cs@zip.com.au>
2000-02-19 04:01:06 +0000

    Initial commit for new CVS repository. [CVS rev 1.1]

---
+++ cs/Extractor.pm
@@ -0,0 +1,67 @@
+#!/usr/bin/perl
+#
+# General extraction code.
+#	- Cameron Simpson <cs@zip.com.au> 30may1997
+#
+
+use strict qw(vars);
+
+BEGIN { use cs::DEBUG; cs::DEBUG::using(__FILE__); }
+
+use cs::Misc;
+
+package cs::Extractor;
+
+sub new
+	{ my($class,$dir)=@_;
+	  $dir=cs::Misc::tmpDir()."/x$$" if ! defined $dir;
+
+	  if (! mkdir($dir,0777))
+		{ 
+		  warn "mkdir($dir): $!";
+		  return undef;
+		}
+
+	  bless { DIR		=> $dir,
+		  N		=> 0,
+		  ENTRIES	=> [],
+		}, $class;
+	}
+
+sub Entries
+	{ @{shift->{ENTRIES}};
+	}
+
+sub Sink
+	{ my($this)=@_;
+
+	  my($n)=$this->{N}+1;
+	  my($path)="$this->{DIR}/$n";
+
+	  my($sink)=new cs::Sink (PATH, $path);
+
+	  return undef if ! defined $sink;
+
+	  $this->{N}=$n;
+
+	  $sink;
+	}
+
+sub Extract
+	{ my($this,$src)=@_;
+	  my($sink)=$this->Sink();
+
+	  return undef if ! defined $sink;
+
+	  my($data);
+
+	  while (defined ($data=$src->Read()) && length($data))
+		{ $sink->Put($data);
+		}
+
+	  push(@{$this->{ENTRIES}},$sink->{PATH});
+
+	  $sink->{PATH};
+	}
+
+1;
