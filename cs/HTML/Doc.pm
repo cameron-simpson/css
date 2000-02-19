aba928bc5b824b02f8687077fe652b4b33e2c3b7
Cameron Simpson <cs@zip.com.au>
2000-02-19 04:01:27 +0000

    Initial commit for new CVS repository. [CVS rev 1.1]

---
+++ cs/HTML/Doc.pm
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+#
+# Object to talk about an HTML document.
+#	- Cameron Simpson <cameron@research.canon.com.au> 15oct94
+#
+
+use strict qw(vars);
+
+use cs::HTML;
+
+package cs::HTML::Doc;
+
+require Exporter;
+@cs::HTML::Doc::ISA=qw(Exporter);
+@cs::HTML::Doc::EXPORT_OK=qw(TABLE TD TR IMG HREF);
+
+sub new
+	{ my($class,$src,$url)=@_;
+
+	  bless { URL	=> $url,
+		  DS	=> $src,
+		}, $class;
+	}
+
+sub Tokens
+	{ my($this)=shift;
+
+	  return $this->{TOKENS} if exists $this->{TOKENS};
+
+	  $this->{TOKENS}={};
+	}
+
+1;
