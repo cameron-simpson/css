c4cb7c9dc1111e28ebc0d785c3e792a81f8cd6be
Cameron Simpson <cs@zip.com.au>
2000-02-19 04:01:11 +0000

    Initial commit for new CVS repository. [CVS rev 1.1]

---
+++ cs/getopt.pl
@@ -0,0 +1,53 @@
+;# cs/getopt.pl - a even better getopts.pl
+;#
+;# Hacked by Cameron Simpson <cameron@cs.unsw.oz.au> to collect lists for
+;# options with arguments. 02jun93
+
+;# Usage:
+;#      do Getopts('a:bc');  # -a takes arg. -b & -c not. Sets opt_* as a
+;#                           #  side effect.
+
+sub Getopts {
+    local($argumentative) = @_;
+    local(@args,$_,$first,$rest);
+    local($errs) = 0;
+    local($[) = 0;
+
+    @args = split( / */, $argumentative );
+    while(@ARGV && ($_ = $ARGV[0]) =~ /^-(.)(.*)/) {
+	($first,$rest) = ($1,$2);
+	$pos = index($argumentative,$first);
+	if($pos >= $[) {
+	    if($args[$pos+1] eq ':') {
+		shift(@ARGV);
+		if($rest eq '') {
+		    ++$errs unless @ARGV;
+		    $rest = shift(@ARGV);
+		}
+		eval "push(opt_$first,\$rest)";
+	    }
+	    else {
+		eval "\$opt_$first = 1";
+		if($rest eq '') {
+		    shift(@ARGV);
+		}
+		else {
+		    $ARGV[0] = "-$rest";
+		}
+	    }
+	}
+	else {
+	    print STDERR "$cmd: -$first: unknown option: \n";
+	    ++$errs;
+	    if($rest ne '') {
+		$ARGV[0] = "-$rest";
+	    }
+	    else {
+		shift(@ARGV);
+	    }
+	}
+    }
+    $errs == 0;
+}
+
+1;
