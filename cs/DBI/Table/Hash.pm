c9142f7644eba7233dd9a08cabf88508dc1e704d
Cameron Simpson <cs@zip.com.au>
2000-02-25 04:31:51 +0000

    Initial code. [CVS rev 1.1]

---
+++ cs/DBI/Table/Hash.pm
@@ -0,0 +1,118 @@
+#!/usr/bin/perl
+#
+# cs::DBI::Table::Hash: Treat an indexed DBI table as a hash.
+#	- Cameron Simpson <cs@zip.com.au> 25feb2000
+#
+
+=head1 NAME
+
+cs::DBI::Table::Hash - treat an indexed DBI table as a hash
+
+=head1 SYNOPSIS
+
+use cs::DBI::Table::Hash;
+
+tie %h, I<dbh>, I<table>, I<keyfield>
+
+=head1 DESCRIPTION
+
+The cs::DBI::Table::Hash module permits you to tie a database
+table to a hash.
+
+=cut
+
+use strict qw(vars);
+
+BEGIN { use cs::DEBUG; cs::DEBUG::using(__FILE__); }
+
+use cs::HASH;
+use cs::DBI;
+
+package cs::DBI::Table::Hash;
+
+require Exporter;
+
+@cs::DBI::Table::Hash::ISA=qw(cs::HASH);
+
+sub TIEHASH($$$$)
+{ my($class,$dbh,$table,$keyfield)=@_;
+
+  bless { DBH => $dbh,
+	  TABLE => $table,
+	  LIVE => {},
+	  KEY => $keyfield,
+	}, $class;
+}
+
+sub KEYS($)
+{ my($this)=@_;
+
+  my $dbh = $this->{DBH};
+
+  my $sth = cs::DBI::sql($this->{DBH},"SELECT $this->{KEY} from $this->{TABLE}");
+  if (! defined $sth)
+  { warn "$0: can't look up keys from $this->{TABLE}";
+    return ();
+  }
+
+  return () if ! $sth->execute();
+
+  my @keys = ();
+  my @r;
+
+  while (@r=$sth->fetchrow_array())
+  { push(@keys,$r[0]);
+  }
+
+  @keys;
+}
+
+sub FETCH($$)
+{ my($this,$key)=@_;
+
+  my @rows = cs::DBI::find($this->{DBH},$this->{KEY},$key);
+
+  return undef if ! @rows;
+
+  warn "$0: FETCH($this->{TABLE},$key): multiple hits!"
+  if @rows > 1;
+
+  my $row = $rows[0];
+
+  tie %$row, cs::DBI::Table::Row, $this;
+
+  $row;
+}
+
+sub EXISTS($$)
+{ my($this,$key)=@_;
+  defined $this->FETCH($key);
+}
+
+sub STORE($$$)
+{ my($this,$key,$value)=@_;
+
+  my($dbh,$table)=($this->{DBH}, $this->{TABLE});
+
+  $this->DELETE($key);
+
+  cs::DBI::insert($dbh, $table, keys %$value)
+  ->ExecuteWithRec($value);
+
+  $value;
+}
+
+sub DELETE($$)
+{ my($this,$key)=@_;
+
+  # purge old records
+  cs::DBI::dosql($this->{DBH},"DELETE FROM $this->{TABLE} WHERE $this->{KEY} = ?", $key);
+}
+
+=head1 AUTHOR
+
+Cameron Simpson E<lt>cs@zip.com.au<gt>
+
+=cut
+
+1;
