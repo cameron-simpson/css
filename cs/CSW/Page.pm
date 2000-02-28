6a8f83161fbc8891d850f49f82df25cec866d6df
Cameron Simpson <cs@zip.com.au>
2000-02-28 07:44:33 +0000

    Beginnings of the CSW web browser. [CVS rev 1.1]

---
+++ cs/CSW/Page.pm
@@ -0,0 +1,95 @@
+#!/usr/bin/perl
+#
+# cs::Web::CSW::Page: a web page
+#	- Cameron Simpson <cs@zip.com.au> 28feb2000
+#
+
+=head1 NAME
+
+cs::Web::CSW::Page - a web page
+
+=head1 SYNOPSIS
+
+use cs::Web::CSW::Page;
+
+=head1 DESCRIPTION
+
+The cs::Web::CSW::Page module tracks the state of an active web page
+
+=cut
+
+use strict qw(vars);
+
+BEGIN { use cs::DEBUG; cs::DEBUG::using(__FILE__); }
+
+use cs::URL;
+use cs::Web::CSW;
+
+package cs::Web::CSW::Page;
+
+require Exporter;
+
+@cs::Web::CSW::Page::ISA=qw();
+
+=head1 GENERAL FUNCTIONS
+
+=over 4
+
+=back
+
+=head1 OBJECT CREATION
+
+=over 4
+
+=item new cs::Web::CSW::Page I<widget>, I<url>
+
+Establish a new B<cs::Web::CSW::Page> object showing I<url>
+using the Canvas I<widget> supplied.
+
+=cut
+
+sub new($$$)
+{ my($class,$w,$url)=@_;
+
+  my $dbh = cs::Web::CSW::dbh();
+
+  my $id = cs::DBI::addRow($dbh,'WINDOWS', { SESSION_ID => 0,
+					     HISTORY_ID => 0,
+					     STATE => BLANK,
+					   });
+
+  my $wdb = cs::Web::CSW::table('WINDOWS');
+
+  my $this = { REC => $wdb->{$id},
+	     };
+
+  warn "this=".cs::Hier::h2a($this,1);
+
+  bless $this, $class;
+}
+
+=back
+
+=head1 OBJECT METHODS
+
+=over 4
+
+=item Id()
+
+Return the WINDOW_ID of this window.
+
+=cut
+
+sub Id($)
+{ shift->{REC}->{WINDOW_ID};
+}
+
+=back
+
+=head1 AUTHOR
+
+Cameron Simpson E<lt>cs@zip.com.au<gt>
+
+=cut
+
+1;
