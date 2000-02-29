2ece6b7d9f49e667436a316679ffe0a20391100c
Cameron Simpson <cs@zip.com.au>
2000-02-29 04:15:03 +0000

    cs::Hier doco and a little cleanup. [CVS rev 1.3]

--- cs/Hier.pm
+++ cs/Hier.pm
@@ -26,6 +26,20 @@
 # wordlist
 #
 
+=head1 NAME
+
+cs::Hier - manipulate data hierachies
+
+=head1 SYNOPSIS
+
+use cs::Hier;
+
+=head1 DESCRIPTION
+
+This module transcribes and manipulates data structures.
+
+=cut
+
 use strict qw(vars);
 
 BEGIN { use cs::DEBUG; cs::DEBUG::using(__FILE__); }
@@ -39,8 +53,22 @@ $cs::Hier::PtrTags=0;
 $cs::Hier::PackageTags=1;
 $cs::Hier::Indent=0;
 
+=head1 GENERAL FUNCTIONS
+
+=over 4
+
+=item h2a(I<ref>,I<useindent>)
+
+Return a textual representation of a data structure references by I<ref>.
+I<useindent> is an optional praremeter
+indicating whether to generate multiline indented text or single line text,
+defaulting to 1.
+
+=cut
+
 sub h2a	# scalar or ref -> text version
-{ my($a)='';
+{
+  my $a = '';
 
   { ::need(cs::Sink);
     my($s)=new cs::Sink (SCALAR,\$a);
@@ -49,6 +77,16 @@ sub h2a	# scalar or ref -> text version
 
   $a;
 }
+
+=item h2s(I<sink>,I<ref>,I<useindent>)
+
+Transcribe the data structure references by I<ref>
+to the B<cs::Sink> I<sink>.
+I<useindent> is an optional praremeter
+indicating whether to generate multiline indented text or single line text,
+defaulting to 1.
+
+=cut
 
 sub h2s
 { my($s,$ref,$ui,$pt,$pk,$preindent)=@_;
@@ -119,7 +157,22 @@ sub h2s
   $cs::Hier::_Active{$refkey}=0;
 }
 
-sub getKVLine
+=item getKVLine(I<source>,I<noparse>)
+
+Read a single key-value entry from the B<cs::Source> I<source>,
+returning a reference to an array of (I<key>, I<value>).
+I<noparse> indicates whether to defer parsing the I<value>,
+defaulting to 0.
+If I<noparse> is set
+then the textual representation of the value is returned as I<value>,
+otherwise the line is parsed and the actual value returned.
+On EOF or error
+returns the string B<EOF> or B<ERROR>, respectively,
+instead of the arrayref.
+
+=cut
+
+sub getKVLine($;$)
 { my($s,$noParse)=@_;
   $noParse=0 if ! defined $noParse;
 
@@ -155,6 +208,13 @@ sub getKVLine
     return [ $key, $text ];
   }
 }
+
+=item putKVLine(I<sink>,I<key>,I<value>)
+
+Transcribe I<key> and I<value> to the B<cs::Sink>
+as a multiline indented transcription.
+
+=cut
 
 sub putKVLine
 { my($s,$key,$value)=@_;
@@ -236,6 +296,15 @@ sub _hash2s
   $s->Put('}');
 }
 
+=item a2h(I<text>)
+
+Convert the I<text> to a reference to a data structure.
+Return the reference in a scalar context,
+or an array of the reference and any unparsed text
+in an array context.
+
+=cut
+
 sub a2h	# string -> (scalar or ref, unparsed)
 { local(%cs::Hier::_Prior);
   _a2h(@_);
@@ -249,20 +318,20 @@ sub _a2h
 
   # check for reference to prior object
   if (/^&\(([^)+])\)/)
-	{ @both=($cs::Hier::_Prior{$1},$');
-	}
+  { @both=($cs::Hier::_Prior{$1},$');
+  }
   else
   {
     # grab leading package:: prefix for blessed things
     if (/^(\w+(::\w+)*)::\s*/)
-	{ $package=$1; $_=$';
-	}
+    { $package=$1; $_=$';
+    }
 
     my($refkey);
 
     if (/^\(([^)]+)\)/)
-	{ $refkey=$1; $_=$';
-	}
+    { $refkey=$1; $_=$';
+    }
 
     if (/^\{/)		{ @both=_a2hash($_); }
     elsif (/^\[/)	{ @both=_a2array($_); }
@@ -275,13 +344,13 @@ sub _a2h
 
     # note reference for later
     if (defined $refkey && ref $both[0])
-	{ $cs::Hier::_Prior{$refkey}=$both[0];
-	}
+    { $cs::Hier::_Prior{$refkey}=$both[0];
+    }
 
     # bless reference if necessary
     if (defined $package && ref $both[0])
-	{ bless $both[0], $package;
-	}
+    { bless $both[0], $package;
+    }
   }
   
   ## warn "$::cmd: both=".cs::Hier::h2a(\@both,0)."\n";
@@ -400,373 +469,455 @@ sub _a2hash	# string -> (\%hash,unparsed)
   ($h,$_);
 }
 
-sub wordlist
-{ local($_)=@_;
-  my($r);
+##=item wordlist(I<something>)
+##
+##DEPRECIATED.
+##Convert I<something> into a reference to an array of strings.
+##If I<something> is an arrayref, leave unchanged.
+##If I<something> is a string, split on whitespace.
+##Otherwise convert to text with B<h2a> and return.
+##I can't imagine what I wrote this for :-(
+##
+##=cut
+##
+##sub wordlist
+##{ local($_)=@_;
+##  
+##  my $r;
+##
+##  if (ref eq ARRAY)
+##  { $r=$_;
+##  }
+##  elsif (! ref)
+##  { $r=[ grep(length,split(/\s+/,$_)) ];
+##  }
+##  else
+##  { warn "$::cmd: Hier::wordlist($_): can't convert to words\n";
+##    $r=[ &h2a($_) ];
+##  }
+##
+##  ## warn "$::cmd: wordlist(",&h2a($_),") => ",&h2a($r),"\n";
+##
+##  $r;
+##}
 
-  if (ref eq ARRAY)
-	{ $r=$_;
-	}
-  elsif (! ref)
-	{ $r=[ grep(length,split(/\s+/,$_)) ];
-	}
+##sub load
+##{ my($fname,$silent)=@_;
+##  my($h,$junk);
+##  my($s);
+##
+##  ::need(cs::Source);
+##
+##  if (ref $fname)
+##	{ $s=$fname;
+##	}
+##  elsif (! defined ($s=new cs::Source PATH, $fname))
+##	{ $silent
+##	    || ! -e $fname
+##	    || warn "$::cmd: can't open $fname for read: $!\n";
+##	  return undef;
+##	}
+##
+##  ($h,$junk)=&a2h(join('',$s->GetAllLines()));
+##
+##  $junk =~ s/^\s+//;
+##  ($silent || length($junk) == 0) || warn "$::cmd: unparsed stuff from $fname: \"$junk\"\n";
+##
+##  $h;
+##}
+##
+##sub save
+##{ my($fname,$h,$silent)=@_;
+##  my($s);
+##
+##  ::need(cs::Sink);
+##
+##  if (ref $fname)
+##  { $s=$fname;
+##  }
+##  elsif (! defined ($s=new cs::Sink PATH, $fname))
+##  { $silent || warn "$::cmd: can't open $fname for write: $!\n";
+##    return undef;
+##  }
+##
+##  $s->Put(h2a($h));
+##  $s->Put("\n") if $cs::Hier::_UseIndent;
+##}
+
+=item loadhash(I<source>)
+
+Load a hash from a B<cs::Source> I<source>.
+If I<source> is not an object reference
+consider it a filename and open it.
+Return a hashref.
+
+=cut
+
+sub loadhash($)
+{ my($fname)=@_;
+  
+  my $s;
+
+  if (! ref $fname)
+  { ::need(cs::Source);
+    $s=cs::Source->new(PATH,$fname);
+    if (! defined $s)
+    { warn "$::cmd: open($fname): $!";
+      return undef;
+    }
+  }
   else
-  { warn "$::cmd: Hier::wordlist($_): can't convert to words\n";
-    $r=[ &h2a($_) ];
+  { $s=$fname;
   }
 
-  ## warn "$::cmd: wordlist(",&h2a($_),") => ",&h2a($r),"\n";
+  my $hash = {};
+  local($_);
+  my($n);
 
-  $r;
+  my $a;
+
+  HASHLINE:
+    while (defined ($a=getKVLine($s)))
+    {
+      if (! ref $a)
+      { warn "$::cmd: getKVLine returns \"$a\"" if $a ne EOF;
+	last HASHLINE;
+      }
+
+      $hash->{$a->[0]}=$a->[1];
+    }
+
+  $hash;
 }
 
-sub load
-{ my($fname,$silent)=@_;
-  my($h,$junk);
-  my($s);
+=item savehash(I<sink>,I<hashref>)
 
-  ::need(cs::Source);
+Write the I<hashref> to the B<cs::Sink> I<sink>.
+if I<sink> is not an object reference
+consider it a filename and try to open it.
+Returns B<undef> on error
+or the number of keys written (which may be zero) on success.
 
-  if (ref $fname)
-	{ $s=$fname;
-	}
-  elsif (! defined ($s=new cs::Source PATH, $fname))
-	{ $silent
-	    || ! -e $fname
-	    || warn "$::cmd: can't open $fname for read: $!\n";
-	  return undef;
-	}
+=cut
 
-  ($h,$junk)=&a2h(join('',$s->GetAllLines()));
+sub savehash($$)
+{ my($fname,$hash)=@_;
+  
+  my $s;
 
-  $junk =~ s/^\s+//;
-  ($silent || length($junk) == 0) || warn "$::cmd: unparsed stuff from $fname: \"$junk\"\n";
+  if (! ref $fname)
+  { ::need(cs::Sink);
+    $s = cs::Sink->new(PATH,$fname);
+    if (! deifned $s)
+    { warn "$::cmd: open($fname): $!";
+      return undef;
+    }
+  }
+  else
+  { $s=$fname;
+  }
 
-  $h;
+  my(@keys)=sort keys %$hash;
+
+  for (@keys)
+  { putKVLine($s,$_,$hash->{$_});
+  }
+
+  scalar(@keys);
 }
 
-sub save
-	{ my($fname,$h,$silent)=@_;
-	  my($s);
+=item cleanhash(I<hashref>)
 
-	  ::need(cs::Sink);
+Recursively remove empty hashes and arrays from a data structure.
 
-	  if (ref $fname)
-		{ $s=$fname;
-		}
-	  elsif (! defined ($s=new cs::Sink PATH, $fname))
-		{ $silent || warn "$::cmd: can't open $fname for write: $!\n";
-		  return undef;
-		}
-
-	  $s->Put(h2a($h));
-	  $s->Put("\n") if $cs::Hier::_UseIndent;
-	}
-
-sub loadhash
-	{ my($fname)=@_;
-	  my($s);
-
-	  ::need(cs::Source);
-
-	  if (ref $fname)
-		{ $s=$fname;
-		}
-	  elsif (! defined ($s=new cs::Source PATH, $fname))
-		{ warn "$::cmd: can't load from $fname: $!";
-		  return undef;
-		}
-
-	  my($hash)={};
-	  my(@keys)=sort keys %$hash;
-	  local($_);
-	  my($n);
-
-	  my($key,$tail,$datum);
-
-	  $n=0;
-	  HASHLINE:
-	    while (defined ($_=$s->GetLine()) && length)
-		{ $n++;
-		  chomp;
-		  if (! /^("(\\.|[^\\"])*"|[^"\s]\S*)\s+(\S)/)
-			{ warn "$::cmd: $fname, line $n: bad data [$_]";
-			  next HASHLINE;
-			}
-
-		  ($key,$tail)=($1,"$3$'");
-		  $key=cs::Hier::a2h($key);
-		  ($datum,$tail)=a2h($tail);
-
-		  $hash->{$key}=$datum;
-
-		  $tail =~ s/^\s+//;
-		  if (length $tail)
-			{ warn "$::cmd: $fname, line $n: unparsed data [$tail]";
-			}
-		}
-
-	  $hash;
-	}
-
-sub savehash
-	{ my($fname,$hash)=@_;
-	  my($s);
-
-	  ::need(cs::Sink);
-
-	  if (ref $fname)
-		{ $s=$fname;
-		}
-	  elsif (! defined ($s=new cs::Sink PATH, $fname))
-		{ warn "$::cmd: can't save to $fname: $!";
-		  return undef;
-		}
-
-	  my(@keys)=sort keys %$hash;
-
-	  for (@keys)
-		{ _putHashLine($s,$_,$hash->{$_});
-		}
-
-	  scalar(@keys);
-	}
-
-sub _putHashLine
-	{ my($s,$key,$datum)=@_;
-	  $s->Put(cs::Hier::h2a($key,0), "\t", h2a($datum,0), "\n");
-	}
+=cut
 
 sub cleanhash
-	{ my($h,$recurse)=@_;
-	  $recurse=0 if ! defined $recurse;
+{ my($h,$recurse)=@_;
+  $recurse=0 if ! defined $recurse;
 
-	  my($f,$v,@k);
+  my($f,$v,@k);
 
-	  # clean empty fields
-	  KEY:
-	    for (keys %$h)
-		{ next KEY if ! defined $h->{$_}
-			   || ref($f=$h->{$_}) ne HASH;
+  # clean empty fields
+  KEY:
+    for (keys %$h)
+    { next KEY if ! defined $h->{$_}
+	       || ref($f=$h->{$_}) ne HASH;
 
-		  for (keys %$f)
-			{ if (! defined $f->{$_})
-				{ delete $f->{$_};
-				}
-			  else
-			  { $v=$f->{$_};
-			    if (ref($v) eq ARRAY && @$v == 0)
-				{ delete $f->{$_};
-				}
-			    elsif (ref($v) eq HASH)
-				{ if ($recurse)
-					{ cleanhash($v,$recurse);
-					}
-
-				  @k=keys %$v;
-				  if (@k == 0)
-					{ delete $f->{$_};
-					}
-				}
-			    elsif (! ref($v) && ! length $v)
-				{ delete $f->{$_};
-				}
-			  }
-			}
-		}
+      for (keys %$f)
+      { if (! defined $f->{$_})
+	{ delete $f->{$_};
 	}
+	else
+	{ $v=$f->{$_};
+	  if (ref($v) eq ARRAY && @$v == 0)
+	  { delete $f->{$_};
+	  }
+	  elsif (ref($v) eq HASH)
+	  { if ($recurse)
+	    { cleanhash($v,$recurse);
+	    }
+
+	    @k=keys %$v;
+	    if (@k == 0)
+	    { delete $f->{$_};
+	    }
+	  }
+	  elsif (! ref($v) && ! length $v)
+	  { delete $f->{$_};
+	  }
+	}
+      }
+    }
+}
+
+sub emaciate
+{ my($var)=@_;
+
+  if (::reftype($var) eq HASH)
+  {
+    for (keys %$var)
+    { if (! ref $var->{$_})
+      { delete $var->{$_} if ! defined $var->{$_};
+      }
+      else
+      { emaciate($var->{$_});
+	my $t = reftype($var->{$_});
+	if ($t eq ARRAY)
+	{ delete $var->{$_} if ! @{$var->{$_}};
+	}
+      }
+    }
+  }
+}
 
 sub reftype { ::reftype(@_); }
+
+=item hcmp(I<value1>,I<value2>)
+
+Compare two data structures for equality by value.
+Returns 0 if they are identical and a signed value
+to indicate difference
+(for use with the B<sort> operator).
+
+=cut
 
 # compare two structures - return 0 for match
 # some hacks to "compare" different types - a least a consistent ordering
 # is returned, which should render sorts stable
 # caveat - no loops!
 sub hcmp
-	{ my($s1,$s2)=@_;
-	  my($cmp);
+{ my($s1,$s2)=@_;
+  my($cmp);
 
-	  if (! ref $s1)
-	    { if (! ref $s2)
-		# neither is a ref - fall through to native comparison
-		{
-		  $cmp=$s1 cmp $s2;
-		  return $cmp;
-		}
-	      else
-	      # s2 a ref, s1 not a ref
-	      {
-		return 1;	# scalars < references
-	      }
-	    }
-	  elsif (! ref $s2)
-	    { # warn "$::cmd: ref/nonref($s2)";
-	      return -1;	# references > scalars
-	    }
-	  elsif ("$s1" eq "$s2")
-	  	# common refs - match ok!
-		{ return 0;
-		}
-	  else
-	  # distinct refs - fall through
-	  {}
-
-	  my($rt1,$rt2)=(reftype($s1),reftype($s2));
-
-	  # different types - order on type
-	  $cmp=( $rt1 cmp $rt2 );
-	  # warn "$::cmd: cmp [$rt1],[$rt2] gives undef!" if ! defined $cmp;
-
-	  if ($cmp != 0)
-		{ # warn "$::cmd: $rt1/$s1 cmp $rt2/$s2";
-	  	  return $cmp;
-		}
-
-	  if ($rt1 eq SCALAR)
-		{
-		  $cmp=$$s1 cmp $$s2;
-		# warn "$::cmd: terminal2: $$s1 cmp $$s2";
-		  return $cmp;
-		}
-
-	  if ($rt1 eq ARRAY)
-	    # unusual result - short arrays order before long arrays
-	    # regardless of array contents
-	    { $cmp = @$s1 <=> @$s2;
-	      # warn "$::cmd: <=> lengths [@$s1],[@$s2] gives undef!" if ! defined $cmp;
-
-	      if ($cmp != 0)
-		{ # warn "$::cmd: differing array lengths";
-		  return $cmp;
-		}
-
-	      my($i);
-
-	      for $i ($[..$#$s1)
-		{ $cmp=hcmp($s1->[$i],$s2->[$i]);
-	        # warn "$::cmd: hcmp($s1 sub $i,$s2 sub $i) gives undef!" if ! defined $cmp;
-
-		  if ($cmp != 0)
-		  	{ # warn "$::cmd: element mismatch($s1->[$i],$s2->[$i])";
-			  return $cmp;
-			}
-		}
-
-	      return 0;
-	    }
-
-	  # sanity check
-	  die "$::cmd: can't deal with type $rt1" if $rt1 ne HASH;
-
-	  my(@k1)=sort keys %$s1;
-	  my(@k2)=sort keys %$s2;
-
-	  # smaller hashes < larger hashes
-	  $cmp=(@k1 <=> @k2);
-	  # warn "$::cmd: <=> lengths [@k1],[@k2] gives undef!" if ! defined $cmp;
-
-	  if ($cmp != 0)
-		{ # warn "$::cmd: differing hash sizes";
-		  return $cmp;
-		}
-
-	  # compare keys of hashes first
-	  $cmp=hcmp(\@k1,\@k2);
-	  # warn "$::cmd: hcmp [@k1],[@k2] gives undef!" if ! defined $cmp;
-	  if ($cmp != 0)
-		{ # warn "$::cmd: differing hash key sets [@k1] vs [@k2]";
-		  return $cmp;
-		}
-
-	  my($key);
-
-	  # ok, try values
-	  for $key (@k1)
-		{
-		  $cmp=hcmp($s1->{$key},$s2->{$key});
-		# warn "$::cmd: hcmp $s1 key $key,$s2 key $key gives undef!" if ! defined $cmp;
-		  if ($cmp != 0)
-			{ # warn "$::cmd: differing key elements for key=[$key]";
-			  return $cmp;
-			}
-		}
-
-	  # identical things!
-	  return 0;
+  if (! ref $s1)
+    { if (! ref $s2)
+	# neither is a ref - fall through to native comparison
+	{
+	  $cmp=$s1 cmp $s2;
+	  return $cmp;
 	}
+      else
+      # s2 a ref, s1 not a ref
+      {
+	return 1;	# scalars < references
+      }
+    }
+  elsif (! ref $s2)
+    { # warn "$::cmd: ref/nonref($s2)";
+      return -1;	# references > scalars
+    }
+  elsif ("$s1" eq "$s2")
+	# common refs - match ok!
+	{ return 0;
+	}
+  else
+  # distinct refs - fall through
+  {}
+
+  my($rt1,$rt2)=(reftype($s1),reftype($s2));
+
+  # different types - order on type
+  $cmp=( $rt1 cmp $rt2 );
+  # warn "$::cmd: cmp [$rt1],[$rt2] gives undef!" if ! defined $cmp;
+
+  if ($cmp != 0)
+	{ # warn "$::cmd: $rt1/$s1 cmp $rt2/$s2";
+	  return $cmp;
+	}
+
+  if ($rt1 eq SCALAR)
+	{
+	  $cmp=$$s1 cmp $$s2;
+	# warn "$::cmd: terminal2: $$s1 cmp $$s2";
+	  return $cmp;
+	}
+
+  if ($rt1 eq ARRAY)
+    # unusual result - short arrays order before long arrays
+    # regardless of array contents
+    { $cmp = @$s1 <=> @$s2;
+      # warn "$::cmd: <=> lengths [@$s1],[@$s2] gives undef!" if ! defined $cmp;
+
+      if ($cmp != 0)
+	{ # warn "$::cmd: differing array lengths";
+	  return $cmp;
+	}
+
+      my($i);
+
+      for $i ($[..$#$s1)
+	{ $cmp=hcmp($s1->[$i],$s2->[$i]);
+	# warn "$::cmd: hcmp($s1 sub $i,$s2 sub $i) gives undef!" if ! defined $cmp;
+
+	  if ($cmp != 0)
+		{ # warn "$::cmd: element mismatch($s1->[$i],$s2->[$i])";
+		  return $cmp;
+		}
+	}
+
+      return 0;
+    }
+
+  # sanity check
+  die "$::cmd: can't deal with type $rt1" if $rt1 ne HASH;
+
+  my(@k1)=sort keys %$s1;
+  my(@k2)=sort keys %$s2;
+
+  # smaller hashes < larger hashes
+  $cmp=(@k1 <=> @k2);
+  # warn "$::cmd: <=> lengths [@k1],[@k2] gives undef!" if ! defined $cmp;
+
+  if ($cmp != 0)
+	{ # warn "$::cmd: differing hash sizes";
+	  return $cmp;
+	}
+
+  # compare keys of hashes first
+  $cmp=hcmp(\@k1,\@k2);
+  # warn "$::cmd: hcmp [@k1],[@k2] gives undef!" if ! defined $cmp;
+  if ($cmp != 0)
+	{ # warn "$::cmd: differing hash key sets [@k1] vs [@k2]";
+	  return $cmp;
+	}
+
+  my($key);
+
+  # ok, try values
+  for $key (@k1)
+	{
+	  $cmp=hcmp($s1->{$key},$s2->{$key});
+	# warn "$::cmd: hcmp $s1 key $key,$s2 key $key gives undef!" if ! defined $cmp;
+	  if ($cmp != 0)
+		{ # warn "$::cmd: differing key elements for key=[$key]";
+		  return $cmp;
+		}
+	}
+
+  # identical things!
+  return 0;
+}
+
+=item hdup(I<value>)
+
+Return a complete duplicate of some I<value>.
+Note that any class B<bless>ing is lost.
+
+=cut
 
 # duplicate a hierachy - note: the dup is unblessed
-sub hdup
-	{ my($r1)=@_;
+sub hdup($);
+sub hdup($)
+{ my($r1)=@_;
 
-	  # simple case
-	  return $r1 if ! ref $r1;
+  # simple case
+  return $r1 if ! ref $r1;
 
-	  my($type)=reftype($r1);
+  my($type)=reftype($r1);
 
-	  # new object
-	  my($r2);
+  # new object
+  my($r2);
 
-	  if ($type eq SCALAR)
-		{ my($v)=$$r1;
+  if ($type eq SCALAR)
+  { my($v)=$$r1;
 
-		  # I hope this does the right thing
-		  $r2=\$v;
-		}
-	  elsif ($type eq ARRAY)
-		{ $r2=[];
-		  for (@$r1)
-			{ push(@$r2,hdup($_));
-			}
-		}
-	  elsif ($type eq HASH)
-		{ $r2={};
-		  for (keys %$r1)
-			{ $r2->{$_}=hdup($r1->{$_});
-			}
-		}
-	  else
-	  { die "$::cmd: can't dup objects of type \"$type\"";
-	  }
+    # I hope this does the right thing
+    $r2=\$v;
+  }
+  elsif ($type eq ARRAY)
+  { $r2=[];
+    for (@$r1)
+    { push(@$r2,hdup($_));
+    }
+  }
+  elsif ($type eq HASH)
+  { $r2={};
+    for (keys %$r1)
+    { $r2->{$_}=hdup($r1->{$_});
+    }
+  }
+  else
+  { die "$::cmd: can't dup objects of type \"$type\"";
+  }
 
-	  $r2;
-	}
+  $r2;
+}
+
+=item diff(I<old>,I<new>)
+
+Compare two hashrefs at the top level and return a hashref
+loosely describing the differences.
+The resulting hashref has
+B<undef> for fields which were in I<old> and are not in I<new>
+and the value from I<new> for fields which are in I<new> and not in I<old>
+or in I<new> and differ from the value in I<old>.
+
+=cut
 
 # summarise changes between two records
 sub diff
-	{ my($old,$new)=@_;
-	  my($diff)={};
+{ my($old,$new)=@_;
+  
+  my $diff = {};
 
-	  # find deletions
-	  for (keys %$old)
-		{ $diff->{$_}=undef if ! exists $new->{$_};
-		}
+  # find deletions
+  for (keys %$old)
+  { $diff->{$_}=undef if ! exists $new->{$_};
+  }
 
-	  # find additions or changes
-	  for (keys %$new)
-		{ $diff->{$_}=$new->{$_}
-			if ! exists $old->{$_}
-			|| hcmp($old->{$_},$new->{$_}) != 0;
-		}
+  # find additions or changes
+  for (keys %$new)
+  { $diff->{$_}=$new->{$_}
+	  if ! exists $old->{$_}
+	  || hcmp($old->{$_},$new->{$_}) != 0;
+  }
 
-	  $diff;
-	}
+  $diff;
+}
+
+=item apply(I<old>,I<diff>,I<dodel>,I<subkeys>)
+
+Apply the changes returned from B<diff> above
+to the record I<old>.
+
+=cut
 
 # apply changes to a record
-sub apply
-	{ my($this,$diff,$dodel,$subkeys)=@_;
-	  $dodel=1 if ! defined $dodel;
-	  $subkeys=0 if ! defined $subkeys;
+sub apply($$;$$)
+{ my($this,$diff,$dodel,$subkeys)=@_;
+  $dodel=1 if ! defined $dodel;
+  $subkeys=0 if ! defined $subkeys;
 
-	  KEY:
-	    for (keys %$diff)
-		{
-		  if (defined $diff->{$_} || $dodel)
-			{ setSubKey($this,$diff->{$_},
-				    ($subkeys ? split(m:/:) : $_));
-			}
-		}
-	}
+  KEY:
+    for (keys %$diff)
+    {
+      if (defined $diff->{$_} || $dodel)
+      { setSubKey($this,$diff->{$_},
+		  ($subkeys ? split(m:/:) : $_));
+      }
+    }
+}
 
 # get value stored at end of keychain
 sub getSubKey
@@ -859,24 +1010,17 @@ sub fleshOut
   }
 }
 
-sub emaciate
-{ my($var)=@_;
+=back
 
-  if (::reftype($var) eq HASH)
-  {
-    for (keys %$var)
-    { if (! ref $var->{$_})
-      { delete $var->{$_} if ! defined $var->{$_};
-      }
-      else
-      { emaciate($var->{$_});
-	my $t = reftype($var->{$_});
-	if ($t eq ARRAY)
-	{ delete $var->{$_} if ! @{$var->{$_}};
-	}
-      }
-    }
-  }
-}
+=head1 SEE ALSO
+
+B<cs::Source>(3),
+B<cs::Sink>(3)
+
+=head1 AUTHOR
+
+Cameron Simpson E<lt>cs@zip.com.auE<gt>
+
+=cut
 
 1;
