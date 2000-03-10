bc60e1e0a1ccd80042ffb71eacecb3f45d6a42b7
Cameron Simpson <cs@zip.com.au>
2000-03-10 05:18:49 +0000

    SUPER:: fixups, im2html misfeature fixing, cs::Upd debugging. [CVS rev 1.2]

--- cs/Object.pm
+++ cs/Object.pm
@@ -15,49 +15,50 @@ package cs::Object;
 
 # ([preserve,]hashref,class[,TIEHASH-args])
 sub reTIEHASH
-	{
-	  {my(@c)=caller;warn "reTIEHASH(@_) from [@c]";}
+{
+  {my(@c)=caller;warn "reTIEHASH(@_) from [@c]";}
 
-	  my($preserve)=($_[0] =~ /^[01]$/
-			? shift(@_)
-			: 1
-			);
-	  my($phash,$impl)=(shift,shift);
+  my($preserve)=($_[0] =~ /^[01]$/
+		? shift(@_)
+		: 1
+		);
+  my($phash,$impl)=(shift,shift);
 
-	  my %tmp;
+  my %tmp;
 
-	  if ($preserve)
-		{
-		  # copy the contents
-		  for my $key (keys %$phash)
-			{ $tmp{$key}=$phash->{$key};
-			}
-		}
+  if ($preserve)
+  {
+    # copy the contents
+    for my $key (keys %$phash)
+    { $tmp{$key}=$phash->{$key};
+    }
+  }
 
-	  tie(%$phash,$impl,@_)
-		|| die "tie($phash,$impl,@_) fails";
+  tie(%$phash,$impl,@_)
+	|| die "tie($phash,$impl,@_) fails";
 
-	  if (! defined $preserve)
-		# ignore
-		{}
-	  elsif ($preserve)
-		# overwrite
-		{
-		  # put the contents back
-		  for my $key (keys %tmp)
-			{ $phash->{$key}=$tmp{$key};
-			}
-		}
-	  else
-		# supply if missing - a bit dubious
-		{
-		  for my $key (keys %tmp)
-			{ $phash->{$key}=$tmp{$key}
-				if ! exists $phash->{$key};
-			}
-		}
-	}
+  if (! defined $preserve)
+  # ignore
+  {}
+  elsif ($preserve)
+  # overwrite
+  {
+    # put the contents back
+    for my $key (keys %tmp)
+    { $phash->{$key}=$tmp{$key};
+    }
+  }
+  else
+  # supply if missing - a bit dubious
+  {
+    for my $key (keys %tmp)
+    { $phash->{$key}=$tmp{$key}
+	    if ! exists $phash->{$key};
+    }
+  }
+}
 
-sub DESTROY	{}
+sub DESTROY
+{}
 
 1;
