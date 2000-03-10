90b9e57c5c201adf9680b35e1fd0edbf36b92282
Cameron Simpson <cs@zip.com.au>
2000-03-10 05:18:45 +0000

    SUPER:: fixups, im2html misfeature fixing, cs::Upd debugging. [CVS rev 1.2]

--- cs/BaseClass.pm
+++ cs/BaseClass.pm
@@ -12,23 +12,26 @@ use cs::Object;
 
 package cs::BaseClass;
 
-@cs::BaseClass::ISA=(cs::Object);
+@cs::BaseClass::ISA=qw(cs::Object);
 
-sub DESTROY	{ SUPER::DESTROY(@_); }
+sub DESTROY
+{ my($this)=shift;
+  $this->SUPER::DESTROY(@_);
+}
 
 # the meta data for the immediate superclass
 sub _Meta
-	{ my($this)=@_;
-	  die if @_ != 1;
-	  $this->{META}={} if ! exists $this->{META};
-	  $this->{META};
-	}
+{ my($this)=@_;
+  die if @_ != 1;
+  $this->{META}={} if ! exists $this->{META};
+  $this->{META};
+}
 
 # the meta data for the supers of the immediate super
 sub Meta
-	{ my($_meta)=_Meta(@_);
-	  $_meta->{META}={} if ! exists $_meta->{META};
-	  $_meta->{META};
-	}
+{ my($_meta)=_Meta(@_);
+  $_meta->{META}={} if ! exists $_meta->{META};
+  $_meta->{META};
+}
 
 1;
