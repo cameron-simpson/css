5598e1ac1d6bdb4cedc655f89791141fa29b0ef0
Cameron Simpson <cs@zip.com.au>
2000-05-04 00:31:20 +0000

    Indenting. [CVS rev 1.2]

--- cs/tty.pl
+++ cs/tty.pl
@@ -3,34 +3,34 @@
 require 'stat.pl';
 
 sub isatty
-	{ local($_)=$_[0];
-	  my($isatty);
-	  # local($dev,$ino,$mode,@etc);
+{ local($_)=$_[0];
+  my($isatty);
+  # local($dev,$ino,$mode,@etc);
 
-	  if (/^\d+$/)
-		{ if (!open(_FD_ISATTY,"<&$_"))
-			{ print STDERR "isatty: can't open &$_: $!\n";
-			  return undef;
-			}
+  if (/^\d+$/)
+  { if (!open(_FD_ISATTY,"<&$_"))
+    { warn "isatty: can't open &$_: $!\n";
+      return undef;
+    }
 
-		  $isatty = -t _FD_ISATTY;
-		  # ($dev,$ino,$mode,@etc)=stat _FD_ISATTY;
-		  # no close since it may eat the fd
-		}
-	  elsif (/^[A-Z_]+$/)
-		{ # ($dev,$ino,$mode,@etc)=eval "stat $_";
-		  $isatty = eval "-t $_";
-		}
-	  else
-	  { # ($dev,$ino,$mode,@etc)=stat($_);
-	    $isatty = -t $_;
-	  }
+    $isatty = -t _FD_ISATTY;
+    # ($dev,$ino,$mode,@etc)=stat _FD_ISATTY;
+    # no close since it may eat the fd
+  }
+  elsif (/^[A-Z_]+$/)
+  { # ($dev,$ino,$mode,@etc)=eval "stat $_";
+    $isatty = eval "-t $_";
+  }
+  else
+  { # ($dev,$ino,$mode,@etc)=stat($_);
+    $isatty = -t $_;
+  }
 
-	  $isatty;
+  $isatty;
 
 #	  return (defined($mode)
 #		? POSIX->S_ISCHR($mode)
 #		: undef);
-	}
+}
 
 1;
