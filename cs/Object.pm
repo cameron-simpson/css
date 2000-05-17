023662027bf0cf68d452ba4f567efb5b03d13673
Cameron Simpson <cs@zip.com.au>
2000-05-17 02:59:59 +0000

    Add GetSet() method. Add doco. [CVS rev 1.3]

--- cs/Object.pm
+++ cs/Object.pm
@@ -5,6 +5,22 @@
 #	- Cameron Simpson <cs@zip.com.au> 21jul97
 #
 
+=head1 NAME
+
+cs::Object - root class for objects
+
+=head1 SYNOPSIS
+
+use cs::Object;
+
+@ISA=(cs::Object);
+
+=head1 DESCRIPTION
+
+The B<cs::Object> module provided a few common methods for most objects.
+
+=cut
+
 use strict qw(vars);
 
 BEGIN { use cs::DEBUG; cs::DEBUG::using(__FILE__); }
@@ -12,6 +28,21 @@ BEGIN { use cs::DEBUG; cs::DEBUG::using(__FILE__); }
 package cs::Object;
 
 @cs::Object::ISA=();
+
+=head1 GENERAL FUNCTIONS
+
+=over 4
+
+=item reTIEHASH(I<preserve>,I<hashref>,I<class>,I<tiehashargs>...)
+
+Tie the hash referenced by I<hashref>
+to the specified I<class>,
+passing the I<tiehashargs> to the B<tie> call.
+If the optional parameter I<preserve> is B<1>,
+store the original contents of the hash
+in the tied object.
+
+=cut
 
 # ([preserve,]hashref,class[,TIEHASH-args])
 sub reTIEHASH
@@ -58,7 +89,43 @@ sub reTIEHASH
   }
 }
 
+=back
+
 sub DESTROY
 {}
+
+=head1 OBJECT METHODS
+
+=over 4
+
+=item GetSet(I<field>,I<value>)
+
+If the optional parameter I<value> is supplied,
+set the specified I<filed> of the object to I<value>.
+Otherwise
+return the current value of I<field>
+or B<undef> if it does not exist.
+
+=cut
+
+sub GetSet($$;$)
+{ my($this,$field,$value)=@_;
+
+  if (defined $value)
+  { $this->{$field}=$value;
+  }
+  else
+  { return undef if ! exists $this->{$field};
+    $this->{$field};
+  }
+}
+
+=back
+
+=head1 AUTHOR
+
+Cameron Simpson <cs@zip.com.au> 21jul1997
+
+=cut
 
 1;
