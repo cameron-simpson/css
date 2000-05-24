78b87b8e9c111645ebd17bbefddb5c674d409f60
Cameron Simpson <cs@zip.com.au>
2000-05-24 01:39:41 +0000

    Further work on attributes. [CVS rev 1.2]

--- cs/Palm/PDB/Record.pm
+++ cs/Palm/PDB/Record.pm
@@ -6,7 +6,7 @@
 
 =head1 NAME
 
-cs::Palm::PDB::Record - a Oalm Pilot database record
+cs::Palm::PDB::Record - a Palm Pilot database record
 
 =head1 SYNOPSIS
 
@@ -24,42 +24,106 @@ use strict qw(vars);
 
 BEGIN { use cs::DEBUG; cs::DEBUG::using(__FILE__); }
 
+use cs::Object;
+use cs::Flags;
+
 package cs::Palm::PDB::Record;
 
 require Exporter;
 
-@cs::Palm::PDB::Record::ISA=qw();
+@cs::Palm::PDB::Record::ISA=qw(cs::Object);
 
 =head1 GENERAL FUNCTIONS
 
 =over 4
 
-=item thing(I<arg1>)
+=item unpackAttrib(I<attrib>)
 
-Blah.
+Return an array containing a B<cs::Flags> object
+(containing a subset of B<SECRET>, B<BUSY>, B<DIRTY> and B<DEL_ON_SYNC>)
+and the category number.
 
 =cut
 
-sub thing($)
-{ my(
+sub attribUnpack($)
+{ my($attrib)=@_;
+
+  # extract flags
+  my @a = ();
+
+  for my $A ( [ 0x10,	SECRET ],
+	      [ 0x20,	BUSY ],
+	      [ 0x40,	DIRTY ],
+	      [ 0x80,	DEL_ON_SYNC ]
+	    )
+  { push(@a,$A->[1]) if $attrib & $A->[0];
+  }
+
+  ( (new cs::Flags @a), $attrib & 0x0f );
 }
+
+=item attribPack(I<flags>, I<category>)
+
+Return the byte value representing the I<flags> and I<category> number
+as obtained from B<attribUnpack>.
 
 =back
 
+sub attribPack($$)
+{ my($F,$cat)=@_;
+
+  my $n = 0;
+
+  for my $A ( [ 0x10,	SECRET ],
+	      [ 0x20,	BUSY ],
+	      [ 0x40,	DIRTY ],
+	      [ 0x80,	DEL_ON_SYNC ]
+	    )
+  { $n|=$A->[0] if $F->Test($A->[1]);
+  }
+
+  $n|($cat & 0x0f);
+}
+
 =head1 OBJECT CREATION
 
-Preamble on creation methods.
+New objects are created only from the B<cs::Palm::PDB> module,
+and obtained by others via the B<Record()> method of that class.
+The B<cs::Palm::App::*> modules typically rebless records
+obtained through that method
+into their own record subclasses.
 
 =over 4
 
-=item new cs::Palm::PDB::Record I<arg1>
+=item new cs::Palm::PDB::Record I<parent>, I<nr>, I<new>
 
-Creates a new blah ...
+This creates a new record object
+given the supplied B<cs::Palm::PDB> object I<parent>
+and record number I<nr>
+(which counts from 0).
+If set to true, the optional parameter I<new>
+creates a new record.
 
 =cut
 
-sub new
-{ my(,
+sub new($$$)
+{ my($class,$parent,$nr,$new)=@_;
+  $new=0 if ! defined $new;
+
+  if ($nr < 0
+   || ($new
+       ? $nr != $parent->Nrecords()
+       : $nr >= $parent->NRecords()
+       )
+     )
+  { my @c = caller;
+    warn "$::cmd: bad record number $nr from [@c]";
+    return undef;
+  }
+
+  my $this = { NR => $nr,
+	       PARENT => $parent,
+	     };
 }
 
 =back
@@ -68,17 +132,68 @@ sub new
 
 =over 4
 
-=item $Record->Method1(I<arg1>...
+=cut
 
-Does thing ...
+sub _R($)
+{ my($this)=@_;
+  $this->{PARENT}->{R}->[$this->{NR}];
+}
+
+sub _AttribUnpack($)
+{ my($this)=@_;
+
+  my $R = $this->_R();
+
+  ($this->{FLAGS}, $this->{CATEGORY})
+  =
+  attribUnpack($R->{ATTRIB});
+}
+
+sub _AttribRepack($)
+{ my($this)=@_;
+
+  if (! exists $this->{FLAGS})
+  { my @c = caller;
+    die "$::cmd: _AttribRepack() without prior _AttribUnpack on record $this->{NR}\n\tfrom [@c]";
+  }
+
+  my $R = $this->_R();
+
+  $R->{ATTRIB}=_attribPack($this->{FLAGS}, $this->{CATEGORY});
+}
+
+=item Attribs()
+
+Return the record attributes as a B<cs::Flags> object.
 
 =cut
 
-sub Method1($
-{ my($this,
+sub Attribs($)
+{ my($this)=@_;
+
+  $this->_AttribUnpack() if ! exists $this->{FLAGS};
+  $this->{FLAGS};
+}
+
+=item Category(I<category>)
+
+Return the record category number.
+If the optional parameter I<category> is supplied, set the category number.
+
+=cut
+
+sub Category($)
+{ my($this)=@_;
+
+  $this->_AttribUnpack() if ! exists $this->{FLAGS};
+  $this->GetSet(CATEGORY);
 }
 
 =back
+
+=head1 SEE ALSO
+
+cs::Palm::PDB(3), cs::Flags(3)
 
 =head1 AUTHOR
 
