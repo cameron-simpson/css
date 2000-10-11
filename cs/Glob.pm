8fd98d9192e367b63a0e57e612f26d613ba3cc17
Cameron Simpson <cs@zip.com.au>
2000-10-11 06:59:19 +0000

    Documentation. [CVS rev 1.2]

--- cs/Glob.pm
+++ cs/Glob.pm
@@ -6,6 +6,24 @@
 #	%glob'bed{pattern} -> NULfile-list
 #	%glob'ptn{pattern-part} -> regexp
 
+=head1 NAME
+
+cs::Glob - shell style pattern matching
+
+=head1 SYNOPSIS
+
+use cs::Glob;
+
+@files=cs::Glob::glob($pattern)
+
+=head1 DESCRIPTION
+
+This module provides shell style pattern matching
+without using perl's inbuilt I<glob()> function,
+which has historically been based of I<csh(1)> (gack!).
+
+=cut
+
 use strict qw(vars);
 
 BEGIN { use cs::DEBUG; cs::DEBUG::using(__FILE__); }
@@ -17,7 +35,18 @@ package cs::Glob;
 undef %cs::Glob::_Bed;	# NUL separated globbed lists		( *.c -> a.c, b.c, ... )
 undef %cs::Glob::_Ptn;	# regexp matching glob component	( *.c -> \.c$ )
 
-sub glob	# ptn -> @files
+=head1 FUNCTIONS
+
+=over 4
+
+=item glob(I<pattern>)
+
+I<glob> takes a shell I<pattern> as an argument
+and returns an array of all matching paths.
+
+=cut
+
+sub glob($)
 { local($_)=shift;
   my(@globbed);
 
@@ -54,27 +83,27 @@ sub _glob	# (prefix,@parts) -> @files
   local($_);
 
   SOFAR:
-    while (defined($_=shift)
-	&& !/[[\]?*]/)
-	# literal, append directly
-	{ if (length($sofar) && $sofar !~ m:/$:)
-		{ $sofar.='/';
-		}
-	  $sofar.=$_;
-	}
+    while (defined($_=shift) && !/[[\]?*]/)
+    # literal, append directly
+    { if (length($sofar) && $sofar !~ m:/$:)
+      { $sofar.='/';
+      }
+
+      $sofar.=$_;
+    }
 
   if (! defined)
-	# pure literal, return it
-	{ return $sofar;
-	}
+  # pure literal, return it
+  { return $sofar;
+  }
 
   # ok, $_ must be a pattern component
   my($ptn);
 
   if (defined $cs::Glob::_Ptn{$_})
-      # seen this before, extract regexp
-      { $ptn=$cs::Glob::_Ptn{$_};
-      }
+  # seen this before, extract regexp
+  { $ptn=$cs::Glob::_Ptn{$_};
+  }
   else
   { my($_orig)=$_;
 
@@ -85,20 +114,20 @@ sub _glob	# (prefix,@parts) -> @files
     while (length)
       { # match [range]
 	if (/^\[(!)?([^]-](-[^]-])?)+\]/)
-	      { $ptn.='['
-		     .(length($1) ? '^' : '')
-		     .$2
-		     .']';
-	      }
+	{ $ptn.='['
+	       .(length($1) ? '^' : '')
+	       .$2
+	       .']';
+	}
 	elsif (/^\*+/)
-	      { $ptn.='.*';
-	      }
+	{ $ptn.='.*';
+	}
 	elsif (/^\?+/)
-	      { $ptn.='.' x length($&);
-	      }
+	{ $ptn.='.' x length($&);
+	}
 	elsif (/^[^[*?]+/)
-	      { $ptn.=$&;
-	      }
+	{ $ptn.=$&;
+	}
 	else
 	{ warn "can't parse shell pattern at \"$_\"\n";
 	  /.*/;
@@ -120,28 +149,42 @@ sub _glob	# (prefix,@parts) -> @files
   my(@matched)=grep(/$ptn/,cs::Pathname::dirents($sofar));
 
   if ($#matched < $[)
-      # no entries; short circuit
-      { return ();
-      }
+  # no entries; short circuit
+  { return ();
+  }
 
   my(@globbed)=();
 
   if (length($sofar) && $sofar !~ m:/$:)	{ $sofar.='/'; }
 
   if ($#_ < $[)
-	# no further parts, tack globs onto $sofar
-	{ for (@matched)
-		{ push(@globbed,$sofar.$_);
-		}
-	}
+  # no further parts, tack globs onto $sofar
+  { for (@matched)
+    { push(@globbed,$sofar.$_);
+    }
+  }
   else
   # more components; tack onto sofar and glob further
   { for (@matched)
-	{ push(@globbed,&_glob($sofar.$_,@_));
-	}
+    { push(@globbed,&_glob($sofar.$_,@_));
+    }
   }
 
   @globbed;
 }
+
+=back
+
+=head1 SEE ALSO
+
+B<glob> in the perlfunc(3) manual.
+
+sh(1), cs::Shell(3)
+
+=head1 AUTHOR
+
+Cameron Simpson E<lt>cs@zip.com.auE<gt>
+
+=cut
 
 1;
