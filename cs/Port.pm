d0335820d768b798977399678f91a32d0ecc6903
Cameron Simpson <cs@zip.com.au>
2001-01-17 11:56:39 +0000

    Pod. [CVS rev 1.3]

--- cs/Port.pm
+++ cs/Port.pm
@@ -5,6 +5,21 @@
 #	- Cameron Simpson <cs@zip.com.au> 11dec96
 #
 
+=head1 NAME
+
+cs::Port - an I/O pairing of a B<cs::Source> and B<cs::Sink>
+
+=head1 SYNOPSIS
+
+use cs::Port;
+
+=head1 DESCRIPTION
+
+A B<cs::Port> is a subclass of B<cs::Port>
+tttached to the supplied command.
+
+=cut
+
 use strict qw(vars);
 
 BEGIN { use cs::DEBUG; cs::DEBUG::using(__FILE__); }
@@ -17,6 +32,24 @@ use cs::Hier;
 package cs::Port;
 
 @cs::Port::ISA=();
+
+=head1 OBJECT CREATION
+
+=over 4
+
+=item new B<cs::Port> (I<source>,I<sink>)
+
+Return a wrapper for the I<source> and I<sink> pair
+supplied.
+I<source> is normally a B<cs::Source> object
+but may be a filehandle.
+I<sink> is normally a B<cs::Sink> object
+but may be a filehandle.
+If I<sink> is omitted
+and I<source> is a filehandle
+then I<sink> is taken to be the same file handle.
+
+=cut
 
 sub new
 { my($class,$source,$sink)=@_;
@@ -52,6 +85,18 @@ sub new
 	}, $class;
 }
 
+=back
+
+=head1 OBJECT METHODS
+
+Most of the B<cs::Source> and B<cs::Sink> methods
+can be called on a B<cs::Port>
+and they will be handled by the source or sink as appropriate.
+
+=over 4
+
+=cut
+
 sub DESTROY{}
 
 $cs::Port::_Handle='Handle0000';
@@ -59,21 +104,65 @@ sub _portHandle
 { "cs::Port::".$cs::Port::_Handle++;
 }
 
-# stubs to treat a Port object as a Source or Sink
+=item Source()
+
+Return the B<cs::Source>.
+
+=item Sink()
+
+Return the B<cs::Sink>.
+
+=cut
+
 sub Source	{ shift->{cs::Port::IN}; }
 sub Sink	{ shift->{cs::Port::OUT}; }
+
+=item SourceHandle()
+
+Return the file handle of the B<cs::Source>, if any.
+
+=item SinkHandle()
+
+Return the file handle of the B<cs::Sink>, if any.
+
+=cut
 
 sub SourceHandle{ shift->Source()->Handle(); }
 sub SinkHandle	{ shift->Sink()->Handle(); }
 
+=item Get(), GetLine(), GetContLine(), Read(), NRead()
+
+Called on the B<cs::Source>.
+
+=cut
+
 sub Get		{ my($this)=shift; $this->Source()->Get(@_); }
 sub GetLine	{ my($this)=shift; $this->Source()->GetLine(@_); }
 sub GetContLine	{ my($this)=shift; $this->Source()->GetContLine(@_); }
+sub Read	{ my($this)=shift; $this->Source()->Read(@_); }
+sub NRead	{ my($this)=shift; $this->Source()->NRead(@_); }
+
+=item Put(), PutFlush(), Flush()
+
+Called on the B<cs::Sink>.
+
+=cut
+
 sub Put		{ my($this)=shift; $this->Sink()->Put(@_); }
 sub PutFlush	{ my($this)=shift; $this->Sink()->Put(@_);
 				   $this->Sink()->Flush(); }
 sub Flush	{ my($this)=shift; $this->Sink()->Flush(@_); }
-sub Read	{ my($this)=shift; $this->Source()->Read(@_); }
-sub NRead	{ my($this)=shift; $this->Source()->NRead(@_); }
+
+=back
+
+=head1 SEE ALSO
+
+cs::Source(3), cs::Sink(3)
+
+=head1 AUTHOR
+
+Cameron Simpson E<lt>cs@zip.com.auE<gt>
+
+=cut
 
 1;
