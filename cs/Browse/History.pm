c2c3605e5e7c8435f89ea0a785b5919190fd64ff
Cameron Simpson <cs@zip.com.au>
2001-08-15 03:46:47 +0000

    *** empty log message *** [CVS rev 1.1]

---
+++ cs/Browse/History.pm
@@ -0,0 +1,124 @@
+#!/usr/bin/perl
+#
+# cs::Browse::History - trivial history node object for browse history.
+#	- Cameron Simpson <cs@zip.com.au> 1feb2001
+#
+
+=head1 NAME
+
+cs::Browse::History - trivial history node object for browse history
+
+=head1 SYNOPSIS
+
+use cs::Browse::History;
+
+=head1 DESCRIPTION
+
+=cut
+
+use strict qw(vars);
+
+BEGIN { use cs::DEBUG; cs::DEBUG::using(__FILE__); }
+
+use cs::URL;
+use cs::DBI;
+use cs::MyDB;
+use cs::Date;
+
+package cs::Browse::History;
+
+require Exporter;
+
+@cs::Browse::History::ISA=qw();
+
+=head1 GENERAL FUNCTIONS
+
+=over 4
+
+=cut
+
+sub _db
+{ cs::MyDB::mydb(undef,WEB_DB);
+}
+
+sub _byId
+{ cs::DBI::hashtable(_db(),HISTORY,ID);
+}
+
+=back
+
+=head1 OBJECT CREATION
+
+Preamble on creation methods.
+
+=over 4
+
+=item new cs::Browse::History I<url>
+
+Obtains a new object.
+
+=cut
+
+sub new($$;$)
+{ my($class,$url,$forcenew)=@_;
+
+  my $U = new cs::URL $url;
+  $url=$U->Text();
+
+  my $urlndx = _byURL();
+
+  if (! exists $urlndx->{$url})
+  { $urlndx->{$url}={};
+  }
+
+  my $this = $urlndx->{$url};
+  warn "this=".cs::Hier::h2a($this,0);
+
+  bless $this, $class;
+}
+
+=back
+
+=head1 OBJECT METHODS
+
+=over 4
+
+=item Id()
+
+Return the B<URL_ID> of the URL.
+
+=cut
+
+sub Id($)
+{ shift->{URL_ID};
+}
+
+=item Touch()
+
+Set the B<LAST_VISIT> timeshtamp of the URL.
+
+=cut
+
+sub Touch()
+{ shift->{LAST_VISIT}=cs::Date::timecode(time,1);
+}
+
+=item Url()
+
+Return a B<cs::URL> object representing the URL.
+
+=cut
+
+sub Url($)
+{ new cs::URL (shift->{URL});
+}
+
+=back
+
+=head1 AUTHOR
+
+Cameron Simpson E<lt>cs@zip.com.auE<gt>
+
+=cut
+
+1;
