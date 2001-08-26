1c1652da3bc332bc14c3012d439f99fc3fdfbf74
Cameron Simpson <cs@zip.com.au>
2001-08-26 07:11:28 +0000

    Cleanups and fixes. [CVS rev 1.2]

--- cs/MIME/Base64.pm
+++ cs/MIME/Base64.pm
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 #
 # Process MIME base64 encoded data.
-#	- Cameron Simpson <cameron@research.canon.com.au> 25jul96
+#	- Cameron Simpson <cameron@research.canon.com.au> 25jul1996
 #
 # Bugs: Needs text-mode to make CR-LF <=> LF. Ugh!
 #
@@ -27,170 +27,172 @@ sub _n2code { substr($cs::MIME::Base64::_Codes,$[+$_[0]+1,1); }
 sub _code2n { index($cs::MIME::Base64::_Codes,$_[0])-$[-1; }
 
 sub encode
-	{ my($data64)='';
-	  my($sink64)=new cs::MIME::Base64(Encode,new cs::Sink(SCALAR,\$data64));
-	  $sink64->Put(@_);
-	  undef $sink64;
-	  $data64;
-	}
+{ my($data64)='';
+  my($sink64)=new cs::MIME::Base64(Encode,new cs::Sink(SCALAR,\$data64));
+  $sink64->Put(@_);
+  undef $sink64;
+  $data64;
+}
 
 sub decode
-	{ my($data64,$isText)=@_;
-	  $isText=0 if ! defined $isText;
+{ my($data64,$isText)=@_;
+  $isText=0 if ! defined $isText;
 
-	  (new cs::MIME::Base64
-		(Decode,
-		 (new cs::Source (SCALAR,\$data64)),
-		 $isText)
-	  )->Get();
-	}
+  (new cs::MIME::Base64
+	(Decode,
+	 (new cs::Source (SCALAR,\$data64)),
+	 $isText)
+  )->Get();
+}
 
 # return a Decode or Encode object using this as its sub-source
 sub new
-	{ my($class,$type,$s,$isText)=@_;
-	  $isText=0 if ! defined $isText;
+{ my($class,$type,$s,$isText)=@_;
+  $isText=0 if ! defined $isText;
 
-	  my($this);
+  my($this);
 
-	  if ($type eq Decode)
-		{ $this=new cs::Decode $s, \&_Decode, { ISTEXT => $isText };
-		}
-	  elsif ($type eq Encode)
-		{ $this=new cs::Encode $s, \&_encode, { BUF => '',
-						    ISTEXT => $isText,
-						  };
-		}
-	  else
-	  { die "MIME::Base64::new: unknown type \"$type\"";
-	  }
+  if ($type eq Decode)
+  { $this=new cs::Decode $s, \&_Decode, { ISTEXT => $isText };
+  }
+  elsif ($type eq Encode)
+  { $this=new cs::Encode $s, \&_encode, { BUF => '',
+				      ISTEXT => $isText,
+				    };
+  }
+  else
+  { die "MIME::Base64::new: unknown type \"$type\"";
+  }
 
-	  return undef if ! defined $this;
+  return undef if ! defined $this;
 
-	  $this;
-	}
+  $this;
+}
 
 sub _Decode
-	{ my($this,$newdata,$state)=@_;
-	  local($_)=$this->{BUF};
+{ my($this,$newdata,$state)=@_;
+  local($_)=$this->{BUF};
 
-	  ## warn "into MIME::Base64::_decode($_,[@_])\n";
+  ## warn "into MIME::Base64::_decode($_,[@_])\n";
 
-	  # catch EOF sentinel
-	  return (undef,$_) if ! defined $newdata;
+  # catch EOF sentinel
+  return (undef,$_) if ! defined $newdata;
 
-	  my($data)='';
-	  my($a,$b,$c,$d);	# unpacked values
-	  my($g4);		# encoded values
+  my($data)='';
+  my($a,$b,$c,$d);	# unpacked values
+  my($g4);		# encoded values
 
-	  # silently ignore other chars
-	  s:[^A-Za-z0-9+/=]+::g;
+  # silently ignore other chars
+  s:[^A-Za-z0-9+/=]+::g;
 
-	  while (length >= 4)
-		{ $g4=substr($_,$[,4);
-		  substr($_,$[,4)='';
+  while (length >= 4)
+  { $g4=substr($_,$[,4);
+    substr($_,$[,4)='';
 
-		  # break up into code values
-		  ($a,$b,$c,$d)=map(_code2n($_),split(//,$g4));
+    # break up into code values
+    ($a,$b,$c,$d)=map(_code2n($_),split(//,$g4));
 
-		  ## warn "a=$a, b=$b, c=$c, d=$d\n";
+    ## warn "a=$a, b=$b, c=$c, d=$d\n";
 
-		  # append data, accomodating '=' padding
-		  if ($a < 0)
-			{
-			}
-		  else
-		  { $data.=chr(($a<<2) + (($b&0x30)>>4));
+    # append data, accomodating '=' padding
+    if ($a < 0)
+    {
+    }
+    else
+    { $data.=chr(($a<<2) + (($b&0x30)>>4));
 
-		    if ($c >= 0)
-			{ $data.=chr((($b&0x0f)<<4) + (($c&0x3c)>>2));
+      if ($c >= 0)
+      { $data.=chr((($b&0x0f)<<4) + (($c&0x3c)>>2));
 
-			  if ($d >= 0)
-				{ $data.=chr((($c&0x03)<<6) + $d);
-				}
-			}
-		  }
-
-		  ## warn "data=[$data]\n";
-		}
-
-	  ($data,$_);
+	if ($d >= 0)
+	{ $data.=chr((($c&0x03)<<6) + $d);
 	}
+      }
+    }
+
+    ## warn "data=[$data]\n";
+  }
+
+  ($data,$_);
+}
 
 sub _encode
-	{ my($data,$state)=@_;
+{ my($data,$state)=@_;
 
-	  if (defined $data)
-		{ $state->{BUF}.=$data;
-		}
+  if (defined $data)
+  { $state->{BUF}.=$data;
+  }
 
-	  my($encoded)='';
-	  my($subencode);
+  my($encoded)='';
+  my($subencode);
 
-	  while (length($state->{BUF}) >= 57)
-		{ $subencode=substr($state->{BUF},$[,57);
-		  substr($state->{BUF},$[,57)='';
+  while (length($state->{BUF}) >= 57)
+  { $subencode=substr($state->{BUF},$[,57);
+    substr($state->{BUF},$[,57)='';
 
-		  $encoded.=_encodeN($subencode)."\r\n";
-		}
+    $encoded.=_encodeN($subencode)."\r\n";
+  }
 
-	  if (! defined $data)
-		{ $encoded.=_encodeN($state->{BUF})."\r\n";
-		  $state->{BUF}='';
-		}
+  if (! defined $data)
+  { $encoded.=_encodeN($state->{BUF})."\r\n";
+    $state->{BUF}='';
+  }
 
-	  $encoded;
-	}
+  $encoded;
+}
 
 sub _encodeN
-	{ my($subencode)=@_;
-	  my($subdata);
-	  my($encoded)='';
+{ my($subencode)=@_;
+  my($subdata);
+  my($encoded)='';
 
-	  while (length $subencode)
-		{ $subdata=substr($subencode,$[,3);
-		  substr($subencode,$[,3)='';
-		  $encoded.=_encode3($subdata);
-		}
+  while (length $subencode)
+  { $subdata=substr($subencode,$[,3);
+    substr($subencode,$[,3)='';
+    $encoded.=_encode3($subdata);
+  }
 
-	  $encoded;
-	}
+  $encoded;
+}
 
 sub _encode3
-	{ my($data)=shift;
+{ my($data)=shift;
 
-	  die "\"$data\" is too long" if length $data > 3;
+  die "\"$data\" is too long" if length $data > 3;
 
-	  return '' if ! length $data;
+  return '' if ! length $data;
 
-	  ## warn "e3($data)\n";
+  ## warn "e3($data)\n";
 
-	  my($a,$b,$c)=split(//,$data);
+  my($a,$b,$c)=split(//,$data);
 
-	  $a=ord($a);
-	  $b=(length $b ? ord($b) : undef);
-	  $c=(length $c ? ord($c) : undef);
+  $a=ord($a);
+  $b=(length $b ? ord($b) : undef);
+  $c=(length $c ? ord($c) : undef);
 
-	  my(@codes);
+  my(@codes);
 
-	  $codes[0]=($a&0xfc)>>2;
-	  $codes[1]=($a&0x03)<<4;
+  $codes[0]=($a&0xfc)>>2;
+  $codes[1]=($a&0x03)<<4;
 
-	  if (defined $b)
-		{ $codes[1]|=($b&0xf0)>>4;
-		  $codes[2]=($b&0x0f)<<2;
+  if (defined $b)
+  { $codes[1]|=($b&0xf0)>>4;
+    $codes[2]=($b&0x0f)<<2;
 
-		  if (defined $c)
-			{ $codes[2]|=($c&0xc0)>>6;
-			  $codes[3]=$c&0x3f;
-			}
-		  else	{ $codes[3] = -1;
-			}
-		}
-	  else	{ $codes[3] = -1;
-		  $codes[2] = -1;
-		}
+    if (defined $c)
+    { $codes[2]|=($c&0xc0)>>6;
+      $codes[3]=$c&0x3f;
+    }
+    else
+    { $codes[3] = -1;
+    }
+  }
+  else
+  { $codes[3] = -1;
+    $codes[2] = -1;
+  }
 
-	  join('',map(_n2code($_),@codes));
-	}
+  join('',map(_n2code($_),@codes));
+}
 
 1;
