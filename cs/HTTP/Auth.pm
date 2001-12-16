1201bf16eded67afc7432f98d0c310aa92be1703
Cameron Simpson <cs@zip.com.au>
2001-12-16 05:54:16 +0000

    Use cs::HTTP::Auth in fetches. [CVS rev 1.2]

--- cs/HTTP/Auth.pm
+++ cs/HTTP/Auth.pm
@@ -17,86 +17,87 @@ package cs::HTTP::Auth;
 @cs::HTTP::Auth::ISA=qw();
 
 sub finish
-	{ cs::Persist::finish();
-	}
-
-sub new
-	{ my($class,$db,$rw)=@_;
-	  $db="$ENV{HOME}/.http-auth-db" if ! defined $db;
-	  $rw=0 if ! defined $rw;
-
-	  if (! ref $db)
-		{ return undef if ! defined ($db=cs::Persist::db($db,$rw))
-		}
-
-	  bless { DB => $db,
-		}, $class;
-	}
+{ cs::Persist::finish();
+}
 
 # hdr -> (scheme,label)
-sub ParseWWW_AUTHENTICATE
-	{ my($this,$wahdr)=@_;
-	  if (ref $wahdr)
-		# we can take an RFC822 object, too
-		{ $wahdr=$wahdr->Hdr(WWW_AUTHENTICATE);
-		}
+sub parseWWW_AUTHENTICATE
+{ my($wahdr)=@_;
+  if (ref $wahdr)
+  # we can take an RFC822 object, too
+  { $wahdr=$wahdr->Hdr(WWW_AUTHENTICATE);
+  }
 
-	  ## warn "WWW_AUTHENTICATE=[$wahdr]";
+  ## warn "WWW_AUTHENTICATE=[$wahdr]";
 
-	  if ($wahdr =~ /^\s*(\w+)\s+realm\s*=\s*"([^"]*)\"/i)
-		{ warn "1=$1, 2=$2\n";return ($1,$2);
-		}
+  if ($wahdr =~ /^\s*(\w+)\s+realm\s*=\s*"([^"]*)\"/i)
+  { ## warn "1=$1, 2=$2\n";
+    return ($1,$2);
+  }
 
-	  return undef;
-	}
+  return undef;
+}
+
+sub new
+{ my($class,$db,$rw)=@_;
+  $db="$ENV{HOME}/.http-auth-db" if ! defined $db;
+  $rw=0 if ! defined $rw;
+
+  if (! ref $db)
+  { return undef if ! defined ($db=cs::Persist::db($db,$rw))
+  }
+
+  bless { DB => $db,
+	}, $class;
+}
 
 # Look up authority for challenge.
 # (scheme,host,label) -> { USERID => userid, PASSWORD => password }
 sub GetAuth
-	{ my($this,$scheme,$host,$label)=@_;
+{ my($this,$scheme,$host,$label)=@_;
 
-	  my($h)=$this->{DB};
-	  my(@keys)=(uc($scheme),lc($host),$label);
+  my($h)=$this->{DB};
+  my(@keys)=(uc($scheme),lc($host),$label);
 
-	  local($_);
+  local($_);
 
-	  while (@keys)
-		{ $_=shift(@keys);
-		  return undef if ! exists $h->{$_};
-		  $h=$h->{$_};
-		}
+  while (@keys)
+  { $_=shift(@keys);
+    return undef if ! exists $h->{$_};
+    $h=$h->{$_};
+  }
 
-	  $h;
-	}
+  $h;
+}
 
 sub SaveAuth
-	{ my($this,$scheme,$host,$label,$userid,$password)=@_;
+{ my($this,$scheme,$host,$label,$userid,$password)=@_;
 
-	  my($db)=$this->{DB};
+  my($db)=$this->{DB};
 
-	  $scheme=uc($scheme);
-	  $host=lc($host);
+  $scheme=uc($scheme);
+  $host=lc($host);
 
-	  $db->{$scheme}={} if ! exists $db->{$scheme};
-	  $db=$db->{$scheme};
-	  $db->{$host}={} if ! exists $db->{$host};
-	  $db=$db->{$host};
+  $db->{$scheme}={} if ! exists $db->{$scheme};
+  $db=$db->{$scheme};
+  $db->{$host}={} if ! exists $db->{$host};
+  $db=$db->{$host};
 
-	  $db->{$label}={ USERID => $userid, PASSWORD => $password };
-	}
+  $db->{$label}={ USERID => $userid, PASSWORD => $password };
+}
 
 # annotate some headers with an authority
 sub HdrsAddAuth
-	{ my($this,$hdrs,$scheme,$auth)=@_;
-	  
-	  ## {my(@c)=caller;warn "HdrsAddAuth(@_) from [@c]";}
+{ my($this,$hdrs,$scheme,$auth)=@_;
+  
+  ## warn "auth=".cs::Hier::h2a($auth,0)."\n";
 
-	  $hdrs->Add([AUTHORIZATION,
-		"$scheme ".base64("$auth->{USERID}:$auth->{PASSWORD}")]);
-	}
+  $hdrs->Add([AUTHORIZATION,
+	"$scheme ".base64("$auth->{USERID}:$auth->{PASSWORD}")]);
+}
 
 sub base64
-	{ cs::MIME::Base64::encode(@_);
-	}
+{ cs::MIME::Base64::encode(@_);
+}
 
 1;
