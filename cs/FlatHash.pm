78a38d2495c1116b45a2fcffbd71c730ede1eecd
Cameron Simpson <cs@zip.com.au>
2002-01-14 01:41:03 +0000

    Fix prototype. [CVS rev 1.2]

--- cs/FlatHash.pm
+++ cs/FlatHash.pm
@@ -16,182 +16,182 @@ package cs::FlatHash;
 @cs::FlatHash::ISA=(cs::HASH);
 
 sub flattened($$;$)
-	{
-	  my($thisHash)={};
+{
+  my($thisHash)={};
 
-	  my($obj)=tie (%$thisHash, cs::FlatHash, @_);
-	  die "$::cmd: TIEHASH(..,cs::FlatHash,@_) fails"
-		if ! defined $obj;
+  my($obj)=tie (%$thisHash, cs::FlatHash, @_);
+  die "$::cmd: TIEHASH(..,cs::FlatHash,@_) fails"
+	if ! defined $obj;
 
-	  wantarray
-		? ($thisHash,$obj)
-		: $thisHash;
-	}
+  wantarray
+	? ($thisHash,$obj)
+	: $thisHash;
+}
 
 # make an object to flatten %$otherHash
 #
 sub TIEHASH
-	{ my($class,$otherHash,$depth,$sep)=@_;
-	  die "no otherHash!" if ! defined $otherHash;
-	  die "otherHash should be a hash ref!" if ! ref $otherHash
-						|| ::reftype($otherHash) ne HASH;
-	  die "no depth!" if ! defined $depth;
-	  $sep='/' if ! defined $sep;
+{ my($class,$otherHash,$depth,$sep)=@_;
+  die "no otherHash!" if ! defined $otherHash;
+  die "otherHash should be a hash ref!" if ! ref $otherHash
+					|| ::reftype($otherHash) ne HASH;
+  die "no depth!" if ! defined $depth;
+  $sep='/' if ! defined $sep;
 
-	  my($this);
+  my($this);
 
-	  $this=bless { OTHER	=> $otherHash,
-			DEPTH	=> $depth,
-			SEP	=> $sep,
-		      }, $class;
+  $this=bless { OTHER	=> $otherHash,
+		DEPTH	=> $depth,
+		SEP	=> $sep,
+	      }, $class;
 
-	  ## warn "this=".cs::Hier::h2a($this,1);
+  ## warn "this=".cs::Hier::h2a($this,1);
 
-	  $this;
-	}
-
+  $this;
+}
 
 sub _KeySplit
-	{ my($this,$key)=@_;
+{ my($this,$key)=@_;
 
-	  my($depth)=$this->{DEPTH};
-	  my($sep)=  $this->{SEP};
+  my($depth)=$this->{DEPTH};
+  my($sep)=  $this->{SEP};
 
-	  my(@splat);
-	  my($i);
+  my(@splat);
+  my($i);
 
-	  my($okey)=$key;
+  my($okey)=$key;
 
-	  while ($depth > 1 && ($i=index($key,$sep)) >= 0)
-		{ push(@splat,substr($key,0,$i));
-		  $key=substr($key,$i+length($sep));
-		  $depth--;
-		}
+  while ($depth > 1 && ($i=index($key,$sep)) >= 0)
+  { push(@splat,substr($key,0,$i));
+    $key=substr($key,$i+length($sep));
+    $depth--;
+  }
 
-	  push(@splat,$key);
+  push(@splat,$key);
 
-	  ## warn "keysplit($okey)=[@splat]";
+  ## warn "keysplit($okey)=[@splat]";
 
-	  @splat;
-	}
+  @splat;
+}
 
 sub FETCH
-	{ my($this,$key)=@_;
+{ my($this,$key)=@_;
 
-	  my($db)=$this->{OTHER};
+  my($db)=$this->{OTHER};
 
-	  my(@k)=$this->_KeySplit($key);
+  my(@k)=$this->_KeySplit($key);
 
-	  for my $subkey (@k)
-		{ return undef if ! ref $db;
-		  return undef if ::reftype($db) ne HASH;
-		  return undef if ! exists $db->{$subkey};
+  for my $subkey (@k)
+  { return undef if ! ref $db;
+    return undef if ::reftype($db) ne HASH;
+    return undef if ! exists $db->{$subkey};
 
-		  $db=$db->{$subkey};
-		}
+    $db=$db->{$subkey};
+  }
 
-	  $db;
-	}
+  $db;
+}
 
 sub EXISTS
-	{ my($this,$key)=@_;
+{ my($this,$key)=@_;
 
-	  my($v)=$this->FETCH($key);
+  my($v)=$this->FETCH($key);
 
-	  defined $v;
-	}
+  defined $v;
+}
 
 sub STORE
-	{ my($this,$key,$value)=@_;
+{ my($this,$key,$value)=@_;
 
-	  ## warn "STORE(@_)\n";
+  ## warn "STORE(@_)\n";
 
-	  my($db)=$this->{OTHER};
+  my($db)=$this->{OTHER};
 
-	  my(@k)=$this->_KeySplit($key);
-	  die "too few components to [$key]" if @k != $this->{DEPTH};
+  my(@k)=$this->_KeySplit($key);
+  die "too few components to [$key]" if @k != $this->{DEPTH};
 
-	  my($subkey,$v);
+  my($subkey,$v);
 
-	  while (@k > 1)
-		{ $subkey=shift(@k);
-		  if (! exists $db->{$subkey})
-			{ $db->{$subkey}={};
-			  $db=$db->{$subkey};
-			}
-		  else
-		  { $v=$db->{$subkey};
-		    if (! ref $v
-		     || ::reftype($v) ne HASH)
-			{ my(@c)=caller;
-			  die "$this isn't that deep from [@c]";
-			}
+  while (@k > 1)
+  { $subkey=shift(@k);
+    if (! exists $db->{$subkey})
+    { $db->{$subkey}={};
+      $db=$db->{$subkey};
+    }
+    else
+    { $v=$db->{$subkey};
+      if (! ref $v
+       || ::reftype($v) ne HASH)
+      { my(@c)=caller;
+	die "$this isn't that deep from [@c]";
+      }
 
-		    $db=$v;
-		  }
-		}
+      $db=$v;
+    }
+  }
 
-	  $db->{$k[0]}=$value;
-	}
+  $db->{$k[0]}=$value;
+}
 
 sub DELETE
-	{ my($this,$key)=@_;
+{ my($this,$key)=@_;
 
-	  my($db)=$this->{OTHER};
+  my($db)=$this->{OTHER};
 
-	  my(@k)=$this->_KeySplit($key);
-	  die "too few components to [$key]" if @k != $this->{DEPTH};
+  my(@k)=$this->_KeySplit($key);
+  die "too few components to [$key]" if @k != $this->{DEPTH};
 
-	  my($subkey,$v);
+  my($subkey,$v);
 
-	  while (@k > 1)
-		{ $subkey=shift(@k);
-		  if (! exists $db->{$subkey})
-			{ return;	# ==> already deleted
-			}
-		  else
-		  { $v=$db->{$subkey};
-		    if (! ref $v
-		     || ::reftype($v) ne HASH)
-			{ my(@c)=caller;
-			  die "$this isn't that deep from [@c]";
-			}
+  while (@k > 1)
+  { $subkey=shift(@k);
+    if (! exists $db->{$subkey})
+    { return;	# ==> already deleted
+    }
+    else
+    { $v=$db->{$subkey};
+      if (! ref $v
+       || ::reftype($v) ne HASH)
+      { my(@c)=caller;
+	die "$this isn't that deep from [@c]";
+      }
 
-		    $db=$v;
-		  }
-		}
+      $db=$v;
+    }
+  }
 
-	  delete $db->{$k[0]};
-	}
+  delete $db->{$k[0]};
+}
 
 sub KEYS
-	{ my($this)=@_;
+{ my($this)=@_;
 
-	  _deepKeys($this->{OTHER},$this->{DEPTH},$this->{SEP});
-	}
+  _deepKeys($this->{OTHER},$this->{DEPTH},$this->{SEP});
+}
 
-sub _deepKeys(\%$$)
-	{ my($db,$depth,$sep)=@_;
+sub _deepKeys($$$);
+sub _deepKeys($$$)
+{ my($db,$depth,$sep)=@_;
 
-	  die if $depth < 1;
+  die if $depth < 1;
 
-	  my(@k)=keys %$db;
+  my(@k)=keys %$db;
 
-	  $depth--;
+  $depth--;
 
-	  return @k if $depth == 0;
+  return @k if $depth == 0;
 
-	  my(@subk,$v);
+  my(@subk,$v);
 
-	  for my $key (@k)
-		{ $v=$db->{$key};
-		  if (ref $v && ::reftype($v) eq HASH)
-			{ push(@subk,
-			       map("$key$sep$_",_deepKeys($v,$depth,$sep)));
-			}
-		}
+  for my $key (@k)
+  { $v=$db->{$key};
+    if (ref $v && ::reftype($v) eq HASH)
+    { push(@subk,
+	   map("$key$sep$_",_deepKeys($v,$depth,$sep)));
+    }
+  }
 
-	  @subk;
-	}
+  @subk;
+}
 
 1;
