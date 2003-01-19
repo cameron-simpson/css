e5f09912df059536ec02da288ae8c46e5cb485af
Cameron Simpson <cs@zip.com.au>
2003-01-19 01:28:21 +0000

    Drop 	BEGIN { use cs::DEBUG; cs::DEBUG::using(__FILE__); from everything. [CVS rev 1.6]

--- cs/Object.pm
+++ cs/Object.pm
@@ -23,7 +23,7 @@ The B<cs::Object> module provided a few common methods for most objects.
 
 use strict qw(vars);
 
-BEGIN { use cs::DEBUG; cs::DEBUG::using(__FILE__); }
+##BEGIN { use cs::DEBUG; cs::DEBUG::using(__FILE__); }
 
 package cs::Object;
 
