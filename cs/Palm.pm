0d793e1b01df0dbb9cb94a21f11b7a658e6727f3
Cameron Simpson <cs@zip.com.au>
2003-01-19 01:28:21 +0000

    Drop 	BEGIN { use cs::DEBUG; cs::DEBUG::using(__FILE__); from everything. [CVS rev 1.4]

--- cs/Palm.pm
+++ cs/Palm.pm
@@ -21,7 +21,7 @@ for working with Palm Pilots.
 
 use strict qw(vars);
 
-BEGIN { use cs::DEBUG; cs::DEBUG::using(__FILE__); }
+##BEGIN { use cs::DEBUG; cs::DEBUG::using(__FILE__); }
 
 use cs::Misc;
 
