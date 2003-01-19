380eb9ace1bf8b2726cae248811b6979599342b8
Cameron Simpson <cs@zip.com.au>
2003-01-19 01:28:21 +0000

    Drop 	BEGIN { use cs::DEBUG; cs::DEBUG::using(__FILE__); from everything. [CVS rev 1.15]

--- cs/URL.pm
+++ cs/URL.pm
@@ -20,7 +20,7 @@ This module implements methods for dealing with URLs.
 
 use strict qw(vars);
 
-BEGIN { use cs::DEBUG; cs::DEBUG::using(__FILE__); }
+##BEGIN { use cs::DEBUG; cs::DEBUG::using(__FILE__); }
 
 use cs::Misc;
 use cs::Net::TCP;
