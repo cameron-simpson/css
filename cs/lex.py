3ddc5ab8f1a5766adef4b3e0e70239c71b3be6e2
Cameron Simpson <cs@zip.com.au>
2004-08-26 00:53:05 +0000

    Add lastlinelen for measuring final "width" of multiline string. [CVS rev 1.2]

--- cs/lex.py
+++ cs/lex.py
@@ -1,7 +1,11 @@
 import string
 
+""" location of next nonwhite in string """
 def skipwhite(s,start=0):
   while start < len(s) and s[start] in string.whitespace:
     start+=1
   return start
 
+""" length of text after last newline in string """
+def lastlinelen(s):
+  return len(s)-string.rfind(s,'\n')-1
