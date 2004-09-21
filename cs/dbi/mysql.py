065b7d0934c46d2bc3d546d524113d2ce27b75f1
Cameron Simpson <cs@zip.com.au>
2004-09-21 00:05:30 +0000

    MySQL specific hooks. [CVS rev 1.1]

---
+++ cs/dbi/mysql.py
@@ -0,0 +1,28 @@
+import os
+import cs.dbi
+import cs.secret
+import MySQLdb
+
+def conn(db,systemid=os.environ['SYSTEMID']):
+  secret=cs.secret.get(db+'-mysql@'+systemid)
+  return Conn(host=secret['HOST'],db=db,
+	      user=secret['LOGIN'],passwd=secret['PASSWORD'])
+
+""" a connection to a MySQL server
+"""
+class Conn:
+  def __init__(self,host="localhost",db=None,user="",passwd=""):
+    self.__conn=MySQLdb.connect(host=host,db=db,user=user,passwd=passwd)
+    print "paramstyle=", MySQLdb.paramstyle
+
+  def newCursor(self):
+    return self.__conn.cursor()
+
+  def view(self,*args):
+    return cs.dbi.View(self.__conn,*args)
+
+  def table(self,table,idfields=["ID"]):
+    return cs.dbi.UpdateableTableView(self,table,idfields)
+
+  def close():
+    self.conn.close()
