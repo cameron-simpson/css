78d0d9cd0368ffc5a9d7ce3e89d1bddfc3c6595c
Cameron Simpson <cs@zip.com.au>
2005-07-29 06:19:31 +0000

    *** empty log message *** [CVS rev 1.9]

--- cs/misc.py
+++ cs/misc.py
@@ -14,7 +14,8 @@ def chomp(s):
 
 def index(seq,val):
   for i in xrange(len(seq)-1):
-    if val == seq[i]: return i
+    if val == seq[i]:
+      return i
   return -1
 
 def dict2ary(d,keylist=None):
