85b989e8dd1c7d017c9a9dbb1c4d9815499ac638
Cameron Simpson <cs@zip.com.au>
2005-08-13 06:47:04 +0000

    Add mysql() convenient function to get a MySQL connection using a secret. Add ldap() convenient function to get an LDAP connection using a secret. [CVS rev 1.2]

--- cs/secret.py
+++ cs/secret.py
@@ -4,3 +4,22 @@ import cs.hier
 
 def get(secret,base=os.path.join(os.environ["HOME"],".secret")):
   return cs.hier.load(os.path.join(base,secret))
+
+def mysql(secret,db=None):
+  if not(hasattr(secret,'__keys__') or hasattr(o,'keys')):
+    # transmute secret name into structure
+    secret=get(secret)
+
+  import cs.dbi.mysql
+  return cs.dbi.mysql.Conn(host=secret['HOST'],
+			   db=db,
+			   user=secret['LOGIN'],
+			   password=secret['PASSWORD'])
+
+def ldap(secret,basedn,user=None,password=None):
+  if not(hasattr(secret,'__keys__') or hasattr(o,'keys')):
+    # transmute secret name into structure
+    secret=get(secret)
+
+  import ldap
+  return ldap.open(secret['HOST'])
