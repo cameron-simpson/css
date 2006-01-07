c040abbebfc4ec71096bd0d76512136df54220fd
Cameron Simpson <cs@zip.com.au>
2006-01-07 20:48:16 +0000

    Add isint() lexical test. I really means iscardinal and sould probably not use an RE. [CVS rev 1.4]

--- cs/lex.py
+++ cs/lex.py
@@ -1,13 +1,20 @@
 import string
+import re
 
-""" location of next nonwhite in string """
+int_re=re.compile(r'^\d+$')
+
+def isint(s):
+  m=int_re.match(s)
+  return m is not None and m.group() == s
+
 def skipwhite(s,start=0):
+  """ location of next nonwhite in string """
   while start < len(s) and s[start] in string.whitespace:
     start+=1
   return start
 
-""" length of text after last newline in string
-    initially used by cs.hier to compute effective text width
-"""
 def lastlinelen(s):
+  """ length of text after last newline in string
+      initially used by cs.hier to compute effective text width
+  """
   return len(s)-string.rfind(s,'\n')-1
