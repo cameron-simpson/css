4fc7aa6b7558ecf7b61025d80254958c8cc7c522
Cameron Simpson <cs@zip.com.au>
2006-03-11 21:41:59 +0000

    Add parseline: parse a line consisting of words or "quoted strings". Add undq: strip quotes from a "quoted string". [CVS rev 1.5]

--- cs/lex.py
+++ cs/lex.py
@@ -18,3 +18,43 @@ def lastlinelen(s):
       initially used by cs.hier to compute effective text width
   """
   return len(s)-string.rfind(s,'\n')-1
+
+dq_re=re.compile(r'"(([^\\"]|\\[\\"])*)"')
+nq_re=re.compile(r'\S+')
+ 
+# parse a line consisting of words or "quoted strings"
+def parseline(line):
+  words=[]
+  line=string.lstrip(line)
+  while len(line) > 0:
+    m=dq_re.match(line)
+    if m is not None:
+      words.append(undq(m.group(1)))
+      line=line[m.end():]
+    else:
+      m=nq_re.match(line)
+      if m is not None:
+        words.append(m.group(0))
+        line=line[m.end():]
+      else:
+        cmderr("aborting parseline at:",line)
+        return None
+  
+    line=string.lstrip(line)
+ 
+  return words
+ 
+# strip quotes from a "quoted string"
+dqch_re=re.compile(r'([^\\]|\\.)')
+def undq(s):
+  result=''
+  bs=s.find('\\')
+  while bs >= 0:
+    if bs > 0: result+=s[:bs]
+    result.append(s[bs+1])
+    s=s[bs+2:]
+      
+  result+=s
+      
+  return result
+
