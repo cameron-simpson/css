74580e012d6d3e8ef917e1a0ecaf501df6483284
Cameron Simpson <cs@zip.com.au>
2006-04-17 04:50:55 +0000

    First cut at __delitem__; still buggy. [CVS rev 1.2]

--- cs/sparse.py
+++ cs/sparse.py
@@ -1,15 +1,14 @@
 from cs.misc import debug, warn
 
+# An on-demand sparse cache over an existing sequence.
 class SparseSeq:
   def __init__(self,seq):
     self.__seq=seq
     self.__cache=[]
     self.__lastchunk=None
 
-  def __len__(self):
-    return len(self.__seq)
-  def __repr__(self):
-    return `self.__seq`
+  def __len__(self):  return len(self.__seq)
+  def __repr__(self): return `self.__seq`
 
   def __getitem__(self,ndx):
     debug("__getitem__", `ndx`)
@@ -99,3 +98,69 @@ class SparseSeq:
 
     debug("preload done")
 
+  def __delitem__(self,ndx):
+    self.__seq[ndx]
+
+    if type(ndx) is slice:
+      (start,end,stride)=ndx.indices(self.__len__())
+      if stride == 1:
+	self.forget(start,end)
+      elif stride == -1:
+	self.forget(end+1,start)
+      else:
+	for i in range(start,end,stride):
+	  self.forget(i)
+    else:
+      forget(ndx)
+
+  def forget(self,low,high=None):
+    if high is None: high=low+1
+    debug("forget[",low,':',high,"]")
+
+    p=self.findChunkIndex(low)
+    cache=self.__cache
+
+    debug("  while", low, "<", high, "p =", p)
+    while low < high:
+      debug("  low =", low, "p =", p)
+      if p >= len(cache):
+	return
+
+      chunk=cache[p]
+      if low < chunk[0]:
+	# low is too low, advance to chunk
+	low=chunk[0]
+      elif low == chunk[0]:
+	# low matches start of chunk
+	if high >= chunk[1]:
+	  # drop chunk entirely
+	  debug("  drop", chunk[0], ":", chunk[1])
+	  low=chunk[1]
+	  del cache[p]
+	else:
+	  # drop leading portion of chunk
+	  debug("  drop chunk[", chunk[0], ":", high, "]")
+	  chiplen=high-low
+	  del chunk[2][:chiplen]
+	  chunk[0]+=chiplen
+	  chunk[1]+=chiplen
+	  low=high
+      else:
+	# low lies in the chunk, remove portion or tail
+	if high >= chunk[1]:
+	  # drop tail
+	  debug("  drop", low, ":", chunk[1])
+	  chiplen=chunk[1]-low
+	  del chunk[2][-chiplen:]
+	  low=chunk[1]
+	  chunk[1]-=chiplen
+	  p+=1
+	else:
+	  # drop portion within chunk, split it in two
+	  debug("  drop", low, ":", high)
+	  frontlen=low-chunk[0]
+	  cache.insert(p,(chunk[0],low,chunk[2][:frontlen]))
+	  frontlen=high-chunk[0]
+	  chunk[0]=high
+	  del chunk[2][:frontlen]
+	  low=high
