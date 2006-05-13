299b1244dbf34bc9e416c3a6169305929b3c580c
Cameron Simpson <cs@zip.com.au>
2006-05-13 05:16:06 +0000

    MacOSX related facilities. Initially Latin-1 to UTF-8 Normal Form D transliteration. [CVS rev 1.1]

---
+++ cs/mac.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+#
+# MacOSX related facilities.
+#	- Cameron Simpson <cs@zip.com.au> 09may2006
+#
+
+import unicodedata
+
+#############################################################################
+# Functions to handle general filenames on the basis that they are a sequence
+# of (by default) Latin-1 bytes, and recode them as a Normal Form D UTF-8
+# sequence of bytes.
+# Such names will still be valid on a UNIX filesystem, and be accepted by
+# a MacOSX HFS filesystem, which rejects attempts to make filenames with
+# invalid NFD UTF8 byte sequence names.
+#
+
+def nfd(name,srcencoding='iso8859-1'):
+  ''' Convert a name from another encoding (default ISO8859-1, Latin-1)
+      into MacOSX HFS friendly UTF-8 Normal Form D.
+  '''
+  uf=unicode(name,srcencoding)		# get Unicode version
+  nfduf=unicodedata.normalize('NFD',uf)	# transform to Normal Form D
+  utf8f=nfduf.encode('utf8')		# transcribe to UTF-8
+  return utf8f
