9609b8db6286930113177c39a8bc7aa376ed55cd
Cameron Simpson <cs@zip.com.au>
2006-10-13 04:40:34 +0000

    Add exactlyOne() convenience method to return element 0 of an array or throw an IndexError exception. Arguably this should be in cs.misc or something. Add testType(obj). Return True if obj.TYPE in type list or obj is a word in the typelist. Permit single letter capwords. Add createDB() function to instantiate tables in empty db. Add preload() method. Anonymous nodes now have NAME == None. Generally discard functions returning nodeids in favour of functions returning nodes; more db I/O but less latency and more caching. Add a bunc of nodesBy*() methods. Nodes now print themselves with nice debug friendly names. Recast EDGEs to directly connect to their parent and not method their child - the child connects. MUCH faster and more convenient. Add AttrTable and AttrSet, presenting the ATTRS table via an nodeid index. [CVS rev 1.4]

--- cs/dbdigraph.py
+++ cs/dbdigraph.py
@@ -1,45 +1,72 @@
 import string
 import re
-from cs.misc import cmderr, debug, warn, die, uniq
+from cs.misc import cmderr, debug, warn, die, uniq, exactlyOne
 from cs.hier import flavour
 from cs.db import dosql, SQLQuery, sqlise, today
+from cs.lex import strlist
+import cs.cache
 
 NodeCoreAttributes=('NAME','TYPE')
 
-def SQLtypeTest(*types):
+def SQLtestType(*types):
   assert len(types) > 0
   if len(types) == 1:
     return "TYPE = "+sqlise(types[0])
   return "TYPE IN ("+string.join([ sqlise(type) for type in types ], ",")+")"
 
-CapWord_re = re.compile(r'^[A-Z][A-Z_]*[A-Z]$')
+def testType(t, *types):
+  if type(t) is not str: t=t.TYPE
+  return t in types
+
+CapWord_re = re.compile(r'^[A-Z][A-Z_]*$')
 
 def testAllCaps(s):
   return CapWord_re.match(s)
 
+def createDB(conn,nodes='NODES',attrs='ATTRS'):
+  dosql(conn, 'CREATE TABLE '+nodes+' (ID autoincrement, NAME varchar(32), TYPE varchar(16));')
+  dosql(conn, 'CREATE TABLE '+attrs+' (ID autoincrement, ID_REF unsigned, ATTR varchar(64), VALUE varchar(255), IS_IDREF tinyint(1));')
+
 class DBDiGraph:
   def __init__(self,nodeTable,attrTable):
     self.nodes=nodeTable
-    self.attrs=attrTable
+    self.attrs=AttrTable(self,attrTable)
     self.__nodeCache={}
-    self.__selectIdsBaseQuery='SELECT ID FROM '+self.nodes.name
+    self.__selectIdsBaseQuery='SELECT DISTINCT(ID) FROM '+self.nodes.name
+    self.__soleUser=False
+    self.__lastid=None
+
+  def preload(self):
+    self.nodes.preload()
+    self.attrs.preload()
 
   def keys(self):
-    return self.nodeidsWhere()
+    return self.nodes.keys()
 
   def createNode(self,name,type,multipleNamesOk=False):
     ''' Creates a new node with the specified name and type.
         Returns the ID of the new node.
     '''
-    if not multipleNamesOk and self.nodeByNameAndType(name,type):
+    if name is not None and not multipleNamesOk and self.nodeByNameAndType(name,type):
       die("createNode(name="+str(name)+", type="+type+"): already exists")
 
-    self.nodes.insert({'NAME': name, 'TYPE': type});
-    return self.nodeByNameAndType(name,type)
+    if not self.__soleUser or self.__lastid is None:
+      newId=[row[0] for row in SQLQuery(self.nodes.conn,'SELECT MAX(ID) FROM NODES')][0]
+      if newId is None: newId=0
+    else:
+      newId=self.__lastid
+
+    newId+=1
+    self.__lastid=newId
+
+    self.nodes.insert({'ID': newId, 'NAME': name, 'TYPE': type});
+    return self[newId]
+
+  def setSoleUser(self,state):
+    self.__soleUser=state
 
   def addAnonNode(self):
-    maxNodeId=[row[0] for row in SQLQuery(self.nodes.conn,'SELECT MAX(ID) FROM NODES')][0]
-    return self.createNode(str(maxNodeId+1),'_NODE')
+    return self.createNode(None,'')
 
   def _newNode(self,nodeid):
     ''' Constructs a new node instance associated with an id.
@@ -48,76 +75,109 @@ class DBDiGraph:
     return DBDiGraphNode(nodeid,self)
 
   def __getitem__(self,nodeid):
-    if nodeid not in self.__nodeCache:
-      self.__nodeCache[nodeid]=self._newNode(nodeid)
-    return self.__nodeCache[nodeid]
+    if type(nodeid) is int or type(nodeid) is long:
+      if nodeid not in self.__nodeCache:
+        self.__nodeCache[nodeid]=self._newNode(nodeid)
+      return self.__nodeCache[nodeid]
 
-  def nodeByNameAndType(self,name,*types):
-    node=None
-    for id in self.nodeidsByNameAndType(name,*types):
-      if node is None:
-        node=self[id]
-      else:
-        cmderr("multiple hits for node named", name, "- ignoring id", str(id))
+    if type(nodeid) is tuple:
+      return exactlyOne(self.nodesByNameAndType(nodeid[0],*nodeid[1:]), `tuple`)
 
-    return node
+    raise IndexError, "invalid index "+`nodeid`
+
+  def __delitem__(self,nodeid):
+    refids=self[nodeid].referringNodeids()
+    warn("refids =", `refids`)
+    if len(refids) > 0:
+      node=self[nodeid]
+      raise IndexError, "node "+str(node)+" has referring nodes: "+strlist([self[id] for id in refids])
+
+    del self.attrs[nodeid]
+    del self.nodes[nodeid]
 
   def need(self,name,type):
-    node=self.nodeByNameAndType(name,type)
-    if node is None:
+    nodes=self.nodesByNameAndType(name,type)
+    if len(nodes) > 1:
+      raise IndexError, "multiple nodes with index "+`(name,type)`+": "+strlist(nodes)
+    if len(nodes) == 0:
       node=self.createNode(name,type)
+    else:
+      node=nodes[0]
     return node
 
-  def nodeidsWhere(self,where):
-    query=self.__selectIdsBaseQuery
-    if where is not None: query=query+' WHERE '+where
-    debug("query =", query)
-    return [ row[0] for row in SQLQuery(self.nodes.conn,query) ]
+  def nodesWhere(self,where):
+    return [self[row[0]] for row in self.nodes.selectRows(where)]
 
-  def nodeidsByNameAndType(self,name,*types):
-    return self.nodeidsWhere('NAME = '+sqlise(name)+' AND '+SQLtypeTest(*types)) 
+  def nodesFromIds(self,nodeids):
+    if len(nodeids) == 0:
+      return ()
+    if len(nodeids) == 1:
+      return (self[nodeids[0]],)
+    return self.nodesWhere("ID IN ("+strlist(nodeids)+")")
 
-  def nodeidsByAttr(self,attr,value):
-    return [ row[0] for row in SQLQuery(self.attrs.conn,
-                                        'SELECT DISTINCT(ID) FROM '+self.attrs.name
-                                        +' WHERE ATTR = '+sqlise(attr)+' AND VALUE = '+sqlise(value)) ]
+  def nodesByType(self,*types):
+    return nodesWhere(SQLtestType(*types))
 
-  def nodeidsByAttrs(self,attrs):
-    return [ row[0] for row in SQLQuery(self.attrs.conn,
-                                        'SELECT DISTINCT(ID) FROM '+self.attrs.name
-                                        +' WHERE ATTR = '+sqlise(attr)+' AND VALUE = '+sqlise(value)) ]
+  def nodesByNameAndType(self,name,*types):
+    return self.nodesWhere('NAME = '+sqlise(name)+' AND '+SQLtestType(*types))
 
-  def nodeidsByType(self,*types):
-    return self.nodeidsWhere(SQLtypeTest(*types))
+  def _nodeByNameAndType(self,name,*types):
+    return exactlyOne(self.nodesByNameAndType(name,*types), `(name,types)`)
+
+  def nodeByNameAndType(self,name,*types):
+    try:
+      node=self._nodeByNameAndType(name,*types)
+    except IndexError:
+      return None
+
+    return node
+
+  def nodesLike(self,like,types=(),where=None):
+    likewhere="NAME LIKE "+sqlise(like)
+    if len(types) > 0:
+      likewhere=likewhere+" AND "+SQLtestType(*types)
+    if where is not None:
+      likewhere=likewhere+" AND ("+where+")"
+    return self.nodesWhere(likewhere)
+
+  def nodesByAttr(self,attr,value):
+    nodeids=uniq([row['ID_REF'] for row in self.attrs.table.selectRows("ATTR = "+sqlise(attr)+" AND VALUE = "+sqlise(str(value)))])
+    return self.nodesFromIds(nodeids)
+
+  def nodesByType(self,*types):
+    return self.nodesWhere(SQLtestType(*types))
 
 class DBDiGraphNode:
   def __init__(self,id,digraph):
     self.id=id
     self.digraph=digraph
 
-  def clone(self):
+  def clone(self,pruneFields=()):
     N={}
     for k in self.keys():
-      N[k]=self[k]
+      if k not in pruneFields:
+        N[k]=self[k]
     for k in NodeCoreAttributes:
-      N[k]=self[k]
+      if k not in pruneFields:
+        N[k]=self[k]
     return N
 
   def keys(self):
-    return [ row[0]
-             for row in SQLQuery(self.digraph.attrs.conn,
-	     			 'SELECT DISTINCT(ATTR) FROM '+self.digraph.attrs.name
-				 +' WHERE ID_REF = '+str(self.id))
-	   ]
+    return self.digraph.attrs[self.id].keys()
 
   def attrs(self):
     return uniq(NodeCoreAttributes+self.digraph.attrs.getAttrs(self.id))
 
+  def __str__(self):
+    ''' Returns "NAME(TYPE#id)".
+    '''
+    label=self.TYPE+"#"+str(self.id)
+    if self.NAME is not None:
+      label=self.NAME+"("+label+")"
+
+    return label
+
   def __getattr__(self,attr):
-    if attr in self.__dict__:
-      raise AttributeError, "WTF?"
-    if attr == "id":
-      raise AttributeError, "WTF2? "+`self.__dict__`
     if testAllCaps(attr):
       return self.__fieldAccess(attr)
     raise AttributeError, "node id="+str(self.id)+" has no attribute named "+attr
@@ -169,69 +229,213 @@ class DBDiGraphNode:
       raise IndexError, "can't delete NodeCoreAttribute "+key
 
     # TODO: recursively delete any linked hashes?
-    dosql(self.digraph.attrs.conn,
-	  'DELETE FROM '+self.digraph.attrs.name
-	  +' WHERE ID_REF = '+str(self.id)+' AND ATTR = '+sqlise(key))
+    del self.digraph.attrs[self.id][key]
+
+  def delete(self):
+    del self.digraph[self.id]
 
   def setAttr(self,attr,values):
-    del self[attr]
-    self.addAttr(attr,values)
+    self.digraph.attrs[self.id][attr]=values
 
-  def addAttr(self,attr,values,is_idref=0):
+  def addAttr(self,attr,values):
+    self.digraph.attrs.addAttr(self.id,attr,values)
+
+  def getAttr(self,attr):
+    ##warn(str(self)+": getAttr("+`attr`+")")
+    if type(attr) is not str:
+      raise IndexError, "non-string attr: "+`attr`
+    values=self.digraph.attrs[self.id]
+    return values[attr]
+
+  def referringNodeids(self):
+    ''' Returns a list of node ids which refer to this node.
+    '''
+    return [row[0] for row in
+            SQLQuery(self.attrs.table.conn,
+                     'SELECT DISTINCT(ID_REF+0) FROM '+self.digraph.attrs.table.name \
+                     +' WHERE IS_IDREF AND VALUE = '+sqlise(str(self.id)))]
+
+  def edges(self,upstream=False):
+    ''' Returns edges running from this node.
+    '''
+    if not upstream:
+      nodes=self.EDGES
+      if nodes is None:
+        return ()
+      if type(nodes) is not tuple:
+        nodes=(nodes,)
+    else:
+      nodes=self.digraph.nodesByAttr('EDGE',self.id)
+
+    return nodes
+
+  def connectedNodes(self):
+    nodes=[]
+    for edge in self.edges():
+      if edge.TYPE == 'EDGE':
+        nodes.extend(edge.edges())
+      else:
+        nodes.append(edge)
+    
+    return nodes
+
+  def addEdge(self,toNode,edge=None):
+    if edge is None:
+      # an anonymous edge
+      edge=self.digraph.addAnonNode()
+      edge.TYPE='EDGE'
+    elif type(edge) is str:
+      edge=self.digraph.createNode(edge,'EDGE')
+
+    ##warn("addEdge "+str(self)+"->"+str(toNode))
+    self.addAttr('EDGES',edge)
+    edge.addAttr('EDGES',toNode)
+
+class AttrTable(cs.cache.Cache):
+  def __init__(self,digraph,table):
+    self.__direct=DirectAttrTable(digraph,table)
+    cs.cache.Cache.__init__(self,self.__direct)
+
+  def preload(self):
+    self.preloadForNodeids()
+
+  def preloadForNodeids(self,nodeids=None):
+    NV={}
+    where=None
+    if nodeids is not None:
+      where="ID_REF IN ("+strlist(nodeids)+")"
+    for row in self.table.selectRows(where):
+      id=row.key
+      nodeid=row['ID_REF']
+      if nodeid not in NV:
+        nv=NV[nodeid]={}
+      else:
+        nv=NV[nodeid]
+      attr=row['ATTR']
+      v=(row['VALUE'],row['IS_IDREF'])
+      if attr in nv:
+        nv[attr].append(v)
+      else:
+        nv[attr]=[v]
+
+    for nodeid in NV:
+      self.store(AttrSet(self.__direct,nodeid,NV[nodeid]),nodeid)
+
+  def attrsFromNodeids(self,nodeids):
+    ''' Returns AttrSets for the nodes specified in nodeids.
+    '''
+    attrs=[]
+    missing=[nodeid for nodeid in nodeids if not self.__cache.inCache(nodeid)]
+    self.preloadForNodeids(missing)
+    return [self[nodeid] for nodeid in nodeids]
+
+  def nodesWhere(self,where):
+    return self.digraph.nodesFromIds([ row['ID_REF'] for row in self.table.selectRows(where) ])
+
+class DirectAttrTable:
+  def __init__(self,digraph,table):
+    self.digraph=digraph
+    self.table=table
+
+  def addAttr(self,nodeid,attr,values):
+    if flavour(values) != "ARRAY":
+      values=(values,)
+    AttrSet(self,nodeid).addValues(attr,values)
+
+  def __delitem__(self,nodeid):
+    AttrSet(self,nodeid).deleteAll()
+
+  def __getitem__(self,nodeid):
+    return AttrSet(self,nodeid)
+
+  def __setitem__(self,nodeid,newattrs):
+    A=AttrSet(self,nodeid)
+    A.deleteAll()
+    for k in newattrs.keys():
+      A[k]=newattrs[k]
+
+  def insert(self,row):
+    self.table.insert(row)
+
+class AttrSet:
+  def __init__(self,attrs,nodeid,values=None):
+    self.attrs=attrs
+    self.nodeid=nodeid
+    self.__values=values
+
+  def values(self):
+    if self.__values is None:
+      A={}
+      for row in self.attrs.table.selectRows('ID_REF = '+sqlise(self.nodeid)):
+        key=row['ATTR']
+        value=(row['VALUE'],row['IS_IDREF'])
+        if key in A:
+          A[key].append(value)
+        else:
+          A[key]=[value]
+      self.__values=A
+
+    return self.__values
+
+  def keys(self):
+    return self.values().keys()
+
+  def deleteAll(self):
+    import inspect
+    for frame in inspect.stack():
+      warn("stack:", frame[1]+":"+str(frame[2]))
+    self.attrs.table.deleteRows('ID_REF = '+sqlise(self.nodeid))
+    self.__values={}
+
+  def __getitem__(self,key):
+    scalars=[]
+    A=self.values()
+    if key not in A:
+      return ()
+
+    values=A[key]
+    for v in values:
+      if v[1] == 0:
+        scalars.append(v[0])
+      else:
+        scalars.append(self.attrs.digraph[int(v[0])])
+
+    return tuple(scalars)
+
+  def __setitem__(self,key,values):
+    self.attrs.table.deleteRows('ID_REF = '+sqlise(self.nodeid)+' AND ATTR = '+sqlise(key))
+    self.values()[key]=[]
+    self.addValues(key,values)
+
+  def __delitem__(self,key):
+    del self.values()[key]
+    self.attrs.table.deleteRows('ID_REF = '+sqlise(self.nodeid)+' AND ATTR = '+sqlise(key))
+
+  def addValues(self,key,values):
+    A=self.values()
+    if key not in A:
+      A[key]=[]
+
     if flavour(values) != "ARRAY":
       values=(values,)
 
     for value in values:
-      assert is_idref == 0 or int(value) > 0, "is_idref="+`is_idref`+" and value="+`value`
-      if flavour(value) == "HASH":
-        hash=self.digraph.addAnonNode()
-        self.addAttr(attr,str(hash.id),1)
-        for k in value.keys():
-          hash[k]=value[k]
+      if flavour(value) != "HASH":
+        is_idref=0
       else:
-        debug("insert(ID_REF="+str(self.id)+",ATTR="+attr+",VALUE="+`value`+")")
-        self.digraph.attrs.insert({'ID_REF': self.id,
-                           'ATTR': attr,
-                           'VALUE': value,
-                           'IS_IDREF': is_idref});
+        is_idref=1
+        if isinstance(value,DBDiGraphNode) and self.attrs.digraph is value.digraph:
+          value=value.id
+        else:
+          hash=self.attrs.digraph.addAnonNode()
+          for k in value.keys():
+            hash[k]=value[k]
+          value=hash.id
 
-  def getAttr(self,attr):
-    # suck in all the rows
-    rows=[row for row
-          in SQLQuery(self.digraph.attrs.conn,
-                      'SELECT VALUE, IS_IDREF FROM '+self.digraph.attrs.name
-                      +' WHERE ID_REF = '+str(self.id)+' AND ATTR = '+sqlise(attr))]
+        value=str(value)
 
-    # stash the scalar values
-    values=[row[0] for row in rows if not row[1]]
-
-    # stash the nested values
-    for hashid in [int(row[0]) for row in rows if row[1]]:
-      values.append(self.digraph[hashid])
-
-    return values
-
-  def edges(self,idField='FROM_ID'):
-    ''' Returns edges running from this node.
-        Optional parameter idField defaults to ID_FROM.
-    '''
-    nodetbl=self.digraph.nodes.name
-    nodeid=nodetbl+'.ID'
-    attrtbl=self.digraph.attrs.name
-    attridref=attrtbl+'.ID_REF'
-    attrattr =attrtbl+'.ATTR'
-    attrvalue=attrtbl+'.VALUE'
-    query='SELECT DISTINCT('+attridref+') FROM '+nodetbl+' INNER JOIN '+attrtbl+' ON '+attrattr+' = '+sqlise(idField)+' AND '+attrvalue+' = '+str(self.id)
-    return [ self.digraph[row[0]] for row in SQLQuery(self.digraph.nodes.conn,query) ]
-
-  def addEdge(self,toNode,name=None):
-    if name is None:
-      # an anonymous edge
-      edge=self.digraph.addAnonNode()
-      edge.TYPE='EDGE'
-    else:
-      edge=self.digraph.createNode(name,'EDGE')
-
-    edge.FROM_ID=self.id
-    edge.TO_ID=toNode.id
-    edge.START_DATE=today()
+      self.attrs.insert({ 'ID_REF': self.nodeid,
+                          'ATTR': key,
+                          'VALUE': value,
+                          'IS_IDREF': is_idref});
+      A[key].append((value,is_idref))
