fc0cb41797ee126eba3aa3a7f94dfc8625f4006b
Cameron Simpson <cs@zip.com.au>
2007-01-08 00:29:22 +0000

    _JSON_HIER_OPTS: add 'nullToken': 'null'. [CVS rev 1.3]

--- cs/json.py
+++ cs/json.py
@@ -1,7 +1,7 @@
 from cStringIO import StringIO
 import cs.hier
 
-_JSON_HIER_OPTS = { 'dictSep': ':', 'bareWords': False }
+_JSON_HIER_OPTS = { 'dictSep': ':', 'bareWords': False, 'nullToken': 'null' }
 
 def json(obj,i=None):
   return cs.hier.h2a(obj,i,_JSON_HIER_OPTS)
