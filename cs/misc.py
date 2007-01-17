2534683dcb083b2892c361645703b3c2338906b8
Cameron Simpson <cs@zip.com.au>
2007-01-17 01:03:51 +0000

    Add netgroup() function. [CVS rev 1.36]

--- cs/misc.py
+++ cs/misc.py
@@ -322,6 +322,12 @@ def mailsubj(addrs,subj,body):
 
   return pipe.close() is None
 
+def netgroup(*names):
+  ''' Return hosts in a netgroup. Requires the 'ngr' script.
+  '''
+  import cs.sh
+  return [chomp(line) for line in cs.sh.vpopen(('ngr',)+names, mode="r")]
+
 def runCommandPrompt(fnmap,prompt=None):
   ''' Accept a dict of the for key->(fn, help_string)
       and perform entered commands.
