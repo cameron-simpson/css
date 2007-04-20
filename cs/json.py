d87d8d54ea97a6556a4de7ad99bd6634f5569a62
Cameron Simpson <cs@zip.com.au>
2007-04-20 10:55:07 +0000

    Remove json2f - there's no h2f. [CVS rev 1.4]

--- cs/json.py
+++ cs/json.py
@@ -5,6 +5,3 @@ _JSON_HIER_OPTS = { 'dictSep': ':', 'bareWords': False, 'nullToken': 'null' }
 
 def json(obj,i=None):
   return cs.hier.h2a(obj,i,_JSON_HIER_OPTS)
-
-def json2f(fp,obj):
-  cs.hier.h2f(fp,obj,_JSON_HIER_OPTS)
