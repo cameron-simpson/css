56a57c343b6a8cdb7445402ee3e3b9e547c368ff
Cameron Simpson <cs@zip.com.au>
2007-12-07 15:35:23 +1100

    Add JobQueue class for tracking asynchronous returns. Add bgReturn, for doing asynchronous return of immediately available data.

--- cs/threads.py
+++ cs/threads.py
@@ -5,7 +5,7 @@
 #
 
 from __future__ import with_statement
-from threading import BoundedSemaphore
+from threading import Thread, BoundedSemaphore
 
 class Channel:
   ''' A zero-storage data passage.
@@ -55,6 +55,48 @@ class Channel:
     returnChannel(ch)
     return result
 
+class JobQueue:
+  ''' A job queue.
+      Q.queue(token) -> channel
+      Q.dequeue(token, result) -> sends result to channel, releases channel
+  '''
+  def __init__(self,maxq=None):
+    self.q={}
+    self.lock=BoundedSemaphore(1)
+    self.maxq=maxq
+    if maxq is not None:
+      assert maxq > 0
+      self.maxsem=BoundedSemaphore(maxq)
+
+  def enqueue(self,n,ch=None):
+    ''' Queue a token, return a channel to recent the job completion.
+        Allocate a channel for the result if none supplied.
+    '''
+    if self.maxq is not None:
+      self.maxsem.acquire()
+    if ch is None:
+      ch=getChannel()
+      doRelease=True
+    else:
+      doRelease=False
+    with self.lock:
+      assert n not in self, "\"%s\" already queued"
+      self.q[n]=(ch,doRelease)
+    return ch
+  
+  def dequeue(self,n,result):
+    ''' Dequeue a token, send the result and token down the channel allocated.
+        Release the channel if we allocated it.
+    '''
+    with self.lock:
+      ch, doRelease = self.q[n]
+      del self.q[n]
+    if self.maxq is not None:
+      self.maxsem.release()
+    ch.write((n,result))
+    if doRelease:
+      returnChannel(ch)
+
 ''' A pool of Channels.
 '''
 __channels=[]
@@ -69,3 +111,19 @@ def getChannel():
 def returnChannel(ch):
   with __channelsLock:
     __channels.append(ch)
+
+def bgReturn(result):
+  ''' Class to return an asynchronous result.
+      Takes the result, returns a channel from which to read it back.
+      The caller must call returnChannel() on the returned channel after use.
+  '''
+  bg=_BGReturn(result)
+  ch=bg.ch
+  bg.start()
+  return ch
+class _BGReturn(Thread):
+  def __init__(self,result):
+    self.result=result
+    self.ch=getChannel()
+  def run(self):
+    self.ch.write(result)
