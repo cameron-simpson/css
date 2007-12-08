13432f7418f09768efb49ed9b0620a374e42b273
Cameron Simpson <cs@zip.com.au>
2007-12-08 23:34:42 +1100

    Complete by adding trivial TCPStore, wrapper for StreamStore.

--- cs/venti/tcp.py
+++ cs/venti/tcp.py
@@ -4,8 +4,11 @@
 #       - Cameron Simpson <cs@zip.com.au> 07dec2007
 #
 
+import os
+from socket import socket
 from SocketServer import ThreadingTCPServer, StreamRequestHandler
-from cs.venti.stream import StreamDaemon
+from cs.venti.stream import StreamDaemon, StreamStore
+from cs.misc import debug
 
 class Server(ThreadingTCPServer):
   def __init__(self,bindaddr,S):
@@ -20,3 +23,10 @@ class _RequestHandler(StreamRequestHandler):
   def handle(self):
     SD=StreamDaemon(self.S,self.rfile,self.wfile)
     SD.resultsThread.join()
+
+class TCPStore(StreamStore):
+  def __init__(self,bindaddr):
+    self.sock=socket()
+    self.sock.connect(bindaddr)
+    fd=self.sock.fileno()
+    StreamStore.__init__(self,os.fdopen(fd,'wb'),os.fdopen(fd,'rb'))
