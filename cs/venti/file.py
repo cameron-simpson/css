839726d7de803a5cef53f9ba95070b54a13d5dba
Cameron Simpson <cs@zip.com.au>
2008-01-05 12:25:00 +1100

    Remove debugging.

--- cs/venti/file.py
+++ cs/venti/file.py
@@ -21,18 +21,12 @@ def storeFile(S,ifp,rsize=None,findEdge=None):
   ''' Store the data from ifp, return BlockRef.
   '''
   if rsize is None: rsize=8192
-  debug("1")
   ofp=WriteFile(S,findEdge)
-  debug("2")
   buf=ifp.read(rsize)
-  debug("2a: buf=[%s]" % buf)
   while len(buf) > 0:
     ofp.write(buf)
     buf=ifp.read(rsize)
-    debug("2b: buf=[%s]" % buf)
-  debug("3")
   ref=ofp.close()
-  debug("stored, ref=%s" % ref)
   S.log("store file %s %s" % (tohex(ref.encode()), ifp.name))
   return ref
 
