e5b76d29b9c183402fc79c7e9d12ab6500d1483a
Cameron Simpson <cs@zip.com.au>
2008-05-03 10:29:17 +1000

    Add docstrings.

--- lib/cs/venti/cache.py
+++ lib/cs/venti/cache.py
@@ -14,6 +14,15 @@ from heapq import heappush, heappop
 import sys
 
 class CacheStore(BasicStore):
+  ''' A CacheStore is a Store front end to a pair of other Stores, a backend
+      store and a cache store. The backend store is the "main" store, while
+      the cache store is normally a faster and possibly lossy store such as a
+      MemCacheStore or a local disc store.
+      
+      A block read is satisfied from the cache is possible, otherwise from
+      the backend. A block store is stored to the cache and then
+      asynchronously to the backend.
+  '''
   def __init__(self,backend,cache):
     BasicStore.__init__(self,"Cache(cache=%s,backend=%s)"%(cache,backend))
     self.backend=backend
@@ -81,7 +90,9 @@ class CacheStore(BasicStore):
     ch.put((tag,None))                  # report completion
 
 class MemCacheStore(BasicStore):
-  ''' A lossy store that keeps an in-memory cache of recent blocks.
+  ''' A lossy store that keeps an in-memory cache of recent blocks.  It may
+      discard older blocks if new ones come in when full and would normally
+      be used as the cache part of a CacheStore pair.
   '''
   def __init__(self,max=1024):
     BasicStore.__init__(self,"MemCacheStore")
