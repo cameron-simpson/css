ec214986f10ca3ed92aac80e15caaada9dac8428
Cameron Simpson <cs@zip.com.au>
2008-07-10 11:30:57 +1000

    wini.py: redo to throw exceptions on syntax errors.

--- lib/cs/wini.py
+++ lib/cs/wini.py
@@ -8,20 +8,22 @@ clausehdr_re=re.compile(r'^\[\s*([^\s\]]+)\s*\]')
 # regexp to recognise an assignment
 assign_re   =re.compile(r'^\s*([^\s=]+)\s*=\s*(.*)')
 
-# regexp to recognise non-negative integers
-int_re      =re.compile(r'^(0|[1-9][0-9]*)$')
+# regexp to recognise integers
+int_re      =re.compile(r'^(0|-?[1-9][0-9]*)$')
 
 # read a win.ini file, return a dictionary of dictionaries
 def load(fp,parseInts=False):
   contents={}   # empty clause dictionary
   clause=None   # no current clause
 
+  lineno=0
   for line in fp:
+    lineno+=1
 
     # trim newline
-    llen=len(line)
-    if llen > 0 and line[llen-1] == '\n':
-      line=line[:llen-1]
+    assert line[-1:] == '\n', \
+           "%s, line $d: missing newline" % (fp, lineno)
+    line=line[:-1]
 
     # skip blank lines and comments
     line=string.strip(line)
@@ -32,25 +34,24 @@ def load(fp,parseInts=False):
     match=clausehdr_re.match(line)
     if match is not None:
       clausename=match.group(1)
-      if clausename not in contents: contents[clausename]={}
-      clause=contents[clausename]
+      clause=contents.setdefault(clausename,{})
       continue
 
-    # not inside a clause? complain
-    if clause is None:
-      print >>sys.stderr, `fp`+": unexpected data outside of [clause]: "+line
-      continue
+    assert clause is not None, \
+        "%s, line $d: unexpected data outside of [clause]: %s" \
+          % (fp, lineno, line)
 
     # look for var=value
     match=assign_re.match(line)
-    if match is not None:
-      value=match.group(2)
-      if parseInts:
-        valmatch=int_re.match(value)
-        if valmatch is not None: value=int(value)
-      clause[match.group(1)]=value
-      continue
+    assert match is not None, \
+           "%s, line %d: non-assignment inside clause [%s]: %s" \
+             % (fp, lineno, clausename, line)
 
-    print >>sys.stderr, `fp`+": non-assignment inside clause \""+clausename+"\": "+line
+    value=match.group(2)
+    if parseInts:
+      valmatch=int_re.match(value)
+      if valmatch is not None:
+        value=int(value)
+    clause[match.group(1)]=value
 
   return contents
