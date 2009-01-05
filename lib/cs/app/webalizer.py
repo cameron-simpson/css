74ac810332a2c3cd9d716a72421ac0e6f4bd2c32
Cameron Simpson <cs@zip.com.au>
2009-01-05 20:38:51 +1100

    Decode webalizer's DNS cache entries.

--- lib/cs/app/webalizer.py
+++ lib/cs/app/webalizer.py
@@ -2,15 +2,27 @@
 #
 
 import dbhash
+import struct
 
 dnscachefile='/var/lib/webalizer/dns_cache.db'
 
+def dbseq(db):
+  key=db.first()
+  yield key
+  dblen=len(db)
+  for i in xrange(1, dblen):
+    yield db.next()
+
 def cacheEntries(cachefile=dnscachefile):
+  print "open", cachefile
   db=dbhash.open(cachefile,"r")
-  for key, value in db.items():
-    yield key, value
+  for key, value in dbseq(db):
+    hostlen=len(value)-12
+    when, numeric, foo, s = struct.unpack("III%ds"%hostlen, value)
+    s=s.rstrip('\x00')
+    yield key, when, numeric, s
   db.close()
 
 if __name__ == '__main__':
-  for key, value in cacheEntries():
-    print key, value
+  for key, when, numeric, value in cacheEntries():
+    print key, when, `value`
