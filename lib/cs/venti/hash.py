29ea442cc73aad655f93901468c2e8608ce40ee1
Cameron Simpson <cs@zip.com.au>
2009-06-28 17:27:08 +1000

    Enclose default hash functionality in Hash_SHA1 class.

--- lib/cs/venti/hash.py
+++ lib/cs/venti/hash.py
@@ -4,27 +4,45 @@ if sys.hexversion < 0x02050000:
   sha1 = _mod_sha.new
 else:
   from hashlib import sha1
+if sys.hexversion < 0x02060000:
+  bytes = str
 import unittest
-  
+
+# enums for hash types, used in encode/decode
 HASH_SHA1_T=0
-HASH_SIZE_SHA1=20                               # size of SHA-1 hash
-HASH_SIZE_DEFAULT=20                            # default size of hash
-                                                # NEVER CHANGE HASH_SIZE!!!
-assert HASH_SIZE_DEFAULT == 20
-MIN_BLOCKSIZE=80                                # less than this seems silly
-MAX_BLOCKSIZE=16383                             # fits in 2 octets BS-encoded
-MAX_SUBBLOCKS=int(MAX_BLOCKSIZE/(3+HASH_SIZE_DEFAULT))  # flags(1)+span(2)+hash
 
-def hash_sha1(block):
-  ''' Returns the SHA-1 checksum for the supplied block.
-  '''
-  hash=sha1(block)
-  h=hash.digest()
-  assert len(h) == HASH_SIZE_SHA1
-  return h
+class Hash_SHA1(bytes):
+  hashlen = 20
+  hashenum = HASH_SHA1_T
+  @classmethod
+  def hash(cls, data):
+    ''' Compute hash digest from data.
+    '''
+    return sha1(data).digest()
 
-hash=hash_sha1
-HASH_T=HASH_SHA1_T
+  def __init__(self, data=None, hashcode=None):
+    ''' Initialise a Hash from data or a pre-supplied hash digest.
+    '''
+    assert (data is None) ^ (hashcode is None)
+    assert hashcode is None or len(hashcode) == Hash_SHA1.hashlen
+    if hashcode is None:
+      hashcode = self.hash(data)
+    self.hashcode = hashcode
+
+  def encode(self):
+    ''' Return the serialised form of this hash object.
+    '''
+    # no hashenum, no hashlen
+    return self.hashcode
+
+  @classmethod
+  def decode(cls, encdata):
+    ''' Pull off the encoded hash from the start of the encdata.
+        Return Hash_SHA1 object and tail of encdata.
+    '''
+    hashcode = encdata[:self.hashlen]
+    assert len(hashcode) == self.hashlen, "encdata (%d bytes) too short" % (len(encdata),)
+    return Hash_SHA1(hashcode=hashcode), encdata[20:]
 
 class TestAll(unittest.TestCase):
   def setUp(self):
@@ -34,7 +52,7 @@ class TestAll(unittest.TestCase):
     import random
     for i in range(10):
       rs = ''.join( chr(random.randint(0,255)) for x in range(100) )
-      self.assertEqual( sha1(rs).digest(), hash_sha1(rs) )
+      self.assertEqual( sha1(rs).digest(), Hash_SHA1.hash(rs) )
 
 if __name__ == '__main__':
   unittest.main()
