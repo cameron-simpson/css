6d0dd889b41fb1237177a58948d4d00bbb645f02
Cameron Simpson <cs@zip.com.au>
2009-08-30 20:41:49 +1000

    cs.logutils: new Pfx context manager to push new prefix for messages

--- lib/cs/logutils.py
+++ lib/cs/logutils.py
@@ -4,11 +4,40 @@
 #       - Cameron Simpson <cs@zip.com.au> 29aug2009
 #
 
+from __future__ import with_statement
 import logging
 import time
+import threading
 import cs.misc
 
-class LogElapsedTime(object):
+class _PrefixState(threading.local):
+  def __init__(self):
+    self.current = cs.misc.cmd
+    self.prior = []
+_prefix = _PrefixState()
+
+class Pfx(object):
+  def __enter__(self, mark, absolute=False):
+    global _prefix
+    if absolute:
+      new = mark
+    else:
+      new = self.current + ': ' + mark
+    self.prior.append(self.current)
+    self.current = new
+  def __exit__(self, exc_type, exc_value, traceback):
+    self.current = self.prior.pop()
+    return False
+
+def current_prefix():
+  global _prefix
+  return _prefix.current
+
+class LogTime(object):
+  ''' LogTime is a content manager that logs the elapsed time of the enclosed
+      code. After the run, the field .elapsed contains the elapsed time in
+      seconds.
+  '''
   def __init__(self, tag, level=None, threshold=None):
     if level is None:
       level = cs.misc.logging_level
@@ -22,4 +51,5 @@ class LogElapsedTime(object):
     elapsed = now - self.start
     if elapsed >= self.threshold:
       logging.log(self.level, "%s: %5.3fs" % (self.tag, elapsed))
+    self.elapsed = elapsed
     return False
