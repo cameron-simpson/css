ae304d12a02a22de8821478bd24aaad1ad93c42a
Cameron Simpson <cs@zip.com.au>
2010-04-16 12:22:17 +1000

    cs.misc,cs.excutils: move NoExceptions into cs.excutils, obsolete cs.misc.logException

---
+++ lib/cs/excutils.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python -tt
+#
+# Convenience facilities for exceptions.
+#       - Cameron Simpson <cs@zip.com.au>
+#
+
+import logging
+from cs.logutils import log, warn, exception
+
+class NoExceptions(object):
+  ''' A context manager to catch _all_ exceptions and log them.
+      Arguably this should be a bare try...except but that's syntacticly
+      noisy and separates the catch from the top.
+  '''
+
+  def __init__(self, handleException):
+    ''' Initialise the NoExceptions context manager.
+        The handleException is a callable which
+        expects (exc_type, exc_value, traceback)
+        and returns True or False for the __exit__
+        method of the manager.
+        If handleException is None, the __exit__ method
+        always returns True, suppressing any exception.
+    '''
+    self.__handler = handleException
+
+  def __enter__(self):
+    pass
+
+  def __exit__(self, exc_type, exc_value, tb):
+    if exc_type is not None:
+      if self.__handler is not None:
+        # user supplied handler
+        return self.__handler(exc_type, exc_value, tb)
+      # report handled exception
+      exception("IGNORE "+str(exc_type)+": "+`tb`)
+    return True
+
+  def simpleExceptionReport(exc_type, exc_value, traceback, mark=None, loglevel=logging.WARNING):
+    ''' Convenience method to log exceptions to standard error.
+    '''
+    if mark is None:
+      mark=cmd
+    else:
+      mark="%s: %s" % (cmd, mark)
+    log("%s: EXCEPTION: %s: %s [%s]" % (mark, exc_type, exc_value, traceback), level=loglevel)
+    return True
+  # backward compatible static method arrangement
+  simpleExceptionReport = staticmethod(simpleExceptionReport)
--- lib/cs/misc.py
+++ lib/cs/misc.py
@@ -309,53 +309,11 @@ class Loggable:
     t, result = self.logTime2(tag, func, *args, **kw)
     return result
 
-  def logException(self, exc_type, exc_value, traceback, doSimpleExceptionReport=False):
+  def OBSOLETElogException(self, exc_type, exc_value, traceback, doSimpleExceptionReport=False):
     self.logfn("EXCEPTION: %s(%s)" % (exc_type, exc_value))
     if doSimpleExceptionReport:
+      ##NO##from cs.excutils import NoExceptions
       NoExceptions.simpleExceptionReport(exc_type, exc_value, traceback, mark=self.__logMark)
-
-class NoExceptions(object):
-  ''' A context manager to catch _all_ exceptions and log them.
-      Arguably this should be a bare try...except but that's syntacticly
-      noisy and separates the catch from the top.
-  '''
-
-  def __init__(self, handleException):
-    ''' Initialise the NoExceptions context manager.
-        The handleException is a callable which
-        expects (exc_type, exc_value, traceback)
-        and returns True or False for the __exit__
-        method of the manager.
-        If handleException is None, the __exit__ method
-        always returns True, suppressing any exception.
-    '''
-    self.__handler = handleException
-
-  def __enter__(self):
-    pass
-
-  def __exit__(self, exc_type, exc_value, tb):
-    if exc_type is not None:
-      if self.__handler is not None:
-        # user supplied handler
-        return self.__handler(exc_type, exc_value, tb)
-      # report handled exception
-      warning("IGNORE "+str(exc_type)+": "+`tb`)
-      import traceback
-      traceback.print_exception(exc_type, exc_value, tb)
-    return True
-
-  def simpleExceptionReport(exc_type, exc_value, traceback, mark=None, loglevel=logging.WARNING):
-    ''' Convenience method to log exceptions to standard error.
-    '''
-    if mark is None:
-      mark=cmd
-    else:
-      mark="%s: %s" % (cmd, mark)
-    log("%s: EXCEPTION: %s: %s [%s]" % (mark, exc_type, exc_value, traceback), level=loglevel)
-    return True
-  # backward compatible static method arrangement
-  simpleExceptionReport = staticmethod(simpleExceptionReport)
 
 T_SEQ = 'ARRAY'
 T_MAP = 'HASH'
