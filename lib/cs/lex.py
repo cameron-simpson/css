1f87b996eb1654f7da86eefec4c03a10ec8f1d86
Cameron Simpson <cs@zip.com.au>
2010-05-31 15:12:53 +1000

    cs.lex.texthexify: pull unimplemented modein parameter

--- lib/cs/lex.py
+++ lib/cs/lex.py
@@ -142,8 +142,9 @@ def hexify(s, sep=''):
   return sep.join( '%02x' % ord(c) for c in s )
 _texthexify_white_re = re.compile(r'[a-zA-Z0-9_\-+.,/]+')
 
-def texthexify(s, shiftin='[', shiftout=']', modein=False, whitelist_re=None):
+def texthexify(s, shiftin='[', shiftout=']', whitelist_re=None):
   ''' Transcribe the byte string `s` to text.
+      TODO: add and implement modein parameter, for starting in text mode.
   '''
   if whitelist_re is None:
     whitelist_re = _texthexify_white_re
