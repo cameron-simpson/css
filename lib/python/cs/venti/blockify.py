3d2318c1912602408b4a0d05c4ec0358c34ba26d
Cameron Simpson <cs@zip.com.au>
2010-06-14 22:24:16 +1000

    cs.venti.blockify: recode data blocking edge detector for greater speed

--- lib/python/cs/venti/blockify.py
+++ lib/python/cs/venti/blockify.py
@@ -8,6 +8,7 @@
 import sys
 from struct import unpack_from
 from threading import Thread
+import unittest
 from cs.logutils import debug, D
 from cs.threads import IterableQueue
 from cs.misc import D, eachOf
@@ -108,12 +109,11 @@ def blocksOf(dataSource, vocab=None):
     global DFLT_VOCAB
     vocab = DFLT_VOCAB
 
-  buf = []      # list of strings-to-collate-into-a block
+  buf = []      # list of strings-to-collate-into-a-block
   buflen = 0    # cumulative length of buf
-  RH=RollingHash() # rolling hash of contents of buf
 
-  # invariant: all the bytes in buf[]
-  # have been fed to the rolling hash
+  # invariant: no block edge has been seen in buf based on the vocabulary
+  #            the rolling hash has not been used yet
   for data in dataSource:
     while len(data) > 0:
       # if buflen < MIN_BLOCKSIZE pad with stuff from data
@@ -123,7 +123,6 @@ def blocksOf(dataSource, vocab=None):
           skip = len(data)
         left = data[:skip]; data = data[skip:]
         buf.append(left); buflen += len(left)
-        RH.addString(left)
         continue
 
       # we don't like to make blocks bigger than MAX_BLOCKSIZE
@@ -135,47 +134,41 @@ def blocksOf(dataSource, vocab=None):
       probe_len = min(probe_len, len(data))
 
       # look for a vocabulary word
-      word,  woffset = vocab.findWord(data, 0, probe_len)
-      if word is not None:
-        # word match found
-        vword, voffset, vsubvocab = vocab[word]
-        if vsubvocab is not None:
-          # change vocabulary
-          vocab = vsubvocab
-        # put the desired part of the data into the buffer
-        # and flush the buffer
-        cutoff = woffset + voffset      # start of word plus offset to boundary
-        left = data[:cutoff]; data = data[cutoff:]
-        buf.append(left)
+      m = vocab.match(data, 0, probe_len)
+      if m:
+        edgepos, word, offset, subVocab = m
+        if subVocab:
+          vocab = subVocab
+        buf.append(data[:edgepos])
+        data = data[edgepos:]
         yield Block("".join(buf))
-        buf = []; buflen = 0
-        RH.reset()
+        buf = []
+        buflen = 0
         continue
 
-      # no vocabulary word - look for the magic hash value
-      cutoff = RH.findEdge(data,probe_len)
-      assert cutoff == -1 or cutoff > 0, "findEdge()=%d" % (cutoff,)
-      # POST: if cutoff == -1, probe_len bytes added to RH
-      #       otherwise, cutoff bytes added to RH
-      if cutoff > 0:
-        left = data[:cutoff]; data = data[cutoff:]
-        buf.append(left)
-        yield Block("".join(buf))
-        buf = []; buflen = 0
-        RH.reset()
-        continue
+      # no vocabulary words seen - append data to buf
+      buf.append(data); buflen += len(data)
+      data = ''
 
-      assert probe_len > 0
-      left = data[:probe_len]; data = data[probe_len:]
-      buf.append(left); buflen += len(left)
-      RH.addString(left)
-
-      assert buflen <= MAX_BLOCKSIZE
-      if buflen == MAX_BLOCKSIZE:
-        # full block - release it
-        yield Block("".join(buf))
-        buf = []; buflen = 0
-        RH.reset()
+      # if buf gets too big, scan it with the rolling hash
+      # we may have to rescan after finding an edge
+      while buflen >= MAX_BLOCKSIZE:
+        buf2 = []
+        RH = RollingHash()
+        for b in buf:
+          while len(b):
+            edgepos = RH.findEdge(b, len(b))
+            if edgepos >= 0:
+              buf2.append(b[:edgepos])
+              yield Block("".join(buf2))
+              buf2 = []
+              b = b[edgepos:]
+              RH.reset()
+            else:
+              buf2.append(b)
+              break
+        buf = buf2
+        buflen = sum( len(b) for b in buf )
 
   # no more data - yield remaining buffer
   if buflen > 0:
@@ -189,12 +182,12 @@ class RollingHash:
     self.reset()
 
   def reset(self):
-    self.n=0
+    self.n = 0
 
   def value(self):
     return self.n
 
-  def findEdge(self,data,probe_len):       ## hashcodes):
+  def findEdge(self, data, probe_len):
     ''' Add characters from data to the rolling hash until probe_len characters
         are accumulated or the magic hash code is encountered.
         Return the offset where the match was found, or -1 if no match.
@@ -234,63 +227,63 @@ class RollingHash:
 class Vocabulary(dict):
   ''' A class for representing match vocabuaries.
   '''
-  def __init__(self,vocabDict=None):
+
+  def __init__(self, vocabDict=None):
     dict.__init__(self)
-    self.__startChars={}
-    self.__maxWordLen=0
+    self.__startChars = ""
     if vocabDict is not None:
       for word in vocabDict:
-        self.addWord(word,vocabDict[word])
+        self.addWord(word, vocabDict[word])
 
-  def addWord(self,word,info):
+  def addWord(self, word, info):
+    ''' Add a word to the vocabulary.
+        `word` is the string to match.
+        `info` is either an integer offset or a tuple of (offset, Vocabulary).
+        The offset is the position within the match string of the boundary.
+        If supplied, Vocaulary is a new Vocabulary to use if this word matches.
+    '''
     assert len(word) > 0
-    assert word not in self, "word \"%s\" already present" % unctrl(word)
     if type(info) is int:
-      offset=info
-      subVocab=None
+      offset = info
+      subVocab = None
     else:
       offset, subVocab = info
-      subVocab=Vocabulary(subVocab)
-    self.__startChars.setdefault(word[0],[]).append(word)
-    self.__maxWordLen=max(self.__maxWordLen,len(word))
-    self[word]=(word,offset,subVocab)
-    ##RH=RollingHash()
-    ##RH.addString(word)
-    ##rh=RH.value()
-    ##self.setdefault(rh,[]).append((word,offset,subVocab))
+      subVocab = Vocabulary(subVocab)
+    ch1 = word[0]
+    if ch1 not in self.__startChars:
+      self.__startChars += ch1
+      self[ch1] = []
+    self[ch1].append( (word, offset, subVocab) )
 
-  def findWord(self,s,offset,maxoffset):
+  def match(self, s, pos, endpos):
     ''' Locate the earliest occurence in the string 's' of a vocabuary word.
-        Return (word, offset) being the matched word and its offset
-        respectively. Return (None, None) on no match.
+        Return (edgepos, word, offset, subVocab) for a match or None on no match.
+        `edgepos` is the boundary position.
+        `word` will be present at edgepos-offset.
+        `subVocab` is the new vocabulary to use from this point, or None
+        for no change.
     '''
-    assert len(s) > 0
-    assert offset >= 0
-    assert maxoffset > offset
-    assert maxoffset <= len(s)
-    fword=None
-    foff=None
-    for ch in self.__startChars.keys():
-      words=self.__startChars[ch]
-      if fword is None:
-        roffset=maxoffset
-      else:
-        assert foff < maxoffset
-        roffset=foff
-      fpos=s.find(ch,offset,roffset)
-      while fpos > 0 and (fword is None or fpos < foff):
-        for w in words:
-          if s[fpos:fpos+len(w)] == w:
-            foff=fpos
-            fword=w
-            roffset=foff
+    assert pos >= 0 and pos < len(s)
+    assert endpos > pos and endpos <= len(s)
+    matched = None
+    for ch in self.__startChars:
+      wordlist = self[ch]
+      findpos = pos
+      while findpos < endpos:
+        cpos = s.find(ch, findpos, endpos)
+        if cpos < 0:
+          break
+        findpos = cpos + 1      # start here on next find
+        for word, offset, subVocab in wordlist:
+          if s.startswith(word, cpos):
+            edgepos = cpos + offset
+            matched = (edgepos, word, offset, subVocab)
+            endpos = edgepos
             break
-        offset=fpos+1
-        fpos=s.find(ch,offset,roffset)
-    return fword, foff
+    return matched
 
 # TODO: reform as list of (str, offset, sublist).
-DFLT_VOCAB=Vocabulary({
+DFLT_VOCAB = Vocabulary({
                 "\ndef ": 1,          # python top level function
                 "\n  def ": 1,        # python class method, 2 space indent
                 "\n    def ": 1,      # python class method, 4 space indent
@@ -306,3 +299,22 @@ DFLT_VOCAB=Vocabulary({
                     },
                   ],
               })
+
+class TestAll(unittest.TestCase):
+
+  def setUp(self):
+    self.fp = open(__file__)
+
+  def tearDown(self):
+    self.fp.close()
+
+  def test00(self):
+    data = self.fp.read()
+    blocks = list(blocksOf([data]))
+    data2 = "".join( b.data() for b in blocks )
+    self.assertEqual(len(data), len(data2), "data mismatch: len(data)=%d, len(data2)=%d" % (len(data), len(data2)))
+    self.assertEqual(data, data2, "data mismatch: data and data2 same length but contents differ")
+    ##for b in blocks: print "[", b.data(), "]"
+
+if __name__ == '__main__':
+  unittest.main()
