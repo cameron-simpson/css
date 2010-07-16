b02ece138cbc3c565cf7af5e24a231963b38772b
Cameron Simpson <cs@zip.com.au>
2010-07-16 18:51:56 +1000

    cs.app.maildb: add self test command line mode, redo message import

--- lib/python/cs/app/maildb.py
+++ lib/python/cs/app/maildb.py
@@ -1,24 +1,55 @@
 #!/usr/bin/python -tt
 
 from __future__ import with_statement
-from cs.logutils import Pfx, info
+from cs.logutils import Pfx, info, warn, error
 from cs.mail import ismaildir, ismbox, messagesFromPath
 from cs.nodedb import NodeDB, Node
 from cs.misc import the
 from email.utils import getaddresses, formataddr
+from itertools import chain
+import logging
 import sys
 import os
 import unittest
+
+def main(argv):
+  cmd = os.path.basename(argv.pop(0))
+  usage = '''Usage:
+    %s test Run selftests.''' \
+    % (cmd,)
+
+  xit = 1
+  badopts = False
+
+  if len(argv) == 0:
+    error("missing op")
+    badopts = True
+  else:
+    op = argv.pop(0)
+    with Pfx(op):
+      if op == 'test':
+        tests = TestAll('test01makeNodes')
+        tests()
+      else:
+        error("unsupported op")
+        badopts = True
+
+  if badopts:
+    error(usage)
+    xit = 2
+
+  return xit
 
 AddressNode=Node
 PersonNode=Node
 
 class MessageNode(Node):
 
-  def references(self):
-    return [ N for N in self.parentsByAttr('FOLLOWUPS') if N.TYPE == 'MESSAGE' ]
+  def referers(self):
+    return [ N for N in self.parentsByAttr('FOLLOWUPs') if N.TYPE == 'MESSAGE' ]
+
   def followups(self):
-    return self.FOLLOWUPS
+    return self.FOLLOWUPs
 
 TypeFactory = { 'MESSAGE':      MessageNode,
                 'PERSON':       PersonNode,
@@ -36,10 +67,15 @@ class MailDB(NodeDB):
       raise ValueError("unsupported type \"%s\"" % (t,))
     return TypeFactory[t](t, name, self)
 
-  def getAddrNode(self, addr):
-    ''' Obtain the Node for the specified address `addr`.
+  def getAddrNode(self, addr, name=None):
+    ''' Obtain the AddressNode for the specified address `addr`.
+        If the optional parameter `name` is specified and
+        the Node has no .REALNAME attribute, sets .REALNAME to `name`.
     '''
-    return self.get( ('ADDRESS', addr), doCreate=True)
+    A = self.get( ('ADDRESS', addr), doCreate=True)
+    if name and 'REALNAME' not in A:
+      A.NAME = name
+    return A
 
   def getMessageNode(self, message_id):
     ''' Obtain the Node for the specified Message-ID `message_id`.
@@ -53,31 +89,32 @@ class MailDB(NodeDB):
       for M in messagesFromPath(path):
         self.importMessage(M)
 
-  def importMessage(self, M):
-    ''' Import the message `M`.
+  def addrtexts_to_AddressNodes(self, addrtexts):
+    return [ self.getAddrNode(addr.lower(), name)
+             for name, addr
+             in getaddresses(addrtexts)
+           ]
+
+  def importMessage(self, msg):
+    ''' Import the message `msg`.
         Returns the MessageNode.
     '''
-    info("import %s->%s: %s" % (M['from'], M['to'], M['subject']))
-    N = self.getMessageNode(M['message-id'])
-    N.SUBJECT = M['subject']
-    if 'date' in M:
-      N.DATE = M['date']
-    name, addr = the(getaddresses(M.get_all('from')))
-    N.FROM = self.getAddrNode(addr.lower())
+    info("import %s->%s: %s" % (msg['from'], msg['to'], msg['subject']))
+    msgid = msg['message-id'].strip()
+    M = self.getMessageNode(msgid)
+    M.MESSAGE = msg
+    M.SUBJECT = msg['subject']
+    if 'date' in msg:
+      M.DATE = msg['date']
+    M.FROMs = self.addrtexts_to_AddressNodes(msg.get_all('from', []))
     addrs = {}
-    for hdr in ('to', 'cc', 'resent-to', 'resent-cc'):
-      hdrs = M.get_all(hdr)
-      if hdrs is None:
-        continue
-      for name, addr in getaddresses(hdrs):
-        addr = addr.lower()
-        if addr not in addrs:
-          A = self.getAddrNode(addr)
-          addrs[addr] = A
-          if len(name) > 0 and not hasattr(A, 'REALNAME'):
-            A.REALNAME = name
-    N.RECIPIENTs = [ addrs[addr] for addr in addrs.keys() ]
-    return N
+    M.RECIPIENTS = self.addrtexts_to_AddressNodes(
+                       chain( msg.get_all(hdr, [])
+                              for hdr
+                              in ('to', 'cc', 'bcc', 'resent-to', 'resent-cc')
+                            )
+                     )
+    return M
 
 class TestAll(unittest.TestCase):
 
@@ -90,4 +127,5 @@ class TestAll(unittest.TestCase):
     print >>sys.stderr, "ADDRESSes =", `self.db.ADDRESSes`
 
 if __name__ == '__main__':
-  unittest.main()
+  logging.basicConfig(format="%(message)s")
+  sys.exit(main(list(sys.argv)))
