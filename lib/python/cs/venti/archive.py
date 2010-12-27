55c88ebd01514f0b3cb4928e53f62bb46d54eb39
Cameron Simpson <cs@zip.com.au>
2010-12-28 10:37:29 +1100

    cs.venti.archive: rewrite, cleaner and simpler

--- lib/python/cs/venti/archive.py
+++ lib/python/cs/venti/archive.py
@@ -1,60 +1,67 @@
 #!/usr/bin/python
 
+''' Archive files for venti data.
+
+    Archive files are records of data saved to a Store.
+    Lines are written to the archive file of the form:
+
+      unixtime dirent
+
+    where unixtime is UNIX time (seconds since epoch) and dirent is the text
+    encoding of a cs.venti.dir.Dirent.
+'''
+
 import os
+import sys
 from datetime import datetime
 from cs.venti import totext, fromtext
-from cs.venti.dir import decodeDirent
+from cs.venti.dir import Dir, decodeDirent, storeDir
+from cs.venti.file import storeFile
+from cs.logutils import Pfx
 
-def archive2dir(archive):
-  ''' Open an archive file, return the last dirent mentioned.
-      TODO: accept timestamp or something to retrieve older snapshots.
+def archive(arfile, path, verbose):
+    ''' Archive the named file path.
+    '''
+    with Pfx("archive(%s)" % (path,)):
+      if os.path.isdir(path):
+        E = storeDir(path)
+      else:
+        E = storeFile(open(path, "rb"))
+      if arfile is None:
+        writeDirent(sys.stdout, D)
+      else:
+        with open(arfile, "a") as fp:
+          writeDirent(fp, D)
+
+def retrieve(arfile, paths, verbose):
+  ''' Retrieve Dirents for the named file paths.
   '''
-  if not os.path.exists(archive):
-    return None
-  assert os.path.isfile(archive)
-  fp = open(archive)
-  refword = None
-  for line in fp:
-    assert line[-1] == '\n'
-    words = line[:-1].strip().split(' ')
-    if len(words) == 0:
-      continue
-    if words[0].startswith("#"):
-      continue
-    refword = words[0]
-  fp.close()
-  if refword is None:
-    return None
-  E = decodeDirent(fromtext(refword), justone=True)
-  if len(words) > 2 and (E.name is None or len(E.name) == 0):
-    E.name = words[2]
-  return E
+  with Pfx(arfile):
+    found = {}
+    with open(arfile) as fp:
+      for unixtime, D in getDirents(fp):
+        if D.name in paths:
+          found[D.name] = D
+    return [ (path, found.get(path)) for path in paths ]
 
-def archiveAppend(archive, E, when=None, path=None):
-  ''' Store a dirent and optional datetime and path in an archive.
-      If archive is a string, treat it as a pathname to open.
-      Otherwise treat it as a file.
-      The optional arguments when and path will come from the dirent
-      if None or not supplied.
+def getDirents(fp):
+  ''' Generator to yield (unixtime, Dirent) from archive file.
+  '''
+  for line in fp:
+    assert line.endswith('\n'), "%s: unexpected EOF" % (fp,)
+    unixtime, dent = line.split(None, 1)
+    when = float(unixtime)
+    D, etc = decodeDirent(dent)
+    assert len(etc) == 0 or etc[0].iswhite(), \
+      "failed to decode dent %s: unparsed: %s" % (`dent`, `etc`)
+    yield when, D
+
+def writeDirent(fp, D, when=None):
+  ''' Write a Dirent to an archive file.
   '''
   if when is None:
-    M = E.meta
-    when = M.mtime
-    if when is 0:
-      when = datetime.now()
-  if path is None:
-    path = E.name
-
-  if type(archive) is str:
-    fp = open(archive, mode='a')
-  else:
-    fp = archive
-
-  if path is None:
-    fp.write("%s %s\n"
-             % (totext(E.encode(noname=True)), datetime.now()))
-  else:
-    fp.write("%s %s %s\n"
-           % (totext(E.encode(noname=True)), datetime.now(), path))
-  if fp is not archive:
-    fp.close()
+    when = time.time()
+  fp.write(str(when))
+  fp.write(' ')
+  fp.write(str(D))
+  fp.write('\n')
