67e18bdba1926b4558e57242bb1b3bc0ae9f289f
Cameron Simpson <cs@zip.com.au>
2010-12-31 21:14:17 +1100

    cs.venti.archive: TOC mode, minor changes

--- lib/python/cs/venti/archive.py
+++ lib/python/cs/venti/archive.py
@@ -20,10 +20,14 @@ from cs.venti.dir import Dir, decodeDirent, storeDir
 from cs.venti.file import storeFile
 from cs.logutils import Pfx
 
-def archive(arfile, path, verbose=False):
+def archive(arfile, path, verbose=False, fp=None):
     ''' Archive the named file path.
     '''
+    if fp is None:
+      fp = sys.stdout
     with Pfx("archive(%s)" % (path,)):
+      if verbose:
+        print >>fp, path
       if os.path.isdir(path):
         E = storeDir(path)
       else:
@@ -35,7 +39,7 @@ def archive(arfile, path, verbose=False):
         with open(arfile, "a") as fp:
           writeDirent(fp, E)
 
-def retrieve(arfile, paths=None, verbose=False):
+def retrieve(arfile, paths=None):
   ''' Retrieve Dirents for the named file paths, or None if a
       path does not resolve.
       If `paths` if missing or None, retrieve the latest Dirents
@@ -51,16 +55,18 @@ def retrieve(arfile, paths=None, verbose=False):
       paths = found.keys()
     return [ (path, found.get(path)) for path in paths ]
 
-def walk_report(path, E):
-  print path
+def toc_report(fp, path, E, verbose):
+  print >>fp, path
   if E.isdir():
     entries = sorted(E.keys())
     for subpath in entries:
-      walk_report(os.path.join(path, subpath), E[subpath])
+      toc_report(fp, os.path.join(path, subpath), E[subpath], verbose)
 
-def walk(arfile, paths=None, verbose=False):
-  for path, E in retrieve(arfile, paths, verbose):
-    walk_report(path, E)
+def toc(arfile, paths=None, verbose=False, fp=None):
+  if fp is None:
+    fp = sys.stdout
+  for path, E in retrieve(arfile, paths):
+    toc_report(fp, path, E, verbose)
 
 def getDirents(fp):
   ''' Generator to yield (unixtime, Dirent) from archive file.
