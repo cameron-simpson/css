2c952cab51b2a498ceaa1121a553d36c79fb14fe
Cameron Simpson <cs@zip.com.au>
2011-07-01 09:49:44 +1000

    cs.app.osx.addressbook: start on AddressBookWrapper class

--- lib/python/cs/app/osx/addressbook.py
+++ lib/python/cs/app/osx/addressbook.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Original code from:
+# Based on code found at:
 #
 #   Python and Apple AddressBook
 #   http://www.programmish.com/?p=26
@@ -10,30 +10,49 @@
 # so I may need to add that to sys.path in for other python installs.
 #
 
-from AddressBook import ABAddressBook
-from .objc import convertObjCtype
 import pprint
 import sys
+from AddressBook import ABAddressBook
+from .objc import convertObjCtype
 
 def main(argv):
-  for P in addressBookPeople():
+  AB = AddressBookWrapper()
+  print "dir(AB.address_book) =",
+  pprint.pprint(dir(AB.address_book))
+  for P in AB.people:
     ##pprint.pprint(P)
     pass
 
-def addressBookPeople():
-  """
-  Read the current user's AddressBook database, converting each person
-  in the address book into a Dictionary of values. Some values (addresses,
-  phone numbers, email, etc) can have multiple values, in which case a
-  list of all of those values is stored. The result of this method is
-  a List of Dictionaries, with each person represented by a single record
-  in the list.
-  """
-  for abPerson in ABAddressBook.sharedAddressBook().people():
-    yield dict( [ (k, convertObjCtype(abPerson.valueForProperty_(k)))
-                  for k in abPerson.allProperties()
-                  if k not in ('com.apple.ABPersonMeProperty','com.apple.ABImageData',)
-                ] )
+class AddressBookWrapper(object):
+  ''' Wrapper class for Mac OSX AddressBook with more pythonic facilities.
+  '''
+
+  def __init__(self, address_book=None):
+    if address_book is None:
+      address_book = ABAddressBook.sharedAddressBook()
+    self.address_book = address_book
+
+  @property
+  def people(self):
+    ''' Return an iterator that yields people from the addressbook
+        converted to pure python types.
+    '''
+    for abPerson in self.address_book.people():
+      yield dict( [ (k, convertObjCtype(abPerson.valueForProperty_(k)))
+                    for k in abPerson.allProperties()
+                    if k not in ('com.apple.ABPersonMeProperty','com.apple.ABImageData',)
+                  ] )
+
+  @property
+  def groups(self):
+    ''' Return an iterator that yields people from the addressbook
+        converted to pure python types.
+    '''
+    for abPerson in ABAddressBook.sharedAddressBook().people():
+      yield dict( [ (k, convertObjCtype(abPerson.valueForProperty_(k)))
+                    for k in abPerson.allProperties()
+                    if k not in ('com.apple.ABPersonMeProperty','com.apple.ABImageData',)
+                  ] )
 
 if __name__ == '__main__':
   sys.exit(main(sys.argv))
