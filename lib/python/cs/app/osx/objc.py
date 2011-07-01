43869e97aa7fd2986b7128175fb4b8a3771bec1c
Cameron Simpson <cs@zip.com.au>
2011-07-01 10:08:08 +1000

    cs.app.osx.objc: move _offsetTZInfo to cs.dateutils.tzinfoHHMM for general use

--- lib/python/cs/app/osx/objc.py
+++ lib/python/cs/app/osx/objc.py
@@ -4,8 +4,9 @@
 #       - Cameron Simpson <cs@zip.com.au> 29jun2011
 # 
 
-from datetime import datetime, tzinfo, timedelta
+from datetime import datetime
 from AddressBook import objc, NSDate, ABMultiValueCoreDataWrapper, NSCFDictionary
+from cs.dateutils import tzinfoHHMM
 
 def convertObjCtype(o):
   if o is None:
@@ -27,31 +28,5 @@ def convertNSDate(d):
   d_date, d_time, d_zone = d.description().split()
   year, month, day = [ int(d_d) for d_d in d_date.split('-') ]
   hour, minute, second = [ int(d_t) for d_t in d_time.split(':') ]
-  tz = _offsetTZInfo(d_zone)
+  tz = tzinfoHHMM(d_zone)
   return datetime(year, month, day, hour, minute, second, 0, tz)
-
-class _offsetTZInfo(tzinfo):
-  ''' tzinfo class based on +HHMM / -HHMM strings.
-  '''
-
-  def __init__(self, shhmm):
-    sign, hour, minute = shhmm[0], int(shhmm[1:3]), int(shhmm[3:5])
-    if sign == '+':
-      sign = 1
-    elif sign == '-':
-      sign = -1
-    else:
-      raise ValueError, "%s: invalid sign '%s', should be '+' or '-'" % (shhmm, sign,)
-    self._tzname = shhmm
-    self.sign = sign
-    self.hour = hour
-    self.minute = minute
-
-  def utcoffset(self, dt):
-    return self.hour*60 + self.minute
-
-  def dst(self, dt):
-    return timedelta(0)
-
-  def tzname(self, dt):
-    return self._tzname
