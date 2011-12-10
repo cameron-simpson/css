032a42f27d018de152f1ed886696b95e2eaf7f70
Cameron Simpson <cs@zip.com.au>
2011-12-11 10:50:53 +1100

    cs.nodedb.sqla: implement __setitem__ and __delitem__

--- lib/python/cs/nodedb/sqla.py
+++ lib/python/cs/nodedb/sqla.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import os
+from thread import allocate_lock
 from types import StringTypes
 import unittest
 import sys
@@ -42,6 +43,7 @@ def ATTRSTable(metadata, name=None):
 class Backend_SQLAlchemy(Backend):
 
   def __init__(self, engine, nodes=None, attrs=None, readonly=False):
+    self._lock = allocate_lock()
     self.readonly = readonly
     if nodes is None:
       nodes = 'NODES'
@@ -67,11 +69,26 @@ class Backend_SQLAlchemy(Backend):
     self.__IDbyTypeName = {}
 
   def _noteNodeKey(self, t, name, node_id):
+    ''' Remember the mapping between db node_id and (type, name).
+    '''
     nodekey = (t, name)
     self.__nodekeysByID[node_id] = nodekey
     self.__IDbyTypeName[nodekey] = node_id
 
+  def __setitem__(self, nodekey, N):
+    t, name = nodekey
+    with self._lock:
+      node_id = self.__IDbyTypeName.get(nodekey)
+      if node_id is not None:
+        self._forgetNode( t, name, node_id )
+    node_id = self._node_id( t, name )
+    for attr in N.keys():
+      self.saveAttrs(N[attrs])
+
   def _node_id(self, t, name):
+    ''' Return the db node_id for the supplied type and name.
+        Create a new node_id if unknown.
+    '''
     if (t, name) not in self.__IDbyTypeName:
       assert not self.nodedb.readonly
       ins = self.nodes.insert().values(TYPE=t, NAME=name).execute()
@@ -80,8 +97,18 @@ class Backend_SQLAlchemy(Backend):
     return node_id
 
   def _forgetNode(self, t, name, node_id):
+    ''' Forget the mapping between db node_id and (type, name).
+    '''
     del self.__nodekeysByID[node_id]
     del self.__IDbyTypeName[t, name]
+
+  def __delitem__(self, nodekey):
+    assert not self.nodedb.readonly
+    t, name = nodekey
+    node_id = self.__IDbyTypeName[t, name]
+    self.attrs.delete(self.attrs.c.NODE_ID == node_id).execute()
+    self.nodes.delete(self.nodes.c.NODE_ID == node_id).execute()
+    self._forgetNode(t, name, node_id)
 
   def nodedata(self):
     ''' Pull all node data from the database.
@@ -146,14 +173,6 @@ class Backend_SQLAlchemy(Backend):
 
   def close(self):
     warning("cs.nodedb.sqla.Backend_SQLAlchemy.close() unimplemented")
-
-  def __delitem__(self, nodekey):
-    assert not self.nodedb.readonly
-    t, name = nodekey
-    node_id = self.__IDbyTypeName[t, name]
-    self.attrs.delete(self.attrs.c.NODE_ID == node_id).execute()
-    self.nodes.delete(self.nodes.c.NODE_ID == node_id).execute()
-    self._forgetNode(t, name, node_id)
 
   def extendAttr(self, t, name, attr, values):
     assert not self.nodedb.readonly
