8d2b28a5b052295a10b603857f5e622fe88ba39b
Cameron Simpson <cs@zip.com.au>
2011-12-11 16:55:55 +1100

    cs.app.myke.lex: whitepace, comments, remove syntax error

--- lib/python/cs/app/myke/lex.py
+++ lib/python/cs/app/myke/lex.py
@@ -23,23 +23,29 @@ def readMakefile(filename, context=None):
       prevline = None
       for line in fp:
         lineno += 1
+
         if not line.endswith('\n'):
           error("unexpected EOF")
           ok = False
           break
+
         if prevline is not None:
+          # prepend previous continuation line if any
           line = prevline + line
           prevline = None
+
         if line.endswith('\\\n'):
+          # continuation line - gather next line before parse
           prevline = line
           continue
+
         if line.startswith(':'):
           assert False, "directives unimplemented"
-        if 
-        if inTarget
+
         assert False
 
       if prevline is not None:
+        # incomplete continuation line
         error("unexpected EOF: unterminated slosh continued line")
         ok = False
 
