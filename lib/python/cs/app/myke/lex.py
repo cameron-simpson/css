28ee96a9e5033109cb30f1ec44332a3d08114f1c
Cameron Simpson <cs@zip.com.au>
2011-12-11 16:45:20 +1100

    myke3: initial sketch

---
+++ lib/python/cs/app/myke/lex.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+#
+
+from cs.logutils import error, Pfx
+
+class FileContext(object):
+  def __init__(self, filename, lineno, parent=None):
+    self.filename = filename
+    self.lineno = lineno
+    self.parent = parent
+
+def readMakefile(filename, context=None):
+  ''' Read a Mykefile and yield objects: Macros, Targets and finally Boolean
+      indicating an error free parse of the file.
+  '''
+  with Pfx(filename):
+    ok = True
+    inTarget = False    # not in a target
+    ifStack = []        # active ifStates (state, firstbranch)
+    ifState = None      # ifStack[-1]
+    with open(filename) as fp:
+      lineno = 0
+      prevline = None
+      for line in fp:
+        lineno += 1
+        if not line.endswith('\n'):
+          error("unexpected EOF")
+          ok = False
+          break
+        if prevline is not None:
+          line = prevline + line
+          prevline = None
+        if line.endswith('\\\n'):
+          prevline = line
+          continue
+        if line.startswith(':'):
+          assert False, "directives unimplemented"
+        if 
+        if inTarget
+        assert False
+
+      if prevline is not None:
+        error("unexpected EOF: unterminated slosh continued line")
+        ok = False
+
+    yield ok
