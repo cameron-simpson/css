24b70863872242cffe692f75e66f2dc584a56ad5
Cameron Simpson <cs@zip.com.au>
2011-12-18 13:12:31 +1100

    cs.later: split self tests into cs.later_tests module

--- lib/python/cs/later.py
+++ lib/python/cs/later.py
@@ -9,7 +9,6 @@ from thread import allocate_lock
 from threading import Thread, Condition
 from Queue import Queue
 import time
-import unittest
 from cs.threads import AdjustableSemaphore, IterablePriorityQueue, \
                        WorkerThreadPool, TimerQueue
 from cs.misc import seq
@@ -498,101 +497,6 @@ def report(LFs):
   for i in range(n):
     yield Q.get()
 
-class TestLater(unittest.TestCase):
-
-  @staticmethod
-  def _f(x):
-    return x*2
-  @staticmethod
-  def _delay(n):
-    time.sleep(n)
-    return n
-  class _Bang(BaseException):
-    pass
-  @staticmethod
-  def _bang():
-    raise TestLater._Bang()
-
-  def setUp(self):
-    self.L = Later(2)
-    self.L.logTo("/dev/tty")
-
-  def tearDown(self):
-    self.L.close()
-
-  def test00one(self):
-    # compute 3*2
-    L = self.L
-    F = partial(self._f, 3)
-    LF = L.defer(F)
-    x = LF()
-    self.assertEquals(x, 6)
-
-  def test01two(self):
-    # two sleep(2) in parallel
-    L = self.L
-    F = partial(self._delay, 2)
-    LF1 = L.defer(F)
-    LF2 = L.defer(F)
-    now = time.time()
-    x = LF1()
-    y = LF2()
-    again = time.time()
-    elapsed = again - now
-    self.assert_(elapsed < 3, "elapsed (%s) >= 3, now = %s, again = %s" % (elapsed, now, again))
-
-  def test02three(self):
-    # three sleep(2), two in parallel, one delayed
-    L = self.L
-    F = partial(self._delay, 2)
-    LF1 = L.defer(F)
-    LF2 = L.defer(F)
-    LF3 = L.defer(F)
-    now = time.time()
-    x = LF1()
-    y = LF2()
-    z = LF3()
-    elapsed = time.time() - now
-    self.assert_(elapsed >= 4)
-
-  def test03calltwice(self):
-    # compute once, get result twice
-    L = self.L
-    F = partial(self._f, 5)
-    LF = L.defer(F)
-    x = LF()
-    self.assertEquals(x, 10)
-    y = LF()
-    self.assertEquals(y, 10)
-
-  def test04raise(self):
-    # raise exception
-    LF = self.L.defer(self._bang)
-    self.assertRaises(TestLater._Bang, LF)
-
-  def test05raiseTwice(self):
-    # raise exception again
-    LF = self.L.defer(self._bang)
-    self.assertRaises(TestLater._Bang, LF)
-    self.assertRaises(TestLater._Bang, LF)
-
-  def test06partial(self):
-    # compute 7*2 using .partial()
-    LF = self.L.partial(self._f, 7)
-    x = LF()
-    self.assertEquals(x, 14)
-
-  def test07report(self):
-    with Later(3) as L3:
-      LF1 = L3.partial(self._delay, 3)
-      LF2 = L3.partial(self._delay, 2)
-      LF3 = L3.partial(self._delay, 1)
-      results = [ LF() for LF in report( (LF1, LF2, LF3) ) ]
-      self.assertEquals(results, [1, 2, 3])
-
-  def test08delay(self):
-    with Later(3) as L3:
-      LF1 = L3
-
 if __name__ == '__main__':
-  unittest.main()
+  import cs.later_tests
+  cs.later_tests.selftest(sys.argv)
