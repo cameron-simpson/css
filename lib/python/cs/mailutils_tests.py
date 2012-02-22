56f5c15cd7883072536bddaf45eff7d50fbcbdb8
Cameron Simpson <cs@zip.com.au>
2012-02-22 12:14:20 +1100

    cs.mailutils_tests: put test data maildir next to mailutils_tests.py

--- lib/python/cs/mailutils_tests.py
+++ lib/python/cs/mailutils_tests.py
@@ -5,29 +5,33 @@
 #
 
 import os
+import os.path
 import sys
 import time
 import unittest
 from cs.mailutils import Maildir
 
+testdatadir = os.path.join(os.path.dirname(__file__), 'testdata', os.path.basename(__file__).rsplit('.', 1)[0])
+testmaildir = os.path.join(testdatadir, 'maildir')
+
 class TestMaildir(unittest.TestCase):
 
   def test00basic(self):
     t0 = time.time()
-    M = Maildir(os.path.join(os.environ['HOME'], 'ZZM'))
+    M = Maildir(testmaildir)
     t1 = time.time()
-    ##print >>sys.stderr, "Maildir(ZZM): %gs" % (t1-t0,)
+    ##print >>sys.stderr, "Maildir(%s): %gs" % (testmaildir, t1-t0,)
     keys = M.keys()
     t2 = time.time()
-    ##print >>sys.stderr, "Maildir(ZZM).keys(): %gs, %d keys" % (t2-t1,len(keys))
+    ##print >>sys.stderr, "Maildir(%s).keys(): %gs, %d keys" % (testmaildir, t2-t1, len(keys))
     for key in keys[:100]:
       msg = M[key]
     t3 = time.time()
-    ##print >>sys.stderr, "Maildir(ZZM): scan 100 msgs %gs" % (t3-t2,)
+    ##print >>sys.stderr, "Maildir(%s): scan 100 msgs %gs" % (testmaildir, t3-t2,)
     for key in keys[100:200]:
       hdr = M.get_headers(key)
     t4 = time.time()
-    ##print >>sys.stderr, "Maildir(ZZM): scan 100 hdrs %gs" % (t4-t3,)
+    ##print >>sys.stderr, "Maildir(%s): scan 100 hdrs %gs" % (testmaildir, t4-t3,)
 
 def selftest(argv):
   unittest.main(__name__, None, argv)
