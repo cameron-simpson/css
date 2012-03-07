0477c133cded8c11bab558d6b080ad93ee0b23ac
Cameron Simpson <cs@zip.com.au>
2012-03-07 17:05:22 +1100

    cs.venti.datafile: add DataDir, a mapping class to supplant IndexedFileStore; add GDBMDataDir, obsoleting cs.venti.gdbmstore

--- lib/python/cs/venti/datafile.py
+++ lib/python/cs/venti/datafile.py
@@ -5,9 +5,12 @@
 #
 
 import sys
+from collections import namedtuple
+import os
+import os.path
 from thread import allocate_lock
 from zlib import compress, decompress
-from cs.serialise import toBS, fromBSfp
+from cs.serialise import toBS, fromBS, fromBSfp
 
 F_COMPRESSED = 0x01
 
@@ -43,7 +46,14 @@ class DataFile(object):
         self._fp = open(self.pathname, "a+b")
     return self._fp
 
-  def scanData(self, uncompress=False):
+  def size(self):
+    ''' Return the size of this file.
+        TODO: This is quite expensive if we call it a lot (which DataDir may).
+              We should track it as we add data.
+    '''
+    return os.fstat(self.fp.fileno).st_size
+
+  def scan(self, uncompress=False):
     ''' Scan the data file and yield (offset, flags, zdata) tuples.
         If `uncompress` is true, decompress the data and strip that flag value.
     '''
@@ -58,23 +68,24 @@ class DataFile(object):
         if uncompress:
           if flags & F_COMPRESSED:
             data = decompress(data)
-          flags &= ~F_COMPRESSED
+            flags &= ~F_COMPRESSED
         yield offset, flags, data
 
-  def readData(self, offset):
+  def readdata(self, offset):
     ''' Read data bytes from the supplied offset.
     '''
     fp = self.fp
     with self._lock:
       fp.seek(offset)
-      flags, data = self._readRawDataHere(fp)
+      flags, data = self._readhere(fp)
     assert flags is not None, "no data read from offset %d" % (offset,)
     if flags & F_COMPRESSED:
       data = decompress(data)
     return data
 
-  def _readRawDataHere(self, fp):
-    ''' Retrieve the data bytes stored at `offset`.
+  def _readhere(self, fp):
+    ''' Retrieve the data bytes stored at the current file offset.
+        The offset points at the flags ahead of the data bytes.
     '''
     flags = fromBSfp(fp)
     if flags is None:
@@ -90,7 +101,7 @@ class DataFile(object):
     assert len(data) == dsize
     return flags, data
 
-  def saveData(self, data, noCompress=False):
+  def savedata(self, data, noCompress=False):
     ''' Append a chunk of data to the file, return the store offset.
     '''
     flags = 0
@@ -117,6 +128,149 @@ class DataFile(object):
       if self._fp:
         self._fp.close()
         self._fp = None
+
+class DataDir(object):
+  ''' A mapping of hash->Block that manages a directory of DataFiles.
+      Subclasses must implement the _openIndex() method, which
+      should return a mapping to store and retrieve index information.
+  '''
+
+  def __init__(self, dir):
+    self.dir = dir
+    self._index = None
+    self._lock = allocate_lock()
+    self._open = {}
+    self._n = None
+
+  def pathto(self, rpath):
+    ''' Return a pathname within the DataDir given `rpath`, a path
+        relative to the DataDir.
+    '''
+    return os.path.join(self.dir, rpath)
+
+  def _openIndex(self):
+    raise NotImplementedError
+
+  @property
+  def index(self):
+    ''' Return the index mapping.
+    '''
+    I = self._index
+    if not I:
+      with self._lock:
+        I = self._index
+        if not I:
+          I = self._index = self._openIndex()
+    return I
+
+  @staticmethod
+  def decodeIndexEntry(entry):
+    ''' Parse an index entry into n (data file index) and offset.
+    '''
+    n, _ = fromBS(entry)
+    offset, _ = fromBS(_)
+    if len(_) > 0:
+      raise ValueError, "can't decode index entry: %s" % (hexlify(entry),)
+    return n, offset
+
+  @staticmethod
+  def encodeIndexEntry(n, offset):
+    ''' Prepare an index entry from data file index and offset.
+    '''
+    return toBS(n) + toBS(offset)
+
+  def __getitem__(self, hash):
+    n, offset = self.decodeIndexEntry(self.index[hash])
+    return self.open(n).readdata(offset)
+
+  def __setitem__(self, hash, data):
+    h = self.store(data)
+    assert h == hash, "hash mismatch: given %s but computed %s" % (hash, h)
+
+  def store(self, data):
+    ''' Store a data block, update index.
+        Returns hash as sanity check.
+    '''
+    h = self.hash(data)
+    I = self.index
+    if h not in I:
+      n = self.n
+      D = self.open(n)
+      offset = D.savedata(data)
+      I[h] = self.encodeIndexEntry(n, offset)
+
+  @property
+  def n(self):
+    ''' The ordinal of the currently open data file.
+    '''
+    with self._lock:
+      if self._n is None:
+        self._n = self.next_n()
+    return self._n
+
+  def next_n(self):
+    ''' Return a free index not mapping to an existing data file.
+    '''
+    try:
+      n = max(self.datafileindices()) + 1
+    except ValueError:
+      n = 0
+    while os.path.exists(self.pathto(self.datafilename(n))):
+      n += 1
+    return n
+
+  def set_n(self, n):
+    self._n = n
+
+  def contains(self, h):
+    ''' Check if the specified hash is present in the store.
+    '''
+    with self._lock:
+      return h in self._index
+
+  def flush(self):
+    for datafile in self._open:
+      datafile.flush()
+    self.index.flush()
+
+  def open(n):
+    ''' Obtain the Datafile indexed `n`.
+    '''
+    O = self._open
+    with self._lock:
+      D = O.get(n)
+      if D is None:
+        D = O[n] = Datafile(self.pathto(self.datafilename(n)))
+    return D
+
+  def datafilename(self, n):
+    ''' Return the file basename for file index `n`.
+    '''
+    return str(n) + '.vtd'
+
+  def datafileindices(self):
+    ''' Generator that yields the indices of datafiles present.
+    '''
+    for name in os.listdir(self.dir):
+      if name.endswith('.vtd'):
+        prefix = name[:-4]
+        if prefix.isdigit():
+          n = int(prefix)
+          if str(n) == prefix:
+            if os.path.isfile(self.pathto(name)):
+              yield n
+
+class GDBMDataDir(DataDir):
+  ''' A DataDir with a GDBM index.
+  '''
+  def __init__(self, dirpath):
+    DataDir.__init__(self, dirpath)
+    self._index = None
+
+  def _openIndex(self):
+    import gdbm
+    gdbmpath = self.pathto("index.gdbm")
+    return gdbm.open(gdbmpath, "cf")
 
 if __name__ == '__main__':
   import cs.venti.datafile_tests
