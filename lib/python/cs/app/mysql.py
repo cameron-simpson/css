f36607cd7bfb33966c137adc91e83773ad164467
Cameron Simpson <cs@zip.com.au>
2012-03-19 17:33:15 +1100

    mysqlbinlog-parse: read mysql binlog, produce report

--- lib/python/cs/app/mysql.py
+++ lib/python/cs/app/mysql.py
@@ -7,6 +7,15 @@
 from datetime import datetime
 import re
 import sys
+from collections import namedtuple
+from cs.alg import collate
+from cs.lex import str1
+from cs.logutils import Pfx
+
+BinLog_DBQuery = namedtuple('BinLog_DBQuery',
+                            'sql dbname when server_id log_pos thread_id exec_time error_code')
+BinLog_DBThread = namedtuple('BinLog_DBThread',
+                             'thread_id queries')
 
 class BinLogParser(object):
   ''' Read mysqlbinlog(1) output and report.
@@ -14,7 +23,7 @@ class BinLogParser(object):
 
   re_QUERY_MARKER = re.compile(r'#(\d\d)(\d\d)(\d\d) +(\d\d?):(\d\d):(\d\d) +server +id +(\d+) +end_log_pos +(\d+) +Query +thread_id=(\d+) +exec_time=(\d+) +error_code=(\d)+')
   re_USE_DBNAME = re.compile(r'use +([a-z[a-z0-9_]*)')
-  re_SET_TIMESTAMP = re.compile(r'SET TIMESTAMP=(\d+)')
+  ##re_SET_TIMESTAMP = re.compile(r'SET TIMESTAMP=(\d+)')
 
   def __init__(self):
     self.db_threads = {}
@@ -30,17 +39,6 @@ class BinLogParser(object):
     def append_query(self, query):
       self.queries.append(query)
 
-  class DBQuery(object):
-    def __init__(self, dbname, when, server_id, log_pos, thread, exec_time, error_code):
-      self.dbname = dbname
-      self.when = when
-      self.server_id = server_id
-      self.log_pos = log_pos
-      self.thread = thread
-      self.exec_time = exec_time
-      self.error_code = error_code
-      thread.append_query(self)
-
   def thread_by_id(self, thread_id):
     thread = self.db_threads.get(thread_id, None)
     if thread is None:
@@ -48,90 +46,63 @@ class BinLogParser(object):
     return thread
 
   def parse(self, fp):
-    ''' Read lines from the file-like object 'fp'.
+    ''' Read lines from the file-like object 'fp', yield BinLog_DBQuery
+        namedtuples.
     '''
-    dbname = None
-    query = None
-    query_text = None
-    last_ts = None
-    for line in fp:
-      assert line[-1] == '\n', "unexpected EOF: "+line
-      line = line[:-1].rstrip().expandtabs()
-      ##print >>sys.stderr, ">", line
+    with Pfx(fp):
+      dbname = None
+      query_info = None
+      query_text = None
+      last_ts = None
+      lineo = 0
+      for line in fp:
+        lineo += 1
+        with Pfx(str(lineno)):
+          assert line.endswith('\n'), "unexpected EOF: "+line
+          line = line.expandtabs()
+          ##print >>sys.stderr, ">", line
 
-  #(\d\d)(\d\d)(\d\d) +(\d\d?):(\d\d):(\d\d) +server +id +(\d+) +end_log_pos +(\d+) +Query +thread_id=(\d+) +exec_time=(\d+) +error_code=(\d)+
-      m = BinLogParser.re_QUERY_MARKER.match(line)
-      if m:
-        # new query - tidy up old one and set up new one
-        if query is not None:
-          query.query_text = query_text;
-          query_text = None
-          query = None
-        when = datetime( *[int(m.group(i+1)) for i in range(6)] )
-        server_id = int(m.group(7))
-        log_pos = int(m.group(8))
-        thread_id = int(m.group(9))
-        exec_time = int(m.group(10))
-        error_code = int(m.group(11))
-        thread = self.thread_by_id(thread_id)
-        query = BinLogParser.DBQuery(dbname, when, server_id, log_pos, thread, exec_time, error_code)
-        self.db_queries.append(query)
-        query_text = ''
-        continue
+          # ^#(\d\d)(\d\d)(\d\d) +(\d\d?):(\d\d):(\d\d) +server +id +(\d+) +end_log_pos +(\d+) +Query +thread_id=(\d+) +exec_time=(\d+) +error_code=(\d)+
+          m = BinLogParser.re_QUERY_MARKER.match(line)
+          if m:
+            # new query - tidy up old one and set up new one
+            if query_info:
+              yield BinLogParser.DBQuery(query_text, *query_info)
+              ##yield BinLogParser.DBQuery(query_text, dbname, when, server_id, log_pos, thread, exec_time, error_code)
+              query_text = ''
+              query_info = None
+            when = datetime( *[int(m.group(i+1)) for i in range(6)] )
+            server_id = int(m.group(7))
+            log_pos = int(m.group(8))
+            thread_id = int(m.group(9))
+            exec_time = int(m.group(10))
+            error_code = int(m.group(11))
+            query_info = [None, when, server_id, log_pos, thread_id, exec_time, error_code]
+            continue
 
-      m = BinLogParser.re_USE_DBNAME.match(line)
-      if m:
-        dbname = query.dbname = m.group(1)
-        continue
+          # use database
+          m = BinLogParser.re_USE_DBNAME.match(line)
+          if m:
+            dbname = str1(m.group(1))
+            query_info[0] = dbname
+            continue
 
-      m = BinLogParser.re_SET_TIMESTAMP.match(line)
-      if m:
-        ts = query.timestamp = int(m.group(1))
-        continue
+          if ( line.startswith('#')
+             or (line.startswith('/*') and line.endswith('*/;'))
+             or line.startswith('SET ')
+             ##or line.startswith('DELIMITER ')
+             ##or line.startswith('ROLLBACK')
+          ):
+            ##print >>sys.stderr, "SKIP", line
+            continue
 
-      if line.startswith('#') \
-      or (line.startswith('/*') and line.endswith('*/;')) \
-      or line.startswith('SET ') \
-      or line.startswith('DELIMITER ') \
-      or line.startswith('ROLLBACK') \
-      :
-        ##print >>sys.stderr, "SKIP", line
-        continue
+          query_text += line
 
-      if len(line) > 0:
-        if query_text is None:
-          print >>sys.stderr, "LINE=[%s]" % (line,)
-        query_text += ' ' + line.lstrip()
+        if query_info:
+          yield BinLogParser.DBQuery(query_text, *query_info)
 
-    if query is not None:
-      query.query_text = query_text
-
-
-  def report(self):
-    print "thread_ids = %s" % ([ T.thread_id for T in self.db_threads.values() ],)
-    print "%d queries" % (len(self.db_queries),)
-    by_time = {}
-    by_dbname = {}
-    for tid in self.db_threads.keys():
-      T = self.thread_by_id(tid)
-      for Q in T.queries:
-        by_time.setdefault(Q.timestamp, []).append(Q)
-        by_dbname.setdefault(Q.dbname, []).append(Q)
-        if Q.exec_time > 0:
-          print tid, Q.exec_time, Q.query_text
-
-    timestamps = by_time.keys()
-    timestamps.sort()
-    for ts in timestamps:
-      Qs = by_time[ts]
-      print ts, len(Qs), "queries"
-      continue
-      print ts, Qs[0].query_text
-      if len(Qs) > 1:
-        for Q in Qs[1:]:
-          print "\t", Q.dbname, Q.query_text
-
-    dbnames = by_dbname.keys()
-    dbnames.sort()
-    for dbname in dbnames:
-      print "%-16s %d queries" % (dbname, len(by_dbname[dbname]))
+  def collate_queries(self, queries):
+    queries = list(queries)
+    by_thread = collate(queries, 'thread_id')
+    by_dbname = collate(queries, 'dbname')
+    return by_thread, by_dbname
