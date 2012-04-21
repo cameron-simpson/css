f3720341494e053b0e3f816ee84b4c92425bd023
Cameron Simpson <cs@zip.com.au>
2012-04-22 07:52:10 +1000

    cs.app.mailfiler: copy with hypothetical missing From or Subject in log line

--- lib/python/cs/app/mailfiler.py
+++ lib/python/cs/app/mailfiler.py
@@ -669,7 +669,9 @@ class WatchedMaildir(O):
             state = RuleState(M, self.filter_modes)
             state.message_path = mdir.keypath(key)
             state.logto(envsub("$HOME/var/log/mailfiler"))
-            state.log(u"%s %s %s" % (time.strftime("%Y-%m-%d %H:%M:%S"), unrfc2047(M['from']), unrfc2047(M['subject'])))
+            state.log(u"%s %s %s" % (time.strftime("%Y-%m-%d %H:%M:%S"),
+                                     unrfc2047(M.get('from', '_no_from')),
+                                     unrfc2047(M.get('subject', '_no_subject'))))
             state.log("  "+mdir.keypath(key))
             saved_to = []
             reports = []
