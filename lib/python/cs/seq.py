5d0f4ba16cb54ec05d5697c603a4a7f525ea531b
Cameron Simpson <cs@zip.com.au>
2012-05-22 17:12:37 +1000

    cs.seq: discard old test, now in cs.seq_tests

--- lib/python/cs/seq.py
+++ lib/python/cs/seq.py
@@ -118,13 +118,6 @@ def imerge(*seqs):
     else:
       heapq.heappush(heap, (head, S))
 
-class TestAll(unittest.TestCase):
-
-  def testIMerge(self):
-    self.assertEqual( list(imerge([1,2,3],[1,4,7],[2,5,6])),
-                      [1,1,2,2,3,4,5,6,7]
-                    )
-
 if __name__ == '__main__':
   import sys
   import cs.seq_tests
