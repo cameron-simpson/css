b447369888025fb7ead681558ec95fa7c84f096e
Cameron Simpson <cs@zip.com.au>
2012-05-23 15:04:00 +1000

    cs.seq_tests: unit tests for onetoone and onetomany decorators

--- lib/python/cs/seq_tests.py
+++ lib/python/cs/seq_tests.py
@@ -7,7 +7,7 @@
 from StringIO import StringIO
 import sys
 import unittest
-from cs.seq import imerge
+from cs.seq import imerge, onetoone, onetomany
 
 class TestSeq(unittest.TestCase):
 
@@ -17,10 +17,27 @@ class TestSeq(unittest.TestCase):
   def tearDown(self):
     pass
 
-  def testIMerge(self):
+  def test_imerge(self):
     self.assertEqual( list(imerge([1,2,3],[1,4,7],[2,5,6])),
                       [1,1,2,2,3,4,5,6,7]
                     )
+  def test_onetoone(self):
+    class C(list):
+      @onetoone
+      def lower(item):
+        return item.lower()
+    L = C(['Abc', 'Def'])
+    I2 = L.lower()
+    self.assertEquals( list(I2), ['abc', 'def'] )
+
+  def test_onetomany(self):
+    class C(list):
+      @onetomany
+      def angles(item):
+        return [ "<%s>" % (i,) for i in item ]
+    L = C(['Abc', 'Def'])
+    I2 = L.angles()
+    self.assertEquals( list(I2), ['<A>', '<b>', '<c>', '<D>', '<e>', '<f>'])
 
 def selftest(argv):
   unittest.main(__name__, None, argv)
