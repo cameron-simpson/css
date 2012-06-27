17d7eeb6c434815225f36e8ff6b9fc980e5c6123
Cameron Simpson <cs@zip.com.au>
2012-06-28 09:33:50 +1000

    cs.py3: python 2/3 code sharing helpers

---
+++ lib/python/cs/py3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python -tt
+#
+# Python3 helpers to aid code sharing between python2 and python3.
+#       - Cameron Simpson <cs@zip.com.au> 28jun2012
+#
+
+import sys
+
+if  sys.hexversion < 0x03000000:
+  pass
+else:
+  unicode = str
