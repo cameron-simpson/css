2b0352d2bb15867da55c5662e4bd0d345856f1af
Cameron Simpson <cs@zip.com.au>
2012-06-28 12:03:39 +1000

    cs.py3: present StringIO and BytesIO (hackishly on python2)

--- lib/python/cs/py3.py
+++ lib/python/cs/py3.py
@@ -9,6 +9,12 @@ import sys
 if  sys.hexversion < 0x03000000:
   globals()['unicode'] = unicode
   from types import StringTypes
+  try:
+    from cStringIO import StringIO as BytesIO
+  except ImportError:
+    from StringIO import StringIO as BytesIO
+  StringIO = BytesIO    # horribly wrong, I know
 else:
   unicode = str
   StringTypes = (str,)
+  from io import BytesIO, StringIO
