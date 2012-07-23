9731688e06d4dbaf0f4d34c69f9200ef5a5b3012
Cameron Simpson <cs@zip.com.au>
2012-07-23 17:06:26 +1000

    cs.cryptutils: convenient UNIX crypt wrapper, to start with

---
+++ lib/python/cs/cryptutils.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#
+# Trite convenience routines to do with cryptography.
+#
+
+import crypt
+import random
+
+UNIX_SALT_CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/.'
+
+def unixcrypt(password, salt=None):
+  if salt is None:
+    salt = randint(0,4095)
+    salt = ( UNIX_SALT_CHARS[ salt % 64 ]
+           + UNIX_SALT_CHARS[ (salt // 64) % 64 ]
+           )
+  return crypt.crypt(password, salt)
