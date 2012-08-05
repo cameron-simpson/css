b826c7fe3be08a75b0dadcd8265828448a9e7189
Cameron Simpson <cs@zip.com.au>
2012-08-05 15:58:13 +1000

    cs.configutils_tests: initial trite test

--- lib/python/cs/configutils_tests.py
+++ lib/python/cs/configutils_tests.py
@@ -4,19 +4,32 @@
 #       - Cameron Simpson <cs@zip.com.au>
 #
 
+from os.path import basename, dirname, join as joinpath
 import sys
+from shutil import copy, rmtree
+from tempfile import mkdtemp
 import unittest
+from cs.configutils import ConfigWatcher, ConfigSectionWatcher
 
-class TestCOnfigUtils(unittest.TestCase):
+testdatadir = joinpath(dirname(__file__), 'testdata', 'cs.configutils')
+test_config_file = joinpath(testdatadir, 'test.ini')
+
+class TestConfigUtils(unittest.TestCase):
 
   def setUp(self):
-    pass
+    self.tmpdir = mkdtemp()
+    self.tmpfile = joinpath(self.tmpdir, basename(test_config_file))
+    copy(test_config_file, self.tmpfile)
 
   def tearDown(self):
-    pass
+    rmtree(self.tmpdir)
+
+  def testWatcher(self):
+    cfg = ConfigWatcher(self.tmpfile)
 
 def selftest(argv):
   unittest.main(__name__, None, argv)
 
 if __name__ == '__main__':
+  print "testdatadir =", testdatadir
   selftest(sys.argv)
