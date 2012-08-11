15968ba87687987975f8583cbdb798c096a83778
Cameron Simpson <cs@zip.com.au>
2012-08-11 16:46:27 +1000

    cs.venti.store_tests: start out with a trivial test

--- lib/python/cs/venti/store_tests.py
+++ lib/python/cs/venti/store_tests.py
@@ -6,12 +6,21 @@
 
 import sys
 import unittest
+from .store import MappingStore
 
 class TestAll(unittest.TestCase):
 
   def setUp(self):
     import random
     random.seed()
+    self.S = MappingStore({})
+
+  def tearDown(self):
+    self.S.close()
+
+  def test01empty(self):
+    S = self.S
+    self.assertEquals(len(S), 0)
 
 def selftest(argv):
   unittest.main(__name__, None, argv)
