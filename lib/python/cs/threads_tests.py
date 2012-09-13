8eaa40659aeee7e14715b3ca0a2098cc64258997
Cameron Simpson <cs@zip.com.au>
2012-09-13 20:18:33 +1000

    cs.threads_tests: test__01_same_fork: convert runTree() result to list from an iterable for comparison

--- lib/python/cs/threads_tests.py
+++ lib/python/cs/threads_tests.py
@@ -81,7 +81,7 @@ class TestRuntree(unittest.TestCase):
 
   def test__01_same_fork(self):
     L = Later(1)
-    self.assertEquals(runTree( [1,2,3], [ RunTreeOp(self.f_same, True, True, None) ], None, L), [1,2,3])
+    self.assertEquals(list(runTree( [1,2,3], [ RunTreeOp(self.f_same, True, True, None) ], None, L)), [1,2,3])
     L.close()
 
 def selftest(argv):
