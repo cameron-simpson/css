92e5356ab1db9c0c38c8e030242f41c1432f1e4f
Cameron Simpson <cs@zip.com.au>
2012-10-06 15:46:19 +1000

    cs.connect_tests: initial tests of an empty _BufferFile

--- lib/python/cs/connect_tests.py
+++ lib/python/cs/connect_tests.py
@@ -6,7 +6,7 @@
 
 import sys
 import unittest
-from cs.connect import DataQueue
+from cs.connect import _BufferFile, DataQueue
 
 class TestConnect(unittest.TestCase):
 
@@ -16,7 +16,19 @@ class TestConnect(unittest.TestCase):
   def tearDown(self):
     pass
 
-  def test00DataQueue(self):
+  def test_00_BufferFile_00(self):
+    BF = _BufferFile(0)
+    self.assertEquals(b'', BF.read(0, 0))
+    # read before the begiing of the file
+    self.assertRaises(ValueError, BF.read, -1, 0)
+    # read after the end of the file
+    self.assertRaises(ValueError, BF.read, 1, 0)
+    # read more data than is in the file
+    self.assertRaises(ValueError, BF.read, 0, 1)
+    # read less than 0 bytes of data
+    self.assertRaises(ValueError, BF.read, 0, -1)
+
+  def test_01_DataQueue_00(self):
     DQ = DataQueue()
     print "DQ =", DQ
     self.assertRaises(ValueError, DQ.sent, -1)
