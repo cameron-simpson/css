e475493b2a7db7a8fcc59f3188113ef8a73f04a7
Cameron Simpson <cs@zip.com.au>
2012-11-24 14:40:12 +1100

    cs.app.mailfiler: test header.contains() condition, disable D() if not $DEBUG

--- lib/python/cs/app/mailfiler_tests.py
+++ lib/python/cs/app/mailfiler_tests.py
@@ -4,12 +4,17 @@
 #       - Cameron Simpson <cs@zip.com.au>
 #
 
+import os
 import sys
 from os.path import basename, dirname, join as joinpath
 import unittest
 from cs.app.mailfiler import parserules
 from cs.logutils import D
 from cs.misc import O, slist
+
+if not os.environ.get('DEBUG', ''):
+  def D(*a):
+    pass
 
 testdatadir = joinpath(dirname(__file__), 'testdata', 'cs.app.mailfiler')
 test_rules_file = joinpath(testdatadir, 'rules')
@@ -28,7 +33,7 @@ class TestMailFiler(unittest.TestCase):
     if flags is None:
       flags = O(alert=False, halt=False)
     R, = list(parserules(rule_lines))
-    ##D("R = %s", R)
+    D("R = %s", R)
     self.assertEquals(len(R.actions), 1)
     self.assertEquals(R.actions[0], action_tuple)
     self.assertEquals(R.label, labelstr)
@@ -39,10 +44,10 @@ class TestMailFiler(unittest.TestCase):
       for i in range(len(conditions)):
         C = conditions[i]
         RC = R.conditions[i]
-        ##D("C = %s", C)
+        D("C = %s", C)
         for attr in dir(C):
           if attr[0].isalpha():
-            ##D("test attr %s: %s VS %s", attr, getattr(C, attr), getattr(RC, attr))
+            D("test attr %s: %s VS %s", attr, getattr(C, attr), getattr(RC, attr))
             self.assertEquals(getattr(C, attr), getattr(RC, attr))
 
   def testParseRules(self):
@@ -76,6 +81,11 @@ class TestMailFiler(unittest.TestCase):
     self._testSingleRule( "target . to,cc:joe blogs <joe@bar>",
                           ('TARGET', 'target'), '',
                           ( O(addrkeys=('joe@bar',), header_names=('to', 'cc')), ) )
+    self._testSingleRule( "target . list-id.contains(\"<squid-users.squid-cache.org>\")",
+                          ('TARGET', 'target'), '',
+                          ( O(funcname='contains',
+                              header_names=('list-id',),
+                              test_string='<squid-users.squid-cache.org>'), ) )
 
   def testRulesParseFile(self):
     rules = slist(parserules(test_rules_file))
