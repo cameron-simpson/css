32eda124bcf7219cdde60a0a7cbdf338c9030b6a
Cameron Simpson <cs@zip.com.au>
2012-12-25 14:39:08 +1100

    cs.configutils: ConfigWatcher: fix docstring

--- lib/python/cs/configutils.py
+++ lib/python/cs/configutils.py
@@ -29,7 +29,7 @@ def load_config(config_path, parser=None):
 
 class ConfigWatcher(object):
   ''' A monitor for a windows style .ini file.
-      The current SafeConfigParser object is present in the .parser attribute.
+      The current SafeConfigParser object is present in the .config attribute.
   '''
   def __init__(self, config_path):
     self._config_path = config_path
