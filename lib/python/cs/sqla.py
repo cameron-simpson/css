967e810c789ed923e3bfd4427ca985302b5fec87
Cameron Simpson <cs@zip.com.au>
2013-03-03 13:24:14 +1100

    cs.sqla: interactive command loop if no op

--- lib/python/cs/sqla.py
+++ lib/python/cs/sqla.py
@@ -7,14 +7,16 @@
 from __future__ import print_function
 import sys
 import os.path
+from cmd import Cmd
 from getopt import GetoptError
+import shlex
 from sqlalchemy import MetaData, create_engine
 from threading import RLock
 from cs.logutils import setup_logging, D, Pfx, error
 from cs.threads import locked_property
 from cs.obj import O
 
-usage = '''Usage: %s dburl op [args...]
+usage = '''Usage: %s dburl [op [args...]]
   dburl SQLAlchemy compatible database URL.
   op    Operation to perform.'''
 
@@ -23,6 +25,7 @@ def main(argv):
   cmd = os.path.basename(argv.pop(0))
   setup_logging(cmd)
 
+  xit = 0
   badopts = False
 
   if not argv:
@@ -32,8 +35,9 @@ def main(argv):
     dburl = argv.pop(0)
 
   if not argv:
-    error("missing op")
-    badopts = True
+    cmdloop = CmdLoop(dburl)
+    cmdloop.prompt = cmd+'> '
+    cmdloop.cmdloop()
   else:
     op = argv.pop(0)
     DB = SQLA(dburl=dburl)
@@ -87,6 +91,34 @@ class SQLA(O):
       raise GetoptError("extra arguments: %s" % (' '.join(args),))
     for t in self.tables:
       print(t.name, type(t))
+
+class CmdLoop(Cmd):
+
+  def __init__(self, dburl):
+    Cmd.__init__(self, '\t')
+    self.sqla = SQLA(dburl)
+
+  def property(self):
+    return self.sqla.dburl
+
+  def emptyline(self):
+    pass
+
+  def default(self, line):
+    words = shlex.split(line, comments=True)
+    print("words =", words)
+    if not words:
+      return
+    op = words.pop(0)
+    if op == 'EOF':
+      return True
+    with Pfx(op):
+      try:
+        fn = getattr(self.sqla, 'do_'+op)
+      except AttributeError as e:
+        error("unknown command")
+      else:
+        fn(args)
 
 if __name__ == '__main__':
   sys.exit(main(sys.argv))
