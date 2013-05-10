6d94db2629aeb642a4dd0bbaf2b1c89f7e10be2d
Cameron Simpson <cs@zip.com.au>
2013-05-10 11:13:25 +1000

    cs.venti.paths: add walk like os.walk, rename update_dir to copy_in, add copy_out

--- lib/python/cs/venti/paths.py
+++ lib/python/cs/venti/paths.py
@@ -20,9 +20,25 @@ def resolve(rootD, subpath, do_mkdir=False):
     return D, subpaths[0]
   return D, None
 
-def update_dir(rootD, rootpath, delete=False, ignore_existing=False, trust_size_mtime=False):
-  ''' Update a Dir `rootD` with the contents of the specified
-      directory `rootpath` from the OS.
+def walk(rootD):
+  ''' An analogue to os.walk to descend a vt Dir tree.
+      Yields Dir, relpath, dirs, files for each directory in the tree.
+      The top directory (`rootD`) has the relpath ''.
+  '''
+  pending = [ (rootD, '') ]
+  while pending:
+    thisD, relpath, dirs, files = pending.pop(0)
+    yield thisD, path, thisD.dirs(), thisD.files()
+    for dir in dirs:
+      subD = rootD.chdir1(dir)
+      if relpath:
+        subpath = os.path.join(relpath, dir)
+      else:
+        subpath = dir
+      pending.push( (subD, subpath) )
+
+def copy_in(rootpath, rootD, delete=False, ignore_existing=False, trust_size_mtime=False):
+  ''' Copy the os directory tree at `rootpath` over the Dir `rootD`.
   '''
   with Pfx("update_dir(%s)", rootpath):
     rootpath_prefix = rootpath + '/'
@@ -72,3 +88,54 @@ def update_dir(rootD, rootpath, delete=False, ignore_existing=False, trust_size_
               error("%s: %s", filepath, e)
             except IOError as e:
               error("%s: %s", filepath, e)
+
+def copy_out(rootD, rootpath, makedirs=False, delete=False, ignore_existing=False, trust_size_mtime=False):
+  ''' Copy the Dir `rootD` onto the os directory `rootpath`.
+      Notes: `delete` not implemented.
+  '''
+  with Pfx("restore(rootpath=%s)", rootpath):
+    if not os.path.isdir(rootpath):
+      if makedirs:
+        os.makedirs(rootpath)
+    for thisD, relpath, dirs, files in walk(rootD):
+      if relpath:
+        path = os.path.join(rootpath, relpath)
+      else:
+        path = rootpath
+      with Pfx(path):
+        if not os.path.isdir(path):
+          info("mkdir")
+          os.path.mkdir(path)
+        for filename in sorted(files):
+          with Pfx(filename):
+            E = thisD[filename]
+            if not E.isfile:
+              warning("vt source is not a file, skipping")
+              continue
+            filepath = os.path.join(path, filename)
+            if ignore_existing and os.path.exists(filepath):
+              debug("already exists, ignoring")
+              continue
+            try:
+              # TODO: should this be os.stat if we don't support symlinks?
+              st = os.lstat(filepath)
+            except OSError:
+              pass
+            else:
+              B = E.getBlock()
+              M = E.meta
+              if ( trust_size_mtime
+               and ( M.mtime is not None and M.mtime == st.st_mtime
+                     and B.span == st.st_size
+                   )
+                 ):
+                debug("matching mtime and size, not overwriting")
+                continue
+              # create or overwrite the file
+              # TODO: backup mode in case of write errors
+              with Pfx(filepath):
+                with open(filepath, "wb") as fp:
+                  for chunk in D[filename].getBlock().chunks():
+                    fp.write(chunk)
+                if M.mtime is not None:
+                  os.utime(filepath, (st.st_atime, M.mtime))
