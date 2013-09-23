26474ebf53889beed8ab4c6448259366d1e763a2
Cameron Simpson <cs@zip.com.au>
2013-09-24 09:46:17 +1000

    cs.nodedb.mappingdb_tests: all tests now in cs.nodedb.node_tests since that uses a mappingdb backend anyway; should put some fine grained tests here

--- lib/python/cs/nodedb/mappingdb_tests.py
+++ lib/python/cs/nodedb/mappingdb_tests.py
@@ -8,32 +8,14 @@ import os
 import sys
 import unittest
 from cs.logutils import D
+from cs.debug import thread_dump
+from cs.timeutils import sleep
 from . import NodeDB
 from .mappingdb import MappingBackend
 from .node_tests import TestAll as NodeTestAll
 
 class TestAll(NodeTestAll):
-
-  def setUp(self):
-    self.mapping = {}
-    self.backend = MappingBackend(self.mapping)
-    self.db = NodeDB(backend=self.backend)
-
-  def test22persist(self):
-    N = self.db.newNode('HOST:foo1')
-    N.X = 1
-    N2 = self.db.newNode('SWITCH:sw1')
-    N2.Ys = (9,8,7)
-    dbstate = dict(self.db)
-    self.db.close()
-    self.db = NodeDB(backend=MappingBackend(self.mapping))
-    dbstate2 = dict(self.db)
-    self.assertTrue(dbstate == dbstate2, "db state differs:\n\t%s\n\t%s" % (dbstate, dbstate2))
-
-  def tearDown(self):
-    D("TD1: close db...")
-    self.db.close()
-    D("TD3")
+  pass
 
 def selftest(argv):
   unittest.main(__name__, None, argv)
