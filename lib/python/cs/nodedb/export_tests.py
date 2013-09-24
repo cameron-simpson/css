bbee2143b62416872f1e065399f9341380cc3ead
Cameron Simpson <cs@zip.com.au>
2013-09-25 09:16:00 +1000

    cs.nodedb.export_tests: use new close-the-db scheme

--- lib/python/cs/nodedb/export_tests.py
+++ lib/python/cs/nodedb/export_tests.py
@@ -9,54 +9,63 @@ import unittest
 from . import NodeDB
 from .export import export_rows_wide, import_rows_wide
 from .mappingdb import MappingBackend
+from .node_tests import TestAll as NodeTestAll
 
-class TestAll(unittest.TestCase):
+class TestAll(NodeTestAll):
+
+  def nodedb(self):
+    self.backend = MappingBackend(self.mapping)
+    db = self.db = NodeDB(backend=self.backend)
+    self.N1 = db['HOST:host1']
+    self.N2 = db['HOST:host2']
+    return db
 
   def setUp(self):
-    from . import NodeDB
-    db = self.db = NodeDB(backend=MappingBackend({}))
-    N1 = self.N1 = db.newNode('HOST', 'host1')
-    N1.ATTR1 = 1
-    N2 = self.N2 = db.newNode('HOST', 'host2')
-    N2.ATTR2 = 2
+    self.mapping = { ('HOST', 'host1'): { 'ATTR1': [1]},
+                     ('HOST', 'host2'): { 'ATTR2': [2]},
+                   }
 
   def test01export_rows_wide_01raw(self):
-    N1, N2 = self.N1, self.N2
-    rows = tuple( export_rows_wide( (N1, N2) ) )
-    expected_rows = ( [ 'TYPE', 'NAME', 'ATTR1', 'ATTR2' ],
-                      [ N1.type, N1.name, N1.ATTR1, None ],
-                      [ N2.type, N2.name, None,     N2.ATTR2 ],
-                    )
-    self.assertTrue(rows == expected_rows)
+    with self.nodedb() as db:
+      N1, N2 = self.N1, self.N2
+      rows = tuple( export_rows_wide( (N1, N2) ) )
+      expected_rows = ( [ 'TYPE', 'NAME', 'ATTR1', 'ATTR2' ],
+                        [ N1.type, N1.name, N1.ATTR1, None ],
+                        [ N2.type, N2.name, None,     N2.ATTR2 ],
+                      )
+      self.assertTrue(rows == expected_rows)
 
   def test01export_rows_wide_02row_attrs(self):
-    N1, N2 = self.N1, self.N2
-    rows = tuple( export_rows_wide( (N1, N2), attrs=('ATTR1',) ) )
-    expected_rows = ( [ 'TYPE', 'NAME', 'ATTR1' ],
-                      [ N1.type, N1.name, N1.ATTR1 ],
-                    )
-    self.assertTrue(rows == expected_rows)
+    with self.nodedb() as db:
+      N1, N2 = self.N1, self.N2
+      rows = tuple( export_rows_wide( (N1, N2), attrs=('ATTR1',) ) )
+      expected_rows = ( [ 'TYPE', 'NAME', 'ATTR1' ],
+                        [ N1.type, N1.name, N1.ATTR1 ],
+                      )
+      self.assertTrue(rows == expected_rows)
 
   def test01export_rows_wide_03row_attrs_allnodes(self):
-    N1, N2 = self.N1, self.N2
-    rows = tuple( export_rows_wide( (N1, N2), attrs=('ATTR1',), all_nodes=True ) )
-    expected_rows = ( [ 'TYPE', 'NAME', 'ATTR1' ],
-                      [ N1.type, N1.name, N1.ATTR1 ],
-                      [ N2.type, N2.name, None,    ],
-                    )
-    self.assertTrue(rows == expected_rows)
+    with self.nodedb() as db:
+      N1, N2 = self.N1, self.N2
+      rows = tuple( export_rows_wide( (N1, N2), attrs=('ATTR1',), all_nodes=True ) )
+      expected_rows = ( [ 'TYPE', 'NAME', 'ATTR1' ],
+                        [ N1.type, N1.name, N1.ATTR1 ],
+                        [ N2.type, N2.name, None,    ],
+                      )
+      self.assertTrue(rows == expected_rows)
 
   def test02import_rows_wide_01(self):
-    input_rows = ( [ 'TYPE', 'NAME', 'ATTR1', 'ATTR2' ],
-                   [ 'HOST', 'host1', 1, None ],
-                   [ 'HOST', 'host2', None, 2 ],
-                   [ None,   None,    None, 3 ],
-                 )
-    data = tuple( import_rows_wide( input_rows ) )
-    expected_data = ( ( 'HOST', 'host1', {'ATTR1': [1]} ),
-                      ( 'HOST', 'host2', {'ATTR2': [2,3]} ),
-                    )
-    self.assertTrue(data == expected_data)
+    with self.nodedb() as db:
+      input_rows = ( [ 'TYPE', 'NAME', 'ATTR1', 'ATTR2' ],
+                     [ 'HOST', 'host1', 1, None ],
+                     [ 'HOST', 'host2', None, 2 ],
+                     [ None,   None,    None, 3 ],
+                   )
+      data = tuple( import_rows_wide( input_rows ) )
+      expected_data = ( ( 'HOST', 'host1', {'ATTR1': [1]} ),
+                        ( 'HOST', 'host2', {'ATTR2': [2,3]} ),
+                      )
+      self.assertTrue(data == expected_data)
 
 def selftest(argv):
   unittest.main(__name__, None, argv)
