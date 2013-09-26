c9c3b84138c196e95831d68b80a3bd8f0adfe079
Cameron Simpson <cs@zip.com.au>
2013-09-26 10:11:10 +1000

    cs.tty: ttysize: accept a file as well as a file descriptor; always return a WinSize tuple - attributes will be None if not determined; docstring fix

--- lib/python/cs/tty.py
+++ lib/python/cs/tty.py
@@ -13,15 +13,19 @@ WinSize = namedtuple('WinSize', 'rows columns')
 
 def ttysize(fd):
   ''' Return a (rows, columns) tuple for the specified file descriptor.
-      If the window size cannot be determined, None will be returns
+      If the window size cannot be determined, None will be returned
       for either or both of rows and columns.
   '''
-  P = Popen(['stty', '-a'], stdin=f, stdout=PIPE, universal_newlines=True)
+  if not isinstance(fd, int):
+    fd = fd.fileno()
+  P = Popen(['stty', '-a'], stdin=fd, stdout=PIPE, universal_newlines=True)
   stty = P.stdout.read()
   xit = P.wait()
   if xit != 0:
     return None
   m = re.compile(r' rows (\d+); columns (\d+)').search(stty)
-  if not m:
-    return None
-  return WinSize( int(m.group(1)), int(m.group(2)) )
+  if m:
+    row, columns = int(m.group(1)), int(m.group(2)) )
+  else:
+    row, columns = None, None
+  return WinSize( rows, columns )
