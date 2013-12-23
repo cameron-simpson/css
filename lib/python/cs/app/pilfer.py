68856b89c75234f7e251e59ed96ad3009b8069c5
Cameron Simpson <cs@zip.com.au>
2013-12-23 16:54:55 +1100

    cs.app.pilfer: add missing import of cs.fileutils.mkdirn

--- lib/python/cs/app/pilfer.py
+++ lib/python/cs/app/pilfer.py
@@ -28,7 +28,7 @@ try:
 except ImportError:
   import xml.etree.ElementTree as ElementTree
 from cs.debug import thread_dump
-from cs.fileutils import file_property
+from cs.fileutils import file_property, mkdirn
 from cs.later import Later, FUNC_ONE_TO_ONE, FUNC_ONE_TO_MANY, FUNC_SELECTOR, FUNC_MANY_TO_MANY
 from cs.lex import get_identifier
 from cs.logutils import setup_logging, logTo, Pfx, debug, error, warning, exception, trace, pfx_iter, D
