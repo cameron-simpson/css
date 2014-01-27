af4d89ebf64987987fe8a2881149bb5a95addcba
Cameron Simpson <cs@zip.com.au>
2014-01-27 22:00:26 +1100

    cs.app.pilfer: main: beef up construction of main pipeline
    --HG--
    branch : pilfer

--- lib/python/cs/app/pilfer.py
+++ lib/python/cs/app/pilfer.py
@@ -163,7 +163,9 @@ def main(argv):
                 return P, URL(U, None, scope=P)
               pipe_funcs.insert(0, (FUNC_ONE_TO_ONE, add_scope))
               # construct the pipeline
-              inQ, outQ = L.pipeline(pipe_funcs, outQ=NullQueue(blocking=True, open=True))
+              inQ, outQ = L.pipeline(pipe_funcs, name="MAIN",
+                                     outQ=NullQueue(name="MAIN_PIPELINE_END_NQ",
+                                     blocking=True, open=True))
               if url != '-':
                 # literal URL supplied, deliver to pipeline
                 inQ.put(url)
