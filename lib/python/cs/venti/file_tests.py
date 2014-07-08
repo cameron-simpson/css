fa8400654622825efd0f1a89aae3d057b5be5ffc
Cameron Simpson <cs@zip.com.au>
2014-07-08 16:40:31 +1000

    cs.venti.file_tests: start on tests of BlockFile and File
    --HG--
    branch : venti

--- lib/python/cs/venti/file_tests.py
+++ lib/python/cs/venti/file_tests.py
@@ -7,6 +7,9 @@
 import sys
 import unittest
 from random import randint
+from . import defaults
+from .block import Block
+from .store import MappingStore
 from .file import BlockFile, File
 from cs.logutils import X
 
@@ -14,9 +17,18 @@ class TestAll(unittest.TestCase):
 
   def setUp(self):
     self.S = MappingStore({}, name='tesing MappingStore')
+    defaults.pushStore(self.S)
 
   def tearDown(self):
-    pass
+    defaults.popStore()
+
+  def testFile(self):
+    # use this unit test as test data
+    with open(__file__) as testfp:
+      test_text = testfp.read()
+    # an empty read-only file
+    BF = BlockFile(Block(data=b''))
+    self.assertEqual(b'', BF.read())
 
 def selftest(argv):
   unittest.main(__name__, None, argv, failfast=True)
