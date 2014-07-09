60cad890049e6f1c1cbcf2dc24e2ed7ee1ca2917
Cameron Simpson <cs@zip.com.au>
2014-07-09 21:00:28 +1000

    cs.venti.file_tests: also fetch the main test method from cs.fileutils.BackedFile_TestMethods
    --HG--
    branch : venti

--- lib/python/cs/venti/file_tests.py
+++ lib/python/cs/venti/file_tests.py
@@ -7,31 +7,35 @@
 import sys
 import unittest
 from random import randint
+from cs.fileutils import BackedFile, BackedFile_TestMethods
 from . import defaults
 from .block import Block
 from .store import MappingStore
 from .file import BlockFile, File
 from cs.logutils import X
 
-class TestAll(unittest.TestCase):
+class Test_RWFile(unittest.TestCase, BackedFile_TestMethods):
 
   def setUp(self):
-    self.S = MappingStore({}, name='tesing MappingStore')
+    self.store_dict = {}
+    self.S = MappingStore(self.store_dict, name='tesing MappingStore')
     defaults.pushStore(self.S)
+    # construct test backing block
+    with open(__file__, "rb") as fp:
+      self.backing_text = fp.read()
+    self.vt_block = Block(data=self.backing_text)
+    self.backed_fp = File(self.vt_block)
 
   def tearDown(self):
+    X("tearDown: Store=%r", self.store_dict)
+    self.backed_fp.close()
     defaults.popStore()
 
-  def testFile(self):
-    # use this unit test as test data
-    with open(__file__, "rb") as testfp:
-      test_text = testfp.read()
-    # an empty read-only file
-    BF = BlockFile(Block(data=b''))
-    self.assertEqual(b'', BF.read())
-    # an empty read/write file
-    F = File()
-    self.assertEqual(F.write(test_text), len(test_text))
+  def test_Sync(self):
+    self.test_BackedFile()
+    bfp = self.backed_fp
+    B2 = bfp.sync()
+    X("B2 = %s", B2)
 
 def selftest(argv):
   unittest.main(__name__, None, argv, failfast=True)
