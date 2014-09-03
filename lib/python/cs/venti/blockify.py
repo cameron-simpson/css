24bbd5a368ef52d4ddffbb8a445a7a5dbbb0a95e
Cameron Simpson <cs@zip.com.au>
2014-09-03 18:43:44 +1000

    cs.venti.blockify: remove unused imports
    --HG--
    branch : venti

--- lib/python/cs/venti/blockify.py
+++ lib/python/cs/venti/blockify.py
@@ -7,10 +7,7 @@
 
 from itertools import chain
 import sys
-from threading import Thread
-from cs.debug import ifdebug
 from cs.logutils import debug, D
-from cs.queues import IterableQueue
 from cs.venti import defaults
 from .block import Block, IndirectBlock, dump_block
 
