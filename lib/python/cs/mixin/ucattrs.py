5bd61d949a69f78cdd9762e617945d6aff6cb522
Cameron Simpson <cs@zip.com.au>
2015-01-10 15:42:37 +1100

    cs.mixin.ucattrs: distinfo for PyPI release
    --HG--
    branch : pypi

--- lib/python/cs/mixin/ucattrs.py
+++ lib/python/cs/mixin/ucattrs.py
@@ -1,6 +1,26 @@
 #!/usr/bin/python
 #
 
+DISTINFO = {
+    'description': "Mixin for .FOO uppercase attributes mapped to ['FOO'] access.",
+    'keywords': ["python2", "python3"],
+    'classifiers': [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        ],
+    'long_description': """\
+A mixin and a dict subclass where .FOO means ['FOO']
+----------------------------------------------------
+
+Presents:
+
+* WithUC_Attrs, a mixin for other classes which provides .__getattr__ and .__setattr__ which intercept .FOO where FOO would match the rexgep ``^[A-Z][_A-Z0-9]*$`` and maps them to ['FOO'].
+
+* UCdict, a subclass of dict using the WithUC_Attrs mixin.
+""",
+}
+
 class WithUC_Attrs(object):
   ''' An mixin where access to obj.FOO accesses obj['FOO']
       if FOO matches ^[A-Z][_A-Z0-9]*$.
