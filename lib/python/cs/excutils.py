e9be57930a1410ef5c8fc74ed8e5830476de8662
Cameron Simpson <cs@zip.com.au>
2015-01-10 14:18:29 +1100

    cs.excutils: fix up the distinfo a bit
    --HG--
    branch : pypi

--- lib/python/cs/excutils.py
+++ lib/python/cs/excutils.py
@@ -5,7 +5,7 @@
 #
 
 DISTINFO = {
-    'description': "Convenience facilities managing exceptions.",
+    'description': "Convenience facilities for managing exceptions.",
     'keywords': ["python2", "python3"],
     'classifiers': [
         "Programming Language :: Python",
@@ -13,24 +13,18 @@ DISTINFO = {
         "Programming Language :: Python :: 3",
         ],
     'long_description': """\
-Convenience facilities for objects.
------------------------------------
+Convenience facilities for managing exceptions.
+-----------------------------------------------
 
 Presents:
 
-* return_exc_info: call supplied function with arguments,
-    return either (function_result, None)
-    or (None, exc_info) if an exception was raised.
+* return_exc_info: call supplied function with arguments, return either (function_result, None) or (None, exc_info) if an exception was raised.
 
 * @returns_exc_info, a decorator for a function which wraps in it return_exc_info.
 
-* @noexc, a decorator for a function whose exceptions should never escape;
-    instead they are logged. The initial use case was inside logging functions,
-    where I have had a failed logging action abort a program.
-    Obviously this is a decorator which should see very little use.
+* @noexc, a decorator for a function whose exceptions should never escape; instead they are logged. The initial use case was inside logging functions, where I have had a failed logging action abort a program. Obviously this is a decorator which should see very little use.
 
-* @noexc_gen, a decorator for generators with similar effect to
-    @noexc for ordinary functions.
+* @noexc_gen, a decorator for generators with similar effect to @noexc for ordinary functions.
 """,
 }
 
