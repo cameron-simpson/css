af8c07aea4cbaf46798d603fabc75d4d0132f592
Cameron Simpson <cs@zip.com.au>
2015-01-11 08:31:38 +1100

    cs.py.stack: PyPI distinfo
    --HG--
    branch : pypi

--- lib/python/cs/py/stack.py
+++ lib/python/cs/py/stack.py
@@ -5,6 +5,31 @@
 #       - Cameron Simpson <cs@zip.com.au> 14apr2014
 #
 
+DISTINFO = {
+    'description': "Convenience functions for the python execution stack.",
+    'keywords': ["python2", "python3"],
+    'classifiers': [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        ],
+    'long_description': """\
+Convenience functions for the python execution stack.
+-----------------------------------------------------
+
+I find the supplied python traceback facilities quite awkward.
+These functions provide convenient facilities.
+
+Presented:
+
+* Frame, a nametuple for a stack frome with a nice __str__.
+
+* frames(), returning the current stack as a list of Frames.
+
+* caller(), returning the Frame of the caller's caller.
+"""
+}
+
 import sys
 from collections import namedtuple
 from traceback import extract_stack
