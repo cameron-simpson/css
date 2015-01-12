b281da39bc254193405d67c372405973fe440c53
Cameron Simpson <cs@zip.com.au>
2015-01-12 14:47:01 +1100

    cs.seq: move long_description into cs/README-seq.rst
    --HG--
    branch : pypi

--- lib/python/cs/seq.py
+++ lib/python/cs/seq.py
@@ -13,20 +13,6 @@ DISTINFO = {
         "Programming Language :: Python :: 3",
         ],
     'requires': ['cs.logutils', 'cs.py.stack'],
-    'long_description': """\
-Stuff to do with counters, sequences and iterables.
----------------------------------------------------
-
-Presents:
-
-* Seq, a class for thread safe sequential integer generation.
-
-* seq(), a function to return such a number from a default global Seq instance.
-
-* the(), first(), last(), get0(): return the only, first, last or optional-first element of an iterable respectively.
-
-* TrackingCounter, a counting object with facilities for users to wait for it to reach arbitrary values.
-"""
 }
 
 import heapq
