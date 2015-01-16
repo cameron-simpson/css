d68d0ef8b3cd952a83617ee97849314c8b8e8f88
Cameron Simpson <cs@zip.com.au>
2015-01-16 16:33:29 +1100

    cs.env: PyPI setup
    --HG--
    branch : pypi

--- lib/python/cs/env.py
+++ lib/python/cs/env.py
@@ -4,6 +4,17 @@
 #   - Cameron Simpson <cs@zip.com.au>
 #
 
+DISTINFO = {
+    'description': "a few environment related functions",
+    'keywords': ["python2", "python3"],
+    'classifiers': [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        ],
+    'requires': ['cs.lex'],
+}
+
 import os
 import string
 import types
