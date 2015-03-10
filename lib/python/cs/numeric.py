3d493f70ebeb8e4218082a621f5b14c49478c7d2
Cameron Simpson <cs@zip.com.au>
2015-03-10 12:36:51 +1100

    cs.numeric: an ad hoc assortment of numeric functions; in itially primes() and factorise()

---
+++ lib/python/cs/numeric.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+#
+# Ad hoc assortment of numeric functions.
+#   - Cameron Simpson <cs@zip.com.au> 10mar2015
+#
+
+def primes():
+  ''' Generator yielding the primes in order starting at 2.
+  '''
+  yield 2
+  known = [2]
+  n = 3
+  while True:
+    for k in known:
+      if k*k > n:
+        yield n
+        known.append(n)
+        break
+    n += 2
+
+def factorise(n):
+  ''' Generator yielding the prime factors of `n` in order from lowest to highest.
+  '''
+  for p in primes():
+    if p*p > n:
+      if n > 1:
+        yield n
+      break
+    while n % p == 0:
+      yield p
+      n //= p
