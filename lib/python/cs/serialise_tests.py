c7aea3aa15e37b17b0233fd91871141f69470fdb
Cameron Simpson <cs@zip.com.au>
2015-08-19 16:54:54 +1000

    cs.serialise_tests: update for python 2 and 3

--- lib/python/cs/serialise_tests.py
+++ lib/python/cs/serialise_tests.py
@@ -9,14 +9,24 @@ import random
 import sys
 import unittest
 from io import BytesIO
-from cs.logutils import X
 from cs.serialise import get_bs, read_bs, put_bs, \
                          get_bsdata, read_bsdata, put_bsdata, \
                          Packet, get_Packet
 from cs.py3 import bytes
 
 def randblock(size):
-  return bytes( random.randint(0, 255) for x in range(size) )
+  ''' Generate a pseudorandom chunk of bytes of the specified size.
+  '''
+  chunk = bytes( random.randint(0, 255) for x in range(size) )
+  if type(chunk) is not bytes:
+    raise RuntimeError("BANG2")
+  return chunk
+
+if sys.hexversion >= 0x03000000:
+  MyBytesIO = BytesIO
+else:
+  def MyBytesIO(bs):
+    return BytesIO(bs.as_str())
 
 class TestSerialise(unittest.TestCase):
 
@@ -41,38 +51,41 @@ class TestSerialise(unittest.TestCase):
     self.assertEqual(offset, len(data))
     # check that same value decoded as encoded
     self.assertEqual(n, n2)
-    fp = BytesIO(data)
+    fp = MyBytesIO(data)
     n2 = read_bs(fp)
     self.assertEqual(n, n2, "incorrect value read back from BytesIO(put_bs(%d)): %d" % (n, n2))
     tail = fp.read()
     self.assertEqual(len(tail), 0, "%d unparsed bytes from BytesIO(put_bs(%d))" % (len(tail), n))
 
   def test00bs(self):
-    self.assertRaises(IndexError, get_bs, '')
+    self.assertRaises(IndexError, get_bs, bytes(()))
     self.assertEqual(get_bs(bytes((0,))), (0, 1))
     for n in 1, 3, 7, 127, 128, 255, 256, 16383, 16384:
       self._test_roundtrip_bs(n)
 
   def _test_roundtrip_bsdata(self, chunk):
-    data = self._tested_put_bsdata(chunk)
+    data = put_bsdata(chunk)
     chunk2, offset = get_bsdata(data)
     # check that all encoded bytes consumed
     self.assertEqual(offset, len(data))
     # check that same chunk decoded as encoded
     self.assertEqual(chunk, chunk2)
-    fp = BytesIO(data)
+    fp = MyBytesIO(data)
     chunk2 = read_bsdata(fp)
     self.assertEqual(len(chunk), len(chunk2),
-                     "incorrect value read back from BytesIO(put_bsdata(%d bytes)): %d bytes" % (len(chunk), len(chunk2)))
-    self.assertEqual(chunk, chunk2, "incorrect value read back from BytesIO(put_bsdata(%r)): %r" % (chunk, chunk2))
+                     "incorrect value read back from MyBytesIO(put_bsdata(%d bytes)): %d bytes" % (len(chunk), len(chunk2)))
+    self.assertEqual(chunk, chunk2, "incorrect value read back from MyBytesIO(put_bsdata(%r)): %r" % (chunk, chunk2))
     tail = fp.read()
-    self.assertEqual(len(tail), 0, "%d unparsed bytes from BytesIO(put_bs(%d bytes))" % (len(tail), len(chunk)))
+    self.assertEqual(len(tail), 0, "%d unparsed bytes from MyBytesIO(put_bs(%d bytes))" % (len(tail), len(chunk)))
 
   def test01bsdata(self):
     self.assertEqual(get_bsdata(bytes( (0,) )), (bytes(()), 1))
     self.assertEqual(get_bsdata(bytes( (2, 0, 0) )), (bytes((0, 0)), 3))
     for n in 1, 3, 7, 127, 128, 255, 256, 16383, 16384:
-      self._test_roundtrip_bsdata(randblock(n))
+      chunk = randblock(n)
+      if type(chunk) is not bytes:
+        raise RuntimeError("type(chunk)=%s" % (type(chunk),))
+      self._test_roundtrip_bsdata(chunk)
 
   def test02Packet(self):
     ok = True
@@ -84,7 +97,6 @@ class TestSerialise(unittest.TestCase):
               payload = randblock(payload_length)
               P = Packet(channel=channel, tag=tag, is_request=is_request, flags=flags, payload=payload)
               data = P.serialise()
-              ##X("P.serialise(%s): %r", P, data)
               P2, offset = get_Packet(data)
               self.assertEqual(offset, len(data), "get_Packet(P.serialise(%s)): %d unparsed bytes: %r" % (P, len(data) - offset, data[offset:]))
               self.assertEqual(P, P2, "get_Packet(P.serialise(%s)) round trip fails" % (P,))
