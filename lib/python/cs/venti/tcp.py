212bd31ee5c5e572675e65c1696844d70cff19e7
Cameron Simpson <cs@zip.com.au>
2015-08-22 15:17:40 +1000

    cs.venti.tcp: update to modern StreamStore
    --HG--
    branch : venti

--- lib/python/cs/venti/tcp.py
+++ lib/python/cs/venti/tcp.py
@@ -7,15 +7,41 @@
 import os
 from socket import socket, SHUT_WR, SHUT_RD
 from socketserver import TCPServer, ThreadingMixIn, StreamRequestHandler
-from .stream import StreamDaemon, StreamStore
+from .stream import StreamStore
 from cs.logutils import debug
 from cs.queues import NestingOpenCloseMixin
+
+class OpenSock(object):
+  ''' A file-like object for stream sockets, which uses os.shutdown on close.
+  '''
+
+  def __init__(self, sock, for_write):
+    self._for_write = for_write
+    self._sock = sock
+    self._fd = os.dup(sock.fileno)
+    self._fp = os.fdopen(self._fd, 'wb' if for_write else 'rb')
+
+  def write(self, data):
+    return self._fp.write(data)
+
+  def read(self, size=None):
+    return self._fp.read(size)
+
+  def flush(self):
+    self._fp.flush()
+
+  def close(self):
+    if self._for_write:
+      os.shutdown(self._sock, os.SHUT_WR)
+    else:
+      os.shutdown(self._sock, os.SHUT_RD)
 
 class Server(ThreadingMixIn, TCPServer, NestingOpenCloseMixin):
   ''' A threading TCPServer that accepts connections by StreamStore clients.
   '''
-  def __init__(self, bindaddr, S):
-    ThreadingTCPServer.__init__(self, bindaddr, _RequestHandler)
+
+  def __init__(self, bind_addr, S):
+    ThreadingTCPServer.__init__(self, bind_addr, _RequestHandler)
     S.open()
     self.S = S
 
@@ -23,29 +49,33 @@ class Server(ThreadingMixIn, TCPServer, NestingOpenCloseMixin):
     self.S.close()
 
 class _RequestHandler(StreamRequestHandler):
+
   def __init__(self, request, client_address, server):
     self.S = server.S
     StreamRequestHandler.__init__(self, request, client_address, server)
 
   def handle(self):
-    SD = StreamDaemon(self.S, self.rfile, self.wfile)
-    SD.start()
-    debug("tcp.handle: waiting for StreamDaemon.resultsThread")
-    SD.join()
-    debug("tcp.handle: waited for StreamDaemon.resultsThread")
-    self.rfile.close()
-    self.wfile.close()
-    debug("tcp.handle: closed connections to client")
+    RS = StreamStore(str(self.S),
+                     OpenSock(self.request, False),
+                     OpenSock(self.request, True),
+                     local_store=self.S,
+                    )
+    RS.join()
+    RS.shutdown()
 
 class TCPStore(StreamStore):
-  ''' A Store attached to a StreamDaemon served on the specified 'bindaddr'.
+  ''' A Store attached to a remote Store at `bind_addr`.
   '''
-  def __init__(self, bindaddr):
+
+  def __init__(self, bind_addr):
     self.sock = socket()
-    self.sock.connect(bindaddr)
-    self.fd = self.sock.fileno()
-    self.fd2 = os.dup(self.fd)
+    self.sock.connect(bind_addr)
     StreamStore.__init__(self,
-                         "TCPStore(%s)"%(bindaddr,),
-                         os.fdopen(self.fd, 'wb'),
-                         os.fdopen(self.fd2, 'rb'))
+                         "TCPStore(%s)" % (bind_addr,),
+                         OpenSock(self.sock, False),
+                         OpenSock(self.sock, True),
+                        )
+
+  def shutdown(self):
+    StreamStore.shutdown(self)
+    self.sock.close()
