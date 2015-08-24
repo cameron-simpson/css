1ab6e00c02772e1c4e5df4015f6abfa3f33a365c
Cameron Simpson <cs@zip.com.au>
2015-08-25 09:51:21 +1000

    cs.venti.block_tests: pull basic Block tests into test00Block
    --HG--
    branch : venti

--- lib/python/cs/venti/block_tests.py
+++ lib/python/cs/venti/block_tests.py
@@ -7,23 +7,35 @@
 import sys
 import unittest
 from cs.logutils import D, X
+from cs.randutils import rand0, randblock
 from . import totext
 from .block import Block, IndirectBlock
+from .cache import MemoryCacheStore
 
 class TestAll(unittest.TestCase):
 
   def setUp(self):
-    import random
-    random.seed()
+    self.S = MemoryCacheStore()
+
+  def test00Block(self):
+    # make some randbom blocks, check size and content
+    with self.S:
+      for _ in range(10):
+        size = rand0(16384)
+        rs = randblock(size)
+        self.assertEqual(len(rs), size)
+        B = Block(data=rs)
+        self.assertEqual(len(B), size)
+        self.assertEqual(B.span, size)
+        self.assertEqual(B.data, rs)
+        self.assertEqual(B.all_data(), rs)
 
   def testSHA1(self):
-    import random
-    from .cache import MemoryCacheStore
-    S = MemoryCacheStore()
+    S = self.S
     with S:
       subblocks = []
       for i in range(10):
-        rs = bytes( random.randint(0, 255) for x in range(100) )
+        rs = randblock(100)
         self.assertEqual(len(rs), 100)
         B = Block(data=rs)
         self.assertEqual(len(B), 100)
