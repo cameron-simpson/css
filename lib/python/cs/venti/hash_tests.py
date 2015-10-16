16e0f4f6ed0ebf9eb4e356e894023e6f29ad4fe8
Cameron Simpson <cs@zip.com.au>
2015-10-17 09:36:25 +1100

    cs.venti.hash_tests: compare _Hash bytes against raw hash function bytes; hash encode/decode round trip: use main decode function, not _Hash._decode
    --HG--
    branch : venti

--- lib/python/cs/venti/hash_tests.py
+++ lib/python/cs/venti/hash_tests.py
@@ -23,9 +23,10 @@ class TestAll(unittest.TestCase):
     for _ in range(10):
       rs = bytes( random.randint(0, 255) for _ in range(100) )
       H = Hash_SHA1.from_data(rs)
-      self.assertEqual( sha1(rs).digest(), H )
+      self.assertEqual( sha1(rs).digest(), bytes(H) )
+      # bytes(hash_num + hash_bytes)
       Hencode = H.encode()
-      H2, offset = Hash_SHA1.decode(Hencode)
+      H2, offset = decode(Hencode)
       self.assertEqual(offset, len(Hencode))
       self.assertEqual(H, H2)
 
