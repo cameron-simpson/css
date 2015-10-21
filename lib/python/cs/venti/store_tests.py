aa6242bb3c98d4fcecfbdb758fe91ef3bbb6d6bd
Cameron Simpson <cs@zip.com.au>
2015-10-22 09:57:52 +1100

    cs.venti.store_tests: provide assertLen test method accomodating NotImplemenetedError, test length of Stores more frequently, import test01add_new_block
    --HG--
    branch : venti

--- lib/python/cs/venti/store_tests.py
+++ lib/python/cs/venti/store_tests.py
@@ -12,7 +12,7 @@ import sys
 import tempfile
 import unittest
 from cs.excutils import logexc
-from cs.logutils import X
+from cs.logutils import setup_logging, warning, X
 from cs.randutils import rand0, randblock
 from .datafile import GDBMIndex, KyotoIndex
 from .store import MappingStore, DataDirStore
@@ -29,28 +29,42 @@ class _TestStore(unittest.TestCase):
   def tearDown(self):
     self.S.close()
 
+  def assertLen(self, o, length, *a, **kw):
+    try:
+      olen = len(o)
+    except NotImplementedError as e:
+      warning("skip test of len(%s) == %r: %s", o, length, e)
+    else:
+      self.assertEqual(olen, length, *a, **kw)
+
   def test00empty(self):
     S = self.S
-    try:
-      store_len = len(S)
-    except NotImplementedError as e:
-      raise unittest.SkipTest("len(%s) not implemented: %s" % (S, e))
-    else:
-      self.assertEqual(len(S), 0)
+    self.assertLen(S, 0)
     S.flush()
 
   def test01add_new_block(self):
     S = self.S
+    self.assertLen(S, 0)
     size = random.randint(127, 16384)
     data = randblock(size)
+    # compute blakc hash but do not store
     h = S.hash(data)
+    self.assertLen(S, 0)
     ok = S.contains(h)
     self.assertFalse(ok)
     self.assertNotIn(h, S)
+    # now add the block
+    h2 = S.add(data)
+    self.assertEqual(h, h2)
+    self.assertLen(S, 1)
+    ok = S.contains(h)
+    self.assertTrue(ok)
+    self.assertIn(h, S)
     S.flush()
 
   def test02add_get(self):
     S = self.S
+    self.assertLen(S, 0)
     random_chunk_map = {}
     for _ in range(16):
       size = random.randint(127, 16384)
@@ -59,6 +73,7 @@ class _TestStore(unittest.TestCase):
       h2 = S.add(data)
       self.assertEqual(h, h2)
       random_chunk_map[h] = data
+    self.assertLen(S, 16)
     for h in random_chunk_map:
       chunk = S.get(h)
       self.assertIsNot(chunk, None)
@@ -67,10 +82,13 @@ class _TestStore(unittest.TestCase):
 
   def test03first(self):
     S = self.S
+    self.assertLen(S, 0)
     try:
       first_hashcode = S.first()
     except NotImplementedError as e:
       raise unittest.SkipTest("no .first in %s: %s" % (S, e))
+    else:
+      self.assertIs(first_hashcode, None, ".first of empty Store should be None")
     random_chunk_map = {}
     for _ in range(16):
       size = random.randint(127, 16384)
@@ -79,8 +97,10 @@ class _TestStore(unittest.TestCase):
       h2 = S.add(data)
       self.assertEqual(h, h2)
       random_chunk_map[h] = data
+    self.assertLen(S, 16)
     ordered_hashcodes = sorted(random_chunk_map.keys())
     first_hashcode = S.first()
+    self.assertIsNot(first_hashcode, None, ".first of nonempty Store should not be None")
     self.assertEqual(first_hashcode, ordered_hashcodes[0])
 
 class TestMappingStore(_TestStore):
@@ -115,4 +135,5 @@ def selftest(argv):
   unittest.main(__name__, None, argv)
 
 if __name__ == '__main__':
+  setup_logging(__file__)
   selftest(sys.argv)
