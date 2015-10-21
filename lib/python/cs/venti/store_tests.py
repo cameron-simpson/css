0cd511eaf758b7997a6d4e415772d580d00f7b0c
Cameron Simpson <cs@zip.com.au>
2015-10-21 15:44:32 +1100

    cs.venti.store_tests: new _TestStore.test03first; add TestDataDirStoreGDBM and TestDataDirStoreKyoto
    --HG--
    branch : venti

--- lib/python/cs/venti/store_tests.py
+++ lib/python/cs/venti/store_tests.py
@@ -4,13 +4,18 @@
 #       - Cameron Simpson <cs@zip.com.au>
 #
 
+import os
+from os.path import abspath
 import random
+import shutil
 import sys
+import tempfile
 import unittest
 from cs.excutils import logexc
 from cs.logutils import X
 from cs.randutils import rand0, randblock
-from .store import MappingStore
+from .datafile import GDBMIndex, KyotoIndex
+from .store import MappingStore, DataDirStore
 
 class _TestStore(unittest.TestCase):
 
@@ -26,10 +31,17 @@ class _TestStore(unittest.TestCase):
 
   def test00empty(self):
     S = self.S
-    if hasattr(S, '__len__'):
-      self.assertEqual(len(S), 0)
+    try:
+      len_method = self.__len__
+    except AttributeError as e:
+      raise unittest.SkipTest("no __len__ in %s: %s" % (type(S), e))
     else:
-      raise unittest.SkipTest("no __len__ in %s" % (type(S),))
+      try:
+        store_len = len(S)
+      except TypeError as e:
+        raise unittest.SkipTest(".len not implemented: %s" % (e,))
+      else:
+        self.assertEqual(len(S), 0)
     S.flush()
 
   def test01add_new_block(self):
@@ -58,10 +70,51 @@ class _TestStore(unittest.TestCase):
       self.assertEqual(chunk, random_chunk_map[h])
     S.flush()
 
+  def test03first(self):
+    S = self.S
+    try:
+      first_hashcode = S.first()
+    except NotImplementedError as e:
+      raise unittest.SkipTest("no .first in %s: %s" % (S, e))
+    random_chunk_map = {}
+    for _ in range(16):
+      size = random.randint(127, 16384)
+      data = randblock(size)
+      h = S.hash(data)
+      h2 = S.add(data)
+      self.assertEqual(h, h2)
+      random_chunk_map[h] = data
+    ordered_hashcodes = sorted(random_chunk_map.keys())
+    first_hashcode = S.first()
+    self.assertEqual(first_hashcode, ordered_hashcodes[0])
+
 class TestMappingStore(_TestStore):
 
   def _init_Store(self):
     self.S = MappingStore({}).open()
+
+class _TestDataDirStore(_TestStore):
+
+  INDEX_CLASS = None
+
+  def _init_Store(self):
+    indexclass = self.__class__.INDEX_CLASS
+    if indexclass is None:
+      raise unittest.SkipTest("INDEX_CLASS is None, skipping TestCase")
+    random.seed()
+    self.pathname = abspath(tempfile.mkdtemp(prefix="cs.venti.store.testdatadir", suffix=".dir", dir='.'))
+    self.S = DataDirStore(self.pathname, indexclass=indexclass, rollover=200000)
+
+  def tearDown(self):
+    os.system("ls -l "+self.pathname)
+    shutil.rmtree(self.pathname)
+    _TestStore.tearDown(self)
+
+class TestDataDirStoreGDBM(_TestDataDirStore):
+  INDEX_CLASS = GDBMIndex
+
+class TestDataDirStoreKyoto(_TestDataDirStore):
+  INDEX_CLASS = KyotoIndex
 
 def selftest(argv):
   unittest.main(__name__, None, argv)
