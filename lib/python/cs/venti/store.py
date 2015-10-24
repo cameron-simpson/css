4cb28f16e446a8845cbdaba9acb0ff4ce523ab74
Cameron Simpson <cs@zip.com.au>
2015-10-25 10:45:28 +1100

    cs.venti.store: full parameter lists for BasicStoreSync.hashcodes_bg and BasicStoreAsync.hashcodes
    --HG--
    branch : venti

--- lib/python/cs/venti/store.py
+++ lib/python/cs/venti/store.py
@@ -213,8 +213,8 @@ class BasicStoreSync(_BasicStoreCommon):
   def first_bg(self, hashclass=None):
     return self._defer(self.first, hashclass)
 
-  def hashcodes_bg(self, hashcode, length):
-    return self._defer(self.hashcodes, hashcode, length)
+  def hashcodes_bg(self, hashclass=None, hashcode=None, reverse=None, after=False, length=None):
+    return self._defer(self.hashcodes_bg, hashclass=hashclass, hashcode=hashcode, reverse=reverse, after=after, length=length)
 
 class BasicStoreAsync(_BasicStoreCommon):
   ''' Subclass of _BasicStoreCommon expecting asynchronous operations and providing synchronous hooks, dual of BasicStoreSync.
@@ -239,8 +239,8 @@ class BasicStoreAsync(_BasicStoreCommon):
   def first(self, hashclass=None):
     return self.first_bg(hashclass=hashclass)()
 
-  def hashcodes(self, hashcode, length):
-    return self.hashcodes_bg(self.hashcodes, hashcode, length)()
+  def hashcodes(self, hashclass=None, hashcode=None, reverse=None, after=False, length=None):
+    return self.hashcodes_bg(hashclass=hashclass, hashcode=hashcode, reverse=reverse, after=after, length=length)()
 
 def Store(store_spec):
   ''' Factory function to return an appropriate BasicStore* subclass
