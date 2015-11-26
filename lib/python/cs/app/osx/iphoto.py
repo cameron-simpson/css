7e0b8c3c6dd7e24295f6922c41fa22ddbf73dd3d
Cameron Simpson <cs@zip.com.au>
2015-11-27 10:24:47 +1100

    cs.app.osx.iphoto: add mixins for versions and keywords, add various mixin convenience methods, redo the load_* methods, add iPhoto._load_all method and call it from __init__, add for autonames to iPhoto.__getattr__
    --HG--
    branch : iphoto

--- lib/python/cs/app/osx/iphoto.py
+++ lib/python/cs/app/osx/iphoto.py
@@ -53,7 +53,7 @@ def main(argv=None):
           obclass = argv.pop(0)
           with Pfx(obclass):
             if obclass == 'albums':
-              names = [ A.name for A in I.albums ]
+              names = [ A.name for A in I.album_names() ]
             else:
               warning("unknown class %r", obclass)
               badopts = True
@@ -74,8 +74,6 @@ def main(argv=None):
   return xit
 
 def test(argv, I):
-  for album in I.albums.read_rows():
-    print('uuid =', album.uuid, 'albumType =', album.albumType, 'name =', album.name)
   ##for folder in I.read_folders():
   ##  print('uuid =', folder.uuid, 'folderType =', folder.folderType, 'name =', folder.name)
   ##for keyword in I.read_keywords():
@@ -86,6 +84,11 @@ def test(argv, I):
   ##  print('uuid =', v.uuid, 'masterUuid =', v.masterUuid, 'versionNumber =', v.versionNumber)
   ##for kwv in I.keywords_to_versions():
   ##  print('keywordId =', kwv.keywordId, 'versionId =', kwv.versionId)
+  ##for album in I.album_table.read_rows():
+  ##  print('uuid =', album.uuid, 'albumType =', album.albumType, 'name =', album.name)
+  for kwname in sorted(I.keyword_names()):
+    kw = I.keyword_by_name.get(kwname)
+    print(kw.name, [ master.name for master in kw.masters() ])
 
 class iPhoto(O):
 
@@ -101,7 +104,15 @@ class iPhoto(O):
     self.table_by_nickname = {}
     self._lock = RLock()
     self.dbs = iPhotoDBs(self)
+    self._load_all()
+
+  def _load_all(self):
     self.dbs._load_all()
+    self.load_albums()
+    self.load_masters()
+    self.load_keywords()
+    self.load_versions()
+    self.load_keywordForVersions()
 
   def pathto(self, rpath):
     if rpath.startswith('/'):
@@ -115,29 +126,98 @@ class iPhoto(O):
     return self.dbs.pathto(dbname)
 
   def __getattr__(self, attr):
-    # read_*s ==> iterator of rows from table "*"
-    if attr.startswith('read_') and attr.endswith('s'):
-      nickname = attr[5:-1]
-      return self.table_by_nickname[nickname].read_rows
-    # *s ==> table "*"
-    if attr.endswith('s') and attr[:-1] in self.table_by_nickname:
-      return self.table_by_nickname[attr[:-1]]
+    if not attr.startswith('_'):
+      if attr.endswith('s'):
+        if '_' not in attr:
+          # *s ==> iterable of * (obtained from *_by_id)
+          nickname = attr[:-1]
+          by_id = getattr(self, nickname + '_by_id', None)
+          if by_id is not None:
+            return by_id.values()
+        # read_*s ==> iterator of rows from table "*"
+        if attr.startswith('read_'):
+          nickname = attr[5:-1]
+          return self.table_by_nickname[nickname].read_rows
+      elif attr.endswith('_table'):
+        # *_table ==> table "*"
+        nickname = attr[:-6]
+        if nickname in self.table_by_nickname:
+          return self.table_by_nickname[nickname]
     raise AttributeError(attr)
 
-  def folders(self):
-    return list(self.dbs.Library.table_rows('RKFolder'))
+  def load_albums(self):
+    ''' Load Library.RKMaster into memory and set up mappings.
+    '''
+    XP("load_albums...")
+    by_id = self.album_by_id = {}
+    by_uuid = self.album_by_uuid = {}
+    for album in self.read_albums():
+      by_id[album.modelId] = album
+      by_uuid[album.uuid] = album
+
+  def load_masters(self):
+    ''' Load Library.RKMaster into memory and set up mappings.
+    '''
+    XP("load_masters...")
+    by_id = self.master_by_id = {}
+    by_uuid = self.master_by_uuid = {}
+    for master in self.read_masters():
+      by_id[master.modelId] = master
+      by_uuid[master.uuid] = master
+
+  def load_versions(self):
+    ''' Load Library.RKVersion into memory and set up mappings.
+    '''
+    XP("load_versions...")
+    by_id = self.version_by_id = {}
+    by_uuid = self.version_by_uuid = {}
+    master_by_id = self.master_by_versionId = {}
+    for version in self.read_versions():
+      by_id[version.modelId] = version
+      by_uuid[version.uuid] = version
+      master_by_id[version.modelId] = self.master_by_id[version.masterId]
+
+  def load_keywords(self):
+    ''' Load Library.RKKeyword into memory and set up mappings.
+    '''
+    XP("load_keywords...")
+    by_id = self.keyword_by_id = {}
+    by_uuid = self.keyword_by_uuid = {}
+    by_name = self.keyword_by_name = {}
+    for kw in self.read_keywords():
+      by_id[kw.modelId] = kw
+      by_uuid[kw.uuid] = kw
+      by_name[kw.name] = kw
+
+  def keyword_names(self):
+    return self.keyword_by_name.keys()
 
   def keywords(self):
-    return list(self.dbs.Library.table_rows('RKKeyword'))
+    return self.keyword_by_id.values()
 
-  def masters(self):
-    return list(self.dbs.Library.table_rows('RKMaster'))
-
-  def versions(self):
-    return list(self.dbs.Library.table_rows('RKVersion'))
-
-  def keywords_to_versions(self):
-    return list(self.dbs.Library.table_rows('RKKeywordForVersion'))
+  def load_keywordForVersions(self):
+    ''' Load Library.RKKeywordForVersion into memory and set up mappings.
+    '''
+    XP("load_keywordForVersions...")
+    by_kwid = self.versions_by_keywordId = {}
+    by_vid = self.keywords_by_versionId = {}
+    for kw4v in self.read_keywordForVersions():
+      vid = kw4v.versionId
+      version = self.version_by_id[vid]
+      kwid = kw4v.keywordId
+      kw = self.keyword_by_id[kwid]
+      # mapping from keywordId to versions
+      if kwid not in by_kwid:
+        vs = by_kwid[kwid] = set()
+      else:
+        vs = by_kwid[kwid]
+      vs.add(version)
+      # mapping from version id to keywords
+      if vid not in by_vid:
+        kws = by_vid[vid] = set()
+      else:
+        kws = by_vid[vid]
+      kws.add(kw)
 
 class iPhotoDBs(object):
 
@@ -173,6 +253,7 @@ class iPhotoDBs(object):
     return conn
 
   def _load_db(self, dbname):
+    XP("_load_bd(%r)", dbname)
     db = iPhotoDB(self.iphoto, dbname)
     self.dbmap[dbname] = db
     return db
@@ -253,6 +334,43 @@ class Master_Mixin(object):
   def pathname(self):
     return os.path.join(self.I.pathto('Masters'), self.imagePath)
 
+  def versions(self):
+    ''' Iterable of all versions of this master.
+    '''
+    return self.I.versions_by_masterId.get(self.modelId, ())
+
+  def latest_version(self):
+    vs = self.versions()
+    if not vs:
+      return None
+    return max(vs, key=lambda v: v.versionNumber)
+
+class Version_Mixin(object):
+
+  @property
+  def master(self):
+    return self.I.master_by_id[self.masterId]
+
+class Keyword_Mixin(object):
+
+  def versions(self):
+    ''' Return the versions with this keyword.
+    '''
+    return self.I.versions_by_keywordId.get(self.modelId, ())
+
+  def masters(self):
+    ''' Return the masters with this keyword.
+    '''
+    ms = set()
+    for version in self.versions():
+      ms.add(self.I.master_by_versionId[version.modelId])
+    return ms
+
+  def latest_versions(self):
+    ''' Return the latest version of all masters with this keyword.
+    '''
+    return set(master.latest_version for master in self.masters())
+
 SCHEMAE = {'Library':
             { 'master':
                 { 'table_name': 'RKMaster',
@@ -285,6 +403,7 @@ SCHEMAE = {'Library':
                 },
               'keyword':
                 { 'table_name': 'RKKeyword',
+                  'mixin': Keyword_Mixin,
                   'columns':
                     ( 'modelId', 'uuid', 'name', 'searchName', 'parentId',
                       'hasChildren', 'shortcut',
@@ -305,6 +424,7 @@ SCHEMAE = {'Library':
                 },
               'version':
                 { 'table_name': 'RKVersion',
+                  'mixin': Version_Mixin,
                   'columns':
                     ( 'modelId', 'uuid', 'name', 'fileName',
                       'versionNumber', 'stackUuid', 'masterUuid',
