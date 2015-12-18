434a1ed329c5c015927b006d2f328cb78b1f1656
Cameron Simpson <cs@zip.com.au>
2015-12-18 19:03:32 +1100

    cs.venti.pushpull.py: initial implementation of missing_hashcodes by scanning hashocdes of both Stores; UNTESTED
    --HG--
    branch : venti

--- lib/python/cs/venti/pushpull.py
+++ lib/python/cs/venti/pushpull.py
@@ -37,3 +37,43 @@ def pull_hashcodes(S1, S2, hashcodes):
       # to that we can run this indefinitely
       LF = fetching[hashcode] = S2._defer(pull_hashcode, S1, S2, hashcode)
       yield hashcode, LF
+
+def missing_hashcodes(S1, S2):
+  ''' Scan Stores `S1` and `S2` and yield hashcodes in `S2` but not in `S1`.
+      This relies on both Stores supporting the .hashcodes method;
+      dumb unordered Stores do not.
+  '''
+  rq_size = 1024
+  hashcodes2 = list(S2.hashcodes(length=rq_size))
+  while hashcodes2:
+    # note end of S2 window so that we can fetch the next window
+    last_hashcode2 = hashcodes2[-1]
+    # test each S2 hashcode for presence in S1
+    for ndx, hashcode in enumerate(hascodes2):
+      if hashcode > last_hashcode1:
+        # past the end of S1 hashcode window
+        # fetch new window starting at current hashcode
+        hashcodes1 = list(S1.hashcodes(hashcode=hashcode, length=rq_size))
+        if not hashcodes1:
+          # no more S1 hashcodes, cease scan
+          # keep the unscanned hashcodes (including current)
+          # to yield in the post-scan loop, where we don't bother consulting S1
+          hashcodes2 = hashcodes2[ndx:]
+          break
+        # note highest hashcode as top of window
+        last_hashcode1 = hashcodes1[-1]
+        # convert to set for fast membership lookup
+        hashcodes1 = set(hashcodes1)
+      if hashcode not in hashcodes1:
+        yield hashcode
+    # fetch next batch of hashcodes from S2
+    hashcodes2 = list(S2.hashcodes(hashcode=last_hashcode2, length=rq_size, after=True))
+  # no more hashcodes in S1 - gather everything else in S2
+  while True:
+    hashcode = None
+    for hashcode in hashcodes2:
+      yield hashcode
+    if hashcode is None:
+      break
+    # fetch next bunch of hashcodes
+    hashcodes2 = S2.hashcodes(hashcode=hashcode, length=rq_size, after=True)
