38dc351dcd60c403dafbba23b4ea041217df8c6c
Cameron Simpson <cs@zip.com.au>
2016-02-05 09:54:14 +1100

    cs.py.stack: _Frame: rename .functionname to .funcname; caller: turn raw frames into Frames
    --HG--
    branch : pilfer

--- lib/python/cs/py/stack.py
+++ lib/python/cs/py/stack.py
@@ -19,7 +19,7 @@ import sys
 from collections import namedtuple
 from traceback import extract_stack
 
-_Frame = namedtuple('Frame', 'filename lineno functionname linetext')
+_Frame = namedtuple('Frame', 'filename lineno funcname linetext')
 
 class Frame(_Frame):
   def __str__(self):
@@ -36,7 +36,7 @@ def caller():
   # -1: caller, this function
   # -2: invoker, who wants to know the caller
   # -3: the calling function of the invoker
-  return frames()[-3]
+  return Frame(*frames()[-3])
 
 def stack_dump(fp=None, indent=0):
   ''' Recite current stack to `fp`, default sys.stderr.
