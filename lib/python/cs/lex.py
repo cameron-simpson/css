b1c9e5d91c9ad5000070f026f1bba82382508227
Cameron Simpson <cs@zip.com.au>
2016-02-15 12:03:57 +1100

    cs.lex: relayout docstrings
    --HG--
    branch : pilfer

--- lib/python/cs/lex.py
+++ lib/python/cs/lex.py
@@ -269,23 +269,19 @@ def get_chars(s, offset, gochars):
   return s[ooffset:offset], offset
 
 def get_white(s, offset=0):
-  ''' Scan the string `s` for characters in string.whitespace starting at
-      `offset` (default 0).
+  ''' Scan the string `s` for characters in string.whitespace starting at `offset` (default 0).
       Return (match, new_offset).
   '''
   return get_chars(s, offset, whitespace)
 
 def get_nonwhite(s, offset=0):
-  ''' Scan the string `s` for characters not in string.whitespace starting at
-      `offset` (default 0).
+  ''' Scan the string `s` for characters not in string.whitespace starting at `offset` (default 0).
       Return (match, new_offset).
   '''
   return get_other_chars(s, offset=offset, stopchars=whitespace)
 
 def get_identifier(s, offset=0, alpha=ascii_letters, number=digits, extras='_'):
-  ''' Scan the string `s` for an identifier (by default an ASCII
-      letter or underscore followed by letters, digits or underscores)
-      starting at `offset` (default 0).
+  ''' Scan the string `s` for an identifier (by default an ASCII letter or underscore followed by letters, digits or underscores) starting at `offset` (default 0).
       Return (match, new_offset).
       The empty string and an unchanged offset will be returned if
       there is no leading letter/underscore.
@@ -310,10 +306,7 @@ def get_uc_identifier(s, offset=0, number=digits, extras='_'):
   return get_identifier(s, offset=offset, alpha=ascii_uppercase, number=number, extras=extras)
 
 def get_dotted_identifier(s, offset=0, **kw):
-  ''' Scan the string `s` for a dotted identifier (by default an ASCII
-      letter or underscore followed by letters, digits or underscores)
-      with optional trailing dot and another dotted identifier,
-      starting at `offset` (default 0).
+  ''' Scan the string `s` for a dotted identifier (by default an ASCII letter or underscore followed by letters, digits or underscores) with optional trailing dot and another dotted identifier, starting at `offset` (default 0).
       Return (match, new_offset).
       The empty string and an unchanged offset will be returned if
       there is no leading letter/underscore.
