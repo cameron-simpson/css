5f5cd2ee0593977e5a41ae445bb4659d3c5ca0f1
Cameron Simpson <cs@zip.com.au>
2016-04-27 07:04:14 +1000

    cs.venti.block_tests: test02RoundTripSingleBlock: test contents of decoded Block, wrap runs in subTest
    --HG--
    branch : venti

--- lib/python/cs/venti/block_tests.py
+++ lib/python/cs/venti/block_tests.py
@@ -10,7 +10,10 @@ from cs.logutils import D, X
 from cs.randutils import rand0, randblock
 from cs.py3 import bytes
 from . import totext
-from .block import Block, IndirectBlock, verify_block
+from .block import Block, IndirectBlock, RLEBlock, LiteralBlock, \
+                verify_block, \
+                BT_HASHREF, BT_RLE, BT_LITERAL, \
+                encodeBlock, decodeBlock
 from .cache import MemoryCacheStore
 
 class TestAll(unittest.TestCase):
@@ -63,6 +66,45 @@ class TestAll(unittest.TestCase):
         self._verify_block(IB2, recurse=True)
         IB2data = IB2.all_data()
         self.assertEqual(IBdata, IB2data, "IB:  %s\nIB2: %s" % (totext(IBdata), totext(IB2data)))
+
+  def test02RoundTripSingleBlock(self):
+    # TODO: round trip indirect blocks?
+    S = self.S
+    with S:
+      for block_type in BT_HASHREF, BT_RLE, BT_LITERAL:
+        size = rand0(16384)
+        with self.subTest(type=block_type, size=size):
+          if block_type == BT_HASHREF:
+            rs = randblock(size)
+            B = Block(data=rs)
+          elif block_type == BT_RLE:
+            rb = bytes((rand0(256),))
+            B = RLEBlock(size, rb)
+          elif block_type == BT_LITERAL:
+            rs = randblock(size)
+            B = LiteralBlock(data=rs)
+          else:
+            raise ValueError("unknow block type %r" % (block_type,))
+          if B.type != block_type:
+            raise RuntimeError("new Block is wrong type: %r, short be %r" % (B.type, block_type,))
+          self._verify_block(B)
+          Bserial = encodeBlock(B)
+          B2, offset = decodeBlock(Bserial, 0)
+          self.assertEqual(offset, len(Bserial), "decoded %d bytes but len(Bserial)=%d" % (offset, len(Bserial)))
+          self._verify_block(B2)
+          self.assertEqual(B.type, B2.type, "block types differ")
+          self.assertEqual(B.indirect, B2.indirect, "block indirects differ")
+          self.assertEqual(B.span, B2.span, "span lengths differ")
+          self.assertEqual(B.data, B2.data, "spanned data differ")
+          if block_type == BT_HASHREF:
+            self.assertEqual(B.hashcode, B2.hashcode)
+          elif block_type == BT_RLE:
+            self.assertFalse(B2.indirect)
+            self.assertEqual(B2.data, B2.octet * B2.span)
+          elif block_type == BT_LITERAL:
+            self.assertFalse(B2.indirect)
+          else:
+            raise RuntimeError("no type specific tests for Block type %r" % (block_type,))
 
 def selftest(argv):
   unittest.main(__name__, None, argv)
