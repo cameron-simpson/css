20ecc5df5e30646065d804a7f7c5591569e89a46
Cameron Simpson <cs@zip.com.au>
2016-07-14 09:40:46 +1000

    cs.venti.datafile_tests: TestDataDir.test002randomblocks: use DataDir.add() instead of DataDir[...], no .__setitem__
    --HG--
    branch : venti

--- lib/python/cs/venti/datafile_tests.py
+++ lib/python/cs/venti/datafile_tests.py
@@ -167,7 +167,7 @@ class TestDataDir(unittest.TestCase):
           # store block/hashcode
           by_hash[hashcode] = data
           by_data[data] = hashcode
-          D[hashcode] = data
+          D.add(data)
           # test integrity afterwards
           self.assertTrue(hashcode in by_hash)
           self.assertTrue(data in by_data)
