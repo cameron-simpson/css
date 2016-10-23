c1ca46fff13fa72a44fa55edc9907136692d916e
Cameron Simpson <cs@zip.com.au>
2016-10-23 14:34:30 +1100

    cs.app.virtualbox: mkimg command and function to produce RAW image from VDI; (untested) VBoxManage clause parser

--- lib/python/cs/app/virtualbox.py
+++ lib/python/cs/app/virtualbox.py
@@ -15,6 +15,7 @@ from cs.cmdutils import run
 from cs.logutils import setup_logging, warning, Pfx
 
 USAGE = r'''Usage:
+  %s mkimg {path.vdi|uuid} [VBoxManage clonemedium options...]
   %s mkvdi img [VBoxManage convertfromraw options...]
   %s pause vmname [VBoxManage controlvm options...]
   %s resume vmname [VBoxManage controlvm options...]
@@ -25,7 +26,7 @@ VBOXMANAGE = 'VBoxManage'
 
 def main(argv):
   cmd = basename(argv.pop(0))
-  usage = USAGE % (cmd, cmd, cmd, cmd, cmd)
+  usage = USAGE % (cmd, cmd, cmd, cmd, cmd, cmd)
   setup_logging(cmd)
   badopts = False
   if not argv:
@@ -34,7 +35,11 @@ def main(argv):
   else:
     op = argv.pop(0)
     with Pfx(op):
-      if op == 'mkvdi':
+      if op == 'mkimg':
+        if not argv:
+          warning("missing VDI")
+          badopts = True
+      elif op == 'mkvdi':
         if not argv:
           warning("missing img")
           badopts = True
@@ -49,6 +54,7 @@ def main(argv):
     print(usage, file=sys.stderr)
     return 2
   with Pfx(op):
+    if op == "mkimg":   return cmd_mkimg(argv)
     if op == "mkvdi":   return cmd_mkvdi(argv)
     if op == "pause":   return cmd_pause(argv)
     if op == "resume":  return cmd_resume(argv)
@@ -63,14 +69,38 @@ def cmd_mkvdi(argv):
     vdipath = imgpfx + '.vdi'
   else:
     vdipath = imgpath + '.vdi'
-  return mkvdi(imgpath, vdipath, trace=True)
+  try:
+    return mkvdi(imgpath, vdipath, argv, trace=True)
+  except ValueError as e:
+    error("mkvdi fails: %s", e)
+    return 1
 
-def mkvdi(srcimg, dstvdi, trace=False):
-  ''' Create VDI image `dstvdi` from source raw image `srcimg`. Return VBoxManage exit code.
+def mkvdi(srcimg, dstvdi, argv, trace=False):
+  ''' Create VDI image `dstvdi` from source raw image `srcimg`. Return VBoxManage convertfromraw exit code.
   '''
   if os.path.exists(dstvdi):
     raise ValueError("destination VDI image already exists: %r" % (dstvdi,))
-  return run([VBOXMANAGE, 'convertfromraw', imgpath, vdipath, '--format', 'VDI'] + argv, trace=trace)
+  return run([VBOXMANAGE, 'convertfromraw', srcimg, dstvdi, '--format', 'VDI'] + argv, trace=trace)
+
+def cmd_mkimg(argv):
+  src = argv.pop(0)
+  srcpfx, srcext = splitext(src)
+  if srcext == '.vdi':
+    dstimg = srcpfx + '.img'
+  else:
+    dstimg = src + '.img'
+  try:
+    return mkimg(src, dstimg, argv, trace=True)
+  except ValueError as e:
+    error("mkimg fails: %s", e)
+    return 1
+
+def mkimg(src, dstimg, argv, trace=False):
+  ''' Create raw image `dstimg` from source `src`. Return VBoxManage clonemedium exit code.
+  '''
+  if os.path.exists(dstimg):
+    raise ValueError("destination RAW image already exists: %r" % (dstimg,))
+  return run([VBOXMANAGE, 'clonemedium', 'disk', src, dstimg, '--format', 'RAW'] + argv, trace=trace)
 
 def cmd_pause(argv):
   vmspec = argv.pop(0)
@@ -87,6 +117,28 @@ def cmd_start(argv):
 def cmd_suspend(argv):
   vmspec = argv.pop(0)
   return run([VBOXMANAGE, 'controlvm', vmspec, 'savestate'] + argv, trace=True)
+
+def parse_clauses(fp):
+  ''' Generator that parses VBoxManage clause output and yields maps from field name to field value.
+  '''
+  clause = {}
+  for lineno, line in enumerate(fp, 1):
+    with Pfx(lineno):
+      if not line.endswith('\n'):
+        raise ValueError('missng end of line')
+      line = line.rstrip()
+      if not line:
+        yield clause
+        clause = {}
+      else:
+        k, v = line.strip().split(':', 1)
+        v = v.lstrip()
+        if k in clause:
+          warning("repeated key %r: keeping old %r, ignoring new %r", k, clause[k], v)
+        else:
+          clause[k] = v
+  if clause:
+    yield clause
 
 if __name__ == '__main__':
   sys.exit(main(sys.argv))
