b7728d7a6f973870d9d3c5f1f31dd1f0714bb870
Cameron Simpson <cs@zip.com.au>
2016-11-13 10:09:55 +1100

    cs.threads: locked_property: use cs.py.func.prop instead of builtin property
    --HG--
    branch : iphoto

--- lib/python/cs/threads.py
+++ lib/python/cs/threads.py
@@ -33,7 +33,7 @@ from cs.debug import Lock, RLock, Thread
 from cs.logutils import LogTime, error, warning, debug, exception, OBSOLETE, D, X
 from cs.obj import O
 from cs.queues import IterableQueue, Channel, MultiOpenMixin, not_closed
-from cs.py.func import funcname
+from cs.py.func import funcname, prop
 from cs.py3 import raise3, Queue, PriorityQueue
 
 def bg(func, daemon=None, name=None):
@@ -440,7 +440,7 @@ def locked_property(func, lock_name='_lock', prop_name=None, unset_object=None):
       ##debug("outside lock, already computed %s", prop_name)
       pass
     return p
-  return property(getprop)
+  return prop(getprop)
 
 def via(cmanager, func, *a, **kw):
   ''' Return a callable that calls the supplied `func` inside a
