63f0c4be55187d14bb87b88a0276b3cebd00419a
Cameron Simpson <cs@zip.com.au>
2016-12-04 16:26:02 +1100

    cs.asynchron_tests: strip debugging
    --HG--
    branch : myke

--- lib/python/cs/asynchron_tests.py
+++ lib/python/cs/asynchron_tests.py
@@ -10,7 +10,6 @@ import threading
 import time
 import unittest
 from cs.asynchron import Result, after
-from cs.logutils import X
 
 def D(msg, *a):
   if a:
@@ -49,30 +48,19 @@ class TestResult(unittest.TestCase):
     R2 = Result()
     self.assertFalse(R2.ready)
     def add_R_R2():
-      X("ADD_R_R2...")
       value = R.result + R2.result
-      X("ADD_R_R2 added")
       return value
     A = after([R, R2], None, add_R_R2)
-    X("after ==> A=%s", A)
     self.assertFalse(A.ready)
     self.assertFalse(R.ready)
     self.assertFalse(R2.ready)
     def delayed_completion():
-      X("delayed: sleep 2...")
       time.sleep(2)
-      X("delayed: R.result=1")
       R.result = 1
-      X("delayed: sleep 2...")
       time.sleep(2)
-      X("delayed: R2.result=2")
       R2.result = 2
-      X("delayed: complete")
-    X("dispatch delayed")
     threading.Thread(target=delayed_completion).start()
-    X("A.get...")
     Aresult = A.get()
-    X("A done")
     self.assertEqual(Aresult, 3)
     self.assertTrue(A.ready)
     self.assertTrue(R.ready)
