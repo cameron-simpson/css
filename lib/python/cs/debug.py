c8f74f8c885e8e412ac1f491bd3c414ad93b17d1
Cameron Simpson <cs@zip.com.au>
2016-12-17 21:19:33 +1100

    cs.debug: add missing import
    --HG--
    branch : venti

--- lib/python/cs/debug.py
+++ lib/python/cs/debug.py
@@ -28,7 +28,7 @@ import traceback
 from cs.py3 import Queue, Queue_Empty, exec_code
 from cs.py.stack import caller
 import cs.logutils
-from cs.logutils import infer_logging_level, debug, error, setup_logging, D, Pfx, PrePfx, ifdebug, X
+from cs.logutils import infer_logging_level, debug, error, warning, setup_logging, D, Pfx, PrePfx, ifdebug, X
 from cs.obj import O, Proxy
 from cs.seq import seq
 from cs.timeutils import sleep
