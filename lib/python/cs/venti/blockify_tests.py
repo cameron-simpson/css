541ec2b71f1e72d724eebb8fda0f6c8ad0f1e259
Cameron Simpson <cs@zip.com.au>
2017-03-18 09:17:08 +1100

    cs.venti.blockify_tests: test both offsets+chunks parsers and offsets-only parsers by shimming the test parsers
    --HG--
    branch : venti

--- lib/python/cs/venti/blockify_tests.py
+++ lib/python/cs/venti/blockify_tests.py
@@ -25,7 +25,29 @@ def random_blocks(max_size=65536, count=64):
     size = rand0(max_size) + 1
     yield randblock(size)
 
+def nochunks(parser):
+  ''' Take a parser yielding a mix of offsets and chunks, and return
+      a parser yielding only offset.
+  '''
+  def parser2(chunks):
+    for parsed in parser(chunks):
+      if isinstance(parsed, (bytes, memoryview)):
+        ##X("parser2(%s): skip %d byte chunk", parser, len(parsed))
+        pass
+      else:
+        ##X("parser2(%s): yield offset %d", parser, parsed)
+        yield parsed
+  return parser2
+
 class TestAll(unittest.TestCase):
+
+  # load my_code from this test suite
+  with open(__file__, 'rb') as myfp:
+    mycode = myfp.read()
+
+  X("generate random test data")
+  random_data = list(random_blocks(max_size=12000, count=1280))
+  ## quicker ## random_data = list(random_blocks(max_size=1200, count=12))
 
   def setUp(self):
     self.fp = open(__file__, "rb")
@@ -33,12 +55,20 @@ class TestAll(unittest.TestCase):
   def tearDown(self):
     self.fp.close()
 
+  @skip
   def test01parsers(self):
-    random_data = list(random_blocks())
     rand_total = sum(len(chunk) for chunk in random_data)
     for parser in (parse_text, parse_mp3):
       with self.subTest(parser.__name__):
-        Q = parser(random_data)
+        input_chunks = self.random_data
+        if parser is parse_mp3:
+          if os.path.exists('TEST.mp3'):
+            X("mp3 parse: replace input data with chunks from TEST.mp3")
+            input_chunks = read_from(open('TEST.mp3', 'rb'))
+            input_desc = 'TEST.mp3'
+          else:
+            X("no TEST.mp3 in ".os.getcwd())
+        Q = parser(input_chunks)
         offset = 0
         last_qoffset = 0
         for qitem in Q:
@@ -50,78 +80,82 @@ class TestAll(unittest.TestCase):
             chunk = qitem
             self.assertTrue(len(chunk) > 0)
             offset += len(chunk)
-        self.assertEqual(rand_total, offset)
+        if input_chunks is self.random_data:
+          self.assertEqual(rand_total, offset)
 
   def test02blocked_chunks_of(self):
-    global rand_total
-    with open(__file__, 'rb') as myfp:
-      mycode = myfp.read()
-    random_data = list(random_blocks(max_size=12000, count=1280))
-    for parser in (
-        None,
-        parse_text,
-        parse_mp3,
-      ):
-      parser_desc = 'None' if parser is None else parser.__name__
-      for input_desc, input_chunks in (
-          ('random data', random_data),
-          (__file__, [ mycode for _ in range(100) ]),
+    for dup_chunks in True,:    ##False, True:
+      for parser in (
+          None,
+          parse_text,
+          parse_mp3,
         ):
-        if parser is parse_mp3:
-          if os.path.exists('TEST.mp3'):
-            X("mp3 parse: replace input data with chunks from TEST.mp3")
-            input_chunks = read_from(open('TEST.mp3', 'rb'))
-            input_desc = 'TEST.mp3'
-          else:
-            X("no TEST.mp3 in ".os.getcwd())
-        with self.subTest("blocked_chunks_of",
-                          parser=parser_desc, input_chunks=input_desc):
-          X("test parser %s vs %s...", parser, input_desc)
-          src_total = 0
-          source_chunks = list(input_chunks)
-          for chunk in source_chunks:
-            src_total += len(chunk)
-          X("scan and block...")
-          chunk_total = 0
-          nchunks = 0
-          all_chunks = []
-          start_time = time.time()
-          offset = 0
-          prev_chunk = None
-          for chunk in blocked_chunks_of(source_chunks, parser):
-            if prev_chunk is not None:
-              # this avoids issues with the final block, which may be short
-              self.assertTrue(len(prev_chunk) >= MIN_BLOCKSIZE,
-                              "len(prev_chunk)=%d < MIN_BLOCKSIZE=%d"
-                              % (len(prev_chunk), MIN_BLOCKSIZE))
-            if parser is parse_text:
-              ##X("BLOCKED_CHUNK offset=%d len=%d: %r", offset, len(chunk), chunk)
-              pass
+        parser_desc = 'None' if parser is None else parser.__name__
+        for input_desc, input_chunks in (
+            ('random data', self.random_data),
+            (__file__, [ self.mycode for _ in range(100) ]),
+          ):
+          if parser is parse_mp3:
+            if os.path.exists('TEST.mp3'):
+              X("mp3 parse: replace input data with chunks from TEST.mp3")
+              input_chunks = read_from(open('TEST.mp3', 'rb'))
+              input_desc = 'TEST.mp3'
             else:
-              ##X("BLOCKED_CHUNK offset=%d len=%d", offset, len(chunk))
-              pass
-            offset += len(chunk)
-            ##if parser is not None:
-            ##  X("  CHUNK=%r", chunk)
-            nchunks += 1
-            chunk_total += len(chunk)
-            all_chunks.append(chunk)
-            # the pending.flush operation can return short blocks
-            self.assertTrue(len(chunk) <= MAX_BLOCKSIZE,
-                            "len(chunk)=%d > MAX_BLOCKSIZE=%d"
-                            % (len(chunk), MAX_BLOCKSIZE))
-            self.assertTrue(chunk_total <= src_total,
-                            "chunk_total:%d > src_total:%d"
-                            % (chunk_total, src_total))
-            prev_chunk = chunk
-          end_time = time.time()
-          X("%s|%s: %d chunks in %gs, %d bytes at %g B/s",
-            input_desc, parser_desc,
-            nchunks, end_time-start_time, chunk_total,
-            float(chunk_total) / (end_time-start_time))
-          self.assertEqual(src_total, chunk_total)
-          self.assertEqual(b''.join(source_chunks),
-                           b''.join(all_chunks))
+              X("no TEST.mp3 in ".os.getcwd())
+          with self.subTest("blocked_chunks_of",
+                            parser=parser_desc,
+                            input_chunks=input_desc,
+                            dup_chunks=dup_chunks):
+            if dup_chunks:
+              # wrap parsers in chunk-stripper
+              if parser is None:
+                continue
+              parser0 = parser
+              parser = nochunks(parser)
+            source_chunks = list(input_chunks)
+            src_total = 0
+            for chunk in source_chunks:
+              src_total += len(chunk)
+            chunk_total = 0
+            nchunks = 0
+            all_chunks = []
+            start_time = time.time()
+            offset = 0
+            prev_chunk = None
+            for chunk in blocked_chunks_of(source_chunks, parser, dup_chunks=dup_chunks):
+              if prev_chunk is not None:
+                # this avoids issues with the final block, which may be short
+                self.assertTrue(len(prev_chunk) >= MIN_BLOCKSIZE,
+                                "len(prev_chunk)=%d < MIN_BLOCKSIZE=%d"
+                                % (len(prev_chunk), MIN_BLOCKSIZE))
+              if parser is parse_text:
+                ##X("BLOCKED_CHUNK offset=%d len=%d: %r", offset, len(chunk), chunk)
+                pass
+              else:
+                ##X("BLOCKED_CHUNK offset=%d len=%d", offset, len(chunk))
+                pass
+              offset += len(chunk)
+              ##if parser is not None:
+              ##  X("  CHUNK=%r", chunk)
+              nchunks += 1
+              chunk_total += len(chunk)
+              all_chunks.append(chunk)
+              # the pending.flush operation can return short blocks
+              self.assertTrue(len(chunk) <= MAX_BLOCKSIZE,
+                              "len(chunk)=%d > MAX_BLOCKSIZE=%d"
+                              % (len(chunk), MAX_BLOCKSIZE))
+              self.assertTrue(chunk_total <= src_total,
+                              "chunk_total:%d > src_total:%d"
+                              % (chunk_total, src_total))
+              prev_chunk = chunk
+            end_time = time.time()
+            X("%s|%s: %d chunks in %gs, %d bytes at %g B/s",
+              input_desc, parser_desc,
+              nchunks, end_time-start_time, chunk_total,
+              float(chunk_total) / (end_time-start_time))
+            self.assertEqual(src_total, chunk_total)
+            self.assertEqual(b''.join(source_chunks),
+                             b''.join(all_chunks))
 
   def test03blockifyAndRetrieve(self):
     with MemoryCacheStore("TestAll.test00blockifyAndRetrieve"):
