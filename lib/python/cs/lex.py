2be0a253860dd7773e54ab379924619ee519518e
Cameron Simpson <cs@zip.com.au>
2017-04-17 13:03:48 +1000

    cs.lex,cs.rfc2047,cs.app.mailfiler: move unrfc2047 into new cs.rfc2047 module

--- lib/python/cs/lex.py
+++ lib/python/cs/lex.py
@@ -204,61 +204,6 @@ def untexthexify(s, shiftin='[', shiftout=']'):
     chunks.append(unhexify(s))
   return joinbytes(chunks)
 
-# regexp to match RFC2047 text chunks
-re_RFC2047 = re.compile(r'=\?([^?]+)\?([QB])\?([^?]*)\?=', re.I)
-
-def unrfc2047(s, warning=None):
-  ''' Accept a string `s` containing RFC2047 text encodings (or the whitespace
-      littered varieties that come from some low quality mail clients) and
-      decode them into flat Unicode.
-      `warning`: optional parameter specifying function to report warning messages, default cs.logutils.warning
-  '''
-  if warning is None:
-    from cs.logutils import warning
-  if not isinstance(s, unicode):
-    s = unicode(s, 'iso8859-1')
-  chunks = []
-  sofar = 0
-  for m in re_RFC2047.finditer(s):
-    start = m.start()
-    end = m.end()
-    if start > sofar:
-      chunks.append(s[sofar:start])
-    enccset = m.group(1)
-    enctype = m.group(2).upper()
-    # default to undecoded text
-    enctext = m.group(3)
-    if enctype == 'B':
-      try:
-        enctext = base64.b64decode(enctext)
-      except TypeError as e:
-        warning("%r: %e", enctext, e)
-        enctext = m.group()
-    elif enctype == 'Q':
-      try:
-        enctext = quopri.decodestring(enctext.replace('_', ' '))
-      except UnicodeEncodeError as e:
-        warning("%r: %e", enctext, e)
-        ##enctext = enctext.decode('iso8859-1')
-    else:
-      raise RuntimeError("unhandled RFC2047 string: %r" % (m.group(),))
-    try:
-      enctext = enctext.decode(enccset)
-    except LookupError as e:
-      warning("decode(%s): %e: %r", enccset, e, enctext)
-      enctext = enctext.decode('iso8859-1')
-    except UnicodeDecodeError as e:
-      warning("decode(%s): %s: %r", enccset, e, enctext)
-      enctext = enctext.decode(enccset, 'replace')
-    except UnicodeEncodeError as e:
-      warning("decode(%s): %e: %r", enccset, e, enctext)
-      enctext = enctext.decode(enccset, 'replace')
-    chunks.append(enctext)
-    sofar = end
-  if sofar < len(s):
-    chunks.append(s[sofar:])
-  return unicode('').join(chunks)
-
 def get_chars(s, offset, gochars):
   ''' Scan the string `s` for characters in `gochars` starting at `offset`.
       Return (match, new_offset).
