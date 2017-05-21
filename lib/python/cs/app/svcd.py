51e50ff2ebbbb9e8e96e8158d7504e5fae7ef02a
Cameron Simpson <cs@zip.com.au>
2017-05-21 11:42:57 +1000

    cs.app.svcd: recode of svcd in Python, supporting other apps running multiple svcd instances

---
+++ lib/python/cs/app/svcd.py
@@ -0,0 +1,337 @@
+#!/usr/bin/env python3
+#
+# Daemon to run and restart a command that provides a service, such
+# as an ssh port forward.
+#       - Cameron Simpson <cs@zip.com.au> 10dec2012
+#
+# Recode as Python module.  - cameron 14may2017
+#
+
+from __future__ import print_function
+from getopt import getopt, GetoptError
+import os
+from os.path import basename, join as joinpath
+from subprocess import Popen, DEVNULL, call as callproc
+import sys
+from time import sleep, time as now
+from cs.app.flag import Flags, uppername, FlaggedMixin
+from cs.env import LOGDIR, VARRUN
+from cs.logutils import setup_logging, warning, PfxThread as Thread
+from cs.py.func import prop
+
+TEST_RATE = 7
+KILL_TIME = 5
+
+USAGE = '''Usage:
+  %s disable names...
+          FOr each name set the flag {NAME}_DISABLE, causing the matching
+          svcd to shut down its daemon process.
+  %s enable names
+          For each name clear the flag {NAME}_DISABLE, allowing the matching
+          svcd to start up its daemon process.
+  %s restart names...
+          For each name set the flag {NAME}_RESTART, causing the matching
+          svcd to shut down and then restart its daemon process.
+  %s stop names...
+          For each name kill the svcd running the named daemon process and
+          the daemon process.
+  %s [-1] [-l] [-L lockname] [-n name] [-t testcmd] [-x] command [args...]
+    -1    Run command only once.
+    -l    Use lock \"svcd-<name>\" to prevent multiple instances of this svcd.
+    -L lockname
+          Use lock \"lockname\" to prevent multiple instances of this svcd.
+    -n name
+          Specify a name for this svcd.
+          Also create a subprocess pid file at $VARRUN/name.pid for the command.
+          This also causes svcd to consult the flags {NAME}_OVERRIDE
+          and {NAME}_DISABLE and {NAME}_RESTART.
+    -p svcd-pidfile
+          Specify $cmd pid file instead of default.
+    -P subp-pidfile
+          Specify $cmd subprocess pid file instead of default.
+    -q    Quiet. Do not issue alerts.
+    -s sigcmd
+          Run the signature shell command \"sigcmd\" whose output is
+          used to check for changed circumstances requiring the service
+          to restart.
+    -t testcmd
+          Run the test shell command \"testcmd\" periodically to
+          govern whether the command should be active.
+    -T testrate
+          Interval between test polls in seconds. Default: $testrate
+    -u username
+          Run command as the specified username.
+    -U username
+          Run test and related commands as the specified username.
+    -x    Trace execution.'''
+
+def main(argv, environ=None):
+  cmd = basename(argv.pop(0))
+  usage = USAGE % (cmd, cmd, cmd, cmd)
+  setup_logging(cmd)
+
+  flags = Flags()
+
+  badopts = False
+  try:
+    if not argv:
+      raise GetoptError("missing arguments")
+    arg0 = argv[0]
+    if arg0 == 'disable':
+      for name in argv:
+        Svcd(name=name).disable()
+      return 0
+    if arg0 == 'enable':
+      for name in argv:
+        Svcd(name=name).enable()
+      return 0
+    if arg0 == 'restart':
+      for name in argv:
+        Svcd(name=name).restart()
+      return 0
+    if arg0 == 'stop':
+      for name in argv:
+        SvcD(name=name).stop()
+      return 0
+    once = False
+    use_lock = False
+    lock_name = None
+    name = None
+    svcd_pidfile = None
+    subprocess_pidfile = None
+    quiet = False
+    sig_shcmd = None
+    test_shcmd = None
+    test_rate = TEST_RATE
+    uid = os.geteuid()
+    username = getpwuid(uid).pw_name
+    run_uid = uid
+    run_username = username
+    test_uid = uid
+    test_username = username
+    trace = False
+    opts, argv = getopt('1lL:n:p:P:qs:t:T:u:U:x', argv)
+    for opt, value in opts:
+      with Pfx(opt):
+        if opt == '-1':
+          once = True
+        elif opt == '-l':
+          use_lock = True
+        elif opt == '-L':
+          use_lock = True
+          lock_name = value
+        elif opt == '-n':
+          name = value
+        elif opt == '-p':
+          svcd_pidfile = value
+        elif opt == '-P':
+          subprocess_pidfile = value
+        elif opt == '-q':
+          quiet = True
+        elif opt == '-s':
+          sig_shcmd = value
+        elif opt == '-t':
+          test_shcmd = value
+        elif opt == '-T':
+          try:
+            test_rate = int(value)
+          except ValueError as e:
+            raise GetoptError("testrate should be a valid integer: %s" % (e,))
+        elif opt == '-u':
+          run_username = value
+          run_uid = getpwnam(run_username).pw_uid
+        elif opt == '-U':
+          test_username = value
+          test_uid = getpwnam(test_username).pw_uid
+        elif opt == '-x':
+          trace = True
+        else:
+          raise RuntimeError("unhandled option")
+    if use_lock and name is None:
+      raise GetoptError("-l (lock) requires a name (-n)")
+    if not argv:
+      raise GetoptError("missing command")
+  except GetoptError as e:
+    warning("%s", e)
+    badopts = True
+  if badopts:
+    print(usage, file=sys.stderr)
+    return 2
+  if sig_shcmd is None:
+    sig_func = None
+  else:
+    def sig_func():
+      argv = ['sh', '-c', sig_shcmd]
+      if test_uid != uid:
+        argv = ['sux', '-u', test_username, '--'] + argv
+      P = Popen(argv, stdin=DEVNULL, stdout=PIPE) == 0
+      sig_text = P.stdout.read()
+      returncode = P.wait()
+      if returncode != 0:
+        warning("returncode %s from %r", returncode, sig_shcmd)
+        sig_text = None
+      return sig_text
+  if test_shcmd is None:
+    test_func = None
+  else:
+    def test_func():
+      argv = ['sh', '-c', test_shcmd]
+      if test_uid != uid:
+        argv = ['sux', '-u', test_username, '--'] + argv
+      return callproc(argv, stdin=DEVNULL) == 0
+  if run_uid != uid:
+    argv = ['sux', '-u', run_username, '--'] + argv
+  if use_lock:
+    argv = ['lock', '--', 'svcd-' + name] + argv
+  S = Svcd(argv, name=name, sig_func=sig_func, test_func=test_func, test_rate=test_rate)
+
+class SvcD(FlaggedMixin, object):
+
+  def __init__(self, argv, name=None,
+        environ=None,
+        flags=None,
+        logdir=None,
+        pidfile=None,
+        sig_func=None,
+        test_func=None,
+        test_rate=None,
+        varrun=None
+    ):
+    X("INIT SVCD %s %r", name, argv)
+    if environ is None:
+      environ = os.environ
+    if logdir is None:
+      logdir = joinpath(LOGDIR(environ), 'svc')
+    if varrun is None:
+      varrun = VARRUN(environ)
+    if flags is None:
+      flags = Flags(environ=environ)
+    FlaggedMixin(self, flags=flags)
+    if test_rate is None:
+      test_rate = TEST_RATE
+    self.name = name
+    self.logdir = logdir
+    self.varrun = varrun
+    self.test_func = test_func
+    self.test_rate = test_rate
+    self.active = False # flag to end the monitor Thread
+    self.subp = None    # current subprocess
+    self.monitor = None # monitoring Thread
+    if pidfile is None and name is not None:
+        pidfile = joinpath(self.varrun, name + '.pid')
+    self.pidfile = pidfile
+
+  def __str__(self):
+    if self.name is None:
+      return self.__class__.__name__
+    return self.__class__.__name__ + ':' + self.name
+
+  def __repr__(self):
+    return str(self) + repr(self.argv)
+
+  def test(self):
+    if self.flag_override:
+      return True
+    if self.flag_disabled:
+      return False
+    if self.test_func is not None:
+      return self.test_func()
+    return True
+
+  def alert(self, msg, *a):
+    if a:
+      msg = msg % a
+    subprocess.Popen(['alert', 'SVCD %s: %s' % (self.name, msg)], stdin=DEVNULL)
+
+  def spawn(self):
+    X("SPAWN %r", self.argv)
+    if self.subp is not None:
+      raise RuntimeError("already running")
+    self.subp = Popen.subprocess(self.argv, stdin=DEVNULL)
+    self.alert('STARTED')
+    if self.pidfile is not None:
+      with open(self.pidfile, 'w') as pidfp:
+        print(self.subp.pid, file=pidfp)
+
+  def reap(self):
+    X("REAP")
+    if self.subp is None:
+      raise RuntimeError("not running")
+    returncode = self.subp.wait()
+    self.alert('EXITED')
+    self.subp = None
+    if self.pidfile is not None:
+      with open(self.pidfile, 'w') as pidfp:
+        pass
+      try:
+        os.remove(self.pidfile)
+      except OSError as e:
+        if e.errno != errno.EPERM:
+          warning("remove %r: %s", self.pidfile, e)
+    return returncode
+
+  def _kill_subproc(self):
+    ''' Kill the subprocess and return its exit code.
+        Sends SIGTERM, then SIGKILL if the process does not die promptly.
+    '''
+    self.subp.terminate()
+    final_time = now() + KILL_TIME
+    while self.probe() and now() < final_time():
+      sleep(1)
+    if self.probe():
+      self.subp.kill()
+    return self.reap()
+
+  def start(self):
+    X("START %s", self.name)
+    self.active = True
+    old_sig = ''
+    def monitor():
+      while self.active:
+        if self.test():
+          # test passes, start service
+          if self.subp is None:
+            self.spawn()
+          else:
+            # service already running, check for restart
+            restart = self.flag_restart
+            if not restart and self.sig_func is not None:
+              new_sig = self.sig_func()
+              if new_sig is not None and new_sig != old_sig:
+                old_sig = new_sig
+                restart = True
+            if restart:
+              self.alert('RESTART')
+              self.flag_restart = False
+              if self.subp is not None:
+                self.subp.terminate()
+                self.subp.reap()
+        else:
+          # test fails, stop service
+          if self.subp is not None:
+            self._kill_proc()
+        sleep(self.test_rate)
+    T = Thread(name=str(self)+':monitor', target=monitor)
+    T.start()
+    self.monitor = T
+
+  def restart(self):
+    self.flag_restart = True
+
+  def disable(self):
+    X("DISABLE %s", self.name)
+    self.flag_disable = True
+
+  def enable(self):
+    X("ENABLE %s", self.name)
+    self.flag_disabled = False
+
+  def probe(self):
+    if self.subp is None:
+      return False
+    try:
+      os.kill(self.subp.pid, 0)
+    except OSError as e:
+      return False
+    else:
+      return True
