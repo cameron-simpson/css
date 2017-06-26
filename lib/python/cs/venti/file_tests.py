9d4a0c9367d5236123a836ea45d60a97b97c01d1
Cameron Simpson <cs@zip.com.au>
2017-06-26 10:14:37 +1000

    cs.ventifile_tests: Block.blockify not requires an iterable of chunks
    --HG--
    branch : venti

--- lib/python/cs/venti/file_tests.py
+++ lib/python/cs/venti/file_tests.py
@@ -8,7 +8,7 @@ import sys
 import unittest
 from random import randint
 from cs.fileutils import BackedFile, BackedFile_TestMethods
-from cs.logutils import X
+from cs.logutils import X, XP
 from . import defaults
 from .block import Block
 from .blockify import blockify, top_block_for
@@ -24,7 +24,7 @@ class Test_RWFile(unittest.TestCase, BackedFile_TestMethods):
     # construct test backing block
     with open(__file__, "rb") as fp:
       self.backing_text = fp.read()
-    self.vt_block = top_block_for(blockify(self.backing_text))
+    self.vt_block = top_block_for(blockify([self.backing_text]))
     self.backed_fp = File(self.vt_block)
 
   def tearDown(self):
