56851fd73bde09b068482b742da46853ad18c632
Cameron Simpson <cs@zip.com.au>
2017-07-06 18:20:16 +1000

    cs.x: X(): tighter implementation of file= parameter, document the modes in the docstring

--- lib/python/cs/x.py
+++ lib/python/cs/x.py
@@ -12,24 +12,31 @@ X_logger = None
 # set to true to write direct to /dev/tty
 X_via_tty = False
 
-def X(msg, *args, **kwargs):
+def X(msg, *args, **kw):
   ''' Unconditionally write the message `msg` to sys.stderr.
       If `args` is not empty, format `msg` using %-expansion with `args`.
+      `file`: optional keyword argument specifying the output file.
+      If `file` is not None, write to it unconditionally.
+      Otherwise, if X_logger then log a warning to that logger.
+      Otherwise, if X_via_tty then open /dev/tty and write the message to it.
+      Otherwise, write the message to sys.stderr.
   '''
+  fp = kw.pop('file', None)
+  if kw:
+    raise ValueError("unexpected keyword arguments: %r" % (kw,))
   msg = str(msg)
   if args:
     msg = msg % args
-  if X_logger:
-    # NB: ignores any kwargs
-    X_logger.warning(msg)
-  elif X_via_tty:
-    # NB: ignores any kwargs
-    with open('/dev/tty', 'w') as fp:
-      fp.write(msg)
-      fp.write('\n')
-      fp.flush()
-  else:
-    fp = kwargs.pop('file', None)
-    if fp is None:
-      fp = sys.stderr
-    print(msg, file=fp)
+  if fp is None:
+    if X_logger:
+      # NB: ignores any kwargs
+      X_logger.warning(msg)
+      return
+    if X_via_tty:
+      # NB: ignores any kwargs
+      with open('/dev/tty', 'w') as fp:
+        fp.write(msg)
+        fp.write('\n')
+      return
+    fp = sys.stderr
+  print(msg, file=fp)
