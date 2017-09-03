626c78cf54f774f144c477dada25747a50e67183
Cameron Simpson <cs@cskk.id.au>
2017-09-03 20:23:48 +1000

    cs.lex: lint and PyPI prep
    --HG--
    branch : pypi

--- lib/python/cs/lex.py
+++ lib/python/cs/lex.py
@@ -1,5 +1,63 @@
 #!/usr/bin/python
-#
+
+r'''
+Lexical analysis functions, tokenisers.
+
+An assortment of lexcial and tokenisation functions useful for writing recursive descent parsers, of which I have several.
+
+Generally the get_* functions accept a source string and an offset (often optional, default 0) and return a token and the new offset, raising ValueError on failed tokenisation.
+
+* as_lines(chunks, partials=None): parse text chunks, yield complete individual lines
+
+* get_chars(s, offset, gochars): collect adjacent characters from `gochars`
+
+* get_delimited(s, offset, delim): collect text up to the first ocurrence of the character `delim`.
+
+* get_envvar(s, offset=0, environ=None, default=None, specials=None): parse an environment variable reference such as $foo
+
+* get_identifier(s, offset=0, alpha=ascii_letters, number=digits, extras='_'): parse an identifier
+
+* get_nonwhite(s, offset=0): collect nonwhitespace characters
+
+* get_other_chars(s, offset=0, stopchars=None): collect adjacent characters not from `stopchars`
+
+* get_qstr(s, offset=0, q='"', environ=None, default=None, env_specials=None): collect a quoted string, honouring slosh escapes and optionally expanding environment variable references
+
+* get_sloshed_text(s, delim, offset=0, slosh='\\', mapper=slosh_mapper, specials=None): collect some slosh escaped text with optional special tokens (such as '$' introducing '$foo')
+
+* get_tokens(s, offset, getters): collect a sequence of tokens specified in `getters`
+
+* match_tokens(s, offset, getters): wrapper for get_tokens which catches ValueError and returns None instead
+
+* get_uc_identifier(s, offset=0, number=digits, extras='_'): collect an UPPERCASE identifier
+
+* get_white(s, offset=0): collect whitespace characters
+
+* isUC_(s): test if a string looks like an upper case identifier
+
+* htmlify(s,nbsp=False): transcribe text in HTML safe form, using &lt; for "<", etc
+
+* htmlquote(s): transcribe text as HTML quoted string suitable for HTML tag attribute values
+
+* jsquote(s): transcribe text as JSON quoted string; essentially like htmlquote without its htmlify step
+
+* parseUC_sAttr(attr): parse FOO or FOOs (or FOOes) and return (FOO, is_plural)
+
+* slosh_mapper(c, charmap=SLOSH_CHARMAP): return a string to replace \c; the default charmap matches Python slosh escapes
+
+* texthexify(bs, shiftin='[', shiftout=']', whitelist=None): a function like binascii.hexlify but also supporting embedded "printable text" subsequences for compactness and human readbility in the result; initial use case was for transcription of binary data with frequent text, specificly directory entry data
+
+* untexthexify(s, shiftin='[', shiftout=']'): the inverse of texthexify()
+
+* unctrl(s,tabsize=8): transcribe text removing control characters
+'''
+
+import binascii
+from functools import partial
+import os
+from string import printable, whitespace, ascii_letters, ascii_uppercase, digits
+import sys
+from cs.py3 import bytes, ustr, sorted, StringTypes, joinbytes
 
 DISTINFO = {
     'description': "lexical analysis, tokenisers",
@@ -11,16 +69,6 @@ DISTINFO = {
     ],
     'install_requires': ['cs.py3'],
 }
-
-import base64
-import binascii
-from functools import partial
-import quopri
-from string import printable, whitespace, ascii_letters, ascii_uppercase, digits
-import re
-import sys
-import os
-from cs.py3 import bytes, unicode, ustr, sorted, StringTypes, joinbytes
 
 unhexify = binascii.unhexlify
 if sys.hexversion >= 0x030000:
@@ -132,10 +180,6 @@ def texthexify(bs, shiftin='[', shiftout=']', whitelist=None):
         converted to hexify() and texthexify() output strings may be
         freely concatenated and decoded with untexthexify().
   '''
-  if sys.hexversion < 0x03000000:
-    bschr = lambda bs, ndx: bs[ndx]
-  else:
-    bschr = lambda bs, ndx: chr(bs[ndx])
   if whitelist is None:
     whitelist = _texthexify_white_chars
   if isinstance(whitelist, StringTypes) and not isinstance(whitelist, bytes):
@@ -284,8 +328,8 @@ def get_dotted_identifier(s, offset=0, **kw):
   offset0 = offset
   _, offset = get_identifier(s, offset=offset, **kw)
   if _:
-    while offset < len(s)-1 and s[offset] == '.':
-      _, offset2 = get_identifier(s, offset=offset+1, **kw)
+    while offset < len(s) - 1 and s[offset] == '.':
+      _, offset2 = get_identifier(s, offset=offset + 1, **kw)
       if not _:
         break
       offset = offset2
