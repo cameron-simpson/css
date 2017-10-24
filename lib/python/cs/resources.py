993fd928485e94e2a7137fd7f362d2abf47e5fb2
Cameron Simpson <cs@cskk.id.au>
2017-10-24 15:22:45 +1100

    cs.resources: new class RunState for tracking or controlling a running task; some linting

--- lib/python/cs/resources.py
+++ lib/python/cs/resources.py
@@ -4,6 +4,13 @@
 #       - Cameron Simpson <cs@cskk.id.au> 11sep2014
 #
 
+from contextlib import contextmanager
+from threading import Condition, RLock, Lock
+import time
+from cs.logutils import error
+from cs.obj import O
+from cs.py.stack import caller
+
 DISTINFO = {
     'description': "resourcing related classes and functions",
     'keywords': ["python2", "python3"],
@@ -11,21 +18,9 @@ DISTINFO = {
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
-        ],
-    'install_requires': ['cs.excutils', 'cs.logutils', 'cs.obj', 'cs.py.stack'],
+    ],
+    'install_requires': ['cs.logutils', 'cs.obj', 'cs.py.stack'],
 }
-
-from contextlib import contextmanager
-from threading import Condition, RLock, Lock, current_thread
-import time
-import traceback
-from cs.excutils import logexc
-import cs.logutils
-from cs.logutils import debug, warning, error
-from cs.obj import O
-from cs.pfx import Pfx, PfxCallInfo, XP
-from cs.py.stack import caller, stack_dump
-from cs.x import X
 
 class ClosedError(Exception):
   pass
@@ -58,6 +53,7 @@ class MultiOpenMixin(O):
           to be processed.
         `lock`: if set and not None, an RLock to use; otherwise one will be allocated
     '''
+    O.__init__(self)
     if lock is None:
       lock = RLock()
     self.opened = False
@@ -81,11 +77,11 @@ class MultiOpenMixin(O):
     ''' Increment the open count.
         On the first .open call self.startup().
     '''
-    if True:    ## cs.logutils.D_mode:
+    if True:
       if caller_frame is None:
         caller_frame = caller()
-      Fkey = caller_frame.filename, caller_frame.lineno
-      self._opened_from[Fkey] = self._opened_from.get(Fkey, 0) + 1
+      frame_key = caller_frame.filename, caller_frame.lineno
+      self._opened_from[frame_key] = self._opened_from.get(frame_key, 0) + 1
     self.opened = True
     with self._lock:
       self._opens += 1
@@ -113,9 +109,10 @@ class MultiOpenMixin(O):
       if self._opens < 1:
         error("%s: UNDERFLOW CLOSE", self)
         error("  final close was from %s", self._final_close_from)
-        for Fkey in sorted(self._opened_from.keys()):
-          error("  opened from %s %d times", Fkey, self._opened_from[Fkey])
+        for frame_key in sorted(self._opened_from.keys()):
+          error("  opened from %s %d times", frame_key, self._opened_from[frame_key])
         ##from cs.debug import thread_dump
+        ##from threading import current_thread
         ##thread_dump([current_thread()])
         ##raise RuntimeError("UNDERFLOW CLOSE of %s" % (self,))
         return
@@ -149,9 +146,9 @@ class MultiOpenMixin(O):
   def closed(self):
     if self._opens > 0:
       return False
-    if self._opens < 0:
-      XP("_opens < 0: %r", self._opens)
-      raise RuntimeError("_OPENS UNDERFLOW")
+    ##if self._opens < 0:
+    ##  XP("_opens < 0: %r", self._opens)
+    ##  raise RuntimeError("_OPENS UNDERFLOW")
     if not self.opened:
       # never opened, so not totally closed
       return False
@@ -237,3 +234,121 @@ class Pool(O):
     with self._lock:
       if self.max_size == 0 or len(self.pool) < self.max_size:
         self.pool.append(o)
+
+class RunState(object):
+  ''' A class to track a running task whose cancellation may be requested.
+
+      A RunState has the following properties:
+
+      cancelled: true if .cancel has been called.
+
+      running: true if the task is running. Assigning a true value
+        to it also sets .start_time to now. Assigning a false value
+        to it also sets .end_time to now.
+
+      start_time: the time .running was last set to true.
+      end_time: the time .running was last set to false.
+      run_time: max(0, .end_time - .start_time)
+
+      stopped: true if the task is not running.
+
+      stopping: true if the task is running but has been cancelled.
+
+      notify_start: a set of callables called with the RunState
+        instance to be called whenever .running becomes true.
+      notify_end: a set of callables called with the RunState
+        instance to be called whenever .running becomes false.
+      notify_cancel: a set of callables called with the RunState
+        instance to be called whenever .cancel is called.
+
+      A RunState can be used a a context manager, with the enter
+      and exit methods calling start and .end respectively.
+  '''
+
+  def __init__(self):
+    # core state
+    self._running = False
+    self.cancelled = False
+    # timing state
+    self.start_time = None
+    self.end_time = None
+    self.total_time = 0
+    # callbacks
+    self.notify_start = set()
+    self.notify_cancel = set()
+    self.notify_end = set()
+
+  def __bool__(self):
+    ''' Return true if the task is running.
+    '''
+    return self.running
+  __nonzero__ = __bool__
+
+  def __enter__(self):
+    self.start()
+    return self
+
+  def __exit__(self, exc_type, exc_value, traceback):
+    self.end()
+
+  def start(self):
+    ''' Start: adjust state, set start_time to now.
+        Sets .cancelled to False and sets .running to True.
+    '''
+    assert not self.running
+    self.cancelled = False
+    self.running = True
+
+  def end(self):
+    ''' End: adjust state, set end_time to now.
+        Sets sets .running to False.
+    '''
+    assert self.running
+    self.running = False
+
+  @property
+  def running(self):
+    ''' Property expressing whether the task is running.
+    '''
+    return self._running
+
+  @running.setter
+  def running(self, status):
+    ''' Set the running property.
+    '''
+    if self._running:
+      if not status:
+        self.end_time = time.time()
+        self.total_time += self.run_time
+        for notify in self.notify_end:
+          notify(self)
+    elif status:
+      self.start_time = time.time()
+      for notify in self.notify_start:
+        notify(self)
+    self._running = status
+
+  @property
+  def stopping(self):
+    ''' Is the process stopping?
+    '''
+    return self.running and self.cancelled
+
+  @property
+  def stopped(self):
+    ''' Is the process stopped.
+    '''
+    return self.cancelled and not self.running
+
+  def cancel(self):
+    ''' Set the cancelled flag, the process should notice and stop.
+    '''
+    self.cancelled = True
+    for notify in self.notify_cancel:
+      notify(self)
+
+  @property
+  def run_time(self):
+    ''' Property returning most recent run time (end_time-start_time).
+    '''
+    return max(0, self.end_time - self.start_time)
