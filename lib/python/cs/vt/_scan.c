f2946d7f75ccd0dbbc8b409b5369dc7e888437e2
Cameron Simpson <cs@cskk.id.au>
2017-12-02 19:59:54 +1100

    cs.vt._scan: release the GIL around the buffer scan
    --HG--
    branch : venti

--- lib/python/cs/vt/_scan.c
+++ lib/python/cs/vt/_scan.c
@@ -39,22 +39,26 @@ static PyObject *scan_scanbuf(PyObject *self, PyObject *args) {
     int             noffsets = 0;
 
     if (buflen > 0) {
+        Py_BEGIN_ALLOW_THREADS
         offsets = malloc(buflen * sizeof(unsigned long));
+        if (offsets != NULL) {
+            unsigned long   offset = 0;
+            unsigned char   *cp = buf;
+            for (; buflen; cp++, buflen--, offset++) {
+                unsigned char b = *cp;
+                hash_value = ( ( ( hash_value & 0x001fffff ) << 7
+                               )
+                             | ( ( b & 0x7f )^( (b & 0x80)>>7 )
+                               )
+                             );
+                if (hash_value % 4093 == 4091) {
+                    offsets[noffsets++] = offset;
+                }
+            }
+        }
+        Py_END_ALLOW_THREADS
         if (offsets == NULL) {
             return PyErr_NoMemory();
-        }
-        unsigned long   offset = 0;
-        unsigned char   *cp = buf;
-        for (; buflen; cp++, buflen--, offset++) {
-            unsigned char b = *cp;
-            hash_value = ( ( ( hash_value & 0x001fffff ) << 7
-                           )
-                         | ( ( b & 0x7f )^( (b & 0x80)>>7 )
-                           )
-                         );
-            if (hash_value % 4093 == 4091) {
-                offsets[noffsets++] = offset;
-            }
         }
     }
 
