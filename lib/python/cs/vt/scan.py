52ef80a68b4d770304fa203ec5e733df12ed91c1
Cameron Simpson <cs@cskk.id.au>
2017-12-29 09:18:44 +1100

    cs.vt.scan: cleanups before merge
    --HG--
    branch : venti

--- lib/python/cs/vt/scan.py
+++ lib/python/cs/vt/scan.py
@@ -9,7 +9,7 @@ from distutils.core import setup, Extension
 from os import chdir, getcwd
 from os.path import dirname, join as joinpath
 import sys
-from time import sleep
+##from time import sleep
 from cs.logutils import error, warning
 from cs.x import X
 
@@ -18,23 +18,25 @@ try:
 except ImportError:
   warning("building _scan from _scan.c")
   def do_setup():
+    ''' Run distutils.core.setup from the top of the lib tree.
+        Side effect: changes directory, needs undoing.
+    '''
     pkgdir = dirname(__file__)
     chdir(dirname(dirname(pkgdir)))
-    D = setup(
+    return setup(
       ext_modules=[Extension("cs.vt._scan", [joinpath(pkgdir, '_scan.c')])],
     )
-    return D
-  # delay, seemingly needed to make the C version look "new"
+  ### delay, seemingly needed to make the C version look "new"
   ##sleep(2)
   oargv = sys.argv
   owd = getcwd()
   sys.argv = [oargv[0], 'build_ext', '--inplace']
   try:
-    X("do_setup => %r", do_setup())
+    do_setup()
   except SystemExit as e:
-    error("SETUP FAILS: %s:%s", type(e), e)
-    scanbuf =  None
     chdir(owd)
+    error("SETUP FAILS: %s:%s", type(e), e)
+    scanbuf = None
   else:
     chdir(owd)
     try:
