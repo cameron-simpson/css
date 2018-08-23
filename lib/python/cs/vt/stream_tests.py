5b296d6aaa6e1bb2fdbbf83b4b1962630b68c155
Cameron Simpson <cs@cskk.id.au>
2018-08-23 14:42:22 +1000

    cs.vt.stream_tests: simplify StreamStore construction matching recent PacketConnection improvements
    --HG--
    branch : venti

--- lib/python/cs/vt/stream_tests.py
+++ lib/python/cs/vt/stream_tests.py
@@ -7,7 +7,6 @@
 import os
 import sys
 import unittest
-from cs.x import X
 from .hash import DEFAULT_HASHCLASS
 from .hash_tests import HashUtilDict, _TestHashCodeUtils
 from .store import MappingStore
@@ -15,21 +14,27 @@ from .store_tests import TestStore
 from .stream import StreamStore
 
 def make_stream_store(hashclass, addif):
+  ''' Contruct an in-memory remote Store attached with pipes.
+  '''
   upstream_rd, upstream_wr = os.pipe()
   downstream_rd, downstream_wr = os.pipe()
-  remote_S = StreamStore( "stream_tests.make_stream_store.remote_S",
-                          os.fdopen(upstream_rd, 'rb'),
-                          os.fdopen(downstream_wr, 'wb'),
-                          local_store=MappingStore("stream_tests.make_stream_store.remote_S.local_store", HashUtilDict()).open(),
-                          addif=addif,
-                          hashclass=hashclass
-                        )
-  S = StreamStore( "stream_tests.make_stream_store.S",
-                   os.fdopen(downstream_rd, 'rb'),
-                   os.fdopen(upstream_wr, 'wb'),
-                   addif=addif,
-                   hashclass=hashclass
-                 )
+  remote_S = StreamStore(
+      "stream_tests.make_stream_store.remote_S",
+      upstream_rd,
+      downstream_wr,
+      local_store=MappingStore(
+          "stream_tests.make_stream_store.remote_S.local_store",
+          HashUtilDict()).open(),
+      addif=addif,
+      hashclass=hashclass
+  )
+  S = StreamStore(
+      "stream_tests.make_stream_store.S",
+      downstream_rd,
+      upstream_wr,
+      addif=addif,
+      hashclass=hashclass
+  )
   return S, remote_S
 
 class TestStreamStore(TestStore, unittest.TestCase):
@@ -40,6 +45,7 @@ class TestStreamStoreAddIf(TestStore, unittest.TestCase):
   def _init_Store(self):
     self.S, self.remote_S = make_stream_store(self.hashclass, addif=True)
 
+##@unittest.skip("too noisy")
 class TestHashCodeUtilsStreamStore(_TestHashCodeUtils, unittest.TestCase):
   ''' Test HashUtils on a StreamStore on a HashUtilDict.
   '''
@@ -56,7 +62,7 @@ class TestHashCodeUtilsStreamStore(_TestHashCodeUtils, unittest.TestCase):
     _TestHashCodeUtils.tearDown(self)
 
 class TestHashCodeUtilsStreamStoreAddIf(TestHashCodeUtilsStreamStore):
-  ''' Test HashUtils on a StreamStore on a HashUtilDict.
+  ''' Test HashUtils on a StreamStore on a HashUtilDict with addif=True.
   '''
   ADDIF_MODE = True
 
