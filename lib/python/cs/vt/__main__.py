ced5775955d3b384e08be9a6a15b1942460456ce
Cameron Simpson <cs@cskk.id.au>
2018-11-01 20:15:46 +1100

    cs.vt.__main__: change Block_from_bytes into BlockRecord.value_from_bytes
    --HG--
    branch : venti

--- lib/python/cs/vt/__main__.py
+++ lib/python/cs/vt/__main__.py
@@ -36,7 +36,7 @@ import cs.x
 from cs.x import X
 from . import fromtext, defaults
 from .archive import Archive, ArchiveFTP, CopyModes, copy_out_dir, copy_out_file
-from .block import Block, IndirectBlock, Block_from_bytes
+from .block import Block, IndirectBlock, BlockRecord
 from .blockify import blocked_chunks_of
 from .compose import get_store_spec
 from .config import Config, Store
@@ -410,7 +410,7 @@ class VTCmd:
     for blockref in args:
       with Pfx(blockref):
         blockref_bs = fromtext(blockref)
-        B, offset = Block_from_bytes(blockref_bs)
+        B, offset = BlockRecord.value_from_bytes(blockref_bs)
         if offset < len(blockref_bs):
           raise ValueError("invalid blockref, extra bytes: %r" % (blockref[offset:],))
         if not fsck_Block(B):
