506a02ef22cc1c9a7f712070b3d3e710185d6b34
Cameron Simpson <cs@cskk.id.au>
2018-11-13 15:21:04 +1100

    cs.threads: @locked: probe the lock to see if it is taken, emit a warning if so - may prove too noisy in busy environments
    --HG--
    branch : venti

--- lib/python/cs/threads.py
+++ lib/python/cs/threads.py
@@ -402,11 +402,20 @@ def locked(func):
       Relies upon a ._lock attribute for locking.
   '''
   def lockfunc(self, *a, **kw):
+    # quite probe for contention
     if self._lock.acquire(0):
       self._lock.release()
+      contended = False
     else:
-      debug("@locked(self._lock=%r <%s>, func=%r)...", self._lock, self._lock.__class__, func)
+      warning(
+          "@locked(self._lock=%r <%s>, func=%r): waiting for contended lock...",
+          self._lock, type(self._lock), func)
+      contended = True
     with self._lock:
+      if contended:
+        warning(
+            "@locked(self._lock=%r <%s>, func=%r): ACQUIRED",
+            self._lock, type(self._lock), func)
       return func(self, *a, **kw)
   lockfunc.__name__ = "@locked(%s)" % (funcname(func),)
   return lockfunc
