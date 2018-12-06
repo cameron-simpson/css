f8002fc6256451a3c8cfa3cc903382c7090485b4
Cameron Simpson <cs@cskk.id.au>
2018-12-06 20:57:23 +1100

    cs.vt.index: clean lint
    --HG--
    branch : vt

--- lib/python/cs/vt/index.py
+++ lib/python/cs/vt/index.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
 #
-# An index is a mapping of hashcodes => bytes-records. This module supports
-# several backends and a mechanism for choosing one.
+# INdex classes.
 # - Cameron Simpson <cs@cskk.id.au>
 #
 
+''' An index is a mapping of hashcodes => bytes-records.
+    This module supports several backends and a mechanism for choosing one.
+'''
+
 from contextlib import contextmanager
 from os.path import exists as existspath
-##from cs.debug import TimingOutLock as Lock
 from cs.logutils import warning, info
 from cs.pfx import Pfx
 from cs.resources import MultiOpenMixin
@@ -18,9 +20,13 @@ _CLASSES = []
 _BY_NAME = {}
 
 def class_names():
+  ''' Return the index class names.
+  '''
   return _BY_NAME.keys()
 
 def class_by_name(indexname):
+  ''' Return an index class from its name.
+  '''
   return _BY_NAME[indexname]
 
 def choose(basepath, preferred_indexclass=None):
@@ -49,17 +55,21 @@ def choose(basepath, preferred_indexclass=None):
     if not indexclass.is_supported():
       continue
     return indexclass
-  raise ValueError("no supported index classes available: tried %r", indexclasses)
+  raise ValueError(
+      "no supported index classes available: tried %r"
+      % (indexclasses,))
 
 class _Index(HashCodeUtilsMixin, MultiOpenMixin):
 
   def __init__(self, basepath, hashclass, decode, lock=None):
     ''' Initialise an _Index instance.
-        `basepath`: the base path to the index; the index itself
+
+        Parameters:
+        * `basepath`: the base path to the index; the index itself
           is at `basepath`.SUFFIX
-        `decode`: function to decode a binary index record into the
+        * `decode`: function to decode a binary index record into the
           return type instance
-        `lock`: optional mutex, passed to MultiOpenMixin.__init__.
+        * `lock`: optional mutex, passed to MultiOpenMixin.__init__.
     '''
     MultiOpenMixin.__init__(self, lock=lock)
     self.basepath = basepath
@@ -97,6 +107,8 @@ class LMDBIndex(_Index):
 
   @classmethod
   def is_supported(cls):
+    ''' Test whether this index class is supported by the Python environment.
+    '''
     try:
       import lmdb
     except ImportError:
@@ -120,12 +132,12 @@ class LMDBIndex(_Index):
         subdir=True, readonly=False,
         metasync=False, sync=False,
         writemap=True, map_async=True,
-        map_size = self.map_size,
+        map_size=self.map_size,
     )
 
   def _embiggen_lmdb(self, new_map_size=None):
     if new_map_size is None:
-      new_map_size= self.map_size * 2
+      new_map_size = self.map_size * 2
     self.map_size = new_map_size
     info("change LMDB map_size to %d", self.map_size)
     # reopen the database
@@ -197,6 +209,9 @@ class LMDBIndex(_Index):
     return self.decode(record)
 
   def get(self, hashcode, default=None):
+    ''' Get and decode the record for `hashcode`.
+        Return None for missing `hashcode`.
+    '''
     entry = self._get(hashcode)
     if entry is None:
       return default
@@ -229,6 +244,8 @@ class GDBMIndex(_Index):
 
   @classmethod
   def is_supported(cls):
+    ''' Test whether this index class is supported by the Python environment.
+    '''
     try:
       import dbm.gnu
     except ImportError:
@@ -276,6 +293,9 @@ class GDBMIndex(_Index):
     return self.decode(entry)
 
   def get(self, hashcode, default=None):
+    ''' Get and decode the record for `hashcode`.
+        Return None for missing `hashcode`.
+    '''
     with self._gdbm_lock:
       entry = self._gdbm.get(hashcode, None)
     if entry is None:
@@ -304,6 +324,8 @@ class KyotoIndex(_Index):
 
   @classmethod
   def is_supported(cls):
+    ''' Test whether this index class is supported by the Python environment.
+    '''
     try:
       import kyotocabinet
     except ImportError:
@@ -311,15 +333,21 @@ class KyotoIndex(_Index):
     return True
 
   def startup(self):
+    ''' Open the index.
+    '''
     from kyotocabinet import DB
     self._kyoto = DB()
     self._kyoto.open(self.path, DB.OWRITER | DB.OCREATE)
 
   def shutdown(self):
+    ''' Close the index.
+    '''
     self._kyoto.close()
     self._kyoto = None
 
   def flush(self):
+    ''' Flush pending updates to the index.
+    '''
     try:
       self._kyoto.synchronize(hard=False)
     except TypeError:
@@ -332,6 +360,9 @@ class KyotoIndex(_Index):
     return self._kyoto.check(hashcode) >= 0
 
   def get(self, hashcode):
+    ''' Get and decode the record for `hashcode`.
+        Return None for missing `hashcode`.
+    '''
     record = self._kyoto.get(hashcode)
     if record is None:
       return None
@@ -347,10 +378,13 @@ class KyotoIndex(_Index):
     self._kyoto[hashcode] = value.encode()
 
   def hashcodes_from(self, start_hashcode=None, reverse=False):
-    ''' Generator yielding the keys from the index in order starting with optional `start_hashcode`.
-        `start_hashcode`: the starting hashcode; if missing or None,
+    ''' Generator yielding the keys from the index
+        in order starting with optional `start_hashcode`.
+
+        Parameters:
+        * `start_hashcode`: the starting hashcode; if missing or None,
           iteration starts with the first key in the index
-        `reverse`: iterate backward if true, otherwise forward
+        * `reverse`: iterate backward if true, otherwise forward
     '''
     hashclass = self.hashclass
     cursor = self._kyoto.cursor()
@@ -368,24 +402,24 @@ class KyotoIndex(_Index):
 
 def register(indexclass, indexname=None, priority=False):
   ''' Register a new `indexclass`, making it known.
-      `indexclass`: the index class
-      `indexname`: the index class name, default from indexclass.NAME
-      `priority`: if true, prepend the class to the _CLASSES list otherwise append
+
+      Parameters:
+      * `indexclass`: the index class
+      * `indexname`: the index class name, default from indexclass.NAME
+      * `priority`: if true, prepend the class to the _CLASSES list otherwise append
   '''
-  global _CLASSES
-  global _BY_NAME
   if indexname is None:
     indexname = indexclass.NAME
   if indexname in _BY_NAME:
     raise ValueError(
-            'cannot register index class %s: indexname %r already registered to %s'
-            % (indexclass, indexname, _BY_NAME[indexname]))
+        'cannot register index class %s: indexname %r already registered to %s'
+        % (indexclass, indexname, _BY_NAME[indexname]))
   _BY_NAME[indexname] = indexclass
   if priority:
     _CLASSES.insert(0, (indexclass.NAME, indexclass))
   else:
     _CLASSES.append((indexclass.NAME, indexclass))
 
-for indexclass in LMDBIndex, KyotoIndex, GDBMIndex:
-  if indexclass.is_supported():
-    register(indexclass)
+for klass in LMDBIndex, KyotoIndex, GDBMIndex:
+  if klass.is_supported():
+    register(klass)
