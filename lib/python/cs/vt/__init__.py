6aa38f8f4c33c2a429fd7a7f93078fb8dc2795ae
Cameron Simpson <cs@cskk.id.au>
2019-02-23 10:50:13 +1100

    cs.vt: former README info folded into package docstring
    --HG--
    branch : vt

--- lib/python/cs/vt/__init__.py
+++ lib/python/cs/vt/__init__.py
@@ -4,6 +4,13 @@
     variable sized blocks, arbitrarily sized data and utilising some
     domain knowledge to aid efficient block boundary selection.
 
+    *Note*: the "mount" filesystem facility uses FUSE,
+    which may need manual OS installation.
+    On MacOS this means installing `osxfuse`
+    for example from MacPorts.
+    You will also need the `llfuse` Python module,
+    which is not automatically required by this package.
+
     The package provides the `vt` command to access
     these facilities from the command line.
 
@@ -29,7 +36,7 @@
     See also the Plan 9 Venti system:
     (http://library.pantek.com/general/plan9.documents/venti/venti.html,
     http://en.wikipedia.org/wiki/Venti)
-    which is also based on variable sized blocks.
+    which is also a system based on variable sized blocks.
 '''
 
 import os
