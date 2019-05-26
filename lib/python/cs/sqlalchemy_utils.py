f87591a04e7e94d829cb8113714772ea7041ced4
Cameron Simpson <cs@cskk.id.au>
2019-05-26 20:15:09 +1000

    cs.sqlalchemy_utils: support virtual columns mapped to a JSON column interior value: new functions find_json_field, get_json_field, set_json_field; new decorator @json_column for declaritive_base tables

--- lib/python/cs/sqlalchemy_utils.py
+++ lib/python/cs/sqlalchemy_utils.py
@@ -6,6 +6,7 @@
 from contextlib import contextmanager
 from icontract import require
 from sqlalchemy.ext.declarative import declarative_base
+from cs.deco import decorator
 from cs.py.func import funccite, funcname
 
 DISTINFO = {
@@ -21,6 +22,7 @@ DISTINFO = {
     'install_requires': [
         'icontract',
         'sqlalchemy',
+        'cs.deco',
         'cs.py.func',
     ],
 }
@@ -127,3 +129,199 @@ def orm_auto_session(method):
   wrapper.__name__ = "@orm_auto_session(%s)" % (funcname(method),)
   wrapper.__doc__ = method.__doc__
   return wrapper
+
+@require(
+    lambda field_name: field_name and not field_name.startswith('.') and
+    not field_name.endswith('.') and '..' not in field_name
+)
+def find_json_field(column_value, field_name, *, infill=False):
+  ''' Descend a JSONable Python object `column_value`
+      to `field_name`.
+      Return `column_value` (possibly infilled), `final_field`, `final_field_name`.
+
+      This supports database row columns which are JSON columns.
+
+      Parameters:
+      * `column_value`: the original value of the column
+      * `field_name`: the field within the column to locate
+      * `infill`: optional keyword parameter, default `False`.
+        If true,
+        `column_value` and its innards will be filled in as `dict`s
+        to allow deferencing the `field_name`.
+
+      The `field_name` is a `str`
+      consisting of a period (`'.'`) separated sequence of field parts.
+      Each field part becomes a key to index the column mapping.
+      These keys are split into the leading field parts
+      and the final field part,
+      which is returned as `final_field_name` above.
+
+      The `final_field` return value above
+      is the mapping within which `final_field_value` may lie
+      and where `final_field_value` may be set.
+      Note: it may not be present.
+
+      If a leading key is missing and `infill` is true
+      the corresponding part of the `column_value` is set to an empty dictionary
+      in order to allow deferencing the leading key.
+      This includes the case when `column_value` itself is `None`,
+      which is why the `column_value` is part of the return.
+
+      If a leading key is missing and `infill` is false
+      this function will raise a `KeyError`
+      for the portion of the `field_name` which failed.
+
+      Examples:
+
+          >>> find_json_field({'a':{'b':{}}},'a.b')
+          ({'a': {'b': {}}}, {'b': {}}, 'b')
+          >>> find_json_field({'a':{}},'a.b')
+          ({'a': {}}, {}, 'b')
+          >>> find_json_field({'a':{'b':{}}},'a.b.c.d')
+          Traceback (most recent call last):
+              ...
+          KeyError: 'a.b.c'
+          >>> find_json_field({'a':{'b':{}}},'a.b.c.d', infill=True)
+          ({'a': {'b': {'c': {}}}}, {}, 'd')
+          >>> find_json_field(None, 'a.b.c.d')
+          Traceback (most recent call last):
+              ...
+          KeyError: 'a'
+          >>> find_json_field(None,'a.b.c.d', infill=True)
+          ({'a': {'b': {'c': {}}}}, {}, 'd')
+  '''
+  field_parts = field_name.split('.')
+  if column_value is None:
+    if infill:
+      column_value = {}
+    else:
+      raise KeyError(field_parts[0])
+  final_field = column_value
+  leading_keys = []
+  while len(field_parts) > 1:
+    field_part = field_parts.pop(0)
+    leading_keys.append(field_part)
+    if field_part not in final_field and infill:
+      final_field[field_part] = {}
+    try:
+      final_field = final_field[field_part]
+    except KeyError as e:
+      raise KeyError('.'.join(leading_keys)) from e
+  return column_value, final_field, field_parts[0]
+
+def get_json_field(column_value, field_name, *, default=None):
+  ''' Return the value of `field_name` from `column_value`
+      or a defaault if the field is not present.
+
+      Parameters:
+      * `column_value`: the original value of the column
+      * `field_name`: the field within the column to locate
+      * `default`: default value to return if the field is not present,
+        default: `None`
+
+      Examples:
+
+          >>> get_json_field({'a': 1}, 'a')
+          1
+          >>> get_json_field({'b': 1}, 'a')
+          >>> get_json_field({'a': {}}, 'a.b')
+          >>> get_json_field({'a': {'b': 2}}, 'a.b')
+          2
+  '''
+  try:
+    _, final_field, final_field_name = find_json_field(
+        column_value, field_name
+    )
+  except KeyError:
+    return None
+  else:
+    return final_field.get(final_field_name)
+
+def set_json_field(column_value, field_name, value, *, infill=False):
+  ''' Set a new `value` for `field_name` of `column_value`.
+      Return the new `column_value`.
+
+      Parameters:
+      * `column_value`: the original value of the column
+      * `field_name`: the field within the column to locate
+      * `value`: the value to store as `field_name`
+      * `infill`: optional keyword parameter, default `False`.
+        If true,
+        `column_value` and its innards will be filled in as `dict`s
+        to allow deferencing the `field_name`.
+
+      As with `find_json_field`,
+      a true `infill` may modify `column_value` to provide `field_name`
+      which is why this function returns the new `column_value`.
+
+      Examples:
+
+          >>> set_json_field({'a': 2}, 'a', 3)
+          {'a': 3}
+          >>> set_json_field({'a': 2, 'b': {'c': 5}}, 'b.c', 4)
+          {'a': 2, 'b': {'c': 4}}
+          >>> set_json_field({'a': 2}, 'b.c', 4)
+          Traceback (most recent call last):
+              ...
+          KeyError: 'b'
+          >>> set_json_field({'a': 2}, 'b.c', 4, infill=True)
+          {'a': 2, 'b': {'c': 4}}
+          >>> set_json_field(None, 'b.c', 4, infill=True)
+          {'b': {'c': 4}}
+  '''
+  column_value, final_field, final_field_name = find_json_field(
+      column_value, field_name, infill=infill
+  )
+  final_field[final_field_name] = value
+  return column_value
+
+@decorator
+def json_column(
+    cls, attr, json_field_name=None, *, json_column='info', default=None
+):
+  ''' Class decorator to declare a virtual column name on a table
+      where the value resides inside a JSON column of the table.
+
+      Parameters:
+      * `cls`: the class to annotate
+      * `attr`: the virtual column name to present as a row attribute
+      * `json_field_name`: the field within the JSON column
+        used to store this value,
+        default the same as `attr`
+      * `json_column`: the name of the associated JSON column,
+        default `'info'`
+      * `default`: the default value returned by the getter
+        if the field is not present,
+        default `None`
+
+      Example use:
+
+          Base = declarative_base()
+          ...
+          @json_column('virtual_name', 'json.field.name')
+          class TableClass(Base):
+            ...
+
+      This annotates the class with a `.virtual_name` property
+      which can be accessed or set,
+      accessing or modifying the associated JSON column.
+  '''
+  if json_field_name is None:
+    json_field_name = attr
+
+  def get_col(row):
+    column_value = getattr(row, json_column)
+    return get_json_field(column_value, json_field_name, default=default)
+
+  getter = property(get_col)
+
+  def set_col(row, value):
+    column_value = getattr(row, json_column)
+    column_value = set_json_field(
+        column_value, json_field_name, value, infill=True
+    )
+    setattr(row, json_column, column_value)
+
+  setattr(cls, attr, getter)
+  setattr(cls, attr, getter.setter(set_col))
+  return cls
