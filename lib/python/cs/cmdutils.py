0d917c533aa39ecc97d888bbe8c0d5e115c4eab0
Cameron Simpson <cs@cskk.id.au>
2019-06-10 16:54:23 +1000

    cs.cmdutils: module docstring, improve docmd docstring, update imports, autoformat
    --HG--
    branch : vt

--- lib/python/cs/cmdutils.py
+++ lib/python/cs/cmdutils.py
@@ -1,17 +1,38 @@
 #!/usr/bin/python
 #
-# Convenience functions for working with the Cmd module.
-#   - Cameron Simpson <cs@cskk.id.au> 03sep2015
-#
+
+''' Convenience functions for working with the Cmd module
+    and other command line related stuff.
+    - Cameron Simpson <cs@cskk.id.au> 03sep2015
+'''
 
 from __future__ import print_function, absolute_import
-from getopt import GetoptError
+from contextlib import contextmanager
+from getopt import getopt, GetoptError
 from logging import warning, exception
+from cs.mappings import StackableValues
 from cs.pfx import Pfx
 
 def docmd(dofunc):
-  ''' Decorator for Cmd subclass methods.
+  ''' Decorator for Cmd subclass methods
+      to supply some basic quality of service.
+
+      This decorator:
+      - wraps the function call in a `cs.pfx.Pfx` for context
+      - intercepts `getopt.GetoptError`s, issues a `warning`
+        and runs `self.do_help` with the method name,
+        then returns `None`
+      - intercepts other `Exception`s,
+        issues an `exception` log message
+        and returns `None`
+
+      The intended use is to decorate `cmd.Cmd` `do_`* methods:
+
+        @docmd
+        def do_something(...):
+          ... do something ...
   '''
+
   def wrapped(self, *a, **kw):
     funcname = dofunc.__name__
     if not funcname.startswith('do_'):
@@ -27,5 +48,6 @@ def docmd(dofunc):
       except Exception as e:
         exception("%s", e)
         return None
+
   wrapped.__doc__ = dofunc.__doc__
   return wrapped
