96c45bb656ab43385534518ee53803fcc83bfd27
Cameron Simpson <cs@cskk.id.au>
2019-06-17 09:29:29 +1000

    cs.py.stack: stack_dump(): trim the last 2 frames from the listing by default, being the stck_dump() call and its call to frames(); fill out docstrings
    --HG--
    branch : vt

--- lib/python/cs/py/stack.py
+++ lib/python/cs/py/stack.py
@@ -22,13 +22,14 @@ DISTINFO = {
 _Frame = namedtuple('Frame', 'filename lineno funcname linetext')
 
 class Frame(_Frame):
-  ''' Namedtuple for stack frame contents.
+  ''' A `namedtuple` for stack frame contents.
   '''
+
   def __str__(self):
     return "%s:%d: %s" % (self.filename, self.lineno, self.linetext)
 
 def frames():
-  ''' Return the current stack as a list of Frame objects.
+  ''' Return the current stack as a list of `Frame` objects.
   '''
   return [ Frame(*f) for f in extract_stack()[:-1] ]
 
@@ -39,16 +40,34 @@ def caller(frame_index=-3):
       * `-1`: caller, this function
       * `-2`: invoker, who wants to know the caller
       * `-3`: the calling function of the invoker
+
+      The default `from_index` value is `-3`.
   '''
   return Frame(*frames()[frame_index])
 
-def stack_dump(fp=None, indent=0, Fs=None):
-  ''' Recite current or supplied stack to `fp`, default sys.stderr.
+def stack_dump(fp=None, indent=0, Fs=None, skip=None):
+  ''' Recite current or supplied stack to `fp`, default `sys.stderr`.
+
+      Parameters:
+      * `fp`: the output file object, default `sys.stderr`
+      * `indent`: how many spaces to indent the stack lines, default `0`
+      * `Fs`: the stack `Frame`s to write,
+        default obtained from the current stack
+      * `skip`: the number of `Frame`s to trim from the end of `Fs`;
+        if `Fs` is `None` this defaults to `2` to trim the `Frame`s
+        for the `stack_dump` function and its call to `frames()`,
+        otherwise the default is `0` to use the supplied `Frame`s as is
   '''
   if fp is None:
     fp = sys.stderr
   if Fs is None:
     Fs = frames()
+    if skip is None:
+      skip = 2
+  elif skip is None:
+    skip = 0
+  if skip > 0:
+    Fs = Fs[:-skip]
   for F in Fs:
     if indent > 0:
       fp.write(' ' * indent)
