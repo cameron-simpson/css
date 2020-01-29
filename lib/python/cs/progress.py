8a1256d11ddbff470370a9e8e09c0bad64584587
Cameron Simpson <cs@cskk.id.au>
2020-01-29 17:24:09 +1100

    cs.progress: rewind __version__ slightly during testing

--- lib/python/cs/progress.py
+++ lib/python/cs/progress.py
@@ -14,7 +14,7 @@ from cs.logutils import warning, exception
 from cs.seq import seq
 from cs.units import transcribe_time, transcribe, BINARY_BYTES_SCALE
 
-__version__ = '20200129.2'
+__version__ = '20200129'
 
 DISTINFO = {
     'description':
