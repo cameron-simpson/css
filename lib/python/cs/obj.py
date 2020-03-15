0ecf454c7c93d8709c0b57aa9e5d814a37e0e17c
Cameron Simpson <cs@cskk.id.au>
2020-03-16 10:24:07 +1100

    cs.obj: new singleton() generic factory function and SingletonMixin mixin class for making singleton classes

--- lib/python/cs/obj.py
+++ lib/python/cs/obj.py
@@ -16,6 +16,7 @@ Presents:
 from __future__ import print_function
 from copy import copy as copy0
 import sys
+from weakref import WeakValueDictionary
 from cs.py3 import StringTypes
 
 DISTINFO = {
@@ -320,3 +321,82 @@ class TrackedClassMixin(object):
       f = sys.stderr
     for o, state in TrackedClassMixin.tcm_all_state(cls):
       print(str(type(o)), id(o), repr(state), file=f)
+
+def singleton(registry, key, factory, fargs, fkwargs):
+  ''' Obtain an object for `key` via `registry` (a mapping of `key`=>`object`.
+
+      If the `key` exists in the registry, return the associated object.
+      Otherwise create a new object by calling `factory(*fargs,**fkwargs)`
+      and store it as `key` in the `registry`.
+
+      The `registry` may be any mapping of `key`s to objects
+      but might usually be a `weakref.WeakValueMapping`
+      to that object references expire as normal.
+
+      See the `SingletonMixin` class for a simple mixin to create
+      singleton classes.
+  '''
+  try:
+    instance = registry[key]
+    is_new = False
+  except KeyError:
+    instance = factory(*fargs, **fkwargs)
+    registry[key] = instance
+    is_new = True
+  return is_new, instance
+
+class SingletonMixin:
+  ''' A mixin turning a subclass into a singleton factory.
+
+      *Note*: this should be the *first* superclass of the subclass
+      in order to intercept `__new__` and `__init__`.
+
+      A subclass should:
+      * *not* provide an `__init__` method.
+      * provide a `_singleton_init` method in place of the normal `__init__`
+        with the usual signature `(self,*args,**kwargs)`.
+      * provide a `_singleton_key(cls,*args,**kwargs)` class method
+        returning a key for the single registry
+        computed from the positional and keyword arguments
+        supplied on instance creation
+        i.e. those which `__init__` would normally receive.
+        This should have the same signature as `_singleton_init`
+        (but using `cls` instead of `self`).
+
+      Example:
+
+          class Pool(SingletonMixin):
+
+              def _singleton_init(self, foo, bah=3):
+                 ... normal __init__ stuff here ...
+
+              @classmethod
+              def _singleton_key(cls, foo, bah=3):
+                  return foo, bah
+  '''
+
+  def __new__(cls, *a, **kw):
+    ''' Prepare a new instance of `cls` if required.
+        Return the instance.
+
+        This creates the class registry if missing,
+        and then
+    '''
+    try:
+      registry = cls._singleton_registry
+    except AttributeError:
+      registry = cls._singleton_registry = WeakValueDictionary()
+
+    def factory(*fargs, **fkwargs):
+      ''' Prepare a new object.
+
+          Call `object.__new__(cls)` and then `o._singleton_init(*a,**kw)`
+          on the new object.
+      '''
+      o = object.__new__(cls)
+      o._singleton_init(*fargs, **fkwargs)
+      return o
+
+    okey = cls._singleton_key(*a, **kw)
+    _, instance = singleton(registry, okey, factory, a, kw)
+    return instance
