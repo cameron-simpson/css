dd4ccd1e8519370f2b32dac07888d25e0d83834a
Cameron Simpson <cs@cskk.id.au>
2020-03-31 10:32:22 +1100

    cs.cmdutils: fix import

--- lib/python/cs/cmdutils.py
+++ lib/python/cs/cmdutils.py
@@ -10,8 +10,8 @@
 from __future__ import print_function, absolute_import
 from contextlib import contextmanager
 from getopt import getopt, GetoptError
+from os.path import basename
 import sys
-from sys.path import basename
 from types import SimpleNamespace as NS
 from cs.context import stackattrs
 from cs.lex import cutprefix
