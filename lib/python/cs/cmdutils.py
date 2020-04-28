717559812fb608f046b3e922768bc93216672119
Cameron Simpson <cs@cskk.id.au>
2020-04-28 19:37:26 +1000

    cs.cmdutils: get a portable nullcontext from cs.context instead of cs.py3

--- lib/python/cs/cmdutils.py
+++ lib/python/cs/cmdutils.py
@@ -13,11 +13,10 @@ from getopt import getopt, GetoptError
 from os.path import basename
 import sys
 from types import SimpleNamespace as NS
-from cs.context import stackattrs
+from cs.context import nullcontext, stackattrs
 from cs.lex import cutprefix
 from cs.logutils import setup_logging, warning, exception
 from cs.pfx import Pfx
-from cs.py3 import nullcontext
 from cs.resources import RunState
 
 __version__ = '20200318'
@@ -31,7 +30,7 @@ DISTINFO = {
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
     ],
-    'install_requires': ['cs.context', 'cs.lex', 'cs.pfx', 'cs.py3', 'cs.resources'],
+    'install_requires': ['cs.context', 'cs.lex', 'cs.pfx', 'cs.resources'],
 }
 
 def docmd(dofunc):
