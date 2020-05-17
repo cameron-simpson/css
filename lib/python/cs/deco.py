8c051e9d726bc3ded4ed8a3a712e64aea491f22c
Cameron Simpson <cs@cskk.id.au>
2020-05-17 16:13:52 +1000

    cs.deco: set __version__ to '20200517.2-post'
    --HG--
    branch : pypi

--- lib/python/cs/deco.py
+++ lib/python/cs/deco.py
@@ -15,7 +15,7 @@ import time
 import traceback
 from cs.gimmicks import warning
 
-__version__ = '20200517.2'
+__version__ = '20200517.2-post'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
