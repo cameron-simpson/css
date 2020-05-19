9d4ca93880a84bd93460ae1c15772eef4c2b6aa9
Cameron Simpson <cs@cskk.id.au>
2020-05-19 21:16:30 +1000

    [IGNORE] cs.logutils: bump __version__ to 20200519-post to avoid misleading value for future unreleased changes

--- lib/python/cs/logutils.py
+++ lib/python/cs/logutils.py
@@ -65,7 +65,7 @@ from cs.pfx import Pfx, XP
 from cs.py.func import funccite
 from cs.upd import Upd
 
-__version__ = '20200519'
+__version__ = '20200519-post'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
