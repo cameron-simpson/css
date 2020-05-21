49ea92a3ab7e0ded953e8f4f4678c39b5466e319
Cameron Simpson <cs@cskk.id.au>
2020-05-21 12:11:26 +1000

    cs.result: bump __version__ to 20200521-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/result.py
+++ lib/python/cs/result.py
@@ -64,7 +64,7 @@ from cs.py3 import Queue, raise3, StringTypes
 from cs.seq import seq
 from cs.threads import bg as bg_thread
 
-__version__ = '20200521'
+__version__ = '20200521-post'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
