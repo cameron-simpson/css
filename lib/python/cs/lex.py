6fb120c34b42786da4414f698fcffacb737203c4
Cameron Simpson <cs@cskk.id.au>
2020-06-13 13:59:42 +1000

    Release information for cs.lex-20200613. Summary: cropped_repr: replace hardwired 29 with computed length

--- lib/python/cs/lex.py
+++ lib/python/cs/lex.py
@@ -21,7 +21,7 @@ from textwrap import dedent
 from cs.deco import fmtdoc
 from cs.py3 import bytes, ustr, sorted, StringTypes, joinbytes
 
-__version__ = '20200517-post'
+__version__ = '20200613'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
