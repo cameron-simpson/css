5f7000ef15f455f9a8d30941901f24f054b11a25
Cameron Simpson <cs@cskk.id.au>
2020-06-13 12:58:32 +1000

    cs.units: bump __version__ to 20200613-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/units.py
+++ lib/python/cs/units.py
@@ -17,7 +17,7 @@ Presupplied scales:
 from string import ascii_letters
 from cs.lex import get_chars, get_decimal, skipwhite
 
-__version__ = '20200613'
+__version__ = '20200613-post'
 
 DISTINFO = {
     'description': "unit suffixes for nonnegative integers; parsing and transcription",
