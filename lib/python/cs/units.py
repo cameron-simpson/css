94f118e289b31e985f11246abbc4836c29473510
Cameron Simpson <cs@cskk.id.au>
2020-06-26 10:31:30 +1000

    cs.units: transcribe: use "%.1f" to format the count if it is not == int(count) ==> a float-with-fraction

--- lib/python/cs/units.py
+++ lib/python/cs/units.py
@@ -141,7 +141,8 @@ def transcribe(n, scale, max_parts=None, skip_zero=False, sep=''):
   for count, unit in reversed(components):
     if skip_zero and count == 0:
       continue
-    text.append( str(count) + unit )
+    count_i = int(count)
+    text.append( (str(count) if count_i == count else "%.1f" % count) + unit )
     if max_parts is not None and len(text) == max_parts:
       break
   return sep.join(text)
