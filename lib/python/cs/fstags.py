048697818a1eb577b270bed4e6f0edb2df5e3ada
Cameron Simpson <cs@cskk.id.au>
2020-07-16 17:38:37 +1000

    cs.fstags: bump __version__ to 20200716-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/fstags.py
+++ lib/python/cs/fstags.py
@@ -103,7 +103,7 @@ from cs.tagset import (
 from cs.threads import locked, locked_property
 from cs.upd import Upd
 
-__version__ = '20200716'
+__version__ = '20200716-post'
 
 DISTINFO = {
     'keywords': ["python3"],
