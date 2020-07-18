8816fc121af47f1c199a947a8ccb45811d8e73a8
Cameron Simpson <cs@cskk.id.au>
2020-07-18 10:46:50 +1000

    cs.progress: bump __version__ to 20200718.3-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/progress.py
+++ lib/python/cs/progress.py
@@ -18,7 +18,7 @@ from cs.units import (
 )
 from cs.upd import Upd, print as upd_print
 
-__version__ = '20200718.3'
+__version__ = '20200718.3-post'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
