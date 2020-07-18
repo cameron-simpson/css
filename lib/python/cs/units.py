7e9e6f9f199a25118b881a53fb284e3d3aa40d79
Cameron Simpson <cs@cskk.id.au>
2020-07-18 19:15:14 +1000

    Release information for cs.units-20200718. Summary: Use str.isalpha to recognise "unit" words instead of string.ascii_letters (uses new cs.lex.get_chars "callable gochars" mode).

--- lib/python/cs/units.py
+++ lib/python/cs/units.py
@@ -17,7 +17,7 @@ Presupplied scales:
 
 from cs.lex import get_chars, get_decimal, skipwhite
 
-__version__ = '20200626-post'
+__version__ = '20200718'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
