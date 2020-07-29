3dc0008396546a81e86e6751652a23b768928c39
Cameron Simpson <cs@cskk.id.au>
2020-07-30 09:22:20 +1000

    cs.nodedb.tokcab_tests: keep enabled, we will winnow by import failure if required

--- lib/python/cs/nodedb/tokcab_tests.py
+++ lib/python/cs/nodedb/tokcab_tests.py
@@ -6,9 +6,6 @@
 
 from __future__ import print_function
 import sys
-print("cs.nodedb.tokcab deprecated in favor of KyotoCabinet", file=sys.stderr)
-sys.exit(0)
-
 import os
 import os.path
 import unittest
