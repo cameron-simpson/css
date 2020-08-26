a90c477b280f7401bebad142c04a6eb4374dce53
Cameron Simpson <cs@cskk.id.au>
2020-08-26 20:38:08 +1000

    cs.sqltags: SQLTagsCommand.cmd_tag: parse TagChoices from the command line
    --HG--
    branch : sqltags

--- lib/python/cs/sqltags.py
+++ lib/python/cs/sqltags.py
@@ -529,11 +529,16 @@ class SQLTagsCommand(BaseCommand, TagsCommandMixin):
     name = argv.pop(0)
     if not argv:
       raise GetoptError("missing tags")
-    try:
-      tag_criteria = cls.parse_tagset_criteria(argv)
-    except ValueError as e:
-      warning("bad tag specifications: %s", e)
-      badopts = True
+    tag_choices = []
+    for arg in argv:
+      with Pfx(arg):
+        try:
+          tag_choice = TagChoice.from_str(arg)
+        except ValueError as e:
+          warning("bad tag specifications: %s", e)
+          badopts = True
+        else:
+          tag_choices.append(TagChoice.from_str(arg))
     if badopts:
       raise GetoptError("bad arguments")
     if name == '-':
