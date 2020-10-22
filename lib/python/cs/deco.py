2df8b8788be6a2714c2787aec0b9d6d434f2a5aa
Cameron Simpson <cs@cskk.id.au>
2020-10-22 11:32:16 +1100

    cs.deco: new @contextdecorator decorator for context managers to turn them into setup/teardown decorators

--- lib/python/cs/deco.py
+++ lib/python/cs/deco.py
@@ -121,6 +121,183 @@ def decorator(deco):
   return metadeco
 
 @decorator
+def contextdecorator(cmgrfunc):
+  ''' A decorator for a context manager function `cmgrfunc`
+      which turns it into a decorator for other functions.
+
+      This supports easy implementation of "setup" and "teardown"
+      code around other functions without the tedium of defining
+      the wrapper function itself. See the examples below.
+
+      The resulting context manager accepts an optional keyword
+      parameter `provide_context`, default `False`. If true, the
+      context returned from the context manager is provided as the
+      first argument to the call to the wrapped function.
+
+      Note that the context manager function `cmgrfunc`
+      has _not_ yet been wrapped with `@contextmanager`,
+      that is done by `@contextdecorator`.
+
+      This decorator supports both normal functions and generator functions.
+
+      With a normal function the process is:
+      * call the context manager with `(func,a,kw,*da,**dkw)`,
+        returning `ctxt`,
+        where `da` and `dkw` are the positional and keyword parameters
+        supplied when the decorator was defined.
+      * within the context
+        return the value of `func(ctxt,*a,**kw)` if `provide_context` is true
+        or the value of `func(*a,**kw)` if not (the default)
+
+      With a generator function the process is:
+      * obtain an iterator by calling `func(*a,**kw)`
+      * for iterate over the iterator, yielding its results,
+        by calling the context manager with `(func,a,kw,**da,**dkw)`,
+        around each `next()`
+      Note that it is an error to provide a true value for `provide_context`
+      if the decorated function is a generator function.
+
+      Some examples follow.
+
+      Trace the call and return of a specific function:
+
+          @contextdecorator
+          def tracecall(func, a, kw):
+              """ Trace the call and return from some function.
+                  This can easily be adapted to purposes such as timing a
+                  function call or logging use.
+              """
+              print("call %s(*%r,**%r)" % (func, a, kw))
+              try:
+                yield
+              except Exception as e:
+                print("exception from %s(*%r,**%r)" % (func, a, kw))
+                raise
+              else:
+                print("return from %s(*%r,**%r)" % (func, a, kw))
+
+          @tracecall
+          def f():
+              """ Some function to trace.
+              """
+
+          @tracecall(provide_context=True):
+          def f(ctxt, *a, **kw):
+              """ A function expected the context object as its first argument,
+                  ahead of whatever other arguments it would normally require.
+              """
+
+      See who is making use of a generator's values,
+      when a generator might be invoked in one place and consumed elsewhere:
+
+          from cs.py.stack import caller
+
+          @contextdecorator
+          def genuser(genfunc, *a, **kw):
+              user = caller(-4)
+              print(f"iterate over {genfunc}(*{a!r},**{kw!r}) from {user}")
+              yield
+
+          @genuser
+          def linesof(filename):
+              with open(filename) as f:
+                  yield from f
+
+          # obtain a generator of lines here
+          lines = linesof(__file__)
+
+          # perhaps much later, or in another function
+          for lineno, line in enumerate(lines, 1):
+              print("line %d: %d words" % (lineno, len(line.split())))
+
+      Turn on "verbose mode" around a particular function:
+
+          import sys
+          import threading
+          from cs.context import stackattrs
+
+          class State(threading.local):
+              def __init__(self):
+                  # verbose if stderr is on a terminal
+                  self.verbose = sys.stderr.isatty()
+
+          # per thread global state
+          state = State()
+
+          @contextdecorator
+          def verbose(func):
+              with stackattrs(state, verbose=True) as old_attrs:
+                  if not old_attrs['verbose']:
+                      print(f"enabled verbose={state.verbose} for function {func}")
+                  # yield the previous verbosity as the context
+                  yield old_attrs['verbose']
+
+          # turn on verbose mode
+          @verbose
+          def func(x, y):
+              if state.verbose:
+                  # print if verbose
+                  print("x =", x, "y =", y)
+
+          # turn on verbose mode and also pass in the previous state
+          # as the first argument
+          @verbose(provide_context=True):
+          def func2(old_verbose, x, y):
+              if state.verbose:
+                  # print if verbose
+                  print("old_verbosity =", old_verbose, "x =", x, "y =", y)
+  '''
+  # turn the function into a context manager
+  cmgr = contextmanager(cmgrfunc)
+
+  # prepare a new decorator which wraps functions in a context
+  # manager using `cmgrfunc`
+  @decorator
+  def cmgrdeco(func, *da, provide_context=False, **dkw):
+    ''' Decorator for functions which wraps calls to the function
+        in a context manager, optionally supplying the context
+        as the first argument to the called function.
+    '''
+    if isgeneratorfunction(func):
+      if provide_context:
+        raise ValueError(
+            "provide_context may not be true when func:%s is a generator" %
+            (func,)
+        )
+
+      def wrapped(*a, **kw):
+        ''' Wrapper function:
+            * obtain an iterator by calling `func(*a,**kw)`
+            * for iterate over the iterator, yielding its results,
+              by calling the context manager with `(func,a,kw,**da,**dkw)`,
+              around each `next()`
+        '''
+        it = func(*a, **kw)
+        while True:
+          with cmgr(func, a, kw, *da, **dkw):
+            try:
+              value = next(it)
+            except StopIteration:
+              break
+          yield value
+    else:
+
+      def wrapped(*a, **kw):
+        ''' Wrapper function:
+            * call the context manager with `(func,a,kw,**da,**dkw)`,
+              returning `ctxt`
+            * within the context
+              return the value of `func(ctxt,*a,**kw)` if `provide_context` is true
+              or the value of `func(*a,**kw)` if not (the default)
+        '''
+        with cmgr(func, a, kw, *da, **dkw) as ctxt:
+          return func(*([ctxt, *a] if provide_context else a), **kw)
+
+    return wrapped
+
+  return cmgrdeco
+
+@decorator
 def logging_wrapper(log_call, stacklevel_increment=1):
   ''' Decorator for logging call shims
       which bumps the `stacklevel` keyword argument so that the logging system
@@ -508,7 +685,36 @@ def observable_class(property_names, only_unequal=False):
 
   return make_observable_class
 
-if __name__ == '__main__':
+def _teststuff():
+
+  @contextdecorator
+  def tracecall(func, a, kw):
+    print("call %s(*%r,**%r)" % (func, a, kw))
+    yield 9
+    print("return from %s(*%r,**%r)" % (func, a, kw))
+
+  @tracecall
+  def f(*a, **kw):
+    print("hello from f: a=%r, kw=%r" % (a, kw))
+    return "V"
+
+  @tracecall
+  def g(r):
+    yield from range(r)
+
+  @tracecall(provide_context=True)
+  def f2(ctxt, *a, **kw):
+    print("hello from f2: ctxt=%s, a=%r, kw=%r" % (ctxt, a, kw))
+    return "V2"
+
+  v = f("abc", y=1)
+  print("v =", v)
+  v = f2("abc2", y=1)
+  print("v2 =", v)
+  gg = g(9)
+  for i in gg:
+    print("i =", i)
+  sys.exit(1)
 
   # pylint: disable=too-few-public-methods
   class Foo:
@@ -531,3 +737,6 @@ if __name__ == '__main__':
   time.sleep(3)
   y = F.x(3)
   print("F.x() ==>", y)
+
+if __name__ == '__main__':
+  _teststuff()
