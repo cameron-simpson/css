d9bf3f4aca53c9b63e22638bbaefe5476551a434
Cameron Simpson <cs@cskk.id.au>
2020-10-25 16:20:37 +1100

    Release information for cs.upd-20201025. Summary: Upd: new .disabled property to allow inspection of disabledness. Upd.insert: accept negative insert indices to position from the top of the list. Upd.nl: use clear-to-end-of-line at the end of the message if available. UpdProxy: turn .prefix into a property which causes a redraw when changed. Upd.proxy(index): return None if the index is out of range, accomodates racy or incorrect behaviour by a user. UpdProxy: cropped overflowing text gets a leading '<' to make it apparent. UpdProxy: new .insert() method to support insterting new proxies with respect to an existing proxy. UpdProxy: new reset() method, clears prefix and text. UpdProxy.__init__: BREAKING: make all arguments optional to aid use. Upd: do not make any slots unless required. Make the compute-redraw-strings methods private.

--- lib/python/cs/upd.py
+++ lib/python/cs/upd.py
@@ -81,7 +81,7 @@ except ImportError as e:
   warning("cannot import curses: %s", e)
   curses = None
 
-__version__ = '20200914-post'
+__version__ = '20201025'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
