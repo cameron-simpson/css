895defd222d9fa9b3aa56d4d9350d47cf49cd4df
Cameron Simpson <cs@cskk.id.au>
2020-10-25 14:33:28 +1100

    Release information for cs.threads-20201025. Summary: @locked: bump the default warning timeout to 10s, was firing too often. New State class for thread local state objects with default attribute values and a stacking __call__ context manager.

--- lib/python/cs/threads.py
+++ lib/python/cs/threads.py
@@ -23,7 +23,7 @@ from cs.py3 import raise3
 from cs.queues import IterableQueue, MultiOpenMixin, not_closed
 from cs.seq import seq, Seq
 
-__version__ = '20200718-post'
+__version__ = '20201025'
 
 DISTINFO = {
     'description':
