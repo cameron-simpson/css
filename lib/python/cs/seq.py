8d7c96e02345e9bfdb4be0115373bef14f622687
Cameron Simpson <cs@cskk.id.au>
2020-10-25 14:26:47 +1100

    Release information for cs.seq-20201025. Summary: New splitoff() function to split a sequence into (usually short) prefixes and a tail.

--- lib/python/cs/seq.py
+++ lib/python/cs/seq.py
@@ -17,7 +17,7 @@ import itertools
 from threading import Lock, Condition
 from cs.gimmicks import warning
 
-__version__ = '20200914-post'
+__version__ = '20201025'
 
 DISTINFO = {
     'description':
