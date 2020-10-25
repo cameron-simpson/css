3e4965060315717a7920ca45848617115fff441e
Cameron Simpson <cs@cskk.id.au>
2020-10-26 08:07:28 +1100

    cs.upd: bump __version__ to 20201026-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/upd.py
+++ lib/python/cs/upd.py
@@ -81,7 +81,7 @@ except ImportError as e:
   warning("cannot import curses: %s", e)
   curses = None
 
-__version__ = '20201026'
+__version__ = '20201026-post'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
