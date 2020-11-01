70af6b7ec13715ab3de7c747e6008b54fdb09064
Cameron Simpson <cs@cskk.id.au>
2020-11-02 08:39:30 +1100

    Release information for cs.upd-20201102. Summary: Upd.nl: simple approach when there are no status lines. Upd: new cursor_visible and cursor_invisible methods to show and hide the cursor.

--- lib/python/cs/upd.py
+++ lib/python/cs/upd.py
@@ -81,7 +81,7 @@ except ImportError as e:
   warning("cannot import curses: %s", e)
   curses = None
 
-__version__ = '20201026.1-post'
+__version__ = '20201102'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
