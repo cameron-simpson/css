f0d8f0f927e8a56649fe213b6ca47e8e620c598a
Cameron Simpson <cs@cskk.id.au>
2020-11-05 08:34:50 +1100

    Release information for cs.pfx-20201105. Summary: @pfx: bugfix for generator functions.

--- lib/python/cs/pfx.py
+++ lib/python/cs/pfx.py
@@ -52,7 +52,7 @@ from cs.py.func import funcname
 from cs.py3 import StringTypes, ustr, unicode
 from cs.x import X
 
-__version__ = '20201025-post'
+__version__ = '20201105'
 
 DISTINFO = {
     'description':
