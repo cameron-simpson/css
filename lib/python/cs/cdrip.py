299bbf0b923f5130f007387ccbcfe9488b1a6727
Cameron Simpson <cs@cskk.id.au>
2020-12-03 10:24:10 +1100

    cs.cdrip: new MBDB class to access MusicBrainz with an SQLTags cache

--- lib/python/cs/cdrip.py
+++ lib/python/cs/cdrip.py
@@ -228,5 +228,187 @@ class TOC(namedtuple('TOC', 'disc_id artist title tracks')):
         tracks=list(cdstub['track-list'])
     )
 
+class MBDB(MultiOpenMixin):
+  ''' An interface to MusicBrainz with a local `SQLTags` cache.
+  '''
+
+  VARIOUS_ARTISTS_ID = '89ad4ac3-39f7-470e-963a-56509c546377'
+
+  def __init__(self):
+    sqltags = self.sqltags = SQLTags(expanduser(expandvars(DEFAULT_MBDB_PATH)))
+    with sqltags:
+      sqltags.init()
+    self.artists = sqltags.subdomain('artist')
+    self.discs = sqltags.subdomain('disc')
+    self.recordings = sqltags.subdomain('recording')
+
+  def startup(self):
+    self.sqltags.open()
+
+  def shutdown(self):
+    self.sqltags.close()
+
+  def artists_of(self, te):
+    ''' Return the artists for `te`.
+    '''
+    return [self.artist(artist_id) for artist_id in te.tags['artists']]
+
+  def recordings_of(self, te):
+    ''' Return the recordings for `te`.
+    '''
+    return [
+        self.recording(recording_id)
+        for recording_id in te.tags.get('recordings', ())
+    ]
+
+  def toc(self, disc_id):
+    disc = self.disc(disc_id)
+    artists = self.artists_of(disc)
+    print(
+        "Artist: ",
+        ', '.join([artist.tags['artist_name'] for artist in artists])
+    )
+    print("Title: ", disc.tags.title)
+    for tracknum, recording in enumerate(self.recordings_of(disc), 1):
+      artists = self.artists_of(recording)
+      print(
+          tracknum, recording.tags.title, '--',
+          ', '.join([artist.tags['artist_name'] for artist in artists])
+      )
+
+  def _get(self, typename, id, includes, id_name='id', record_key=None):
+    if record_key is None:
+      record_key = typename
+    getter_name = f'get_{typename}_by_{id_name}'
+    try:
+      getter = getattr(musicbrainzngs, getter_name)
+    except AttributeError:
+      warning(
+          "no musicbrainzngs.%s: %s", getter_name,
+          pformat(dir(musicbrainzngs))
+      )
+      raise
+    with Pfx("%s(%r,includes=%r)", getter_name, id, includes):
+      try:
+        I = getter(id, includes=includes)
+      except musicbrainzngs.InvalidIncludeError:
+        warning("help(%s):\n%s", getter_name, getter.__doc__)
+        raise
+      I = I[record_key]
+    return I
+
+  def _tagif(self, tags, name, value):
+    with self.sqltags:
+      if value is not None:
+        tags.set(name, value)
+
+  @staticmethod
+  def tag_from_tag_list(tags, mb_dict):
+    ''' Set `tags.tag_list` from `mb_dict['tag-list'].
+    '''
+    tags.set(
+        'tags', [tag_elem['name'] for tag_elem in mb_dict.get('tag-list', ())]
+    )
+
+  @staticmethod
+  def tag_artists_from_credits(tags, mb_dict):
+    ''' Set `tags.artist_ids` from `mb_dict['artist-credit']`.
+    '''
+    credits = mb_dict.get('artist-credit')
+    if credits is not None:
+      tags.set(
+          'artists', [
+              credit['artist']['id']
+              for credit in credits
+              if not isinstance(credit, str)
+          ]
+      )
+
+  @typechecked
+  def artist(self, artist_id: str, force=False) -> SQLTaggedEntity:
+    ##force = True
+    te = self.artists.make(artist_id)
+    tags = te.tags
+    A = None
+    if artist_id == self.VARIOUS_ARTISTS_ID:
+      A = {
+          'artist_name': 'various Artists',
+      }
+    else:
+      includes = []
+      for cached in 'tags',:
+        if force or cached not in tags:
+          X("artist: no tags.%s", cached)
+          includes.append(cached)
+      if includes:
+        A = self._get('artist', artist_id, includes)
+    if A is not None:
+      self._tagif(tags, 'artist_name', A.get('name'))
+      self._tagif(tags, 'sort_name', A.get('sort-name'))
+      self.tag_from_tag_list(tags, A)
+    return te
+
+  @typechecked
+  def disc(self, disc_id: str, force=False) -> SQLTaggedEntity:
+    ''' Return the `disc.`*disc_id* entry.
+        Update from MB as required before return.
+    '''
+    ##force = True
+    te = self.discs.make(disc_id)
+    tags = te.tags
+    includes = []
+    for cached in 'artists', 'recordings':
+      if force or cached not in tags:
+        X("disc: no tags.%s", cached)
+        includes.append(cached)
+    if includes:
+      D = self._get('releases', disc_id, includes, 'discid', 'disc')
+      assert D['id'] == disc_id
+      found_medium = None
+      found_release = None
+      for release in D['release-list']:
+        if found_medium:
+          break
+        for medium in release['medium-list']:
+          if found_medium:
+            break
+          for disc in medium['disc-list']:
+            if found_medium:
+              break
+            if disc['id'] == disc_id:
+              # matched disc
+              found_medium = medium
+              found_release = release
+      assert found_medium
+      self._tagif(tags, 'title', found_release.get('title'))
+      self.tag_artists_from_credits(tags, found_release)
+      if 'recordings' in includes:
+        track_list = medium.get('track-list')
+        if not track_list:
+          warning('no medium[track-list]')
+        else:
+          tags.set(
+              'recording_ids',
+              [track['recording']['id'] for track in track_list]
+          )
+    return te
+
+  @typechecked
+  def recording(self, recording_id: str, force=False) -> SQLTaggedEntity:
+    ##force = True
+    te = self.recordings.make(recording_id)
+    tags = te.tags
+    includes = []
+    for cached in 'artists', 'tags':
+      if force or cached not in tags:
+        X("recording: no tags.%s", cached)
+        includes.append(cached)
+    if includes:
+      R = self._get('recording', recording_id, includes)
+      self._tagif(tags, 'title', R.get('title'))
+      self.tag_from_tag_list(tags, R)
+      self.tag_artists_from_credits(tags, R)
+    return te
+
 if __name__ == '__main__':
   sys.exit(main(sys.argv))
