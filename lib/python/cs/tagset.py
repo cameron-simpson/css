2769348676dcb4fbdf8302cb9af6fb01e0862b8a
Cameron Simpson <cs@cskk.id.au>
2020-12-27 11:03:02 +1100

    cs.tagset: update the documentation to provide working examples of TagSet.ns() and its use in format strings
    --HG--
    branch : sqltags

--- lib/python/cs/tagset.py
+++ lib/python/cs/tagset.py
@@ -151,6 +151,26 @@ which is computed as:
   and moving a bracketed suffix to the front as an unbracketed prefix.
   The full conversion process is defined
   by the `TagsOntology.value_to_tag_name` function.
+
+== Format Strings ==
+
+While you can just use `str.format_map` as shown above
+for the directvalues in a `TagSet`
+(and some command line tools like `fstags` use this in output format specifications
+you can also use `TagSet`s in format strings.
+
+There is a `TagSet.ns()` method which constructs
+an enhanced type of `SimpleNamespace`
+from the tags in the set
+which allows convenient dot notation use in format strings,
+for example:
+
+      tags = TagSet(colour='blue', labels=['a','b','c'], size=9, _ontology=ont)
+      ns = tags.ns()
+      print(f'colour={ns.colour}, info URL={ns.colour._meta.url}')
+      colour=blue, info URL=https://en.wikipedia.org/wiki/Blue
+
+There is a detailed run down of this in the `TagSetNamespace` docstring below.
 '''
 
 from abc import ABC, abstractmethod
@@ -1458,6 +1478,54 @@ class TagSetNamespace(ExtendedNamespace):
           "The colours are ['blue']. The labels are ['a', 'b', 'c']."
           >>> 'The first label is {label}.'.format_map(ns)
           'The first label is a.'
+
+      The same `TagSet` with an ontology:
+
+          >>> ont = TagsOntology({
+          ...   'type.colour': TagSet(description="a colour, a hue", type="str"),
+          ...   'meta.colour.blue': TagSet(
+          ...     url='https://en.wikipedia.org/wiki/Blue',
+          ...     wavelengths='450nm-495nm'),
+          ... })
+          >>> tags = TagSet(colour='blue', labels=['a','b','c'], size=9, _ontology=ont)
+          >>> # the colour Tag
+          >>> tags.tag('colour')  # doctest: +ELLIPSIS
+          Tag(name='colour',value='blue',ontology=TagsOntology<...>)
+          >>> # type information about a colour
+          >>> tags.tag('colour').type
+          'str'
+          >>> tags.tag('colour').typedata
+          TagSet:{'description': 'a colour, a hue', 'type': 'str'}
+          >>> # metadata about this particular colour value
+          >>> tags.tag('colour').meta
+          TagSet:{'url': 'https://en.wikipedia.org/wiki/Blue', 'wavelengths': '450nm-495nm'}
+
+      Using a namespace view of the Tag, useful for format strings:
+
+          >>> # the TagSet as a namespace for use in format strings
+          >>> ns = tags.ns()
+          >>> # the namespace .colour node, which has the Tag attached
+          >>> ns.colour         # doctest: +ELLIPSIS
+          TagSetNamespace(..., _tag=Tag(name='colour',value='blue',...))
+          >>> # the underlying colour Tag itself
+          >>> ns.colour._tag    # doctest: +ELLIPSIS
+          Tag(name='colour',value='blue',ontology=TagsOntology<...>)
+          >>> # str() of a namespace with a ._tag is the Tag value
+          >>> # making for easy use in a format string
+          >>> f'{ns.colour}'
+          'blue'
+          >>> # the type information about the colour Tag
+          >>> ns.colour._tag.typedata
+          TagSet:{'description': 'a colour, a hue', 'type': 'str'}
+          >>> # the metadata
+          >>> ns.colour._meta   # doctest: +ELLIPSIS
+          TagSetNamespace(..., url=TagSetNamespace(..., _tag=Tag(name='url',value='https://en.wikipedia.org/wiki/Blue',...)), ...)
+          >>> # the _meta.url is itself a namespace with a ._tag for the URL
+          >>> ns.colour._meta.url   # doctest: +ELLIPSIS
+          TagSetNamespace(..., _tag=Tag(name='url',value='https://en.wikipedia.org/wiki/Blue',...))
+          >>> # but it formats nicely because it has a ._tag
+          >>> f'colour={ns.colour}, info URL={ns.colour._meta.url}'
+          'colour=blue, info URL=https://en.wikipedia.org/wiki/Blue'
   '''
 
   @classmethod
