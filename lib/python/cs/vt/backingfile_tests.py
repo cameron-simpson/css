782d2202a03527725a236edc02eb875a08f1f75b
Cameron Simpson <cs@cskk.id.au>
2021-02-16 10:17:04 +1100

    cs.vt.backingfile_tests: adjust for recent changes, clean some lint
    --HG--
    branch : vt

--- lib/python/cs/vt/backingfile_tests.py
+++ lib/python/cs/vt/backingfile_tests.py
@@ -4,34 +4,41 @@
 ''' BackingFile tests.
 '''
 
-import os
 import random
 import sys
 from tempfile import TemporaryDirectory, NamedTemporaryFile
 import unittest
-from cs.pfx import XP
 from cs.randutils import rand0, make_randblock
-from .backingfile import RawBackingFile, CompressibleBackingFile, VTDStore
+from .backingfile import (
+    RawBackingFile, CompressibleBackingFile, VTDStore, BinaryHashCodeIndex,
+    BackingFileIndexEntry
+)
 from .hash import HASHCLASS_BY_NAME
 
 RUN_SIZE = 128
 MAX_BLOCK_SIZE = 65536  # should exercise 1, 2 and 3 bytes data length prefixes
 
 class TestBackingFile(unittest.TestCase):
+  ''' Unit tests for backing files.
+  '''
 
   def test_shuffled_randomblocks(self):
     ''' Save RUN_SIZE random blocks, close, retrieve in random order.
     '''
     for cls in RawBackingFile, CompressibleBackingFile:
-      for hashclass_name, hashclass in sorted(HASHCLASS_BY_NAME.items()):
+      for _, hashclass in sorted(HASHCLASS_BY_NAME.items()):
         with self.subTest(cls=cls, hashclass=hashclass):
           with NamedTemporaryFile(dir='.', prefix=cls.__name__ + '-') as T:
             blocks = {}
-            index = {}
+            index = BinaryHashCodeIndex(
+                hashclass=hashclass,
+                binary_index={},
+                index_entry_class=BackingFileIndexEntry
+            )
             total_length = 0
             # open and save data
             with cls(T.name, hashclass=hashclass, index=index) as bf:
-              for n in range(RUN_SIZE):
+              for _ in range(RUN_SIZE):
                 data = make_randblock(rand0(MAX_BLOCK_SIZE + 1))
                 h = bf.add(data)
                 blocks[h] = data
@@ -49,7 +56,7 @@ class TestBackingFile(unittest.TestCase):
     ''' Like test_shuffled_randomblocks but using a .vtd file and binary index file:
         save RUN_SIZE random blocks, close, retrieve in random order.
     '''
-    for hashclass_name, hashclass in sorted(HASHCLASS_BY_NAME.items()):
+    for _, hashclass in sorted(HASHCLASS_BY_NAME.items()):
       with self.subTest(hashclass=hashclass):
         with TemporaryDirectory(dir='.') as TDname:
           with NamedTemporaryFile(dir=TDname, prefix='VTDStore-',
@@ -58,7 +65,7 @@ class TestBackingFile(unittest.TestCase):
             total_length = 0
             # open and save data
             with VTDStore(T.name, T.name, hashclass=hashclass) as S:
-              for n in range(RUN_SIZE):
+              for _ in range(RUN_SIZE):
                 data = make_randblock(rand0(MAX_BLOCK_SIZE + 1))
                 h = S.add(data)
                 blocks[h] = data
@@ -73,6 +80,8 @@ class TestBackingFile(unittest.TestCase):
                 self.assertEqual(data, blocks[h])
 
 def selftest(argv):
+  ''' Run unit tests.
+  '''
   unittest.main(__name__, None, argv)
 
 if __name__ == '__main__':
