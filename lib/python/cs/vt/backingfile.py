b32f9a7d00cd0c5c4a177d6f060986960aedb432
Cameron Simpson <cs@cskk.id.au>
2021-02-16 10:08:02 +1100

    cs.vt.backingfile: BackingFileIndexEntry, RawDataRecord, CompressibleDataRecord: all now AbstractBinary subclasses
    --HG--
    branch : vt

--- lib/python/cs/vt/backingfile.py
+++ lib/python/cs/vt/backingfile.py
@@ -21,40 +21,11 @@ from .index import choose as choose_indexclass
 from .store import MappingStore
 from .util import openfd_append, openfd_read
 
-class BackingFileIndexEntry(PacketField):
+class BackingFileIndexEntry(BinaryMultiValue('BackingFileIndexEntry',
+                                             dict(offset=BSUInt,
+                                                  length=BSUInt))):
   ''' An index entry for a backing file.
   '''
-
-  # pylint: disable=super-init-not-called
-  def __init__(self, offset, length):
-    self.offset = offset
-    self.length = length
-
-  def __str__(self):
-    return "%s(offset=%r,length=%r)" % (
-        type(self).__name__, self.offset, self.length
-    )
-
-  @classmethod
-  def from_value(cls, value):
-    ''' Construct an instance from an `(offset,length)` tuple.
-    '''
-    return cls(*value)
-
-  # pylint: disable=arguments-differ
-  @classmethod
-  def from_buffer(cls, bfr):
-    ''' Construct a `BackingFileIndexEntry` from a `CornuCopyBuffer`.
-    '''
-    offset = BSUInt.parse_value(bfr)
-    length = BSUInt.parse_value(bfr)
-    return cls(offset, length)
-
-  def transcribe(self):
-    ''' Transcribe the offset and length.
-    '''
-    yield BSUInt.transcribe_value(self.offset)
-    yield BSUInt.transcribe_value(self.length)
 
 # pylint: disable=too-many-ancestors
 class BackingFile(MutableMapping, MultiOpenMixin):
@@ -67,15 +38,15 @@ class BackingFile(MutableMapping, MultiOpenMixin):
   @pfx_method
   ##@typechecked
   def __init__(
-      self, path: str, *, hashclass: HashCode, data_record_class: PacketField,
-      index: Mapping
+      self, path: str, *, hashclass: HashCode,
+      data_record_class: AbstractBinary, index: Mapping
   ):
     ''' Initialise the file.
 
         Parameters:
         * `path`: the pathname of the file
         * `hashclass`: the `HashCode` subclass
-        * `data_record_class`: a `PacketField` subclass
+        * `data_record_class`: an `AbstractBinary` subclass
           encoding the data for storage in the file
         * `index`: a `HashCode`->`(offset,length)` mapping
     '''
@@ -207,25 +178,15 @@ class BackingFile(MutableMapping, MultiOpenMixin):
           "short pread from fd %d: asked for %d, got %d" %
           (rfd, length, len(data_record_bs))
       )
-    data_record, post_offset = self.data_record_class.from_bytes(
-        data_record_bs
-    )
-    if post_offset != len(data_record_bs):
-      raise ValueError(
-          "%s.value_from_bytes(%d-bytes): %d unparsed bytes: %r" % (
-              type(self.data_record_class).__name__, len(data_record_bs),
-              len(data_record_bs) - post_offset, data_record_bs[post_offset:]
-          )
-      )
+    data_record = self.data_record_class.from_bytes(data_record_bs)
     return data_record
 
   def __getitem__(self, h):
     ''' Return the data for the `HashCode` `h`.
     '''
-    data_record = self.data_record_for(h)
-    return data_record.value
+    return self.data_record_for(h).data
 
-class RawDataRecord(PacketField):
+class RawDataRecord(BinarySingleValue):
   ''' A raw data record ha no encoding: the bytes are read and written directly.
       The index has the offset and length.
   '''
@@ -237,7 +198,13 @@ class RawDataRecord(PacketField):
     '''
     assert offset == 0
     assert length is None or length == len(bs)
-    return cls(bs), len(bs)
+    return cls(bs)
+
+  @property
+  def data(self):
+    ''' Alias `.value` as `.data`.
+    '''
+    return self.value
 
   def transcribe(self):
     ''' Transcribe the `RawDataRecord`: the value is the bytes.
@@ -249,7 +216,7 @@ def RawBackingFile(path: str, **kw):
   '''
   return BackingFile(path, data_record_class=RawDataRecord, **kw)
 
-class CompressibleDataRecord(PacketField):
+class CompressibleDataRecord(SimpleBinary):
   ''' A data chunk file record for storage in a `.vtd` file.
 
       The record format is:
@@ -262,23 +229,23 @@ class CompressibleDataRecord(PacketField):
   FLAG_COMPRESSED = 0x01
 
   # pylint: disable=super-init-not-called
-  def __init__(self, data, *, is_compressed=None):
+  def __init__(self, _data, *, is_compressed=None):
     ''' Initialise a `CompressibleDataRecord` directly.
 
         Parameters:
-        * `data`: the data to store
+        * `_data`: the data to store
     '''
     if is_compressed is None:
-      if len(data) < 16:
+      if len(_data) < 16:
         is_compressed = False
       else:
-        zdata = compress(data)
-        if len(zdata) < len(data) * 0.9:
-          data = zdata
+        zdata = compress(_data)
+        if len(zdata) < len(_data) * 0.9:
+          _data = zdata
           is_compressed = True
         else:
           is_compressed = False
-    self._data = data
+    self._data = _data
     self.is_compressed = is_compressed
 
   def __str__(self):
@@ -296,7 +263,7 @@ class CompressibleDataRecord(PacketField):
 
   # pylint: disable=arguments-differ
   @classmethod
-  def from_buffer(cls, bfr):
+  def parse(cls, bfr):
     ''' Parse a `CompressibleDataRecord` from a buffer.
     '''
     flags = BSUInt.parse_value(bfr)
@@ -306,7 +273,7 @@ class CompressibleDataRecord(PacketField):
       flags &= ~cls.FLAG_COMPRESSED
     if flags:
       raise ValueError("unsupported flags: 0x%02x" % (flags,))
-    return cls(data, is_compressed=is_compressed)
+    return cls(_data=data, is_compressed=is_compressed)
 
   def transcribe(self):
     ''' Transcribe this data chunk as a data record.
@@ -331,12 +298,6 @@ class CompressibleDataRecord(PacketField):
     if self.is_compressed:
       flags |= self.FLAG_COMPRESSED
     return flags
-
-  @property
-  def value(self):
-    ''' `PacketField.value` support: the data bytes.
-    '''
-    return self.data
 
 ##  @property
 ##  def data_offset(self):
@@ -385,6 +346,7 @@ class BinaryHashCodeIndex(Mapping, HashCodeUtilsMixin, MultiOpenMixin):
     self.binary_index = binary_index
     self.hashclass = hashclass
     self.index_entry_class = index_entry_class
+    self._index_entry_decode = index_entry_class.from_bytes
 
   def __str__(self):
     return "%s(hashclass=%s,index_entry_class=%s,binary_index=%s)" % (
@@ -414,12 +376,7 @@ class BinaryHashCodeIndex(Mapping, HashCodeUtilsMixin, MultiOpenMixin):
     ''' Retrieve the index entry for `hashcode`.
     '''
     index_entry_bs = self.binary_index[hashcode]
-    index_entry, offset = self.index_entry_class.from_bytes(index_entry_bs)
-    if offset < len(index_entry_bs):
-      raise ValueError(
-          "%s[%s]: incomplete parse of index entry => %s, %r" %
-          (self, hashcode, index_entry, index_entry_bs[offset:])
-      )
+    index_entry = self._index_entry_decode(index_entry_bs)
     return index_entry
 
   @pfx_method
@@ -431,7 +388,8 @@ class BinaryHashCodeIndex(Mapping, HashCodeUtilsMixin, MultiOpenMixin):
     '''
     index_entry_class = self.index_entry_class
     if not isinstance(index_entry, index_entry_class):
-      index_entry = index_entry_class.from_value(index_entry)
+      offset, length = index_entry
+      index_entry = index_entry_class(offset=offset, length=length)
     self.binary_index[hashcode] = bytes(index_entry)
 
 @pfx_method
