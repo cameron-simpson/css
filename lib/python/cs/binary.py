2c3ad23fa9d939ea7998b6ac6aa45faf026f84a8
Cameron Simpson <cs@cskk.id.au>
2021-03-06 09:51:41 +1100

    Release information for cs.binary-20210306. Summary: MAJOR RELEASE: The PacketField classes and friends were hard to use; this release supplied a suite of easier to use and more consistent Binary* classes, and ports most of those things based on the old scheme to the new scheme.

--- lib/python/cs/binary.py
+++ lib/python/cs/binary.py
@@ -78,7 +78,7 @@ from cs.lex import cropped, cropped_repr, typed_str as s
 from cs.pfx import Pfx, pfx_method
 from cs.seq import Seq
 
-__version__ = '20200229'
+__version__ = '20210306'
 
 DISTINFO = {
     'keywords': ["python3"],
