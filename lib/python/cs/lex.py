555497618bd79642019275eeb5df9255cef34db8
Cameron Simpson <cs@cskk.id.au>
2021-03-06 09:47:59 +1100

    Release information for cs.lex-20210306. Summary: New cropped() function to crop strings. Rework cropped_repr() to do the repr() itself, and to crop the interiors of tuples and lists. cropped_repr: new inner_max_length for cropping the members of collections. cropped_repr: special case for length=1 tuples. New typed_str(o) object returning type(o).__name__:str(o) in the default case, useful for debugging.

--- lib/python/cs/lex.py
+++ lib/python/cs/lex.py
@@ -24,7 +24,7 @@ from cs.deco import fmtdoc
 from cs.py3 import bytes, ustr, sorted, StringTypes, joinbytes  # pylint: disable=redefined-builtin
 from cs.seq import common_prefix_length, common_suffix_length
 
-__version__ = '20201228-post'
+__version__ = '20210306'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
