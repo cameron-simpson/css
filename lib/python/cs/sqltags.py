016a9eb6335cf4a3cffeb08a5f12bf4f8c0a0ce6
Cameron Simpson <cs@cskk.id.au>
2021-03-06 11:31:41 +1100

    cs.sqltags: bump __version__ to 20210306-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/sqltags.py
+++ lib/python/cs/sqltags.py
@@ -67,7 +67,7 @@ from cs.tagset import (
 from cs.threads import locked, State as ThreadState
 from cs.upd import print  # pylint: disable=redefined-builtin
 
-__version__ = '20210306'
+__version__ = '20210306-post'
 
 DISTINFO = {
     'keywords': ["python3"],
