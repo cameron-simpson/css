3008b7a3d80d2837a4115046e42bf26bb129dbd4
Cameron Simpson <cs@cskk.id.au>
2021-03-06 11:37:35 +1100

    Release information for cs.sqltags-20210306.1. Summary: Docstring updates.

--- lib/python/cs/sqltags.py
+++ lib/python/cs/sqltags.py
@@ -67,7 +67,7 @@ from cs.tagset import (
 from cs.threads import locked, State as ThreadState
 from cs.upd import print  # pylint: disable=redefined-builtin
 
-__version__ = '20210306-post'
+__version__ = '20210306.1'
 
 DISTINFO = {
     'keywords': ["python3"],
