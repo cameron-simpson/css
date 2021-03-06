4c5b3b29291892b3adb779fe05e4e87d830eecca
Cameron Simpson <cs@cskk.id.au>
2021-03-06 11:50:15 +1100

    cs.iso14496: bump __version__ to 20210306-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/iso14496.py
+++ lib/python/cs/iso14496.py
@@ -51,7 +51,7 @@ from cs.threads import locked_property
 from cs.units import transcribe_bytes_geek as geek, transcribe_time
 from cs.upd import print, out  # pylint: disable=redefined-builtin
 
-__version__ = '20210306'
+__version__ = '20210306-post'
 
 DISTINFO = {
     'keywords': ["python3"],
