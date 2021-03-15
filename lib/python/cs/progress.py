03b1bafe4aa6c27addfd8b5042f64aae12d8a4bd
Cameron Simpson <cs@cskk.id.au>
2021-03-16 10:26:16 +1100

    Release information for cs.progress-20210316. Summary: Progress.iterbar: only update the status line once per iteration, either before or after the yield according to incfirst. Progress.iterbar: fix the meaning of update_frequency to count iterations, add update_min_size to count progress advance.

--- lib/python/cs/progress.py
+++ lib/python/cs/progress.py
@@ -30,7 +30,7 @@ from cs.units import (
 )
 from cs.upd import Upd, print  # pylint: disable=redefined-builtin
 
-__version__ = '20210306-post'
+__version__ = '20210316'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
