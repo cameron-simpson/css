0340fe3967efdd4cfdead1ad8dedc4e33401c7b0
Cameron Simpson <cs@cskk.id.au>
2021-03-16 10:26:18 +1100

    cs.progress: bump __version__ to 20210316-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/progress.py
+++ lib/python/cs/progress.py
@@ -30,7 +30,7 @@ from cs.units import (
 )
 from cs.upd import Upd, print  # pylint: disable=redefined-builtin
 
-__version__ = '20210316'
+__version__ = '20210316-post'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
