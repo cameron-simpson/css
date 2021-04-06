ea9e55f7ee2755a82409a5df181dfec576c1391e
Cameron Simpson <cs@cskk.id.au>
2021-04-07 09:15:47 +1000

    cs.cmdutils: bump __version__ to 20210407.1-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/cmdutils.py
+++ lib/python/cs/cmdutils.py
@@ -22,7 +22,7 @@ from cs.pfx import Pfx
 from cs.py.doc import obj_docstring
 from cs.resources import RunState
 
-__version__ = '20210407.1'
+__version__ = '20210407.1-post'
 
 DISTINFO = {
     'description':
