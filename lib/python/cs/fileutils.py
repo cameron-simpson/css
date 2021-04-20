2505b67cd25a837eb34783acbfae871b53e662c4
Cameron Simpson <cs@cskk.id.au>
2021-04-20 12:02:41 +1000

    Release information for cs.fileutils-20210420. Summary: Forensic prefix for NamedTemporaryCopy. UUIDNDJSONMapping: provide an empty .scan_errors on instantiation, avoids AttributeError if a scan never occurs.

--- lib/python/cs/fileutils.py
+++ lib/python/cs/fileutils.py
@@ -53,7 +53,7 @@ from cs.threads import locked
 from cs.timeutils import TimeoutError
 from cs.units import BINARY_BYTES_SCALE
 
-__version__ = '20210306-post'
+__version__ = '20210420'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
