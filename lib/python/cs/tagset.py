2b3837055b632c168de0c55cafc18a47830324b9
Cameron Simpson <cs@cskk.id.au>
2021-04-20 12:04:28 +1000

    Release information for cs.tagset-20210420. Summary: TagSet: also subclass cs.dateutils.UNIXTimeMixin. Various TagSetNamespace updates and bugfixes.

--- lib/python/cs/tagset.py
+++ lib/python/cs/tagset.py
@@ -209,7 +209,7 @@ from cs.py3 import date_fromisoformat, datetime_fromisoformat
 from cs.resources import MultiOpenMixin
 from cs.threads import locked_property
 
-__version__ = '20210404-post'
+__version__ = '20210420'
 
 DISTINFO = {
     'keywords': ["python3"],
