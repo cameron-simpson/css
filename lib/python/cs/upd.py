c3d99ceaf32a838926cab664c122dc8edb819bc4
Cameron Simpson <cs@cskk.id.au>
2021-04-28 10:05:07 +1000

    Release information for cs.upd-20210428.3. Summary: Repeat the previous release, again again.

--- lib/python/cs/upd.py
+++ lib/python/cs/upd.py
@@ -85,7 +85,7 @@ except ImportError as e:
   warning("cannot import curses: %s", e)
   curses = None
 
-__version__ = '20210428.2-post'
+__version__ = '20210428.3'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
