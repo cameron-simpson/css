66451a965fc57ad21c68c16685d87fb7611a31f2
Cameron Simpson <cs@cskk.id.au>
2021-04-28 10:00:48 +1000

    cs.upd: bump __version__ to 20210428.1-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/upd.py
+++ lib/python/cs/upd.py
@@ -85,7 +85,7 @@ except ImportError as e:
   warning("cannot import curses: %s", e)
   curses = None
 
-__version__ = '20210428.1'
+__version__ = '20210428.1-post'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
