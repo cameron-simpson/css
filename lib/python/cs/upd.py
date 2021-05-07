017d27cc619581a452b22050ae459f06c5dd4533
Cameron Simpson <cs@cskk.id.au>
2021-05-07 16:31:20 +1000

    Release information for cs.upd-20210507. Summary: Upgrade dependency on cs.context for StackableState.

--- lib/python/cs/upd.py
+++ lib/python/cs/upd.py
@@ -85,7 +85,7 @@ except ImportError as e:
   warning("cannot import curses: %s", e)
   curses = None
 
-__version__ = '20210428.4-post'
+__version__ = '20210507'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
