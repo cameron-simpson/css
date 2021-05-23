1bada16b53ad849a6b7b173c2df472fab0c97e61
Cameron Simpson <cs@cskk.id.au>
2021-05-23 21:34:28 +1000

    cs.tagset: bugfix TagSet.add: recode as add(tag) and call self.set in order to follow subclassing
    --HG--
    branch : tagsets

--- lib/python/cs/tagset.py
+++ lib/python/cs/tagset.py
@@ -604,7 +604,12 @@ class TagSet(dict, UNIXTimeMixin, FormatableMixin, AttrableMappingMixin):
     super().__setitem__(tag_name, value)
 
   # "set" mode
-  add = set
+  # note: cannot just be add=set because it won't follow subclass overrides
+  def add(self, tag, **kw):
+    ''' Adding a `Tag` calls the class `set()` method.
+    '''
+    X("plumb to self.set")
+    return self.set(tag, *kw)
 
   def __delitem__(self, tag_name):
     if tag_name not in self:
