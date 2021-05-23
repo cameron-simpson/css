4d8104849c0c63e2a8074f15e913fffa8052f903
Cameron Simpson <cs@cskk.id.au>
2021-05-24 08:38:23 +1000

    cs.onttags: break out new class Ont(TagsOntology); OntCommand: -f option, usage message etc
    --HG--
    branch : tagsets

--- lib/python/cs/onttags.py
+++ lib/python/cs/onttags.py
@@ -7,9 +7,11 @@ from contextlib import contextmanager
 import os
 from os.path import expanduser, isfile as isfilepath
 import sys
+from typeguard import typechecked
 from cs.context import stackattrs
 from cs.fstags import TagFile
 from cs.pfx import Pfx
+from cs.resources import MultiOpenMixin
 from cs.sqltags import SQLTags
 from cs.tagset import TagsOntology, TagsOntologyCommand
 
@@ -25,27 +27,64 @@ class OntCommand(TagsOntologyCommand):
   ''' Command line tool to manipulate ontologies.
   '''
 
+  GETOPT_SPEC = 'f:'
+
+  USAGE_FORMAT = '''Usage: {cmd} [-f ontology] subcommand [...]
+  -f ontology   The filename of an SQLite or flat text TagFile.
+                Default from ${ONTTAGS_PATH_ENVVAR} or {ONTTAGS_PATH_DEFAULT}.'''
+
+  USAGE_KEYWORDS = {
+      'ONTTAGS_PATH_DEFAULT': ONTTAGS_PATH_DEFAULT,
+      'ONTTAGS_PATH_ENVVAR': ONTTAGS_PATH_ENVVAR,
+  }
+
   def apply_defaults(self):
+    ''' Provide a default `self.options.ont_path`.
+    '''
     options = self.options
     options.ont_path = os.environ.get(ONTTAGS_PATH_ENVVAR)
 
+  def apply_opt(self, opt, val):
+    options = self.options
+    if opt == '-f':
+      options.ont_path = val
+    else:
+      super().apply_opt(opt, val)
+
   @contextmanager
   def run_context(self):
+    ''' Set up the ontology around a run.
+    '''
     options = self.options
     ont_path = options.ont_path
     if ont_path is None:
       ont_path = options.ont_path = expanduser(ONTTAGS_PATH_DEFAULT)
-    with Pfx("open %r", ont_path):
-      if not isfilepath(ont_path):
-        raise ValueError("not a file")
-      if ont_path.endswith('.sqlite'):
-        te_mapping = SQLTags(ont_path)
-      else:
-        te_mapping = TagFile(ont_path)
-    with te_mapping:
-      with TagsOntology(te_mapping) as ont:
-        with stackattrs(options, ontology=ont):
-          yield
+    with Ont(ont_path) as ont:
+      with stackattrs(options, ontology=ont):
+        yield
+
+class Ont(TagsOntology, MultiOpenMixin):
+  ''' A `TagsOntology` based on a persistent store.
+  '''
+
+  @typechecked
+  def __init__(self, ont_path: str):
+    self.ont_path = ont_path
+    tagsets = self.tagsets_from_path(ont_path)
+    super().__init__(tagsets)
+
+  @staticmethod
+  def tagsets_from_path(ont_path):
+    ''' Return a `TagSets` instance from `ont_path`,
+        provided for subclassing.
+    '''
+    if not isfilepath(ont_path):
+      raise ValueError("not a file: %r" % (ont_path,))
+    if ont_path.endswith('.sqlite'):
+      tagsets = SQLTags(ont_path)
+    else:
+      tagsets = TagFile(ont_path)
+    return tagsets
 
 if __name__ == '__main__':
   sys.exit(main(sys.argv))
