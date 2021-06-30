7fec1dca978022301674d971cfa91c2ef3fda24f
Cameron Simpson <cs@cskk.id.au>
2021-07-01 09:43:49 +1000

    cs.tagset: autoformat
    --HG--
    branch : media

--- lib/python/cs/tagset.py
+++ lib/python/cs/tagset.py
@@ -2291,9 +2291,7 @@ class TagsOntology(SingletonMixin):
         return self.type_map.by_type_name[type_name]
       except KeyError:
         name = self.match_func(type_name)
-        self.type_map.add = dict(
-            type_name=type_name, subtype_name=name
-        )
+        self.type_map.add = dict(type_name=type_name, subtype_name=name)
         return name
 
     def type_name(self, subtype_name):
@@ -2306,9 +2304,7 @@ class TagsOntology(SingletonMixin):
         return self.type_map.by_subtype_name[subtype_name]
       except KeyError:
         name = self.unmatch_func(subtype_name)
-        self.type_map.add = dict(
-            type_name=name, subtype_name=subtype_name
-        )
+        self.type_map.add = dict(type_name=name, subtype_name=subtype_name)
         return name
 
   @property
