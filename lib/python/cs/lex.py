2dfd374ca1490dba50deeea5f6a6a38b4cef573d
Cameron Simpson <cs@cskk.id.au>
2021-07-17 11:49:47 +1000

    Release information for cs.lex-20210717. Summary: Many many changes to FormatableMixin, FormatableFormatter and friends around supporting {foo|conv1|con2|...} instead of {foo!conv}. Still in flux. New typed_repr like typed_str but using repr.

--- lib/python/cs/lex.py
+++ lib/python/cs/lex.py
@@ -40,7 +40,7 @@ from cs.py.func import funcname
 from cs.py3 import bytes, ustr, sorted, StringTypes, joinbytes  # pylint: disable=redefined-builtin
 from cs.seq import common_prefix_length, common_suffix_length
 
-__version__ = '20210306-post'
+__version__ = '20210717'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
