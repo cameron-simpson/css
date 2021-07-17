865b27c078516abf0e26827907506df2c8eea228
Cameron Simpson <cs@cskk.id.au>
2021-07-17 12:01:56 +1000

    Release information for cs.mappings-20210717. Summary: New IndexedMapping: wrapper for another mapping providing LoadableMappingMixin stype .by_* attributes. Rename LoadableMappingMixin to IndexedSetMixin and make it abstract, rename .scan_mapping to .scan, .add_to_mapping to .add etc.

--- lib/python/cs/mappings.py
+++ lib/python/cs/mappings.py
@@ -30,7 +30,7 @@ from cs.py3 import StringTypes
 from cs.seq import the
 from cs.sharedfile import SharedAppendLines
 
-__version__ = '20210306-post'
+__version__ = '20210717'
 
 DISTINFO = {
     'description':
