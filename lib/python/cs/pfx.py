de0885a55cf0a26f492c0c547a69ee1acdd617f0
Cameron Simpson <cs@cskk.id.au>
2021-08-01 11:34:12 +1000

    Release information for cs.pfx-20210801. Summary: Bugfix for @pfx.

--- lib/python/cs/pfx.py
+++ lib/python/cs/pfx.py
@@ -55,7 +55,7 @@ from cs.py.func import funcname
 from cs.py3 import StringTypes, ustr, unicode
 from cs.x import X
 
-__version__ = '20210731-post'
+__version__ = '20210801'
 
 DISTINFO = {
     'description':
