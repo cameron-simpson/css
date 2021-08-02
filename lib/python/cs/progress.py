b2e02c2fefc460066ea5838a70f5ad1ab76e4861
Cameron Simpson <cs@cskk.id.au>
2021-08-03 09:30:06 +1000

    cs.progress: BaseProgress.iterbar: make update_min_size properly optional, was making update_frequency ineffective
    --HG--
    branch : vt

--- lib/python/cs/progress.py
+++ lib/python/cs/progress.py
@@ -454,7 +454,7 @@ class BaseProgress(object):
       width=None,
       window=None,
       update_frequency=1,
-      update_min_size=0,
+      update_min_size=None,
       report_print=None,
       runstate=None,
   ):
@@ -494,7 +494,7 @@ class BaseProgress(object):
         * `update_frequency`: optional update frequency, default `1`;
           only update the progress bar after this many iterations,
           useful if the iteration rate is quite high
-        * `update_min_size`: optional update step size, default `0`;
+        * `update_min_size`: optional update step size;
           only update the progress bar after an advance of this many units,
           useful if the iteration size increment is quite small
         * `report_print`: optional `print` compatible function
@@ -543,7 +543,8 @@ class BaseProgress(object):
       nonlocal self, proxy, statusfunc, label, width
       nonlocal iteration, last_update_iteration, last_update_pos
       if (force or iteration - last_update_iteration >= update_frequency
-          or self.position - last_update_pos >= update_min_size):
+          or (update_min_size is not None
+              and self.position - last_update_pos >= update_min_size)):
         last_update_iteration = iteration
         last_update_pos = self.position
         proxy(statusfunc(self, label, width or proxy.width))
