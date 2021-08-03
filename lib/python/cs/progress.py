66d247920b32c115e93ff3ea25558c2a58ee3a5b
Cameron Simpson <cs@cskk.id.au>
2021-08-03 13:25:40 +1000

    Release information for cs.progress-20210803. Summary: progressbar,iterbar: accept optional RunState to cancel iteration. BaseProgress.iterbar: make update_min_size properly optional, was making update_frequency ineffective.

--- lib/python/cs/progress.py
+++ lib/python/cs/progress.py
@@ -30,7 +30,7 @@ from cs.units import (
 )
 from cs.upd import Upd, print  # pylint: disable=redefined-builtin
 
-__version__ = '20210730-post'
+__version__ = '20210803'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
