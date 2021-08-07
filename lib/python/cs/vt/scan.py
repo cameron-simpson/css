aa21170e1d78112a7485b10d6abd71f35f44692c
Cameron Simpson <cs@cskk.id.au>
2021-08-07 16:35:36 +1000

    cs.vt.scan: move scanbuf up the top as py_scanbuf, rework the C import more cleanly, incorporate the MIN_BLOCKSIZE and MAX_BLOCKSIZE constants
    --HG--
    branch : vt

--- lib/python/cs/vt/scan.py
+++ lib/python/cs/vt/scan.py
@@ -1,12 +1,11 @@
-#!/usr/bin/python -tt
-#
-# Import the C buffer scanner, building it if necessary.
-# Fall back to the pure python one if we fail.
-#   - Cameron Simpson <cs@cskk.id.au>
+#!/usr/bin/env python3
 #
 
 ''' The byte scanning function scanbuf.
     In C by choice, in Python if not.
+
+    Import the C buffer scanner, building it if necessary.
+    Fall back to the pure python one if we fail.
 '''
 
 from distutils.core import setup, Extension
@@ -14,13 +13,35 @@ from os import chdir, getcwd
 from os.path import dirname, join as joinpath
 import sys
 ##from time import sleep
+from cs.context import stackattrs
 from cs.logutils import error, warning
-from cs.x import X
 
+# default constraints on the chunk sizes yielded from scans
+MIN_BLOCKSIZE = 80  # less than this seems silly
+MAX_BLOCKSIZE = 16383  # fits in 2 octets BS-encoded
+
+def py_scanbuf(hash_value, chunk):
+  ''' Pure Python scanbuf if there's no C version.
+      (Implementation 1, no block size constraints.)
+  '''
+  offsets = []
+  for offset, b in enumerate(chunk):
+    hash_value = (
+        ((hash_value & 0x001fffff) << 7)
+        | ((b & 0x7f) ^ ((b & 0x80) >> 7))
+    )
+    if hash_value % 4093 == 4091:
+      offsets.append(offset)
+  return hash_value, offsets
+
+
+# endeavour to obtain the C implementations of canbuf and scanbuf2
+# but fall back to the pure Python implementations
 try:
   from ._scan import scanbuf
-except ImportError:
-  warning("building _scan from _scan.c")
+  from ._scan import scanbuf2
+except ImportError as e:
+  warning("%s: building _scan from _scan.c", e)
 
   def do_setup():
     ''' Run distutils.core.setup from the top of the lib tree.
@@ -34,49 +55,20 @@ except ImportError:
 
   ### delay, seemingly needed to make the C version look "new"
   ##sleep(2)
-  oargv = sys.argv
   owd = getcwd()
-  sys.argv = [oargv[0], 'build_ext', '--inplace']
-  try:
-    do_setup()
-  except SystemExit as e:
-    chdir(owd)
-    error("SETUP FAILS: %s:%s", type(e), e)
-    scanbuf = None
-  else:
-    chdir(owd)
+  with stackattrs(sys, argv=[sys.argv[0], 'build_ext', '--inplace']):
     try:
-      from ._scan import scanbuf
-    except ImportError as e:
-      error("import fails after setup: %s", e)
-      scanbuf = None
-  finally:
-    sys.argv = oargv
+      do_setup()
+    except SystemExit as e:
+      chdir(owd)
+      error("SETUP FAILS: %s:%s", type(e), e)
+    else:
+      chdir(owd)
+      try:
+        from ._scan import scanbuf
+        from ._scan import scanbuf2
+      except ImportError as e:
+        error("import fails after setup: %s", e)
+        scanbuf = py_scanbuf
+        scanbuf2 = py_scanbuf2
 
-if scanbuf is None:
-  warning("using pure Python scanbuf")
-
-  def scanbuf(hash_value, chunk):
-    ''' Pure Python scanbuf if there's no C version.
-    '''
-    offsets = []
-    for offset, b in enumerate(chunk):
-      hash_value = (
-          ((hash_value & 0x001fffff) << 7)
-          | ((b & 0x7f) ^ ((b & 0x80) >> 7))
-      )
-      if hash_value % 4093 == 4091:
-        offsets.append(offset)
-    return hash_value, offsets
-
-if False:
-  # debugging wrapper
-  scanbuf0 = scanbuf
-
-  def scanbuf(h, data):
-    ''' Debugging scan function.
-    '''
-    X("scan %d bytes", len(data))
-    h2, offsets = scanbuf0(h, data)
-    ##X("scan => %r", offsets)
-    return h2, offsets
