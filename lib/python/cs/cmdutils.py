eb850876beb8997c9818a228d76fee90862e0495
Cameron Simpson <cs@cskk.id.au>
2021-08-09 11:08:27 +1000

    Release information for cs.cmdutils-20210809. Summary: Bugfix BaseCommand.cmd_help for modern API.

--- lib/python/cs/cmdutils.py
+++ lib/python/cs/cmdutils.py
@@ -23,7 +23,7 @@ from cs.pfx import Pfx
 from cs.py.doc import obj_docstring
 from cs.resources import RunState
 
-__version__ = '20210731-post'
+__version__ = '20210809'
 
 DISTINFO = {
     'description':
