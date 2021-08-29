c5516578d669fba7c104be4c97e57ae2ddd254ee
Cameron Simpson <cs@cskk.id.au>
2021-08-29 10:21:24 +1000

    cs.app.tagger.gui: new PathListWidget subclassing PySimpleGUI.tree to display a list of pathnames; NB the Qt flavour of Tree has a different implementation with a differnet .insert() call
    --HG--
    branch : tagger

--- lib/python/cs/app/tagger/gui.py
+++ lib/python/cs/app/tagger/gui.py
@@ -71,4 +71,73 @@ class ImageWidget(sg.Image):
     else:
       self.update(filename=display_pathname)
     self._pathname = new_pathname
+
+class PathListWidget(sg.Tree):
+
+  DEFAULT_NUM_ROWS = 16
+
+  def __init__(self, pathnames, num_rows=None):
+    if num_rows is None:
+      num_rows = self.DEFAULT_NUM_ROWS
+    key = uuid4()
+    treedata, pathinfo = self.make_treedata(pathnames)
+    super().__init__(
+        key=key,
+        data=treedata,
+        headings=['id', 'name'],
+        enable_events=True,
+        num_rows=num_rows,
+    )
+    self.key = key
+    self.treedata = treedata
+    self.pathinfo = pathinfo
+
+  def __getitem__(self, uuid):
+    return self.pathinfo.by_uuid[uuid]
+
+  @pfx
+  def make_treedata(self, pathnames):
+    treedata = sg.TreeData()
+    for pathname in pathnames:
+      with Pfx(pathname):
+        fullpath = realpath(pathname)
+        pathinfo = IndexedMapping(pk='fullpath')
+        top_record = UUIDedDict(fullpath=fullpath)
+        pathinfo.add(top_record)
+        treedata.insert(
+            "",
+            top_record.uuid,
+            shortpath(top_record.fullpath),
+            [basename(top_record.fullpath)],
+            icon=None,
         )
+      if isdirpath(fullpath):
+        for dirpath, dirnames, filenames in os.walk(fullpath):
+          with Pfx("walk %r", dirpath):
+            record = pathinfo.by_fullpath[dirpath]
+            parent_node = treedata.tree_dict[record.uuid]
+            for dirname in sorted(dirnames):
+              with Pfx(dirname):
+                subdir_path = joinpath(dirpath, dirname)
+                subdir_record = UUIDedDict(fullpath=subdir_path)
+                pathinfo.add(subdir_record)
+                treedata.insert(
+                    record.uuid,
+                    subdir_record.uuid,
+                    dirname,
+                    [dirname],
+                    icon=None,
+                )
+            for filename in sorted(filenames):
+              with Pfx(filenames):
+                filepath = joinpath(dirpath, filename)
+                file_record = UUIDedDict(fullpath=filepath)
+                pathinfo.add(file_record)
+                treedata.insert(
+                    record.uuid,
+                    file_record.uuid,
+                    filename,
+                    [filename],
+                    icon=None,
+                )
+    return treedata, pathinfo
