7c050aeef485205ab0adec8faffb279ae0987516
Cameron Simpson <cs@cskk.id.au>
2021-09-06 09:08:38 +1000

    cs.fileutils: bump __version__ to 20210906-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/fileutils.py
+++ lib/python/cs/fileutils.py
@@ -53,7 +53,7 @@ from cs.threads import locked
 from cs.timeutils import TimeoutError
 from cs.units import BINARY_BYTES_SCALE
 
-__version__ = '20210906'
+__version__ = '20210906-post'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
