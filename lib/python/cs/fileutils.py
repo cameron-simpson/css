999c2ff72e80254110babb0f4f9cc1559c310fd2
Cameron Simpson <cs@cskk.id.au>
2021-09-06 09:08:35 +1000

    Release information for cs.fileutils-20210906. Summary: Additional release because I'm unsure @atomic_filename made it into the previous release.

--- lib/python/cs/fileutils.py
+++ lib/python/cs/fileutils.py
@@ -53,7 +53,7 @@ from cs.threads import locked
 from cs.timeutils import TimeoutError
 from cs.units import BINARY_BYTES_SCALE
 
-__version__ = '20210731-post'
+__version__ = '20210906'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
