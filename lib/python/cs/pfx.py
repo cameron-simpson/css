ebe016c27e0e725899e351dda4b4fa43023642ae
Cameron Simpson <cs@cskk.id.au>
2021-09-06 07:16:42 +1000

    cs.pfx: bump __version__ to 20210906-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/pfx.py
+++ lib/python/cs/pfx.py
@@ -55,7 +55,7 @@ from cs.py.func import funcname
 from cs.py3 import StringTypes, ustr, unicode
 from cs.x import X
 
-__version__ = '20210906'
+__version__ = '20210906-post'
 
 DISTINFO = {
     'description':
