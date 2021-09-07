829215a3f23840b1da720dffeccf0dd4c61bfa80
Cameron Simpson <cs@cskk.id.au>
2021-09-08 09:03:42 +1000

    cs.app.dlog: extract core logging function as dlog() for easy reuse

--- lib/python/cs/app/dlog.py
+++ lib/python/cs/app/dlog.py
@@ -14,14 +14,18 @@ from os.path import expanduser
 import re
 import sys
 import time
+from typing import Optional, Iterable, Union
+
+from typeguard import typechecked
 
 from cs.cmdutils import BaseCommand
 from cs.context import stackattrs
 from cs.dateutils import datetime2unixtime
+from cs.deco import fmtdoc
 from cs.fstags import FSTags
 from cs.lex import get_dotted_identifier
-from cs.logutils import warning, debug
-from cs.pfx import Pfx, pfx_call
+from cs.logutils import debug
+from cs.pfx import Pfx, pfx, pfx_call
 from cs.sqltags import SQLTags
 from cs.tagset import Tag, TagSet
 
@@ -29,6 +33,9 @@ def main(argv=None):
   ''' Run the `dlog` command line implementation.
   '''
   return DLogCommand(argv).run()
+
+DEFAULT_DBPATH = '~/var/sqltags.sqlite'
+DEFAULT_LOGPATH = '~/var/log/dlog-quick'
 
 class DLogCommand(BaseCommand):
   ''' The `dlog` command line implementation.
@@ -43,8 +50,8 @@ class DLogCommand(BaseCommand):
         * `.when` = `time.time()`
     '''
     self.options.categories = set()
-    self.options.dbpath = expanduser('~/var/sqltags.sqlite')
-    self.options.logpath = expanduser('~/var/log/dlog-quick')
+    self.options.dbpath = expanduser(DEFAULT_DBPATH)
+    self.options.logpath = expanduser(DEFAULT_LOGPATH)
     self.options.tags = TagSet()
     self.options.when = time.time()
 
@@ -66,6 +73,7 @@ class DLogCommand(BaseCommand):
     '''
     return (category for category in s.replace(',', ' ').lower().split())
 
+  # pylint: disable=too-many-branches,too-many-locals
   def cmd_log(self, argv):
     ''' Usage: {cmd} [{{CATEGORIES:|tag=value}}...] headline
           Options:
@@ -127,29 +135,94 @@ class DLogCommand(BaseCommand):
           self.cats_from_str(options.fstags['.'].get('cs.dlog', ''))
       )
     headline = ' '.join(argv)
-    tt = time.localtime(options.when)
-    print_args = [
-        '{:4d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}'.format(
-            tt.tm_year,
-            tt.tm_mon,
-            tt.tm_mday,
-            tt.tm_hour,
-            tt.tm_min,
-            tt.tm_sec,
-        )
-    ]
-    if options.categories:
-      print_args.append(','.join(options.categories).upper() + ':')
-    print_args.append(headline)
-    if options.tags:
-      print_args.append('[' + ' '.join(map(str, options.tags)) + ']')
-    with pfx_call(open, options.logpath, 'a') as logf:
-      print(*print_args, file=logf)
-    # add the headline and categories to the tags
-    options.tags.add('headline', headline)
-    if options.categories:
-      options.tags.add('categories', list(options.categories))
-    sqltags.default_factory(None, unixtime=options.when, tags=options.tags)
+    dlog(
+        headline,
+        logpath=options.logpath,
+        sqltags=sqltags,
+        tags=options.tags,
+        categories=options.categories,
+        when=options.when
+    )
+
+@pfx
+@typechecked
+@fmtdoc
+def dlog(
+    headline: str,
+    *,
+    logpath: Optional[str] = None,
+    sqltags: Optional[SQLTags] = None,
+    tags=None,
+    categories: Optional[Iterable] = None,
+    when: Union[None, int, float, datetime] = None,
+):
+  ''' Log `headline` to the dlog.
+
+      Parameters:
+      * `headline`: the log line message
+      * `logpath`: optional text log pathname,
+        default `{DEFAULT_LOGPATH}` from DEFAULT_LOGPATH
+      * `sqltags`: optional `SQLTags` instance,
+        default uses `{DEFAULT_DBPATH}` from DEFAULT_DBPATH
+      * `tags`: optional iterable of `Tag`s to associate with the log entry
+      * `categories`: optional iterable of category strings
+      * `when`: optional UNIX time or `datetime`, default now
+  '''
+  if sqltags is None:
+    # pylint: disable=redefined-argument-from-local
+    with SQLTags(expanduser(DEFAULT_DBPATH)) as sqltags:
+      dlog(
+          headline,
+          logpath=logpath,
+          sqltags=sqltags,
+          tags=tags,
+          categories=categories,
+          when=when
+      )
+  if logpath is None:
+    logpath = expanduser(DEFAULT_LOGPATH)
+  logtags = TagSet()
+  if tags:
+    for tag in tags:
+      logtags.add(tag)
+  categories = sorted(
+      () if categories is None else set(map(str.lower, categories))
+  )
+  if when is None:
+    when = time.time()
+  elif isinstance(when, (int, float)):
+    pass
+  elif isinstance(when, datetime):
+    dt = when
+    if dt.tzinfo is None:
+      # create a nonnaive datetime in the local zone
+      dt = dt.astimezone()
+    when = datetime2unixtime(dt)
+  else:
+    raise TypeError("when=%s:%r: unhandled type" % (type(when).__name__, when))
+  tt = time.localtime(when)
+  print_args = [
+      '{:4d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}'.format(
+          tt.tm_year,
+          tt.tm_mon,
+          tt.tm_mday,
+          tt.tm_hour,
+          tt.tm_min,
+          tt.tm_sec,
+      )
+  ]
+  if categories:
+    print_args.append(','.join(categories).upper() + ':')
+  print_args.append(headline)
+  if logtags:
+    print_args.append('[' + ' '.join(map(str, logtags)) + ']')
+  with pfx_call(open, logpath, 'a') as logf:
+    print(*print_args, file=logf)
+  # add the headline and categories to the tags
+  logtags.add('headline', headline)
+  if categories:
+    logtags.add('categories', sorted(categories))
+  sqltags.default_factory(None, unixtime=when, tags=tags)
 
 if __name__ == '__main__':
   sys.exit(main(sys.argv))
