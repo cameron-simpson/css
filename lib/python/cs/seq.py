5d09b24db846dd0a23ad4515b9695591c6469cc2
Cameron Simpson <cs@cskk.id.au>
2021-09-24 12:29:12 +1000

    cs.seq: bump __version__ to 20210924-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/seq.py
+++ lib/python/cs/seq.py
@@ -18,7 +18,7 @@ from threading import Lock, Condition, Thread
 from cs.deco import decorator
 from cs.gimmicks import warning
 
-__version__ = '20210924'
+__version__ = '20210924-post'
 
 DISTINFO = {
     'description':
