76a70cc3fc5cf4351bf7a271c1135df1c14e1bb1
Cameron Simpson <cs@cskk.id.au>
2021-11-27 10:28:54 +1100

    cs.shims: new GetterSetterProxy class to wrap an object with getter/setter methods and provide snake case attribute access
    --HG--
    branch : 3d

--- lib/python/cs/shims.py
+++ lib/python/cs/shims.py
@@ -62,3 +62,62 @@ def call_setters(objs, settings):
         )
       with Pfx("%s.%s(%s)", s(obj), set_name, r(v)):
         setter(v)
+
+class GetterSetterProxy:
+  ''' A proxy class to convert snake case attribute access
+      to C++ or Javaesque camel cased getter and setter method calls.
+
+      Attributes which already exist on the original object are
+      accessed in preference to the getter/setter methods.
+
+      The original object is available as the `._proxied` attribute.
+  '''
+
+  def __init__(self, proxied):
+    self.__dict__['_proxied'] = proxied
+
+  def __getattr__(self, attr):
+    # try direct access first
+    try:
+      return getattr(self._proxied, attr)
+    except AttributeError as e:
+      # not a native attribute, try a getter method
+      # for a public name
+      if not attr.startswith('_'):
+        camel_attr = camelcase(attr)
+        get_name = 'get' + camel_attr[0].upper() + camel_attr[1:]
+        try:
+          pattr = getattr(self._proxied, get_name)
+        except AttributeError:
+          pass
+        else:
+          return pattr()
+      # no getter, no attribute
+      raise AttributeError(
+          "%s(%s).%s: no .%s on %s" % (
+              type(self).__name__, type(self._proxied).__name__, attr, attr,
+              r(self._proxied)
+          )
+      )
+
+  def __setattr__(self, attr, value):
+    # only intercept public names
+    if not attr.startswith('_'):
+      if hasattr(self._proxied, attr):
+        # if the proxied object has this attribute, set it there
+        setattr(self._proxied, attr, value)
+      else:
+        # not a native of the proxy, try the setter method
+        camel_attr = camelcase(attr)
+        set_name = 'set' + camel_attr[0].upper() + camel_attr[1:]
+        try:
+          pattr = getattr(self._proxied, set_name)
+        except AttributeError:
+          # no setter, set it on the proxied object directly
+          setattr(self._proxied, attr, value)
+        else:
+          # call the setter to set the value
+          pattr(value)
+    else:
+      # private attribute, set it in .__dict__
+      self.__dict__[attr] = value
