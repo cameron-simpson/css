8b716874a2c5106905b097cfa6eb5f556b71db8c
Cameron Simpson <cs@cskk.id.au>
2022-01-26 16:20:59 +1100

    cs.result: Task: supply func and optional args at init time, new cancel_on_exception and cancel_on_result parameters, update docstrings, related updates

--- lib/python/cs/result.py
+++ lib/python/cs/result.py
@@ -69,8 +69,9 @@ from cs.mappings import AttrableMapping
 from cs.pfx import Pfx, pfx_method
 from cs.py.func import funcname
 from cs.py3 import Queue, raise3, StringTypes
+from cs.resources import RunState
 from cs.seq import seq, Seq
-from cs.threads import bg as bg_thread
+from cs.threads import bg as bg_thread, State as ThreadState
 
 __version__ = '20210420-post'
 
@@ -88,6 +89,7 @@ DISTINFO = {
         'cs.pfx',
         'cs.py.func',
         'cs.py3',
+        'cs.resources',
         'cs.seq',
         'cs.threads',
         'icontract',
@@ -573,6 +575,20 @@ class Task(Result):
   ''' A task which may require the completion of other tasks.
       This is a subclass of `Result`.
 
+      Keyword parameters:
+      * `cancel_on_exception`: if true, cancel this `Task` if `.call`
+        raises an exception; the default is `False`, allowing repair
+        and retry
+      * `cancel_on_result`: optional callable to test the `Task.result`
+        after `.call`; if it returns `True` the `Task` is marked
+        as cancelled
+      * `func`: the function to call to complete the `Task`;
+        it will be called as `func(*func_args,**func_kwargs)`
+      * `func_args`: optional positional arguments, default `()`
+      * `func_kwargs`: optional keyword arguments, default `{}`
+      * `lock`: optional lock, default an `RLock`
+      Other arguments are passed to the `Result` initialiser.
+
       Example:
 
           t1 = Task(name="task1")
@@ -582,15 +598,58 @@ class Task(Result):
           t2.require(t1)
           # try to run sleep(5) for t2 immediately after t1 completes
           t1.notify(t2.call, sleep, 5)
+
+      The model here may not be quite as expected; it is aimed at
+      tasks which can be repaired and rerun.
+      As such, if `self.call(func,...)` raises an exception from
+      `func` then this `Task` will still block dependent `Task`s.
+      Dually, a `Task` which completes without an exception is
+      considered complete and does not block dependent `Task`s.
+      To cancel dependent `Tasks` the function should raise a
+      `CancellationError`.
+
+      Users wanting more immediate semantics can supply `cancel_on_exception`
+      and/or `cancel_on_result` to control these behaviours.
+
+      Example:
+
+          t1 = Task(name="task1")
+          t1.bg(time.sleep, 2)
+          t2 = Task("name="task2")
+          # prevent t2 from running until t1 completes
+          t2.require(t1)
+          # try to run sleep(5) for t2 immediately after t1 completes
+          t1.notify(t2.call, sleep, 5)
+
+          >>>
   '''
 
   _seq = Seq()
+  _state = ThreadState(current_task=None)
 
-  def __init__(self, *a, lock=None, **kw):
+  def __init__(
+      self,
+      *a,
+      lock=None,
+      cancel_on_exception=False,
+      cancel_on_result=None,
+      func,
+      func_args=(),
+      func_kwargs=None,
+      **kw
+  ):
     if lock is None:
       lock = RLock()
+    if func_kwargs is None:
+      func_kwargs = {}
     super().__init__(*a, lock=lock, **kw)
     self._required = set()
+    self.cancel_on_exception = cancel_on_exception
+    self.cancel_on_result = cancel_on_result
+    self.func = func
+    self.func_args = func_args
+    self.func_kwargs = func_kwargs
+    self.runstate = RunState(self.name)
 
   def __hash__(self):
     return id(self)
@@ -598,8 +657,22 @@ class Task(Result):
   def __eq__(self, otask):
     return self is otask
 
+  @classmethod
+  def current_task(cls):
+    ''' The current `Task`, valid during `Task.call()`.
+        This allows the function called by the `Task` to access the
+        task, typically to poll its `.runstate` attribute.
+    '''
+    return cls._state.current_task  # pylint: disable=no-member
+
+  def abort(self):
+    ''' Calling `abort()` calls `self.runstate.cancel()` to indicate
+        to the running function that it should cease operation.
+    '''
+    self.runstate.cancel()
+
   def required(self):
-    ''' Return a `set` contained any required tasks.
+    ''' Return a `set` containing any required tasks.
     '''
     with self._lock:
       return set(self._required)
@@ -618,25 +691,26 @@ class Task(Result):
     otask.require(self)
 
   def then(self, func, *a, **kw):
-    ''' Queue a call to `func(*a,**kw)` to run after the completion of this task.
+    ''' Queue a call to `func(*a,**kw)` to run after the completion of
+        this task.
 
         This supports a chain of actions:
 
-            >>> t = Task()
+            >>> t = Task(func=lambda: 1)
             >>> final_t = t.then(print,1).then(print,2)
             >>> final_t.ready   # the final task has not yet run
             False
             >>> # finalise t, wait for final_t (which runs immediately)
-            >>> t.result = 1; print(final_t.join())
+            >>> t.call(); print(final_t.join())
             1
             2
             (None, None)
             >>> final_t.ready
             True
     '''
-    post_task = type(self)()
+    post_task = type(self)(func=func, func_args=a, func_kwargs=kw)
     post_task.require(self)
-    self.notify(lambda _: post_task.bg(func, *a, **kw))
+    self.notify(lambda _: post_task.bg())
     return post_task
 
   def blockers(self):
@@ -657,32 +731,74 @@ class Task(Result):
         yield otask
         continue
 
-  def call(self, func, *a, **kw):
-    ''' Attempt to perform the `Task` by calling `func(*a,**kw)`.
+  # pylint: disable=arguments-differ
+  def bg(self):
+    ''' Submit a function to complete the `Task` in a separate `Thread`,
+        returning the `Thread`.
+
+        This dispatches a `Thread` to run `self.call()`
+        and as such the `Task` must be in "pending" state,
+        and transitions to "running".
+    '''
+    return bg_thread(self.call, name=self.name)
+
+  # pylint: disable=arguments-differ
+  def call(self):
+    ''' Attempt to perform the `Task` by calling `func(*func_args,**func_kwargs)`.
+
         If we are cancelled, raise `CancellationError`.
         If there are blocking required tasks, raise `BlockedError`.
-        Otherwise run `Result.call(func,*a,**kw)`.
-    '''
-    if self.cancelled:
-      raise CancellationError()
-    for otask in self.blockers():
-      raise BlockedError("%s blocked by %s" % (self, otask))
-    if self.cancelled:
-      raise CancellationError()
-    try:
-      super().call(func, *a, **kw)
-    except CancellationError:
-      self.cancel()
-      raise
+        Otherwise run `r=func(self,*self.func_args,**self.func_kwargsw)`
+        with the following effects:
+        * if `func()` raises a `CancellationError`, cancel the `Task`
+        * otherwise, if an exception is raised and `self.cancel_on_exception`
+          is true, cancel the `Task`;
+          store the exception information from `sys.exc_info()` as `self.exc_info`
+          regardless
+        * otherwise, if `self.cancel_on_result` is not `None`
+          and `self.cancel_on_result(r)` is true, cancel the `Task`;
+          store `r` as `self.result` regardless
+        If we were cancelled, raise `CancellationError`.
 
-  def callif(self, func, *a, **kw):
-    ''' Trigger a call to `func(*a,**kw)` if we're pending and not
-        blocked or cancelled.
+        During the duration of the call the property `Task.current_task`
+        is set to `self` allowing access to the `Task`.
+        A typical use is to access the current `Task`'s `.runstate`
+        attribute which can be polled by long running tasks to
+        honour calls to `Task.abort()`.
+    '''
+    if not self.cancelled:
+      for otask in self.blockers():
+        raise BlockedError("%s blocked by %s" % (self, otask))
+      if not self.cancelled:
+        state = type(self)._state
+        with self._lock:
+          with state(current_task=self):
+            try:
+              with self.runstate:
+                r = self.func(*self.func_args, **self.func_kwargs)
+            except CancellationError:
+              self.cancel()
+            except BaseException:
+              if self.cancel_on_exception:
+                self.cancel()
+              # store the exception regardless
+              self.exc_info = sys.exc_info()
+            else:
+              if self.cancel_on_result and self.cancel_on_result(r):
+                self.cancel()
+              # store the result regardless
+              self.result = r
+    if self.cancelled:
+      raise CancellationError()
+
+  def callif(self):
+    ''' Trigger a call to `func(self,*self.func_args,**self.func_kwargsw)`
+        if we're pending and not blocked or cancelled.
     '''
     with self._lock:
       if not self.ready:
         try:
-          self.call(func, *a, **kw)
+          self.call()
         except (BlockedError, CancellationError) as e:
           debug("%s.callif: %s", self, e)
 
@@ -690,6 +806,7 @@ class Task(Result):
 def task(func, task_class=Task):
   ''' Decorator for a function which runs it as a `Task`.
       The function may still be called directly.
+      The function should accept a `Task` as its first argument.
 
       The following function attributes are provided:
       * `dispatch(after=(),deferred=False,delay=0.0)`: run this function
@@ -709,16 +826,29 @@ def task(func, task_class=Task):
           >>> # dispatch f(5) after 0.5s, get Task
           >>> t0 = time.time()
           >>> ft = f.dispatch((5,), delay=0.5)
-          >>> # calling  Task, as with a Result, is like calling the function
+          >>> # calling a Task, as with a Result, is like calling the function
           >>> print(ft())
           10
           >>> # check that we were blocked for 0.5s
           >>> now = time.time()
           >>> now - t0 >= 0.5
           True
-
-
   '''
+
+  def make_task(func, a, kw):
+    ft_name = funcname(func) + '-task'
+    if a:
+      ft_name = ft_name + ':' + repr(a)
+    if kw:
+      ft_name = ft_name + ':' + repr(kw)
+    return task_class(name=ft_name, func=func, func_args=a, func_kwargs=kw)
+
+  def task_func_wrapper(*a, **kw):
+    ''' Run the function via a `Task`.
+    '''
+    ft = make_task(func, a, kw)
+    ft.call()
+    return ft()
 
   # pylint: disable=redefined-outer-name
   def dispatch(a=None, kw=None, after=(), deferred=False, delay=0.0):
@@ -746,29 +876,26 @@ def task(func, task_class=Task):
     if not isinstance(after, list):
       after = list(after)
     if delay > 0.0:
-      delay_task = task_class(name="sleep(%s)" % (delay,))
-      delay_task.bg(time.sleep, delay)
+      delay_task = task_class(
+          name="sleep(%s)" % (delay,), func=time.sleep, func_args=(delay,)
+      )
+      delay_task.bg()
       after.append(delay_task)
-    ft_name = funcname(func) + '-task'
-    if a:
-      ft_name = ft_name + ':' + repr(a)
-    if kw:
-      ft_name = ft_name + ':' + repr(kw)
-    ft = task_class()
+    ft = make_task(func, a, kw)
     if after:
       for otask in after:
         ft.require(otask)
     if not deferred:
       if after:
         for otask in after:
-          otask.notify(lambda _: ft.callif(func, *a, **kw))
+          otask.notify(lambda _: ft.callif())
       else:
         # dispatch the task immediately, but in another Thread
-        ft.bg(func, *a, **kw)
+        ft.bg(ft.call, func, *a, **kw)
     return ft
 
-  func.dispatch = dispatch
-  return func
+  task_func_wrapper.dispatch = dispatch
+  return task_func_wrapper
 
 if __name__ == '__main__':
   import cs.result_tests
