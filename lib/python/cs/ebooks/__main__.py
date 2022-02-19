7c2b4cbadf02fe128280b804aef84f90c7e35114
Cameron Simpson <cs@cskk.id.au>
2022-02-20 10:06:55 +1100

    cs.ebooks.__main__: EBooksCommand: attach the calibre subcommand
    --HG--
    branch : ebooks

--- lib/python/cs/ebooks/__main__.py
+++ lib/python/cs/ebooks/__main__.py
@@ -7,12 +7,14 @@ import sys
 
 from cs.cmdutils import BaseCommand
 
+from .calibre import CalibreCommand
 from .kindle import KindleCommand
 
 class EBooksCommand(BaseCommand):
   ''' Ebooks utility command.
   '''
 
+  cmd_calibre = CalibreCommand
   cmd_kindle = KindleCommand
 
 sys.exit(EBooksCommand(sys.argv).run())
