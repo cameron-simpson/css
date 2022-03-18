09b19c350ba66f973e7f7021492830b1399eb034
Cameron Simpson <cs@cskk.id.au>
2022-03-18 12:08:38 +1100

    cs.app.ydl: bump __version__ to 20220318-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/app/ydl.py
+++ lib/python/cs/app/ydl.py
@@ -43,7 +43,7 @@ from cs.progress import Progress, OverProgress
 from cs.result import bg as bg_result, report
 from cs.upd import Upd, print  # pylint: disable=redefined-builtin
 
-__version__ = '20220318'
+__version__ = '20220318-post'
 
 DISTINFO = {
     'keywords': ["python3"],
