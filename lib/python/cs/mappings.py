65e6b722265201a3b1807e9af841ab7350f6e9ac
Cameron Simpson <cs@cskk.id.au>
2022-03-18 16:12:26 +1100

    Release information for cs.mappings-20220318. Summary: Bump cs.sharedfile requirement to get an import fix.

--- lib/python/cs/mappings.py
+++ lib/python/cs/mappings.py
@@ -30,7 +30,7 @@ from cs.py3 import StringTypes
 from cs.seq import the
 from cs.sharedfile import SharedAppendLines
 
-__version__ = '20211208-post'
+__version__ = '20220318'
 
 DISTINFO = {
     'description':
