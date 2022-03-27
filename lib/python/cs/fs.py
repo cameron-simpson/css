3f99e0cce9ba02cdaa1f8a0dbffa550dee609a7a
Cameron Simpson <cs@cskk.id.au>
2022-03-27 19:20:15 +1100

    cs.fs: new @atomic_directory(infill_func,make_placeholder=False) decorator; remove debugging
    --HG--
    branch : pypi

--- lib/python/cs/fs.py
+++ lib/python/cs/fs.py
@@ -5,9 +5,63 @@
 '''
 
 import os
-from os.path import join as joinpath, relpath
+from os.path import (
+    basename, dirname, exists as existspath, isdir as isdirpath, join as
+    joinpath, relpath
+)
+from tempfile import TemporaryDirectory
 
-from cs.x import X
+from cs.deco import decorator
+from cs.pfx import pfx_call
+
+@decorator
+def atomic_directory(infill_func, make_placeholder=False):
+  ''' Decorator for a function which fills in a directory
+      which calls the function against a temporary directory
+      then renames the temporary to the target name on completion.
+
+      Parameters:
+      * `infill_func`: the function to fill in the target directory
+      * `make_placeholder`: optional flag, default `False`:
+        if true an empty directory will be make at the target name
+        and after completion it will be removed and the completed
+        directory renamed to the target name
+  '''
+
+  def atomic_directory_wrapper(dirpath, *a, **kw):
+    assert isinstance(dirpath, str
+                      ), "dirpath not a str: %s:%r" % (type(dirpath), dirpath)
+    remove_placeholder = False
+    if make_placeholder:
+      # prevent other users from using this directory
+      pfx_call(os.mkdir, dirpath, 0o000)
+      remove_placeholder = True
+    else:
+      if existspath(dirpath):
+        raise ValueError("directory already exists: %r" % (dirpath,))
+    work_dirpath = dirname(dirpath)
+    try:
+      with TemporaryDirectory(
+          dir=work_dirpath,
+          prefix='.tmp--atomic_directory--',
+          suffix='--' + basename(dirpath),
+      ) as tmpdirpath:
+        result = infill_func(tmpdirpath, *a, **kw)
+        if remove_placeholder:
+          pfx_call(os.rmdir, dirpath)
+          remove_placeholder = False
+        elif existspath(dirpath):
+          raise ValueError("directory already exists: %r" % (dirpath,))
+        pfx_call(os.rename, tmpdirpath, dirpath)
+        pfx_call(os.mkdir, tmpdirpath, 0o000)
+    except:
+      if remove_placeholder and isdirpath(dirpath):
+        pfx_call(os.rmdir, dirpath)
+      raise
+    else:
+      return result
+
+  return atomic_directory_wrapper
 
 def rpaths(
     dirpath='.', *, only_suffixes=None, skip_suffixes=None, sort_paths=False
@@ -27,9 +81,7 @@ def rpaths(
     only_suffixes = tuple(only_suffixes)
   if skip_suffixes is not None:
     skip_suffixes = tuple(skip_suffixes)
-  X("os.walk %s", dirpath)
   for subpath, subdirnames, filenames in os.walk(dirpath):
-    X("RPATHS SUBPATH = %s", subpath)
     if sort_paths:
       subdirnames[:] = sorted(subdirnames)
       filenames = sorted(filenames)
