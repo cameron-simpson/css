43d49eb2b4b5599046841c1f532ea6c1c234bba5
Cameron Simpson <cs@cskk.id.au>
2022-03-27 20:17:10 +1100

    cs.fs: bump __version__ to 20220327-post to avoid misleading value for future unreleased changes [IGNORE]
    --HG--
    branch : pypi

--- lib/python/cs/fs.py
+++ lib/python/cs/fs.py
@@ -14,7 +14,7 @@ from tempfile import TemporaryDirectory
 from cs.deco import decorator
 from cs.pfx import pfx_call
 
-__version__ = '20220327'
+__version__ = '20220327-post'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
