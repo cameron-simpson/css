6543f515eae419cdba873bdedd640c5cad2f39f4
Cameron Simpson <cs@cskk.id.au>
2022-04-09 10:03:33 +1000

    cs.timeseries: fill out load/save stuff, docstrings etc
    --HG--
    branch : timeseries

--- lib/python/cs/timeseries.py
+++ lib/python/cs/timeseries.py
@@ -5,10 +5,11 @@
 '''
 
 from array import array, typecodes
+from contextlib import contextmanager
 from functools import partial
 import os
 from struct import pack, Struct
-from typing import Tuple, Union
+from typing import Optional, Tuple, Union
 
 from icontract import ensure, require
 from typeguard import typechecked
@@ -16,6 +17,7 @@ from typeguard import typechecked
 from cs.deco import cachedmethod
 from cs.logutils import warning
 from cs.pfx import pfx, pfx_call
+from cs.resources import MultiOpenMixin
 
 from cs.x import X
 
@@ -59,8 +61,32 @@ def struct_format(typecode, big_endian):
   '''
   return ('>' if big_endian else '<') + typecode
 
-class TimeSeries:
+@contextmanager
+def array_byteswapped(ary):
+  ''' Context manager to byteswap the `array.array` `ary` temporarily.
+  '''
+  ary.byteswap()
+  try:
+    yield
+  finally:
+    ary.byteswap()
+
+class TimeSeries(MultiOpenMixin):
   ''' A single time series for a single data field.
+
+      This provides easy access to a time series data file.
+      The instance can be indexed by UNIX time stamp for time based access
+      or its `.array` property can be accessed for the raw data.
+
+      Read only users can just instantiate an instance.
+      Read/write users should use the instance as a context manager,
+      which will automatically rewrite the file with the array data
+      on exit.
+
+      The data file itself has a header indicating the file data big endianness
+      and datum type (an `array.array` type code).
+      This is automatically honoured on load and save.
+      Note that the header _does not_ indicate the `start`,`step` time range of the data.
   '''
 
   DOTEXT = '.csts'
@@ -68,21 +94,26 @@ class TimeSeries:
   HEADER_LENGTH = 8
 
   @typechecked
-  @require(lambda type_: type_ is int or type_ is float)
-  @require(lambda step: step > 0)
   def __init__(
       self, fspath: str, typecode: str, start: Union[int, float],
       step: Union[int, float]
   ):
     ''' Prepare a new time series stored in the file at `fspath`
-          containing machine data for the time series values.
+        containing machine data for the time series values.
 
-          Parameters:
-          * `fspath`: the filename of the data file
-          * `typecode` the expected `array.typecode` value of the data
-          * `start`: the UNIX epoch time for the first datum
-          * `step`: the increment between data times
-      '''
+        Parameters:
+        * `fspath`: the filename of the data file
+        * `typecode` the expected `array.typecode` value of the data
+        * `start`: the UNIX epoch time for the first datum
+        * `step`: the increment between data times
+    '''
+    if typecode not in SUPPORTED_TYPECODES:
+      raise ValueError(
+          "expected typecode to be one of %r, got %r" %
+          (tuple(SUPPORTED_TYPECODES.keys()), typecode)
+      )
+    if step <= 0:
+      raise ValueError("step should be >0, got %s" % (step,))
     self.fspath = fspath
     self.typecode = typecode
     self.start = start
@@ -98,8 +129,7 @@ class TimeSeries:
         )
     except FileNotFoundError:
       # file does not exist, use our native ordering
-      self.big_endian = NATIVE_BIGENDIANNESS[typecode]
-      self._byte_swapped = False
+      self.file_big_endian = NATIVE_BIGENDIANNESS[typecode]
     else:
       file_typecode, file_big_endian = self.parse_header(header_bs)
       if typecode != file_typecode:
@@ -107,20 +137,38 @@ class TimeSeries:
             "expected typecode %r but the existing file contains typecode %r" %
             (typecode, file_typecode)
         )
-      self.big_endian = file_big_endian
+      self.file_big_endian = file_big_endian
     self._itemsize = array(typecode).itemsize
     assert self._itemsize == 8
-    self._byte_swapped = self.big_endian != NATIVE_BIGENDIANNESS[typecode]
-    self._struct_format = ('>' if self.big_endian else '<') + typecode
-    self._struct = Struct(self._struct_format)
+    struct_format = self.make_struct_format(typecode, self.file_big_endian)
+    self._struct = Struct(struct_format)
     assert self._struct.calcsize() == self._itemsize
+
+  @contextmanager
+  def startup_shutdown(self):
+    yield self
+    if self._array is not None:
+      self.save()
+
+  @staticmethod
+  def make_struct_format(typecode, big_endian):
+    ''' Make a `struct` format string for the data in a file.
+    '''
+    return ('>' if big_endian else '<') + typecode
 
   @property
   def header(self):
     ''' The header magic bytes.
-      '''
-    header_bs = self.MAGIC + self._struct_format.encode('ascii') + b'__'
-    assert len(header_bs) == self.HEADER_LENGTH
+    '''
+    return self.make_header(self.typeocde, self.file_big_endian)
+
+  @classmethod
+  def make_header(cls, typecode, big_endian):
+    header_bs = (
+        cls.MAGIC +
+        cls.make_struct_format(typecode, big_endian).encode('ascii') + b'__'
+    )
+    assert len(header_bs) == cls.HEADER_LENGTH
     return header_bs
 
   @classmethod
@@ -185,22 +233,73 @@ class TimeSeries:
         This loads the array data from `self.fspath` on first use.
     '''
     assert self._array is None
-    ary = array(self.typecode)
     try:
-      with os_open(self.fspath, 'rb') as tsf:
-        header_bs = tsf.read(self.HEADER_LENGTH)
-        assert len(header_bs) == self.HEADERLENGTH
-        flen = os.fstat(tsf.fileno()).st_size
-        datalen = flen - len(header_bs)
-        if flen % self._itemsize != 0:
-          warning(
-              "data length:%d is not a multiple of item size:%d", datalen,
-              self._itemsize
-          )
-        datum_count = datalen // self._item_size
-        ary.from_file(tsf, datum_count)
-      if self._byte_swapped:
-        ary.byteswap()
+      ary = self.load_from(self.fspath, self.typecode)
     except FileNotFoundError:
-      pass
+      # no file, empty array
+      ary = array(self.typecode)
     return ary
+
+  def save(self, fspath=None):
+    ''' Save the time series to `fspath`, default `self.fspath`.
+    '''
+    assert self._array is not None, "array not yet loaded, nothing to save"
+    if fspath is None:
+      fspath = self.fspath
+    self.save_to(self.array, fspath, self.file_bigendian)
+
+  @classmethod
+  @ensure(
+      lambda typecode, result: typecode is None or result.typecode == typecode
+  )
+  def load_from(cls, fspath, typecode=None):
+    ''' Load the data from `fspath`, return an `array.array(typecode)`
+        containing the file data.
+    '''
+    ary = array(typecode)
+    with os_open(fspath, 'rb') as tsf:
+      header_bs = tsf.read(cls.HEADER_LENGTH)
+      assert len(header_bs) == cls.HEADERLENGTH
+      h_typecode, h_bigendian = cls.parse_header(header_bs)
+      if typecode is not None and h_typecode != typecode:
+        raise ValueError(
+            "expected typecode %r, file contains typecode %r" %
+            (typecode, h_typecode)
+        )
+      flen = os.fstat(tsf.fileno()).st_size
+      datalen = flen - len(header_bs)
+      if flen % ary.itemsize != 0:
+        warning(
+            "data length:%d is not a multiple of item size:%d", datalen,
+            ary.itemsize
+        )
+      datum_count = datalen // ary.item_size
+      ary.from_file(tsf, datum_count)
+      if h_bigendian != NATIVE_BIGENDIANNESS[h_typecode]:
+        ary.byteswap()
+    return ary
+
+  @classmethod
+  @typechecked
+  def save_to(cls, ary, fspath: str, big_endian=Optional[bool]):
+    ''' Save the array `ary` to `fspath`.
+        If `big_endian` is specified, write the data in that endianness.
+        The default is to use the native endianness.
+
+        *Warning*:
+        if the file endianness is not the native endianness,
+        the array will be byte swapped temporarily
+        during the file write operation.
+        Concurrent users should avoid using the array during this function.
+    '''
+    native_bigendian = NATIVE_BIGENDIANNESS[ary.typecode]
+    if big_endian is None:
+      big_endian = native_bigendian
+    header_bs = cls.make_header(ary.typecode, big_endian)
+    with os_open(fspath, 'wb') as tsf:
+      tsf.write(header_bs)
+      if big_endian != native_bigendian:
+        with array_byteswapped(ary):
+          ary.tofile(tsf)
+      else:
+        ary.tofile(tsf)
