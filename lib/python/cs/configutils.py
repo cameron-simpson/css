7a82ca7a79d5e7294db61d7298a1b775c1949ac8
Cameron Simpson <cs@cskk.id.au>
2022-04-19 19:23:32 +1000

    cs.configutils: docstring updates, DISTINFO tweak
    --HG--
    branch : timeseries

--- lib/python/cs/configutils.py
+++ lib/python/cs/configutils.py
@@ -21,7 +21,6 @@ from cs.threads import locked
 __version__ = '20210306-post'
 
 DISTINFO = {
-    'description': "utility functions for .ini style configuration files",
     'keywords': ["python2", "python3"],
     'classifiers': [
         "Programming Language :: Python",
@@ -40,8 +39,8 @@ DISTINFO = {
 def load_config(config_path, parser=None):
   ''' Load a configuration from the named `config_path`.
 
-      If `parser` is missing or None, use SafeConfigParser (just
-      ConfigParser in Python 3).
+      If `parser` is missing or `None`, use `SafeConfigParser` (just
+      `ConfigParser` in Python 3).
       Return the parser.
   '''
   if parser is None:
@@ -51,8 +50,8 @@ def load_config(config_path, parser=None):
   return CP
 
 class ConfigWatcher(Mapping):
-  ''' A monitor for a windows style .ini file.
-      The current SafeConfigParser object is presented as the .config property.
+  ''' A monitor for a windows style `.ini` file.
+      The current `SafeConfigParser` object is presented as the `.config` property.
   '''
 
   def __init__(self, config_path):
@@ -98,7 +97,7 @@ class ConfigWatcher(Mapping):
     CP = self.config
     if CP is None or (section != 'DEFAULT' and not CP.has_section(section)):
       return []
-    return [ name for name, value in CP.items(section) ]
+    return [name for name, value in CP.items(section)]
 
   def section_value(self, section, key):
     ''' Return the value of [section]key.
@@ -111,7 +110,7 @@ class ConfigWatcher(Mapping):
   #### Mapping methods.
   @locked
   def __getitem__(self, section):
-    ''' Return the ConfigWatcher for the specified section.
+    ''' Return the `ConfigSectionWatcher` for the specified section.
     '''
     watchers = self._watchers
     if section not in watchers:
@@ -135,9 +134,9 @@ class ConfigSectionWatcher(Mapping):
   '''
 
   def __init__(self, config, section, defaults=None):
-    ''' Initialise a ConfigSectionWatcher to monitor a particular section
+    ''' Initialise a `ConfigSectionWatcher` to monitor a particular section
         of a config file.
-        `config`: path of config file, or ConfigWatcher
+        `config`: path of config file or `ConfigWatcher`
         `section`: the section to watch
         `defaults`: the defaults section to use, default 'DEFAULT'
     '''
@@ -163,7 +162,7 @@ class ConfigSectionWatcher(Mapping):
     return self.config.path
 
   def as_dict(self):
-    ''' Return the config section as a dict.
+    ''' Return the config section as a `dict`.
     '''
     d = {}
     for k in self:
