94944e58e01cb05a52cd272048ad641633658d70
Cameron Simpson <cs@cskk.id.au>
2022-04-29 18:51:42 +1000

    cs.fileutils: bump __version__ to 20220429-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/fileutils.py
+++ lib/python/cs/fileutils.py
@@ -51,7 +51,7 @@ from cs.result import CancellationError
 from cs.threads import locked
 from cs.units import BINARY_BYTES_SCALE
 
-__version__ = '20220429'
+__version__ = '20220429-post'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
