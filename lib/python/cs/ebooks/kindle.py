4dae6767442582201f8fb38934dc50a6b5554bc3
Cameron Simpson <cs@cskk.id.au>
2022-05-05 09:13:19 +1000

    cs.ebooks.kindle: KindleCommand: new cmd_import_tags to import Calibre information into the KindleBook fstags, available as KinbleBook.tags
    --HG--
    branch : ebooks

--- lib/python/cs/ebooks/kindle.py
+++ lib/python/cs/ebooks/kindle.py
@@ -622,10 +622,9 @@ class KindleCommand(BaseCommand):
     force = options.force
     quiet = options.quiet
     verbose = options.verbose
-    if not argv:
-      argv = sorted(kindle.asins())
+    asins = argv or sorted(kindle.asins())
     xit = 0
-    for asin in progressbar(argv, "export"):
+    for asin in progressbar(asins, f"export to {calibre}"):
       if runstate.cancelled:
         break
       with Pfx(asin):
@@ -642,6 +641,72 @@ class KindleCommand(BaseCommand):
         except ValueError as e:
           warning("export failed: %s", e)
           xit = 1
+    if runstate.cancelled:
+      xit = 1
+    return xit
+
+  def cmd_import_tags(self, argv):
+    ''' Usage: {cmd} [-nqv] [ASINs...]
+          Import Calibre book information into the fstags for a Kindle book.
+          This will support doing searches based on stuff like
+          titles which are, naturally, not presented in the Kindle
+          metadata db.
+    '''
+    options = self.options
+    kindle = options.kindle
+    calibre = options.calibre
+    runstate = options.runstate
+    self.popopts(argv, options, n='-doit', q='quiet', v='verbose')
+    doit = options.doit
+    quiet = options.quiet
+    verbose = options.verbose
+    asins = argv or sorted(kindle.asins())
+    xit = 0
+    for asin in progressbar(asins, f"import metadata from {calibre}"):
+      if runstate.cancelled:
+        break
+      with Pfx(asin):
+        kbook = kindle.by_asin(asin)
+        cbooks = list(calibre.by_asin(asin))
+        if not cbooks:
+          # pylint: disable=expression-not-assigned
+          verbose and print("asin %s: no Calibre books" % (asin,))
+          continue
+        cbook = cbooks[0]
+        if len(cbooks) > 1:
+          # pylint: disable=expression-not-assigned
+          quiet or print(
+              f"asin {asin}: multiple Calibre books: {cbooks!r}; choosing {cbook}"
+          )
+        ktags = kbook.tags
+        ctags = ktags.subtags('calibre')
+        import_tags = dict(
+            title=cbook.title,
+            authors=sorted([author.name for author in cbook.authors]),
+            dbfspath=calibre.fspath,
+            dbid=cbook.dbid,
+            identifiers=cbook.identifiers,
+            tags=cbook.tags,
+        )
+        first_update = True
+        for tag_name, tag_value in sorted(import_tags.items()):
+          with Pfx("%s=%r", tag_name, tag_value):
+            old_value = ctags.get(tag_name)
+            if old_value != tag_value:
+              if not quiet:
+                if first_update:
+                  print(f"{asin}: update from {cbook}")
+                  first_update = False
+                if old_value:
+                  print(
+                      f"  calibre.{tag_name}={tag_value!r}, was {old_value!r}"
+                  )
+                else:
+                  print(f"  calibre.{tag_name}={tag_value!r}")
+              if doit:
+                ctags[tag_name] = tag_value
+    if runstate.cancelled:
+      xit = 1
     return xit
 
   def cmd_info(self, argv):
