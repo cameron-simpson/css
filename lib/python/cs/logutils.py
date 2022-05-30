df5ad2b0ae67ceed316d96363938ca2d92afbd57
Cameron Simpson <cs@cskk.id.au>
2022-05-30 12:08:08 +1000

    cs.logutils: bump __version__ to 20220530-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/logutils.py
+++ lib/python/cs/logutils.py
@@ -70,7 +70,7 @@ from cs.pfx import Pfx, XP
 from cs.py.func import funccite
 from cs.upd import Upd
 
-__version__ = '20220530'
+__version__ = '20220530-post'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
