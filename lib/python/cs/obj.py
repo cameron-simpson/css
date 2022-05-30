343c10e68649587d50a18fdc59eb094b4f64bfdc
Cameron Simpson <cs@cskk.id.au>
2022-05-30 11:59:28 +1000

    cs.obj: bump __version__ to 20220530-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/obj.py
+++ lib/python/cs/obj.py
@@ -18,7 +18,7 @@ from weakref import WeakValueDictionary
 from cs.deco import OBSOLETE
 from cs.py3 import StringTypes
 
-__version__ = '20220530'
+__version__ = '20220530-post'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
