2330d9cd870f9a08c32136af762739a26271e540
Cameron Simpson <cs@cskk.id.au>
2022-06-05 21:07:55 +1000

    Release information for cs.upd-20220605. Summary: New Upd.run_task to add a status line for the duration of some task. UpdProxy.extend_prefix: new optional print_elapsed parameter.

--- lib/python/cs/upd.py
+++ lib/python/cs/upd.py
@@ -88,7 +88,7 @@ except ImportError as e:
   warning("cannot import curses: %s", e)
   curses = None
 
-__version__ = '20220530-post'
+__version__ = '20220605'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
