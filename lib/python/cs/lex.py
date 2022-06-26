e6faeca57fb0620ba747864771b7ab72b807c329
Cameron Simpson <cs@cskk.id.au>
2022-06-26 19:37:29 +1000

    cs.lex: bump __version__ to 20220626-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/lex.py
+++ lib/python/cs/lex.py
@@ -40,7 +40,7 @@ from cs.pfx import Pfx, pfx_call, pfx_method
 from cs.py.func import funcname
 from cs.seq import common_prefix_length, common_suffix_length
 
-__version__ = '20220626'
+__version__ = '20220626-post'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
