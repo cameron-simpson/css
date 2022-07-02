36caa0949ea54696efd16e1cb506d22854c58bde
Cameron Simpson <cs@cskk.id.au>
2022-07-02 19:53:47 +1000

    cs.hashutils: drop old code, add _HashCode and SHA256 subclass
    --HG--
    branch : 3d

--- lib/python/cs/hashutils.py
+++ lib/python/cs/hashutils.py
@@ -1,47 +1,45 @@
 #!/usr/bin/python
 
-''' Convenience hash facilities.
-    Portably imports the names `sha1` and `md5`.
-    Provides hashfile(), to update a hash from a file's content.
+''' Convenience hashing facilities.
 '''
 
-from __future__ import with_statement
-try:
-  from hashlib import sha1
-except ImportError:
-  from sha import sha as sha1
-try:
-  from hashlib import md5
-except ImportError:
-  from md5 import md5
+import hashlib
 
-_iosize = 16384
+from cs.buffer import CornuCopyBuffer
+from cs.lex import hexify
 
-def hashfile(fp, H=None):
-  ''' Return a hash of the content of the file `fp`.
-      If `fp` is a str, open it as a file pathname.
-      If `H` is missing or None, allocate a new SHA1 hash object.
-      Returns the hash object updated from the file content.
+class _HashCode(bytes):
+
+  __slots__ = ()
+  hashfunc = None
+
+  def __str__(self):
+    return f'{type(self).__name__.lower()}:{hexify(self)}'
+
+  @classmethod
+  def from_data(cls, bs):
+    ''' Compute hashcode from the data `bs`.
+    '''
+    return cls(cls.hashfunc(bs).digest())
+
+  @classmethod
+  def from_buffer(cls, bfr):
+    ''' Compute hashcode from the contents of the `CornuCopyBuffer` `bfr`.
+    '''
+    h = cls.hashfunc()
+    for bs in bfr:
+      h.update(bs)
+    return cls(h.digest())
+
+  @classmethod
+  def from_pathname(cls, pathname, **kw):
+    ''' Compute hashcode from the contents of the file `pathname`.
+    '''
+    return cls.from_buffer(CornuCopyBuffer.from_filename(pathname, **kw))
+
+class SHA256(_HashCode):
+  ''' SHA256 hashcode class, subclass of `bytes`.
   '''
-  if type(fp) is str:
-    with open(fp, "rb") as fp2:
-      return hashfile(fp2, H)
-  if H is None:
-    H = sha1()
-  while True:
-    s = fp.read(_iosize)
-    if len(s) == 0:
-      break
-    H.update(s)
-  return H
 
-def hashfiles(files, hashclass):
-  ''' Generator that iterates over `files` and yields
-        file, hash
-      for each file, `file` being the item from `files` and `hash`
-      being a hash object updated from the file content.  `file`
-      may be a filename string or an open file. An open file will
-      be consumed.
-  '''
-  for fp in files:
-    return fp, hashfile(fp, hashclass())
+  __slots__ = ()
+  hashfunc = hashlib.sha256
