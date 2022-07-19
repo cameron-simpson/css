5ee72594dfc443c0aaa940b491fe2e68f2d6887b
Cameron Simpson <cs@cskk.id.au>
2022-07-19 10:52:43 +1000

    cs.timeseries: print_figure, save_figure and saved_figure now moved to cs.mplutils
    --HG--
    branch : timeseries

--- lib/python/cs/timeseries.py
+++ lib/python/cs/timeseries.py
@@ -95,6 +95,7 @@ from cs.fstags import FSTags
 from cs.lex import is_identifier, s, r
 from cs.logutils import warning
 from cs.mappings import column_name_to_identifier
+from cs.mplutils import print_figure, save_figure
 from cs.pfx import Pfx, pfx, pfx_call, pfx_method
 from cs.progress import progressbar
 from cs.py.modules import import_extra
@@ -3337,71 +3338,3 @@ def timeseries_from_path(
       )
     return TimeSeriesDataDir(tspath, policy='annual', epoch=epoch)
   raise ValueError("cannot deduce time series type from tspath %r" % (tspath,))
-
-# pylint: disable=redefined-builtin
-@contextmanager
-def saved_figure(figure_or_ax, dir=None, ext=None):
-  ''' Context manager to save a `Figure` to a file and yield the file path.
-
-      Parameters:
-      * `figure_or_ax`: a `matplotlib.figure.Figure` or an object
-        with a `.figure` attribute such as a set of `Axes`
-      * `dir`: passed to `tempfile.TemporaryDirectory`
-      * `ext`: optional file extension, default `'png'`
-  '''
-  figure = getattr(figure_or_ax, 'figure', figure_or_ax)
-  if dir is None:
-    dir = '.'
-  if ext is None:
-    ext = 'png'
-  with TemporaryDirectory(dir=dir or '.') as tmppath:
-    tmpimgpath = joinpath(tmppath, f'plot.{ext}')
-    pfx_call(figure.savefig, tmpimgpath)
-    yield tmpimgpath
-
-def save_figure(figure_or_ax, imgpath: str, force=False):
-  ''' Save a `Figure` to the file `imgpath`.
-
-      Parameters:
-      * `figure_or_ax`: a `matplotlib.figure.Figure` or an object
-        with a `.figure` attribute such as a set of `Axes`
-      * `imgpath`: the filesystem path to which to save the image
-      * `force`: optional flag, default `False`: if true the `imgpath`
-        will be written to even if it exists
-  '''
-  if not force and existspath(imgpath):
-    raise ValueError("image path already exists: %r" % (imgpath,))
-  _, imgext = splitext(basename(imgpath))
-  ext = imgext[1:] if imgext else 'png'
-  with saved_figure(figure_or_ax, dir=dirname(imgpath), ext=ext) as tmpimgpath:
-    if not force and existspath(imgpath):
-      raise ValueError("image path already exists: %r" % (imgpath,))
-    pfx_call(os.link, tmpimgpath, imgpath)
-
-def print_figure(figure_or_ax, imgformat=None, file=None):
-  ''' Print `figure_or_ax` to a file.
-
-      Parameters:
-      * `figure_or_ax`: a `matplotlib.figure.Figure` or an object
-        with a `.figure` attribute such as a set of `Axes`
-      * `imgformat`: optional output format; if omitted use `'sixel'`
-        if `file` is a terminal, otherwise `'png'`
-      * `file`: the output file, default `sys.stdout`
-  '''
-  if file is None:
-    file = sys.stdout
-  if imgformat is None:
-    if file.isatty():
-      imgformat = 'sixel'
-    else:
-      imgformat = 'png'
-  with saved_figure(figure_or_ax) as tmpimgpath:
-    with open(tmpimgpath, 'rb') as imgf:
-      if imgformat == 'sixel':
-        run(['img2sixel'], stdin=imgf, stdout=file.fileno(), check=True)
-      else:
-        for bs in CornuCopyBuffer.from_file(imgf):
-          file.write(bs)
-
-if __name__ == '__main__':
-  sys.exit(main(sys.argv))
