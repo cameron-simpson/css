7a1fbd0758efaca3b7439c7a650854e81747aa47
Cameron Simpson <cs@cskk.id.au>
2022-08-05 10:04:38 +1000

    Release information for cs.result-20220805. Summary: Result now subclasses cs.fsm.FSM.

--- lib/python/cs/result.py
+++ lib/python/cs/result.py
@@ -68,7 +68,7 @@ from cs.py3 import Queue, raise3, StringTypes
 from cs.seq import seq, Seq
 from cs.threads import bg as bg_thread
 
-__version__ = '20220311-post'
+__version__ = '20220805'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
