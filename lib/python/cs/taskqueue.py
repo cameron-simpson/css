1837fed0382db9bf609e9dace652a99d6756e554
Cameron Simpson <cs@cskk.id.au>
2022-08-05 10:37:22 +1000

    cs.taskqueue: bump __version__ to 20220805-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/taskqueue.py
+++ lib/python/cs/taskqueue.py
@@ -25,7 +25,7 @@ from cs.result import Result, CancellationError
 from cs.seq import Seq, unrepeated
 from cs.threads import bg as bg_thread, locked, State as ThreadState
 
-__version__ = '20220805'
+__version__ = '20220805-post'
 
 DISTINFO = {
     'keywords': ["python3"],
