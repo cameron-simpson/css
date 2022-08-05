3e73b68f147e86c31e9cfa75c2872ce15fe9b05d
Cameron Simpson <cs@cskk.id.au>
2022-08-05 10:42:44 +1000

    cs.app.myke: update DISTINFO

--- lib/python/cs/app/myke/__init__.py
+++ lib/python/cs/app/myke/__init__.py
@@ -6,16 +6,31 @@
 DISTINFO = {
     'description':
     "my make program; parallel make tool with superior expression syntax",
-    'keywords': ["python2", "python3"],
+    'keywords': ["python3"],
     'classifiers': [
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
     ],
     'install_requires': [
-        'cs.debug', 'cs.logutils', 'cs.inttypes', 'cs.threads', 'cs.later',
-        'cs.queues', 'cs.result', 'cs.lex'
+        'cs.cmdutils',
+        'cs.debug',
+        'cs.deco',
+        'cs.excutils',
+        'cs.inttypes',
+        'cs.later',
+        'cs.lex',
+        'cs.logutils',
+        'cs.pfx',
+        'cs.py.func',
+        'cs.queues',
+        'cs.result',
+        'cs.threads',
     ],
+    'entry_points': {
+        'console_scripts': [
+            'myke = cs.app.myke__main__:main',
+        ],
+    },
 }
 
 DEFAULT_MAKE_COMMAND = 'myke'
