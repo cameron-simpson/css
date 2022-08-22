b041cd5ab53ec37373797935f97d5c0c8d9d894a
Cameron Simpson <cs@cskk.id.au>
2022-08-22 10:06:38 +1000

    cs.vt.fuse_tests: refactor setupTeardown a bit, push skipIf to out the test case level
    --HG--
    branch : vt

--- lib/python/cs/vt/fuse_tests.py
+++ lib/python/cs/vt/fuse_tests.py
@@ -7,17 +7,21 @@
 from contextlib import contextmanager
 import os
 import sys
+import time
 import unittest
 from random import randint
 from tempfile import TemporaryDirectory
 from cs.fileutils import BackedFile, BackedFile_TestMethods
 from cs.logutils import warning
+from cs.psutils import run
+from cs.resources import stackattrs
 from cs.testutils import SetupTeardownMixin
 from cs.x import X
+
 from . import defaults
 from .dir import Dir
 try:
-  from .fuse import mount
+  from .fuse import mount, umount
 except ImportError as e:
   warning("import fails, no mount function: %s", e)
   mount = None
@@ -25,6 +29,7 @@ from .store import MappingStore
 
 TESTDIR = 'vtfuse_testdir'
 
+@unittest.skipIf(mount is None, "no FUSE mount function")
 class Test_VTFuse(SetupTeardownMixin, unittest.TestCase):
   ''' Tests for `cs.vt.fuse`.
   '''
@@ -33,17 +38,17 @@ class Test_VTFuse(SetupTeardownMixin, unittest.TestCase):
   def setupTeardown(self):
     testname = type(self).__name__
     self.store_dict = {}
-    self.S = MappingStore(testname, self.store_dict)
-    with self.S:
-      self.tmpdir = TemporaryDirectory(prefix=testname + '-', dir='.')
-      self.testdirpath = self.tmpdir.name
-      self.E = Dir(self.testdirpath)
-      mount(self.testdirpath, self.E, S=self.S)
-      yield
-    os.system("set -x; umount '%s'" % self.testdirpath)
-    del self.tmpdir
+    S = MappingStore(testname, self.store_dict)
+    with TemporaryDirectory(prefix=testname + '-', dir='.') as testdirpath:
+      E = Dir(testdirpath)
+      with S:  # TODO: try without this line? mount should do it
+        mount(testdirpath, E, S=S)
+        with stackattrs(self, E=E, S=S, testdirpath=testdirpath):
+          try:
+            yield
+          finally:
+            umount(testdirpath)
 
-  @unittest.skipIf(mount is None, "no FUSE mount function")
   def test_FS(self):
     X("test_FS...")
 
