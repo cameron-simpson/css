ce68b7d406b7e042ed61ab2aba378c332e784a2b
Cameron Simpson <cs@cskk.id.au>
2022-09-03 13:58:00 +1000

    cs.app.osx.objc: autoformat, strip debug, add __main__ exercising whatever I'm testing at the moment
    --HG--
    branch : osx

--- lib/python/cs/app/osx/objc.py
+++ lib/python/cs/app/osx/objc.py
@@ -185,38 +185,38 @@ def convertObjCtype(o):
   if isinstance(o, NSDateComponents):
     return convertNSDateComponents(o)
   if isinstance(o, ABMultiValueCoreDataWrapper):
-    return [ convertObjCtype(o.valueAtIndex_(i)) for i in range(o.count()) ]
+    return [convertObjCtype(o.valueAtIndex_(i)) for i in range(o.count())]
   # specific generic types later
   t = type(o)
   if t is int:
     return o
   if t is objc.pyobjc_unicode:
-    return unicode(o)
+    return str(o)
   if t is objc._pythonify.OC_PythonInt:
     return int(o)
   if t is objc._pythonify.OC_PythonFloat:
     return float(o)
   if hasattr(o, 'keys'):
     ##warning("pretending a dict - unrecognised <%s %s>", t, repr(o).replace("\n", ""))
-    return dict( [ (k, o[k]) for k in o.keys() ] )
+    return dict([(k, o[k]) for k in o.keys()])
   raise TypeError("convertObjCtype: o = %s %r", t, o)
 
 def convertNSDate(d):
   d_date, d_time, d_zone = d.description().split()
-  year, month, day = [ int(d_d) for d_d in d_date.split('-') ]
-  hour, minute, second = [ int(d_t) for d_t in d_time.split(':') ]
+  year, month, day = [int(d_d) for d_d in d_date.split('-')]
+  hour, minute, second = [int(d_t) for d_t in d_time.split(':')]
   tz = tzinfoHHMM(d_zone)
   return datetime(year, month, day, hour, minute, second, 0, tz)
 
 def convertNSDateComponents(d):
   desc = d.description()
-  ##D("NSDateComponents str = %s", d)
-  ##D("NSDateComponents desc = %r", desc)
   year = convertObjCtype(d.year())
-  ##D("year = %s %r", type(year), year)
   month = convertObjCtype(d.month())
-  ##D("month = %s %r", type(month), month)
   day = convertObjCtype(d.day())
-  ##D("day = %s %r", type(day), day)
   return datetime(year, month, day)
 
+if __name__ == '__main__':
+  hi_services = Bundle('HIServices')
+  cg = Bundle('CoreGraphics')
+  cg2 = apple.CoreGraphics
+  assert cg is cg2
