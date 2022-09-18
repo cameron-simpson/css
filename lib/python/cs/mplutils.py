89f0ab62363dc6b05832f76743ac562b60eca056
Cameron Simpson <cs@cskk.id.au>
2022-09-18 10:49:27 +1000

    cs.mplutils: bump __version__ to 20220918-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/mplutils.py
+++ lib/python/cs/mplutils.py
@@ -26,7 +26,7 @@ from cs.deco import fmtdoc
 from cs.lex import r
 from cs.pfx import pfx_call
 
-__version__ = '20220918'
+__version__ = '20220918-post'
 
 DISTINFO = {
     'keywords': ["python3"],
