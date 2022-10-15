a349b3b9299e0ea553b58040a1eaba4bb50262a4
Cameron Simpson <cs@cskk.id.au>
2022-10-16 09:39:17 +1100

    cs.m3: add stub __init__ with package docstring and incomplete DISTINFO
    --HG--
    branch : 3d

---
+++ lib/python/cs/m3/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+''' Some 3d modelling code, using panda3d for rendering (and pandas for computation!)
+'''
+
+DISTINFO = {
+    'keywords': ["python3"],
+    'classifiers': [
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Programming Language :: Python :: 3",
+    ],
+    'install_requires': [
+        'icontract',
+        'numpy',
+        'panda3d',
+        'pandas',
+        'typeguard',
+    ],
+    'entry_points': {
+        'console_scripts': [],
+    },
+    'extras_requires': {},
+}
