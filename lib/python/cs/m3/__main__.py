bff9a2c57fdb317f5cf0d12edf99992e169461db
Cameron Simpson <cs@cskk.id.au>
2022-10-21 10:08:30 +1100

    cs.m3.__main__: M3Command.main: add a "spere" demo mode
    --HG--
    branch : 3d

--- lib/python/cs/m3/__main__.py
+++ lib/python/cs/m3/__main__.py
@@ -5,7 +5,8 @@ from os.path import basename
 import sys
 
 from cs.cmdutils import BaseCommand
-from cs.panda3d_utils.egg import Texture
+from cs.panda3d_utils.egg import Model, Texture
+from cs.panda3d_utils.geom import sphere
 from cs.progress import progressbar
 
 from .material import Material
@@ -15,12 +16,22 @@ class M3Command(BaseCommand):
   '''
 
   def main(self, argv):
+    mode = 'sphere'
     xy_s, texture_path = argv
     xy = int(xy_s)
-    M = Material(xy, xy, 2, mass=1.0)
-    for t in progressbar(range(100), "step"):
-      M.step()
-    model = M.EggModel(texture=Texture(basename(texture_path), texture_path))
+    texture = Texture(basename(texture_path), texture_path)
+    if mode == 'sphere':
+      model = Model(mode)
+      with model:
+        surface = sphere(xy, 40, texture=texture)
+        egg = surface.EggNode()
+        model.append(texture)
+        model.append(surface.EggNode())
+    else:
+      M = Material(xy, xy, 2, mass=1.0)
+      for t in progressbar(range(100), "step"):
+        M.step()
+      model = M.EggModel(texture=texture)
     print(model)
     model.save('model-material.egg', exists_ok=True)
     os.system('pview model-material.egg')
