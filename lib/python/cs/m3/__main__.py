439f62aadbc18b40d10832bfed5de3a52a8008e3
Cameron Simpson <cs@cskk.id.au>
2022-10-24 09:11:41 +1100

    cs.m3.__main__: M3Command.main: define a test material to use on the pyramid, enable lighting in pview to observe the material
    --HG--
    branch : 3d

--- lib/python/cs/m3/__main__.py
+++ lib/python/cs/m3/__main__.py
@@ -5,11 +5,11 @@ from os.path import basename
 import sys
 
 from cs.cmdutils import BaseCommand
-from cs.panda3d_utils.egg import Model, Texture
-from cs.panda3d_utils.geom import pyramid, sphere
+from cs.panda3d_utils.egg import Model, Material, Texture
+from cs.panda3d_utils.geom import equilateral_pyramid, pyramid, sphere
 from cs.progress import progressbar
 
-from .material import Material
+from .material import Material as M3Material
 
 class M3Command(BaseCommand):
   ''' Demo command for cs.m3.
@@ -20,22 +20,36 @@ class M3Command(BaseCommand):
     xy_s, texture_path = argv
     xy = int(xy_s)
     texture = Texture(basename(texture_path), texture_path)
+    material = Material(
+        name="test-mat",
+        emitr=0,
+        emitg=100,
+        emitb=50,
+        ##ambr=40,
+    )
     model = Model(mode)
     with model:
+      model.append(material)
       model.append(texture)
       if mode == 'pyramid':
-        surface = pyramid(5, xy, texture=texture)
+        ##surface = pyramid(3, xy, texture=texture)
+        surface = equilateral_pyramid(
+            3,
+            xy,
+            material=material,
+            ##texture=texture,
+        )
         model.append(surface.EggNode())
       elif mode == 'sphere':
         surface = sphere(xy, 40, texture=texture)
         model.append(surface.EggNode())
       else:
-        M = Material(xy, xy, 2, mass=1.0)
+        M = M3Material(xy, xy, 2, mass=1.0)
         for t in progressbar(range(100), "step"):
           M.step()
         M.EggModel(model=model, texture=texture)
     print(model)
-    model.view()
+    model.view(lighting=True)
 
 if __name__ == '__main__':
   sys.exit(M3Command(sys.argv).run())
