f350b49772aea141f549f90ce1fdbfd55095baa3
Cameron Simpson <cs@cskk.id.au>
2022-10-23 11:52:15 +1100

    cs.m3.__main__: M3Command.main: new pyramid mode, tweak other model construction
    --HG--
    branch : 3d

--- lib/python/cs/m3/__main__.py
+++ lib/python/cs/m3/__main__.py
@@ -6,7 +6,7 @@ import sys
 
 from cs.cmdutils import BaseCommand
 from cs.panda3d_utils.egg import Model, Texture
-from cs.panda3d_utils.geom import sphere
+from cs.panda3d_utils.geom import pyramid, sphere
 from cs.progress import progressbar
 
 from .material import Material
@@ -16,22 +16,24 @@ class M3Command(BaseCommand):
   '''
 
   def main(self, argv):
-    mode = 'sphere'
+    mode = 'pyramid'
     xy_s, texture_path = argv
     xy = int(xy_s)
     texture = Texture(basename(texture_path), texture_path)
-    if mode == 'sphere':
-      model = Model(mode)
-      with model:
-        surface = sphere(xy, 40, texture=texture)
-        egg = surface.EggNode()
-        model.append(texture)
+    model = Model(mode)
+    with model:
+      model.append(texture)
+      if mode == 'pyramid':
+        surface = pyramid(5, xy, texture=texture)
         model.append(surface.EggNode())
-    else:
-      M = Material(xy, xy, 2, mass=1.0)
-      for t in progressbar(range(100), "step"):
-        M.step()
-      model = M.EggModel(texture=texture)
+      elif mode == 'sphere':
+        surface = sphere(xy, 40, texture=texture)
+        model.append(surface.EggNode())
+      else:
+        M = Material(xy, xy, 2, mass=1.0)
+        for t in progressbar(range(100), "step"):
+          M.step()
+        M.EggModel(model=model, texture=texture)
     print(model)
     model.view()
 
