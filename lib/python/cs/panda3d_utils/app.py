779736805bdefc98facca13aaa2cef69eb5c47fa
Cameron Simpson <cs@cskk.id.au>
2022-11-09 15:30:52 +1100

    cs.panda3d_utils.app: put the P3DApp class here
    --HG--
    branch : 3d

---
+++ lib/python/cs/panda3d_utils/app.py
@@ -0,0 +1,57 @@
+#!/usr/bin/envpython3
+
+class P3DApp(MultiOpenMixin, ShowBase):
+
+  DEFAULT_SCALE = 1.0, 1.0, 1.0
+  DEFAULT_POS = -10, 10, 0
+
+  @typechecked
+  def __init__(
+      self,
+      scene: Optional[Union[str, Model]] = None,  # TODO: add scene type
+      *,
+      scale: Optional[Tuple[Numeric, Numeric, Numeric]] = None,
+      pos: Optional[Tuple[Numeric, Numeric, Numeric]] = None
+  ):
+    if scale is None:
+      scale = self.DEFAULT_SCALE
+    if pos is None:
+      pos = self.DEFAULT_POS
+    super().__init__()
+    print("self.render", s(self.render))
+    # Load the environment model.
+    if scene is None:
+      self.scene = None
+    else:
+      self.scene = (
+          self.add_model(scene) if isinstance(scene, (str, Model)) else scene
+      )
+      # Reparent the model to render.
+      self.scene.reparentTo(self.render)
+      # Apply scale and position transforms on the model.
+      self.scene.setScale(*scale)
+      self.scene.setPos(pos)
+
+  @pfx_method
+  def add_model(self, modelref, **lm_kwargs):
+    if isinstance(modelref, str):
+      # filename
+      return self.loader.loadModel(modelref, **lm_kwargs)
+    if isinstance(modelref, Model):
+      with modelref.saved() as eggpath:
+        return self.loader.loadModel(eggpath, **lm_kwargs)
+    raise TypeError(
+        f'do not know how to loadModel from type {modelref.__class__.__name__}'
+    )
+
+  @typechecked
+  def add_task(
+      self,
+      task_func: Callable[[Task.PythonTask], int],
+      name: Optional[str] = None,
+  ):
+    ''' Convenience method to add a task.
+    '''
+    if name is None:
+      name = task_func.__qualname__
+    return self.taskMgr.add(task_func, name)
