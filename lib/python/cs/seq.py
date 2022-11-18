02c9e64b22023509c506a2c477ead1be5d0bd3d9
Cameron Simpson <cs@cskk.id.au>
2022-11-18 14:34:21 +1100

    Release information for cs.seq-20221118. Summary: Small doc improvement.

--- lib/python/cs/seq.py
+++ lib/python/cs/seq.py
@@ -18,7 +18,7 @@ from threading import Lock, Condition, Thread
 from cs.deco import decorator
 from cs.gimmicks import warning
 
-__version__ = '20220530-post'
+__version__ = '20221118'
 
 DISTINFO = {
     'description':
