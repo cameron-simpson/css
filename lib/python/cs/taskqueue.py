375e26da9aa4eaef738adae624c053b616c7ed39
Cameron Simpson <cs@cskk.id.au>
2022-12-07 14:10:05 +1100

    Release information for cs.taskqueue-20221207. Summary: Pull out core stuff from Task into BaseTask, aids subclassing. BaseTask: explainatory docustring about unusual FSM_DEFAULT_STATE design choice. BaseTask.tasks_as_dot: express the edges using the node ids instead of their labels. BaseTask: new tasks_as_svg() method like tasks_as_dot() but returning SVG.

--- lib/python/cs/taskqueue.py
+++ lib/python/cs/taskqueue.py
@@ -25,7 +25,7 @@ from cs.result import Result, CancellationError
 from cs.seq import Seq, unrepeated
 from cs.threads import bg as bg_thread, locked, State as ThreadState
 
-__version__ = '20220805-post'
+__version__ = '20221207'
 
 DISTINFO = {
     'keywords': ["python3"],
