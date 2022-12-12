8cdb5a4db33ade4f6ef61629c470cd8d792696e6
Cameron Simpson <cs@cskk.id.au>
2022-12-12 12:50:27 +1100

    cs.deco: @decorator: simplify logic, and try to use functools.update_wrapper to propagate the decorated function's attributes to the wrapper

--- lib/python/cs/deco.py
+++ lib/python/cs/deco.py
@@ -10,6 +10,7 @@ Assorted decorator functions.
 
 from collections import defaultdict
 from contextlib import contextmanager
+from functools import update_wrapper
 from inspect import isgeneratorfunction, signature, Parameter
 import sys
 import time
@@ -24,7 +25,6 @@ DISTINFO = {
     'keywords': ["python2", "python3"],
     'classifiers': [
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
     ],
     'install_requires': ['cs.gimmicks'],
@@ -100,6 +100,42 @@ def decorator(deco):
             ...
   '''
 
+  def decorate(func, *dargs, **dkwargs):
+    ''' Final decoration when we have the function and the decorator arguments.
+    '''
+    # decorate func
+    decorated = deco(func, *dargs, **dkwargs)
+    # catch mucked decorators which forget to return the new function
+    assert decorated is not None, (
+        "deco:%r(func:%r,...) -> None" % (deco, func)
+    )
+    if decorated is not func:
+      # we got a wrapper function back, pretty up the returned wrapper
+      from cs.x import X
+      X(
+          "decorate(func=%r): decorated=%r : %r", func, decorated,
+          dir(decorated)
+      )
+      # try functools.update_wrapper, otherwise do stuff by hand
+      try:
+        update_wrapper(decorated, func)
+      except AttributeError:
+        try:
+          decorated.__name__ = getattr(func, '__name__', str(func))
+        except AttributeError:
+          pass
+        doc = getattr(func, '__doc__', None) or ''
+        try:
+          decorated.__doc__ = doc
+        except AttributeError:
+          warning("cannot set __doc__ on %r", decorated)
+        func_module = getattr(func, '__module__', None)
+        try:
+          decorated.__module__ = func_module
+        except AttributeError:
+          pass
+    return decorated
+
   def metadeco(*da, **dkw):
     ''' Compute either the wrapper function for `func`
         or a decorator expecting to get `func` when used.
@@ -115,44 +151,12 @@ def decorator(deco):
       # `func` is already supplied, pop it off and decorate it now.
       func = da[0]
       da = tuple(da[1:])
-      # decorate func
-      decorated = deco(func, *da, **dkw)
-      # catch mucked decorators which forget to return the new function
-      assert decorated is not None, (
-          "deco:%r(func:%r,...) -> None" % (deco, func)
-      )
-      if decorated is not func:
-        # we got a wrapper function back, pretty up the returned wrapper
-        try:
-          decorated.__name__ = getattr(func, '__name__', str(func))
-        except AttributeError:
-          pass
-        doc = getattr(func, '__doc__', None) or ''
-        try:
-          decorated.__doc__ = doc
-        except AttributeError:
-          warning("cannot set __doc__ on %r", decorated)
-        func_module = getattr(func, '__module__', None)
-        try:
-          decorated.__module__ = func_module
-        except AttributeError:
-          pass
-      return decorated
+      return decorate(func, *da, **dkw)
 
     # `func` is not supplied, collect the arguments supplied and return a
     # decorator which takes the subsequent callable and returns
     # `deco(func, *da, **kw)`.
-    def overdeco(func):
-      decorated = deco(func, *da, **dkw)
-      decorated.__doc__ = getattr(func, '__doc__', None) or ''
-      func_module = getattr(func, '__module__', None)
-      try:
-        decorated.__module__ = func_module
-      except AttributeError:
-        pass
-      return decorated
-
-    return overdeco
+    return lambda func: decorate(func, *da, **dkw)
 
   metadeco.__doc__ = getattr(deco, '__doc__', '')
   metadeco.__module__ = getattr(deco, '__module__', None)
