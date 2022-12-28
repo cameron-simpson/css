2c5c3f5d96180470104ae32e79fc512e84624677
Cameron Simpson <cs@cskk.id.au>
2022-12-28 17:27:16 +1100

    cs.app.cloudbackup: make the .ndjson paths end in .gz to default to gzip compression
    --HG--
    branch : cloud

--- lib/python/cs/app/cloudbackup.py
+++ lib/python/cs/app/cloudbackup.py
@@ -1229,7 +1229,7 @@ class NamedBackup(SingletonMixin):
     self.state_dirpath = state_dirpath
     # the association of UUIDs with directory subpaths
     self.diruuids = UUIDNDJSONMapping(
-        joinpath(self.state_dirpath, 'diruuids.ndjson')
+        joinpath(self.state_dirpath, 'diruuids.ndjson.gz')
     )
     # the directory holding the DirState files
     self.dirstates_dirpath = joinpath(self.state_dirpath, 'dirstates')
@@ -1237,7 +1237,8 @@ class NamedBackup(SingletonMixin):
     self._dirstates = {}
     # cloud storage stuff
     self.backup_records = UUIDNDJSONMapping(
-        joinpath(self.state_dirpath, 'backups.ndjson'), dictclass=BackupRecord
+        joinpath(self.state_dirpath, 'backups.ndjson.gz'),
+        dictclass=BackupRecord
     )
     # TODO: not using _saved_hashcodes yet
     self._saved_hashcodes = set()
@@ -1327,7 +1328,7 @@ class NamedBackup(SingletonMixin):
     uupath = uuidpath(uu, 2, 2, make_subdir_of=self.dirstates_dirpath)
     dirstate_path = joinpath(
         self.dirstates_dirpath, dirname(uupath), uu.hex
-    ) + '.ndjson'
+    ) + '.ndjson.gz'
     return uu, dirstate_path
 
   @classmethod
