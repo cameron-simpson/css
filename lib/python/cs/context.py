fbfb76a95772fed6ceb5937ca09c2d5a3e00b14d
Cameron Simpson <cs@cskk.id.au>
2023-01-09 13:23:59 +1100

    cs.context: new contextif(flag,cmgr_func,...) context manager to use cmgr_func if flag is true otherwise nullcontext
    --HG--
    branch : ebooks

--- lib/python/cs/context.py
+++ lib/python/cs/context.py
@@ -27,6 +27,22 @@ DISTINFO = {
     'install_requires': [],
 }
 
+@contextmanager
+def contextif(flag, cmgr_func, *cmgr_args, **cmgr_kwargs):
+  ''' A context manager to call call `cmgr_func(*cmgr_args,**cmgr_kwargs)`
+      if `flag` is true or `nullcontext()` otherwise.
+
+      The driving use case in verbosity dependent status lines or
+      progress bars, eg:
+
+          from cs.upd import run_task
+          with contextif(run_task(....)) as proxy:
+            ... do stuff, updating proxy if not None ...
+  '''
+  cmgr = cmgr_func(*cmgr_args, **cmgr_kwargs) if flag else nullcontext()
+  with cmgr as ctxt:
+    yield ctxt
+
 def pushattrs(o, **attr_values):
   ''' The "push" part of `stackattrs`.
       Push `attr_values` onto `o` as attributes,
