16cffbc1160d34291e6b9aebc9058bab936412dd
Cameron Simpson <cs@cskk.id.au>
2023-01-25 20:51:36 +1100

    Release information for cs.threads-20230125. Summary: New HasThreadState mixin for classes with a state=State() attribute to provide a cls.default() class method for the default instance and a context manager to push/pop self.state.current=self.

--- lib/python/cs/threads.py
+++ lib/python/cs/threads.py
@@ -23,7 +23,7 @@ from cs.pfx import Pfx  # prefix
 from cs.py.func import funcname, prop
 from cs.seq import Seq
 
-__version__ = '20221228-post'
+__version__ = '20230125'
 
 DISTINFO = {
     'description':
