2442c7d2e4b034f697f0b2cc79f4a3d95f47b9b1
Cameron Simpson <cs@cskk.id.au>
2023-02-10 11:32:37 +1100

    Release information for cs.lex-20230210. Summary: @has_format_attributes: new optional inherit parameter to inherit superclass (or other) format attributes, default False. New FNumericMixin, FFloat, FInt FormatableMixin subclasses like FStr - they add .localtime and .utctime formattable attributes.

--- lib/python/cs/lex.py
+++ lib/python/cs/lex.py
@@ -42,7 +42,7 @@ from cs.pfx import Pfx, pfx_call, pfx_method
 from cs.py.func import funcname
 from cs.seq import common_prefix_length, common_suffix_length
 
-__version__ = '20220918-post'
+__version__ = '20230210'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
