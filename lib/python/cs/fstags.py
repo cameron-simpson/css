1b1afbe7d9b1cfe0c7bcdb50aad667816b0bc53b
Cameron Simpson <cs@cskk.id.au>
2023-02-12 15:06:24 +1100

    cs.fstags: bump __version__ to 20230212-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/fstags.py
+++ lib/python/cs/fstags.py
@@ -129,7 +129,7 @@ from cs.tagset import (
 from cs.threads import locked, locked_property, State
 from cs.upd import Upd, uses_upd, print  # pylint: disable=redefined-builtin
 
-__version__ = '20230212'
+__version__ = '20230212-post'
 
 DISTINFO = {
     'keywords': ["python3"],
