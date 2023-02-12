98016480053ee5f6079797afc6be2972a0aab1ed
Cameron Simpson <cs@cskk.id.au>
2023-02-12 13:40:59 +1100

    cs.buffer: bump __version__ to 20230212.2-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/buffer.py
+++ lib/python/cs/buffer.py
@@ -23,7 +23,7 @@ from cs.deco import Promotable
 from cs.gimmicks import r
 from cs.py3 import pread
 
-__version__ = '20230212.2'
+__version__ = '20230212.2-post'
 
 DISTINFO = {
     'keywords': ["python3"],
