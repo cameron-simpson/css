8a23866707b3c151fc22a2b9740942b57da6c7b4
Cameron Simpson <cs@cskk.id.au>
2023-02-17 10:14:40 +1100

    cs.lex: drop strip_prefix_n, get_prefix_n is more general and I had not got around to using strip_prefix_n yet - when I did, I ended up writing get_prefix_n
    --HG--
    branch : playon

--- lib/python/cs/lex.py
+++ lib/python/cs/lex.py
@@ -435,61 +435,6 @@ def stripped_dedent(s):
   adjusted = dedent('\n'.join(lines))
   return line1 + '\n' + adjusted
 
-# pylint: disable=redefined-outer-name
-def strip_prefix_n(s, prefix, n=None):
-  ''' Strip a leading `prefix` and numeric value `n` from the start of a
-      string.  Return the remaining string, or the original string if the
-      prefix or numeric value do not match.
-
-      Parameters:
-      * `s`: the string to strip
-      * `prefix`: the prefix string which must appear at the start of `s`
-      * `n`: optional integer value;
-        if omitted any value will be accepted, otherwise the numeric
-        part must match `n`
-
-      Examples:
-
-         >>> strip_prefix_n('s03e01--', 's', 3)
-         'e01--'
-         >>> strip_prefix_n('s03e01--', 's', 4)
-         's03e01--'
-         >>> strip_prefix_n('s03e01--', 's')
-         'e01--'
-  '''
-  s0 = s
-  if prefix:
-    s = cutprefix(s, prefix)
-    if s is s0:
-      # no match, return unchanged
-      return s0
-  else:
-    s = s0
-  if not s or not s[0].isdigit():
-    # no following digits, return unchanged
-    return s0
-  if n is None:
-    # strip all following digits
-    s = s.lstrip(digits)
-  else:
-    # evaluate the numeric part
-    s = s.lstrip('0')  # pylint: disable=no-member
-    if not s or not s[0].isdigit():
-      # all zeroes, leading value is 0
-      sn = 0
-      pos = 0
-    else:
-      pos = 1
-      slen = len(s)
-      while pos < slen and s[pos].isdigit():
-        pos += 1
-      sn = int(s[:pos])
-    if sn != n:
-      # wrong numeric value
-      return s0
-    s = s[pos:]
-  return s
-
 @require(lambda offset: offset >= 0)
 def get_prefix_n(s, prefix, n=None, *, offset=0):
   ''' Strip a leading `prefix` and numeric value `n` from the string `s`
