248eebeae8aa3999dfa421074d1b5b84d4a679fb
Cameron Simpson <cs@cskk.id.au>
2023-04-01 16:04:24 +1100

    Release information for cs.buffer-20230401. Summary: CornuCopyBuffer.promote: document accepting an iterable as an iterable of bytes. CornuCopyBuffer: new readline() method to return a binary line from the buffer. CornuCopyBuffer.promote: assume objects with a .read1 or .read are files.

--- lib/python/cs/buffer.py
+++ lib/python/cs/buffer.py
@@ -23,7 +23,7 @@ from cs.deco import Promotable
 from cs.gimmicks import r
 from cs.py3 import pread
 
-__version__ = '20230212.2-post'
+__version__ = '20230401'
 
 DISTINFO = {
     'keywords': ["python3"],
