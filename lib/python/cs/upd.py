6795213cf723db42bcd7110f4c422a7ca1e7f95d
Cameron Simpson <cs@cskk.id.au>
2023-04-01 15:59:03 +1100

    Release information for cs.upd-20230401. Summary: Upd.run_task: use a local RunState to control the ticker, drop `runstate` parameter. Renamed default HasThreadState.THREAD_STATE_ATTR to 'perthread_state'. Keep a module level name for the default Upd instance. Upd.shutdown: cope with shutdown early enough that there's no self._lock. Honour new $CS_UPD_BACKEND envvar to support eg defaulting Upd to /dev/null.

--- lib/python/cs/upd.py
+++ lib/python/cs/upd.py
@@ -91,7 +91,7 @@ except ImportError as curses_e:
   warning("cannot import curses: %s", curses_e)
   curses = None
 
-__version__ = '20230217-post'
+__version__ = '20230401'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
