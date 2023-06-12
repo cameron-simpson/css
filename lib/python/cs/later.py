cb7e3f772da03eb70c6cb248de1a67c32e97f574
Cameron Simpson <cs@cskk.id.au>
2023-06-12 14:48:27 +1000

    cs.later: bump __version__ to 20230612-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/later.py
+++ lib/python/cs/later.py
@@ -55,7 +55,7 @@ from cs.result import Result, report, after
 from cs.seq import seq
 from cs.threads import ThreadState, HasThreadState
 
-__version__ = '20230612'
+__version__ = '20230612-post'
 
 DISTINFO = {
     'keywords': ["python3"],
