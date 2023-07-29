9df4a13aaa3074ed2f84632a16d23d60b60a1c44
Cameron Simpson <cs@cskk.id.au>
2023-07-29 10:47:06 +1000

    cs.vt.cache: use thread_states=False instead of thread_states=(), better idiom
    --HG--
    branch : vt

--- lib/python/cs/vt/cache.py
+++ lib/python/cs/vt/cache.py
@@ -96,7 +96,7 @@ class FileDataMappingProxy(MultiOpenMixin, RunStateMixin):
           self._work,
           args=(workQ,),
           name="%s WORKER" % (self,),
-          thread_states=(),
+          thread_states=False,
       )
       with stackattrs(self, _workQ=workQ):
         try:
