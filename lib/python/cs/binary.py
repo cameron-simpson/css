56230cea829846bc37c644d5fcc097d43688d7db
Cameron Simpson <cs@cskk.id.au>
2023-11-29 21:34:27 +1100

    Release information for cs.binary-20231129. Summary: BinaryMultiStruct.parse: promote the buffer arguments to a CornuCopyBuffer.

--- lib/python/cs/binary.py
+++ lib/python/cs/binary.py
@@ -81,7 +81,7 @@ from cs.lex import cropped, cropped_repr, typed_str
 from cs.pfx import Pfx, pfx_method, pfx_call
 from cs.seq import Seq
 
-__version__ = '20230401-post'
+__version__ = '20231129'
 
 DISTINFO = {
     'keywords': ["python3"],
