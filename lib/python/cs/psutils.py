aff1ca1102b3b63d9c08d49adec6168480fc4e78
Cameron Simpson <cs@cskk.id.au>
2023-11-29 20:19:25 +1100

    cs.psutils: bump __version__ to 20231129-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/psutils.py
+++ lib/python/cs/psutils.py
@@ -19,7 +19,7 @@ import time
 from cs.gimmicks import trace, warning, DEVNULL
 from cs.pfx import pfx_call
 
-__version__ = '20231129'
+__version__ = '20231129-post'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
