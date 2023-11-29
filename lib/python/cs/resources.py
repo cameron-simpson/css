ad1c7a245a01efbc5d79a7ff360bec8681418a11
Cameron Simpson <cs@cskk.id.au>
2023-11-29 20:22:04 +1100

    cs.resources: bump __version__ to 20231129-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/resources.py
+++ lib/python/cs/resources.py
@@ -26,7 +26,7 @@ from cs.py.func import prop
 from cs.py.stack import caller, frames as stack_frames, stack_dump
 from cs.threads import ThreadState, HasThreadState
 
-__version__ = '20231129'
+__version__ = '20231129-post'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
