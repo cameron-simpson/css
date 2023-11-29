125d40f7d14913474f06152c591474ec96ad9edb
Cameron Simpson <cs@cskk.id.au>
2023-11-29 20:15:19 +1100

    cs.fs: bump __version__ to 20231129-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/fs.py
+++ lib/python/cs/fs.py
@@ -30,7 +30,7 @@ from cs.deco import decorator
 from cs.obj import SingletonMixin
 from cs.pfx import pfx, pfx_call
 
-__version__ = '20231129'
+__version__ = '20231129-post'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
