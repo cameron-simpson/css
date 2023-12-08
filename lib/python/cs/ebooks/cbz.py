27a7f850422da96c9f865242f5ae6ad35a76a55f
Cameron Simpson <cs@cskk.id.au>
2023-12-08 13:37:18 +1100

    cs.ebooks.cbz: CBZ support, initially a make_cbz context manager for constructing a CBZ archive
    --HG--
    branch : pdf

---
+++ lib/python/cs/ebooks/cbz.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+''' Support for Comic Book Archive zipfiles.
+'''
+
+from contextlib import contextmanager
+import json
+from os.path import basename
+from zipfile import ZipFile, ZIP_STORED
+
+from typeguard import typechecked
+
+from cs.fileutils import atomic_filename
+from cs.pfx import Pfx, pfx_call
+
+@contextmanager
+@atomic_filename
+@typechecked
+def make_cbz(
+    fspath: str,
+    *,
+    images,
+    metadata=None,
+    metadata_encoding='utf-8',
+    compression=ZIP_STORED,
+    **zipkw
+):
+  ''' A context manager for creating a CBZ archive file,
+      yielding an open `ZipFile` instance.
+
+      Parameters:
+      * `fspath`: the filesystem path for the new CBZ file
+      * `images`: an iterable of images to save in the CBZ file
+      * `metadata`: optional metadata, which may be a `str` or a JSNONable mapping
+      Other keyword parameters are passed to the `ZipFile` creation
+      along with an `'x'` open mode.
+
+      The `images` iterable may yield either `str` or `(str,str)` tuples.
+      A plain `str` is replaced by a `(str,basename(str))` tuple.
+      The first `str` is the filesystem path of an image file.
+      The second `str` is the `arcname` used when storing the file
+      into the archive.
+  '''
+  with pfx_call(
+      ZipFile,
+      fspath,
+      'x',
+      metadata_encoding=metadata_encoding,
+      compression=compression,
+      **zipkw,
+  ) as cbz:
+    if metadata:
+      if isinstance(metadata, str):
+        cbz.metadata = metadata
+      else:
+        cbz.metadata = json.dumps(metadata, separators=(',', ':'))
+    for img in images:
+      with Pfx(img):
+        if isinstance(img, str):
+          imgfspath, arcname = img, basename(img)
+        # TODO: Path instances?
+        else:
+          imgfspath, arcname = img
+        pfx_call(cbz.write, imgfspath, arcname=arcname)
+    yield cbz
