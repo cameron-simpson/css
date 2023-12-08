2df21f8a8e436188924f4c1d50a00cc5c18c71b2
Cameron Simpson <cs@cskk.id.au>
2023-12-08 14:06:43 +1100

    cs.books.cbz: make_cbz: fix use of atomic_filename, support use before Python 3.11
    --HG--
    branch : pdf

--- lib/python/cs/ebooks/cbz.py
+++ lib/python/cs/ebooks/cbz.py
@@ -6,6 +6,7 @@
 from contextlib import contextmanager
 import json
 from os.path import basename
+import sys
 from zipfile import ZipFile, ZIP_STORED
 
 from typeguard import typechecked
@@ -14,16 +15,15 @@ from cs.fileutils import atomic_filename
 from cs.pfx import Pfx, pfx_call
 
 @contextmanager
-@atomic_filename
 @typechecked
 def make_cbz(
     fspath: str,
     *,
     images,
     metadata=None,
-    metadata_encoding='utf-8',
+    metadata_encoding=None,
     compression=ZIP_STORED,
-    **zipkw
+    **zipkw,
 ):
   ''' A context manager for creating a CBZ archive file,
       yielding an open `ZipFile` instance.
@@ -32,6 +32,9 @@ def make_cbz(
       * `fspath`: the filesystem path for the new CBZ file
       * `images`: an iterable of images to save in the CBZ file
       * `metadata`: optional metadata, which may be a `str` or a JSNONable mapping
+      * `metadata_encoding`: optional metadata encoding;
+        setting this is only supported in Python 3.11 onward
+        but if not supplied, a default of `'utf-8'` will be used in 3.11 onward
       Other keyword parameters are passed to the `ZipFile` creation
       along with an `'x'` open mode.
 
@@ -41,25 +44,30 @@ def make_cbz(
       The second `str` is the `arcname` used when storing the file
       into the archive.
   '''
-  with pfx_call(
-      ZipFile,
-      fspath,
-      'x',
-      metadata_encoding=metadata_encoding,
-      compression=compression,
-      **zipkw,
-  ) as cbz:
-    if metadata:
-      if isinstance(metadata, str):
-        cbz.metadata = metadata
-      else:
-        cbz.metadata = json.dumps(metadata, separators=(',', ':'))
-    for img in images:
-      with Pfx(img):
-        if isinstance(img, str):
-          imgfspath, arcname = img, basename(img)
-        # TODO: Path instances?
+  if metadata_encoding is None:
+    if sys.version_info >= (3, 11):
+      metadata_encoding = 'utf-8'
+  if metadata_encoding is not None:
+    zipkw.update(metadata_encoding=metadata_encoding)
+  with atomic_filename(fspath) as T:
+    with pfx_call(
+        ZipFile,
+        T.name,
+        'w',
+        compression=compression,
+        **zipkw,
+    ) as cbz:
+      if metadata:
+        if isinstance(metadata, str):
+          cbz.metadata = metadata
         else:
-          imgfspath, arcname = img
-        pfx_call(cbz.write, imgfspath, arcname=arcname)
-    yield cbz
+          cbz.metadata = json.dumps(metadata, separators=(',', ':'))
+      for img in images:
+        with Pfx(img):
+          if isinstance(img, str):
+            imgfspath, arcname = img, basename(img)
+          # TODO: Path instances?
+          else:
+            imgfspath, arcname = img
+          pfx_call(cbz.write, imgfspath, arcname=arcname)
+      yield cbz
