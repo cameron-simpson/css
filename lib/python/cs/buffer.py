9821ff4860da7f4b581ec3842a5b37b2eda0122d
Cameron Simpson <cs@cskk.id.au>
2024-02-01 10:58:06 +1100

    Release information for cs.buffer-20240201. Summary: CornuCopyBuffer: read1() method shorthand for read(..,one_fetch=True).

--- lib/python/cs/buffer.py
+++ lib/python/cs/buffer.py
@@ -23,7 +23,7 @@ from cs.deco import Promotable
 from cs.gimmicks import r
 from cs.py3 import pread
 
-__version__ = '20230401-post'
+__version__ = '20240201'
 
 DISTINFO = {
     'keywords': ["python3"],
