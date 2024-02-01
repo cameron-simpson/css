0e3e272d297c4a41f0eea541b09b333501a472ee
Cameron Simpson <cs@cskk.id.au>
2024-02-01 22:49:39 +1100

    Release information for cs.ebooks-20240201.3. Summary: CBZ support. Kobo support. PDF support. Assorted new things and doubtless new bugs.

--- lib/python/cs/ebooks/__init__.py
+++ lib/python/cs/ebooks/__init__.py
@@ -209,7 +209,7 @@
           Run a command prompt via cmd.Cmd using this command's subcommands.
 '''
 
-__version__ = '20240201.2-post'
+__version__ = '20240201.3'
 
 DISTINFO = {
     'keywords': ["python3"],
