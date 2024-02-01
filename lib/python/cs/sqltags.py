32f61b907c49250529456ebb1f99f166d6c682e0
Cameron Simpson <cs@cskk.id.au>
2024-02-01 22:21:38 +1100

    Release information for cs.sqltags-20240201.1. Summary: Release with the "sqltags" script.

--- lib/python/cs/sqltags.py
+++ lib/python/cs/sqltags.py
@@ -78,7 +78,7 @@ from cs.tagset import (
 from cs.threads import locked, ThreadState
 from cs.upd import print  # pylint: disable=redefined-builtin
 
-__version__ = '20240201-post'
+__version__ = '20240201.1'
 
 DISTINFO = {
     'keywords': ["python3"],
