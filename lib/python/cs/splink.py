1f6a5a8edb14255db39ec98901cd63ba2c39f49e
Cameron Simpson <cs@cskk.id.au>
2024-02-01 14:20:26 +1100

    Release information for cs.splink-20240201. Summary: Release with the "splink" script. splink plot: new -U/--update option to run a "pull" before plotting.

--- lib/python/cs/splink.py
+++ lib/python/cs/splink.py
@@ -67,7 +67,7 @@ from cs.timeseries import (
 )
 from cs.upd import Upd, uses_upd, print  # pylint: disable=redefined-builtin
 
-__version__ = '20230612-post'
+__version__ = '20240201'
 
 DISTINFO = {
     'keywords': ["python3"],
