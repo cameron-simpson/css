bec2fa0f850c0ab9f816ee81bd2caf9861c6c2c5
Cameron Simpson <cs@cskk.id.au>
2024-02-01 22:51:34 +1100

    cs.ebooks: dummy commit again

--- lib/python/cs/ebooks/__init__.py
+++ lib/python/cs/ebooks/__init__.py
@@ -207,6 +207,7 @@
                 Extract the images from the named page files.
         shell
           Run a command prompt via cmd.Cmd using this command's subcommands.
+
 '''
 
 __version__ = '20240201.3-post'
