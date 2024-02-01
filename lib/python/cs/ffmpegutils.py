a8564fd7c1d80226d951c378ff5d13b8ab4de5a7
Cameron Simpson <cs@cskk.id.au>
2024-02-01 12:35:51 +1100

    cs.ffmpegutils: bump __version__ to 20240201-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/ffmpegutils.py
+++ lib/python/cs/ffmpegutils.py
@@ -34,7 +34,7 @@ from cs.pfx import Pfx, pfx, pfx_call
 from cs.psutils import pipefrom, print_argv
 from cs.tagset import TagSet
 
-__version__ = '20240201'
+__version__ = '20240201-post'
 
 DISTINFO = {
     'keywords': ["python3"],
