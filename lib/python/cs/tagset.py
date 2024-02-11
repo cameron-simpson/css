6875a64ad78e44c205aa0c45da8be8b0b6614ea0
Cameron Simpson <cs@cskk.id.au>
2024-02-11 15:44:02 +1100

    cs.tagset: bump __version__ to 20240211-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/tagset.py
+++ lib/python/cs/tagset.py
@@ -235,7 +235,7 @@ from cs.py3 import date_fromisoformat, datetime_fromisoformat
 from cs.resources import MultiOpenMixin
 from cs.threads import locked_property
 
-__version__ = '20240211'
+__version__ = '20240211-post'
 
 DISTINFO = {
     'keywords': ["python3"],
