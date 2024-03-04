f11d7e421024096052848ee1347465feea0d25f9
Cameron Simpson <cs@cskk.id.au>
2024-03-05 08:55:31 +1100

    Release information for cs.hashindex-20240305. Summary: HashIndexCommand.cmd_ls: support rhost:rpath paths, honour intterupts in the remote mode. HashIndexCommand.cmd_rearrange: new optional dstdir command line argument, passed to rearrange. merge: symlink_mode: leave identical symlinks alone, just merge tags. rearrange: new optional dstdirpath parameter, default srcdirpath.

--- lib/python/cs/hashindex.py
+++ lib/python/cs/hashindex.py
@@ -45,7 +45,7 @@ from cs.psutils import prep_argv, pipefrom, run
 from cs.resources import RunState, uses_runstate
 from cs.upd import print, run_task, without  # pylint: disable=redefined-builtin
 
-__version__ = '20240216-post'
+__version__ = '20240305'
 
 DISTINFO = {
     'keywords': ["python3"],
