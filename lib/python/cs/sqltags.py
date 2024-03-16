87896aa919ab87bfac990e8983cdf6d52448d0f3
Cameron Simpson <cs@cskk.id.au>
2024-03-16 16:55:39 +1100

    Release information for cs.sqltags-20240316. Summary: Fixed release upload artifacts.

--- lib/python/cs/sqltags.py
+++ lib/python/cs/sqltags.py
@@ -78,7 +78,7 @@ from cs.tagset import (
 from cs.threads import locked, ThreadState
 from cs.upd import print  # pylint: disable=redefined-builtin
 
-__version__ = '20240305-post'
+__version__ = '20240316'
 
 DISTINFO = {
     'keywords': ["python3"],
