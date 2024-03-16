229a7b83a3e45bfd583fa6972945611f2dd3d8eb
Cameron Simpson <cs@cskk.id.au>
2024-03-16 17:53:10 +1100

    Release information for cs.app.playon-20240316. Summary: Fixed release upload artifacts.

--- lib/python/cs/app/playon.py
+++ lib/python/cs/app/playon.py
@@ -45,7 +45,7 @@ from cs.threads import monitor, bg as bg_thread
 from cs.units import BINARY_BYTES_SCALE
 from cs.upd import print  # pylint: disable=redefined-builtin
 
-__version__ = '20240201.1-post'
+__version__ = '20240316'
 
 DISTINFO = {
     'keywords': ["python3"],
