502b2043aa443b0cfef9ba7de3f5b7c7d516af11
Cameron Simpson <cs@cskk.id.au>
2024-03-16 17:59:34 +1100

    Release information for cs.hashutils-20240316. Summary: Fixed release upload artifacts.

--- lib/python/cs/hashutils.py
+++ lib/python/cs/hashutils.py
@@ -11,7 +11,7 @@ import os
 from cs.buffer import CornuCopyBuffer
 from cs.deco import promote
 
-__version__ = '20240211-post'
+__version__ = '20240316'
 
 DISTINFO = {
     'keywords': ["python3"],
