3d37bca4b5556591e671ee7dec76703943494e20
Cameron Simpson <cs@cskk.id.au>
2024-03-16 18:02:06 +1100

    Release information for cs.upd-20240316. Summary: Fixed release upload artifacts.

--- lib/python/cs/upd.py
+++ lib/python/cs/upd.py
@@ -91,7 +91,7 @@ except ImportError as curses_e:
   warning("cannot import curses: %s", curses_e)
   curses = None
 
-__version__ = '20240216-post'
+__version__ = '20240316'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
