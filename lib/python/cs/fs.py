bfe29333c215bb2d2101dbb8ef00b4edf36b1518
Cameron Simpson <cs@cskk.id.au>
2024-03-16 17:35:31 +1100

    Release information for cs.fs-20240316. Summary: Fixed release upload artifacts.

--- lib/python/cs/fs.py
+++ lib/python/cs/fs.py
@@ -31,7 +31,7 @@ from cs.deco import decorator
 from cs.obj import SingletonMixin
 from cs.pfx import pfx, pfx_call
 
-__version__ = '20240201-post'
+__version__ = '20240316'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
