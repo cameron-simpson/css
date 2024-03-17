3ae874ea261504c554a2c3a45a3696aa9da92a63
Cameron Simpson <cs@cskk.id.au>
2024-03-17 12:38:53 +1100

    Release information for cs.hashindex-20240317. Summary: HashIndexCommand.cmd_ls: default to listing the current directory. HashIndexCommand: new -o output_format to allow outputting only hashcodes or fspaths. HashIndexCommand.cmd_comm: new -r (relative) option.

--- lib/python/cs/hashindex.py
+++ lib/python/cs/hashindex.py
@@ -100,7 +100,7 @@ from cs.psutils import prep_argv, pipefrom, run
 from cs.resources import RunState, uses_runstate
 from cs.upd import print, run_task, without  # pylint: disable=redefined-builtin
 
-__version__ = '20240316-post'
+__version__ = '20240317'
 
 DISTINFO = {
     'keywords': ["python3"],
