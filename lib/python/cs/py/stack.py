7e31d29eba33b7da03f109b6cda9c908180435b9
Cameron Simpson <cs@cskk.id.au>
2024-04-04 10:06:06 +1100

    cs.py.stack: provide StackSummary and FrameSummary if Python too old, associated refactors
    --HG--
    branch : vt

--- lib/python/cs/py/stack.py
+++ lib/python/cs/py/stack.py
@@ -24,19 +24,54 @@ DISTINFO = {
     'install_requires': [],
 }
 
-_Frame = namedtuple('Frame', 'filename lineno funcname linetext')
+try:
+  from traceback import FrameSummary, StackSummary
+except ImportError:
+  # provide _incomplete_ simple implementations
 
-class Frame(_Frame):
-  ''' A `namedtuple` for stack frame contents.
-  '''
+  class FrameSummary(namedtuple('FrameSummary', 'filename lineno name line')):
+    ''' A `namedtuple` for stack frame contents.
+    '''
 
-  def __str__(self):
-    return "%s:%d: %s" % (self.filename, self.lineno, self.linetext)
+    def __str__(self):
+      return "%s:%d: %s" % (self.filename, self.lineno, self.line)
+
+    # compatibility with my prior Frame class
+    @property
+    def funcname(self):
+      ''' The frame function name.
+      '''
+      return self.name
+
+    @property
+    def linetext(self):
+      ''' The line of source code.
+      '''
+      return self.line
+
+  class StackSummary(list):
+
+    @classmethod
+    def extract(
+        cls, frame_gen, limit=None, lookup_lines=None, capture_locals=False
+    ):
+      # not yet implemented
+      assert limit is None
+      assert capture_locals is None
+      assert lookup_lines is None
+      return cls(FrameSummary(raw_frame) for raw_frame, lineno in frame_gen)
+
+    @classmethod
+    def from_list(cls, frame_list):
+      return cls(FrameSummary(raw_frame) for raw_frame in frame_list)
+
+    def format(self):
+      return [self.format_frame_summary(frame) for frame in self]
 
 def frames():
   ''' Return the current stack as a list of `Frame` objects.
   '''
-  return [Frame(*f) for f in extract_stack()[:-1]]
+  return StackSummary.from_list(extract_stack())
 
 def caller(frame_index=-3):
   ''' Return the `Frame` of the caller's caller.
@@ -55,16 +90,23 @@ def caller(frame_index=-3):
   except IndexError:
     return None
 
-def stack_dump(f=None, indent=0, Fs=None, skip=None, select=None):
+def stack_dump(
+    f=None,
+    indent=0,
+    frames=None,
+    skip=None,
+    select=None,
+    format_frame=None,
+):
   ''' Recite current or supplied stack to `f`, default `sys.stderr`.
 
       Parameters:
       * `f`: the output file object, default `sys.stderr`
       * `indent`: how many spaces to indent the stack lines, default `0`
-      * `Fs`: the stack `Frame`s to write,
+      * `frames`: the stack `Frame`s to write,
         default obtained from the current stack
-      * `skip`: the number of `Frame`s to trim from the end of `Fs`;
-        if `Fs` is `None` this defaults to `2` to trim the `Frame`s
+      * `skip`: the number of `Frame`s to trim from the end of `frames`;
+        if `frames` is `None` this defaults to `2` to trim the `Frame`s
         for the `stack_dump` function and its call to `frames()`,
         otherwise the default is `0` to use the supplied `Frame`s as is
       * `select`: if not `None`, select particular frames;
@@ -73,15 +115,21 @@ def stack_dump(f=None, indent=0, Fs=None, skip=None, select=None):
   '''
   if f is None:
     f = sys.stderr
-  if Fs is None:
-    Fs = frames()
+  if frames is None:
+    frames = frames()
     if skip is None:
       skip = 2
   elif skip is None:
     skip = 0
   if skip > 0:
-    Fs = Fs[:-skip]
-  for F in Fs:
+    frames = frames[:-skip]
+  summary = StackSummary.from_list(list(frames))
+  if format_frame is None:
+    try:
+      format_frame = summary.format_frame_summary
+    except AttributeError:
+      format_frame = lambda frame: str(frame) + '\n'
+  for F in frames:
     if select is not None:
       if isinstance(select, str):
         if select not in F.filename:
@@ -89,7 +137,21 @@ def stack_dump(f=None, indent=0, Fs=None, skip=None, select=None):
       else:
         if not select(F):
           continue
-    print(' ' * indent, F, file=f, sep='')
+    formatted = format_frame(F)
+    indent_s = ' ' * indent
+    print(
+        indent_s,
+        formatted.rstrip('\n').replace('\n', indent_s + '\n'),
+        file=f,
+        sep='',
+    )
+    print(
+        indent_s,
+        "  ",
+        F.line,
+        file=f,
+        sep='',
+    )
 
 if __name__ == '__main__':
   import cs.py.stack_tests
