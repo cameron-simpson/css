8d77919abc75837b597d45814055b3026c07de5c
Cameron Simpson <cs@cskk.id.au>
2024-04-12 14:42:25 +1000

    cs.resources: bump __version__ to 20240412-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/resources.py
+++ lib/python/cs/resources.py
@@ -28,7 +28,7 @@ from cs.py.stack import caller, frames as stack_frames, stack_dump, StackSummary
 from cs.result import CancellationError
 from cs.threads import ThreadState, HasThreadState, NRLock
 
-__version__ = '20240412'
+__version__ = '20240412-post'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
