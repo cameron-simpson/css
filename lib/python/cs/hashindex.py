888657d3fe8fe38685627771bf368f9449c88df1
Cameron Simpson <cs@cskk.id.au>
2024-04-12 15:46:19 +1000

    cs.hashindex: bump __version__ to 20240412-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/hashindex.py
+++ lib/python/cs/hashindex.py
@@ -100,7 +100,7 @@ from cs.psutils import prep_argv, pipefrom, run
 from cs.resources import RunState, uses_runstate
 from cs.upd import print, run_task, without  # pylint: disable=redefined-builtin
 
-__version__ = '20240412'
+__version__ = '20240412-post'
 
 DISTINFO = {
     'keywords': ["python3"],
