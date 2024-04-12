929088771b86a6186c878c9cf1f319cdc5dde303
Cameron Simpson <cs@cskk.id.au>
2024-04-12 14:42:21 +1000

    Release information for cs.resources-20240412. Summary: RunState: new optional thread_wide=False parameter - if true, set this RunState as the Thread-wide default - this mode used by @uses_runstate, unsure about this default. RunState: new .iter(iterable) method which iterates while not RunState.cancelled. MultiOpenMixin: replace __mo_getstate() method with MultiOpenMixin_state property. RunState.__init__: make most parameters keyword only.

--- lib/python/cs/resources.py
+++ lib/python/cs/resources.py
@@ -28,7 +28,7 @@ from cs.py.stack import caller, frames as stack_frames, stack_dump, StackSummary
 from cs.result import CancellationError
 from cs.threads import ThreadState, HasThreadState, NRLock
 
-__version__ = '20240316-post'
+__version__ = '20240412'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
