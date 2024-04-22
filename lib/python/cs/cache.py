0fb63c75bdb20be559e9de6c1125d9b4d6f4d13c
Cameron Simpson <cs@cskk.id.au>
2024-04-22 15:30:25 +1000

    Release information for cs.cache-20240422. Summary: New ConvCache and convof: a cache for conversions of file contents such as thumbnails or transcoded media.

--- lib/python/cs/cache.py
+++ lib/python/cs/cache.py
@@ -18,7 +18,7 @@ from cs.resources import MultiOpenMixin
 from cs.result import Result
 from cs.seq import unrepeated
 
-__version__ = '20240412-post'
+__version__ = '20240422'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
