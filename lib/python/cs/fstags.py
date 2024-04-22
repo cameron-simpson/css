576d83b45c0cef2746bca727bf91176c852366f1
Cameron Simpson <cs@cskk.id.au>
2024-04-22 12:18:51 +1000

    cs.fstags: FSTagsCommand.cmd_ls: update usage message

--- lib/python/cs/fstags.py
+++ lib/python/cs/fstags.py
@@ -585,7 +585,7 @@ class FSTagsCommand(BaseCommand, TagsCommandMixin):
 
   @uses_runstate
   def cmd_ls(self, argv, *, runstate: RunState):
-    ''' Usage: {cmd} [-d] [--direct] [-o output_format] [paths...]
+    ''' Usage: {cmd} [-dlr] [--direct] [-o output_format] [paths...]
           List files from paths and their tags.
           -d          Treat directories like files, do not recurse.
           --direct    List direct tags instead of all tags.
@@ -594,6 +594,7 @@ class FSTagsCommand(BaseCommand, TagsCommandMixin):
                       Use output_format as a Python format string to lay out
                       the listing.
                       Default: {LS_OUTPUT_FORMAT_DEFAULT}
+          -r          Recurse into subdirectories.
     '''
     options = self.options
     fstags = options.fstags
