8f9e49f845610b3162788023dbf5f60e838371cb
Cameron Simpson <cs@cskk.id.au>
2024-04-23 19:27:50 +1000

    cs.taskqueue: bump __version__ to 20240423-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/taskqueue.py
+++ lib/python/cs/taskqueue.py
@@ -26,7 +26,7 @@ from cs.seq import Seq, unrepeated
 from cs.threads import bg as bg_thread, locked, ThreadState, HasThreadState
 from cs.typingutils import subtype
 
-__version__ = '20240423'
+__version__ = '20240423-post'
 
 DISTINFO = {
     'keywords': ["python3"],
