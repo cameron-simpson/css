7363dcf2cf3eadbf8a15dc188dd19b40213f279e
Cameron Simpson <cs@cskk.id.au>
2024-04-23 19:25:03 +1000

    Release information for cs.resources-20240423. Summary: RunStateMixin: make the optional runstate parameter keyword only.

--- lib/python/cs/resources.py
+++ lib/python/cs/resources.py
@@ -29,7 +29,7 @@ from cs.py.stack import caller, frames as stack_frames, stack_dump, StackSummary
 from cs.result import CancellationError
 from cs.threads import ThreadState, HasThreadState, NRLock
 
-__version__ = '20240422-post'
+__version__ = '20240423'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
