a4631ca3a6f8d82aab6fcbcdbc4049a53e293185
Cameron Simpson <cs@cskk.id.au>
2024-05-05 13:20:17 +1000

    .env.sh: set $VIRTUAL_ENV for uv
    --HG--
    branch : vt

--- .env.sh
+++ .env.sh
@@ -4,6 +4,7 @@
 # - Cameron Simpson <cs@cskk.id.au>
 #
 
-export PYTHON3=$ENV_DEV_DIR/venv/bin/python3
-export PATH=$ENV_DEV_DIR/bin-cs:$ENV_DEV_DIR/bin:$ENV_DEV_DIR/venv/bin:$PATH
+export VIRTUAL_ENV=$ENV_DEV_DIR/venv    # for uv
+export PYTHON3=$VIRTUAL_ENV/bin/python3
+export PATH=$ENV_DEV_DIR/bin-cs:$ENV_DEV_DIR/bin:$VIRTUAL_ENV/bin:$PATH
 ##export PYTHONWARNINGS=default
