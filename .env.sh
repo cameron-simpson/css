9e760fb6ebb860392d5f186fe2157a5b78aee580
Cameron Simpson <cs@cskk.id.au>
2024-05-13 10:20:54 +1000

    .env.sh: look for venv-$ARCH ahead of venv, needed now that I use inmnt

--- .env.sh
+++ .env.sh
@@ -4,6 +4,12 @@
 # - Cameron Simpson <cs@cskk.id.au>
 #
 
-export PYTHON3=$ENV_DEV_DIR/venv/bin/python3
+for venv_sfx in "-$ARCH" ""
+do
+  venv=$ENV_DEV_DIR/venv$venv_sfx
+  [ -d "$venv" ] && [ -x "$venv/bin/python3" ] && break
+  venv=
+done
+[ -n "$venv" ] && export PYTHON3=$ENV_DEV_DIR/venv/bin/python3
 export PATH=$ENV_DEV_DIR/bin-cs:$ENV_DEV_DIR/bin:$ENV_DEV_DIR/venv/bin:$PATH
 ##export PYTHONWARNINGS=default
