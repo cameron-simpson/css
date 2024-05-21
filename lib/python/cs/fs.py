2032e8c378115d57e948453de6a631b754175702
Cameron Simpson <cs@cskk.id.au>
2024-05-22 09:27:30 +1000

    Release information for cs.fs-20240522. Summary: shortpath: new collapseuser=False, foldsymlinks=False parameters, rename DEFAULT_SHORTEN_PREFIXES to SHORTPATH_PREFIXES_DEFAULT.

--- lib/python/cs/fs.py
+++ lib/python/cs/fs.py
@@ -32,7 +32,7 @@ from cs.deco import decorator, fmtdoc
 from cs.obj import SingletonMixin
 from cs.pfx import pfx, pfx_call
 
-__version__ = '20240422-post'
+__version__ = '20240522'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
