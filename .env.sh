12c6b026a53021430abea8e80d55f78630a263d5
Cameron Simpson <cs@cskk.id.au>
2024-05-22 20:13:13 +1000

    .env.sh: let the envrc VIRTUAL_ENV preempt the local venv
    --HG--
    branch : vt

--- .env.sh
+++ .env.sh
@@ -4,15 +4,18 @@
 # - Cameron Simpson <cs@cskk.id.au>
 #
 
-for venv_sfx in "-$ARCH" ""
-do
-  venv=$ENV_DEV_DIR/venv$venv_sfx
-  [ -x "$venv/bin/python3" ] && break
-  venv=
-done
+[ -n "$VIRTUAL_ENV" ] || {
+  for venv_sfx in "-$ARCH" ""
+  do
+    venv=$ENV_DEV_DIR/venv$venv_sfx
+    [ -x "$venv/bin/python3" ] && break
+    venv=
+  done
 
-[ -z "$venv" ] || {
-  export VIRTUAL_ENV=$venv    # for uv
+  [ -z "$venv" ] || export VIRTUAL_ENV=$venv
+}
+
+[ -z "$VIRTUAL_ENV" ] || {
   export PYTHON3=$VIRTUAL_ENV/bin/python3
   export PATH=$ENV_DEV_DIR/bin-cs:$ENV_DEV_DIR/bin:$VIRTUAL_ENV/bin:$PATH
 }
