025a4e7a9e743213bb3acd3f06915e0e1c2515ef
Cameron Simpson <cs@cskk.id.au>
2024-06-01 11:55:53 +1000

    cs.result_tests: clean lint
    --HG--
    branch : vt

--- lib/python/cs/result_tests.py
+++ lib/python/cs/result_tests.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python
-#
-# Self tests for cs.result.
-#       - Cameron Simpson <cs@cskk.id.au>
-#
 
-from __future__ import print_function
+''' Self tests for cs.result.
+    - Cameron Simpson <cs@cskk.id.au>
+'''
 
 import sys
 import threading
@@ -18,15 +16,18 @@ class TestResult(unittest.TestCase):
   '''
 
   def setUp(self):
+    '''Prepare a `Result` for testing.'''
     self.R = Result()
 
   def test00result(self):
+    '''Exercise a `Result` in basic ways.'''
     R = self.R
     self.assertFalse(R.ready)
     countery = [0]
     self.assertEqual(countery[0], 0)
 
-    def count(innerR):
+    def count(_):
+      '''Bump a counter.'''
       countery[0] += 1
 
     count(None)
@@ -44,12 +45,14 @@ class TestResult(unittest.TestCase):
     self.assertEqual(countery[0], 3)
 
   def test01after(self):
+    '''Exercise `cs.result.after`.'''
     R = self.R
     self.assertFalse(R.ready)
     R2 = Result()
     self.assertFalse(R2.ready)
 
     def add_R_R2():
+      '''Add `R.result + R2.result`.'''
       value = R.result + R2.result
       return value
 
@@ -59,6 +62,7 @@ class TestResult(unittest.TestCase):
     self.assertFalse(R2.ready)
 
     def delayed_completion():
+      '''Complete `Result`s after delays.'''
       time.sleep(0.2)
       R.result = 1
       time.sleep(0.2)
@@ -72,23 +76,27 @@ class TestResult(unittest.TestCase):
     self.assertTrue(R2.ready)
 
   def test02bg(self):
+    '''Exercise `Result.bg`.'''
     R = self.R
     self.assertFalse(R.ready)
 
     def f(n):
+      '''Return `n` after a brief delay.'''
       time.sleep(0.1)
       return n
 
     T = R.bg(f, 3)
-    self.assertTrue(type(T) == threading.Thread)
+    self.assertIsInstance(T, threading.Thread)
     self.assertFalse(R.ready)
     time.sleep(0.2)
     self.assertTrue(R.ready)
     self.assertEqual(R.result, 3)
 
   def test02bg2(self):
+    '''Exercise `cs.result.bg`.'''
 
     def f(n):
+      '''Return `n` after a brief delay.'''
       time.sleep(0.1)
       return n
 
@@ -99,12 +107,14 @@ class TestResult(unittest.TestCase):
     self.assertEqual(R.result, 3)
 
   def test00join(self):
+    '''Exercise `Result.join`.'''
     R = self.R
     R.result = 1
     R.join()
     R.join()
 
   def test01post_notify(self):
+    '''Exercise `Result.post_notify`.'''
     R = self.R
     R2 = R.post_notify(lambda r2: print("r2 =", r2))
     R.result = 1
@@ -112,6 +122,7 @@ class TestResult(unittest.TestCase):
     self.assertTrue(R2.ready)
 
 def selftest(argv):
+  '''RUn the unit tests.'''
   unittest.main(__name__, None, argv)
 
 if __name__ == '__main__':
