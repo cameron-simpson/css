4b11f88d9958d7c758ba698b13d0a349e52b088c
Cameron Simpson <cs@cskk.id.au>
2024-06-22 19:27:49 +1000

    cs.app.osx.misc: fix trove classifier

--- lib/python/cs/app/osx/misc.py
+++ lib/python/cs/app/osx/misc.py
@@ -12,7 +12,7 @@ __version__ = '20240622-post'
 DISTINFO = {
     'keywords': ["python3"],
     'classifiers': [
-        "Development Status :: 3 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Environment :: MacOS X",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
