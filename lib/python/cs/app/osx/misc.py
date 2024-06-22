87504c323dcc3a87854b4d0f90a6142b6ad1589e
Cameron Simpson <cs@cskk.id.au>
2024-06-22 16:06:49 +1000

    cs.app.osx.misc: DISTINFO

--- lib/python/cs/app/osx/misc.py
+++ lib/python/cs/app/osx/misc.py
@@ -7,5 +7,15 @@
 
 import platform
 
+DISTINFO = {
+    'keywords': ["python3"],
+    'classifiers': [
+        "Development Status :: 3 - Beta",
+        "Environment :: MacOS X",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+    ],
+}
+
 # The MacOS version as a tuple of ints, eg `(14,5)` for MacOS Sonoma
 macos_version = tuple(int(vs) for vs in platform.mac_ver()[0].split('.'))
