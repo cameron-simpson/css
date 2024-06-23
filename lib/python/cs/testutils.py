791b7d5e5fd6f6b6ba225f333a02e5131967951f
Cameron Simpson <cs@cskk.id.au>
2024-06-23 10:15:01 +1000

    cs.testutils: bump __version__ to 20240623-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/testutils.py
+++ lib/python/cs/testutils.py
@@ -14,7 +14,7 @@ from cs.context import push_cmgr, pop_cmgr
 from cs.debug import thread_dump
 from cs.deco import decorator
 
-__version__ = '20240623'
+__version__ = '20240623-post'
 
 DISTINFO = {
     'keywords': ["python3"],
