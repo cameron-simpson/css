0e0b1b24b2e50ae2ba8ae0290c77d2d94b62cd2b
Cameron Simpson <cs@cskk.id.au>
2024-06-30 11:20:21 +1000

    cs.fsm: bump __version__ to 20240630-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/fsm.py
+++ lib/python/cs/fsm.py
@@ -17,7 +17,7 @@ from cs.lex import cutprefix
 from cs.pfx import Pfx, pfx_call
 from cs.seq import first
 
-__version__ = '20240630'
+__version__ = '20240630-post'
 
 DISTINFO = {
     'keywords': ["python3"],
