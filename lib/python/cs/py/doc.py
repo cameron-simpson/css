1400a794590cea85a646504d89d79524b5f767d4
Cameron Simpson <cs@cskk.id.au>
2024-06-30 14:43:46 +1000

    Release information for cs.py.doc-20240630.1. Summary: module_doc: build the usage message from an instance of the baseCommand, needed since the last cs.cmdutils release.

--- lib/python/cs/py/doc.py
+++ lib/python/cs/py/doc.py
@@ -16,7 +16,7 @@ from cs.logutils import warning
 from cs.pfx import Pfx
 from cs.py.modules import module_attributes
 
-__version__ = '20240630-post'
+__version__ = '20240630.1'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
