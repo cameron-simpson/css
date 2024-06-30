8e44a50531c0868e284495e9f2ef1478f44b4a4b
Cameron Simpson <cs@cskk.id.au>
2024-06-30 11:13:29 +1000

    cs.debug: bump __version__ to 20240630-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/debug.py
+++ lib/python/cs/debug.py
@@ -62,7 +62,7 @@ from cs.threads import ThreadState
 from cs.upd import breakpoint, print  # pylint: disable=redefined-builtin
 from cs.x import X
 
-__version__ = '20240630'
+__version__ = '20240630-post'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
