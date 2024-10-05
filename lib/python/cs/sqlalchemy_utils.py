16e3d03e3746d98d08a576d2eddd4fc3f59b9c33
Cameron Simpson <cs@cskk.id.au>
2024-10-05 19:12:32 +1000

    cs.sqlalchemy_utils: bump __version__ to 20241005-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/sqlalchemy_utils.py
+++ lib/python/cs/sqlalchemy_utils.py
@@ -42,7 +42,7 @@ from cs.threads import ThreadState
 ##  from cs.py.stack import caller
 ##  X("CHECK from %r", caller())
 
-__version__ = '20241005'
+__version__ = '20241005-post'
 
 DISTINFO = {
     'description':
