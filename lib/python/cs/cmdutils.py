f669138a71ab1082231f6a4dad402cce44e55d5c
Cameron Simpson <cs@cskk.id.au>
2024-11-10 14:31:30 +1100

    Release information for cs.cmdutils-20241110. Summary: Various fixes and enhancements to the usage text generation. BaseCommandOptions.COMMON_OPT_SPECS stuff, including inheritance and presentation in usage. BaseCommand.run: if self.run_context() yields not None, exit immediately with that value. Rationalise and improve OptionSpec. Supplant vprint with qvprint which honours quiet and verbose, provide vprint as a shim with a hardwired quiet=False. Make cmd_info the default subcommand and make some improvements. BaseCommandOptions: new fields_as_dict method, update copy() to start with fields_as_dict, overlay as_dict, overlay updates. OptionSpec: new add_argument(parser[,options]) method to add this option to an argparse style option parser (again, for the upcoming cs.djutils module). A heap of small things.

--- lib/python/cs/cmdutils.py
+++ lib/python/cs/cmdutils.py
@@ -58,7 +58,7 @@ from cs.threads import HasThreadState, ThreadState
 from cs.typingutils import subtype
 from cs.upd import Upd, uses_upd, print  # pylint: disable=redefined-builtin
 
-__version__ = '20241007-post'
+__version__ = '20241110'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
