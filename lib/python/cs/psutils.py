daa3c00ad1db3a58bb8c8e4d7635c90eb4d93522
Cameron Simpson <cs@cskk.id.au>
2024-11-22 11:55:04 +1100

    Release information for cs.psutils-20241122. Summary: print_argv: new print= parameter to provide a print() function, refactor to use print instead of file.write. run: new optional print parameter, plumb to print_argv. Use @uses_doit and @uses_quiet to provide the default quiet and doit states.

--- lib/python/cs/psutils.py
+++ lib/python/cs/psutils.py
@@ -25,7 +25,7 @@ from cs.deco import fmtdoc, uses_doit, uses_quiet
 from cs.gimmicks import trace, warning, DEVNULL
 from cs.pfx import pfx_call
 
-__version__ = '20240316-post'
+__version__ = '20241122'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
