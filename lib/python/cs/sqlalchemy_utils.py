594ae8f0f852ba5e91542c05e205c5f74386b93c
Cameron Simpson <cs@cskk.id.au>
2024-11-24 08:23:48 +1100

    cs.sqlalchemy_utils: ORM.startup_shutdown: make the poll interval 2s instead of 0.2s, which seems a bit aggressive when there's contention

--- lib/python/cs/sqlalchemy_utils.py
+++ lib/python/cs/sqlalchemy_utils.py
@@ -364,7 +364,7 @@ class ORM(MultiOpenMixin, ABC):
         bool(self.db_fspath),
         lockfile,
         self.db_fspath,
-        poll_interval=0.2,
+        poll_interval=2,
     ):
       # make sure that the db tables are up to date
       if self.__first_use:
