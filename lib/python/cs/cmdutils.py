292c59b20ac6ffe2bb7ab52652962baa206c833e
Cameron Simpson <cs@cskk.id.au>
2024-12-18 14:50:24 +1100

    cs.cmdutils: bump __version__ to 20241218-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/cmdutils.py
+++ lib/python/cs/cmdutils.py
@@ -62,7 +62,7 @@ from cs.threads import HasThreadState, ThreadState
 from cs.typingutils import subtype
 from cs.upd import Upd, uses_upd, print  # pylint: disable=redefined-builtin
 
-__version__ = '20241218'
+__version__ = '20241218-post'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
