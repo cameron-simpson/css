028858500923348922e8956a4d6f39f2e798aa78
Cameron Simpson <cs@cskk.id.au>
2024-12-22 08:42:41 +1100

    cs.djutils: bump __version__ to 20241222.2-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/djutils.py
+++ lib/python/cs/djutils.py
@@ -19,7 +19,7 @@ from typeguard import typechecked
 from cs.cmdutils import BaseCommand as CSBaseCommand
 from cs.lex import cutprefix, stripped_dedent
 
-__version__ = '20241222.2'
+__version__ = '20241222.2-post'
 
 DISTINFO = {
     'keywords': ["python3"],
