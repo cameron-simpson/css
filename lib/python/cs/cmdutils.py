fad37aab1ceaa09bd80ef90ce8fd302e6baab2fb
Cameron Simpson <cs@cskk.id.au>
2024-12-22 08:36:21 +1100

    Release information for cs.cmdutils-20241222. Summary: BaseCommand.cmd_info: use compact=True with the pforat() call.

--- lib/python/cs/cmdutils.py
+++ lib/python/cs/cmdutils.py
@@ -62,7 +62,7 @@ from cs.threads import HasThreadState, ThreadState
 from cs.typingutils import subtype
 from cs.upd import Upd, uses_upd, print  # pylint: disable=redefined-builtin
 
-__version__ = '20241218-post'
+__version__ = '20241222'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
