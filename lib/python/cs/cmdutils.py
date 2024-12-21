7295dc5da46c3968ba4b4452abeee8010e5deac4
Cameron Simpson <cs@cskk.id.au>
2024-12-22 09:59:14 +1100

    Release information for cs.cmdutils-20241222.1. Summary: BaseCommand.cmd_info: use pformat instead of str. BaseCommand.cmd_repl: enumerate the options as top level names in the banner. If run as main programme, run a demo command.

--- lib/python/cs/cmdutils.py
+++ lib/python/cs/cmdutils.py
@@ -62,7 +62,7 @@ from cs.threads import HasThreadState, ThreadState
 from cs.typingutils import subtype
 from cs.upd import Upd, uses_upd, print  # pylint: disable=redefined-builtin
 
-__version__ = '20241222-post'
+__version__ = '20241222.1'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
