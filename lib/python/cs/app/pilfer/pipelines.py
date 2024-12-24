4b1ec23d371cc4254e88d141c08a7471961fe073
Cameron Simpson <cs@cskk.id.au>
2024-12-25 10:39:41 +1100

    cs.app.pilfer.pipelines: replace PipeSpec with PipeLineSpec, which makes AsyncPipeLines on demand
    --HG--
    branch : pilfer

--- lib/python/cs/app/pilfer/pipelines.py
+++ lib/python/cs/app/pilfer/pipelines.py
@@ -1,27 +1,91 @@
 #!/usr/bin/env puython3
 
+from asyncio import run
 from collections import namedtuple
-try:
-  import xml.etree.cElementTree as ElementTree
-except ImportError:
-  pass
+from dataclasses import dataclass
+from functools import partial
+import shlex
+from typing import Callable, List, Mapping, Union
 
+from cs.deco import Promotable
+from cs.lex import get_identifier
+from cs.naysync import AsyncPipeLine, AnyIterable
+from cs.pfx import Pfx, pfx_method
 
-from cs.excutils import logexc
-from cs.pfx import Pfx
+from .actions import Action
 
-class PipeSpec(namedtuple('PipeSpec', 'name argv')):
+@dataclass
+class PipeLineSpec(Promotable):
   ''' A pipeline specification: a name and list of actions.
   '''
 
-  @logexc
-  def pipe_funcs(self, L, action_map, do_trace):
-    ''' Compute a list of functions to implement a pipeline.
+  name: Union[str, None]
+  stage_specs: List[str]  # source list
 
-        It is important that this list is constructed anew for each
-        new pipeline instance because many of the functions rely
-        on closures to track state.
+  @classmethod
+  def from_str(cls, pipe_spec_s, name=None):
+    ''' Return an `PipeLineSpec` from a string description.
     '''
-    with Pfx(self.name):
-      pipe_funcs, errors = argv_pipefuncs(self.argv, L, action_map, do_trace)
-    return pipe_funcs, errors
+    return cls(name=name, stage_specs=shlex.split(pipe_spec_s))
+
+  @pfx_method
+  def make_pipeline(self) -> AsyncPipeLine:
+    ''' Construct an `AsyncPipeLine` from an iterable of pipeline
+        stage specification strings and an action mapping.
+    '''
+    specs = list(self.stage_specs)
+    stage_funcs = []
+    while specs:
+      spec = specs.pop(0)
+      with Pfx("%r", spec):
+        # support commenting-out of individual actions
+        if spec.startswith('#'):
+          continue
+        if spec == "*":
+          # fork a new pipeline instance per item
+          # terminate this pipeline with a function to spawn subpipelines
+          # using the tail of the action list from this point
+          if not specs:
+            raise ValueError('no actions after')
+
+          async def per(item, pipespec):
+            ''' Process a single `item` through its own pipeline.
+            '''
+            subpipe = pipespec.make_pipeline()
+            async for result in subpipe([item]):
+              yield result
+
+          subpipelinespec = type(self)(
+              name=f'{spec} {" ".join(map(shlex.quote, specs))}',
+              argv=specs,
+          )
+          stage_funcs.append(partial(per, pipespec=subpipelinespec))
+          specs = []
+          continue
+        action = trace(Action.from_str, retval=True)(spec)
+        stage_funcs.append(action.stage_spec)
+    return AsyncPipeLine.from_stages(*stage_funcs)
+
+  async def run_pipeline(self, input_items: AnyIterable):
+    ''' Asynchronous generator to make an `AsyncPipeLine` and run it with `input_items`.
+    '''
+    pipeline = self.make_pipeline()
+    async for item in pipeline(input_items):
+      X("run_pipeline: pipeline(input_items) -> %s", r(item))
+      yield item
+
+if __name__ == '__main__':
+
+  def double(item):
+    yield item
+    yield item
+
+  def stringify(item):
+    yield f'string {item!r}'
+
+  async def demo_pipeline(it: AnyIterable):
+    pipeline = AsyncPipeLine.from_stages(double, stringify)
+    async for result in pipeline(it):
+      print(result)
+
+  run(demo_pipeline([1, 2, 3]))
