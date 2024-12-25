834c2c5404adfb575a26534184064d7127b6ab64
Cameron Simpson <cs@cskk.id.au>
2024-12-25 11:06:58 +1100

    cs.app.pilfer.urls: add srcs()
    --HG--
    branch : pilfer

--- lib/python/cs/app/pilfer/urls.py
+++ lib/python/cs/app/pilfer/urls.py
@@ -5,9 +5,14 @@ from cs.urlutils import URL
 
 from cs.debug import trace, X, r, s
 
-##@trace(retval=True)
 @promote
 def hrefs(U: URL):
   ''' Generator yielding the HREFs from a `URL`.
   '''
   yield from U.hrefs(absolute=True)
+
+@promote
+def srcs(U: URL):
+  ''' Generator yielding the SRCs from a `URL`.
+  '''
+  yield from U.srcs(absolute=True)
