86bf88a2c59740d78161c079037941e568419f1e
Cameron Simpson <cs@cskk.id.au>
2025-01-03 14:33:13 +1100

    Release information for cs.resources-20250103. Summary: RunState: new raiseif() method to raise CancellationError if cancelled. Move ClosedError and @not_closed to cs.semantics.

--- lib/python/cs/resources.py
+++ lib/python/cs/resources.py
@@ -32,7 +32,7 @@ from cs.result import CancellationError
 from cs.semantics import ClosedError, not_closed
 from cs.threads import ThreadState, HasThreadState, NRLock
 
-__version__ = '20241005-post'
+__version__ = '20250103'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
