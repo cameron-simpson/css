fe7ea8fac20096da4fac4488086d301df6b1f1bf
Cameron Simpson <cs@cskk.id.au>
2025-01-03 15:18:44 +1100

    Release information for cs.cmdutils-20250103. Summary: BaseCommand.cmd_info: default skip_names is self.options.INFO_SKIP_NAMES.

--- lib/python/cs/cmdutils.py
+++ lib/python/cs/cmdutils.py
@@ -77,7 +77,7 @@ from cs.threads import HasThreadState, ThreadState
 from cs.typingutils import subtype
 from cs.upd import Upd, uses_upd, print  # pylint: disable=redefined-builtin
 
-__version__ = '20241222.1-post'
+__version__ = '20250103'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
