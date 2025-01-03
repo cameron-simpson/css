5329876e513c36cb9e27eeb0cab09d0f7bd0187a
Cameron Simpson <cs@cskk.id.au>
2025-01-03 14:38:31 +1100

    Release information for cs.fileutils-20250103. Summary: New rename_excl(oldpath,newpath) to rename oldpath to newpath provided newpath does not exist - race free unlike obsolete saferename. atomic_filename: use rename_excl. Moved file_based and @file_property from cs.fileutils to cs.cache.

--- lib/python/cs/fileutils.py
+++ lib/python/cs/fileutils.py
@@ -53,7 +53,7 @@ from cs.result import CancellationError
 from cs.threads import locked
 from cs.units import BINARY_BYTES_SCALE
 
-__version__ = '20241122-post'
+__version__ = '20250103'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
