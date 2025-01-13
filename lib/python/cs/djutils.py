f599776e61d2e07ca5d5c592953db7b54b19bc55
Cameron Simpson <cs@cskk.id.au>
2025-01-13 14:12:01 +1100

    Release information for cs.djutils-20250113.1. Summary: model_batches_qs: new exclude=dict and filter=dict optional parameters to filter before the slice.

--- lib/python/cs/djutils.py
+++ lib/python/cs/djutils.py
@@ -28,7 +28,7 @@ from cs.cmdutils import BaseCommand as CSBaseCommand
 from cs.gimmicks import warning
 from cs.lex import cutprefix, stripped_dedent
 
-__version__ = '20250113-post'
+__version__ = '20250113.1'
 
 DISTINFO = {
     'keywords': ["python3"],
