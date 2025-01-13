0ec90a08e2d6545d15af65a8bccf043cb61c5a79
Cameron Simpson <cs@cskk.id.au>
2025-01-14 09:24:10 +1100

    cs.sitemap: SiteMap: now a dataclass with a .name:str attribute
    --HG--
    branch : pilfer

--- lib/python/cs/app/pilfer/sitemap.py
+++ lib/python/cs/app/pilfer/sitemap.py
@@ -3,18 +3,23 @@
 ''' Base class for site maps.
 '''
 
+from dataclasses import dataclass
+
 from cs.deco import promote
 from cs.urlutils import URL
 
+@dataclass
 class SiteMap:
   ''' A site map.
   '''
 
+  name: str
+
   @promote
-  def url_cache_key(self, url: str | URL) -> str | None:
+  def url_key(self, url: str | URL) -> str | None:
     ''' Return a string which is a persistent cache key for the
-        supplied `url`, or `None` for URLs which shoul not be cached
-        persistently.
+        supplied `url` within the content of this sitemap, or `None`
+        for URLs which shoul not be cached persistently.
 
         This default implementation always returns `None`.
     '''
