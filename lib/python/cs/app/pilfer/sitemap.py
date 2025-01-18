5e50463c878e9b4701e8f857d42f36c873a6aee6
Cameron Simpson <cs@cskk.id.au>
2025-01-19 10:52:34 +1100

    cs.app.pilfer.sitemap: make SiteMap an abstract class, improve docstrings
    --HG--
    branch : pilfer

--- lib/python/cs/app/pilfer/sitemap.py
+++ lib/python/cs/app/pilfer/sitemap.py
@@ -3,18 +3,30 @@
 ''' Base class for site maps.
 '''
 
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
 from cs.deco import promote
 from cs.urlutils import URL
 
 @dataclass
-class SiteMap:
-  ''' A site map.
+class SiteMap(ABC):
+  ''' A base class for site maps.
+
+      A `Pilfer` instance obtains its site maps from the `[sitemaps]`
+      clause in the configuration file, see the `Pilfer.sitemaps`
+      property for specific.
+
+      Example:
+
+          docs.python.org = docs:cs.app.pilfer.sitemap:DocSite
+          docs.mitmproxy.org = docs
+          *.readthedocs.io = docs
   '''
 
   name: str
 
+  @abstractmethod
   @promote
   def url_key(self, url: URL) -> str | None:
     ''' Return a string which is a persistent cache key for the
@@ -22,5 +34,10 @@ class SiteMap:
         for URLs which shoul not be cached persistently.
 
         This default implementation always returns `None`.
+
+        A site with semantic URLs might have keys like
+        *entity_type*`/`*id*`/`*aspect* where the *aspect* was
+        something like `html` or `icon` etc for different URLs
+        associated with the same entity.
     '''
     return None
