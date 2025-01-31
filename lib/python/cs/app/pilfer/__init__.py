e2f3bbc8c77b4ca7c3e6377568cf1ada5754c162
Cameron Simpson <cs@cskk.id.au>
2025-02-01 10:53:00 +1100

    cs.app.pilfer.__init__: initial DISTINFO
    --HG--
    branch : pilfer

--- lib/python/cs/app/pilfer/__init__.py
+++ lib/python/cs/app/pilfer/__init__.py
@@ -6,6 +6,56 @@
 ''' Pilfer, a web scraping tool.
 '''
 
+DISTINFO = {
+    'keywords': ["python3"],
+    'classifiers': [
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Internet :: Proxy Servers",
+        "Topic :: Utilities",
+    ],
+    'entry_points': {
+        'console_scripts': {
+            'pilfer': 'cs.app.pilfer:main'
+        },
+    },
+    'install_requires': [
+        'cs.app.flag',
+        'cs.cmdutils',
+        'cs.context',
+        'cs.debug',
+        'cs.deco',
+        'cs.env',
+        'cs.excutils',
+        'cs.fileutils>=atomic_filename',
+        'cs.fs',
+        'cs.hashutils',
+        'cs.later',
+        'cs.lex',
+        'cs.logutils',
+        'cs.mappings',
+        'cs.naysync',
+        'cs.obj',
+        'cs.pfx>=pfx_call',
+        'cs.pipeline',
+        'cs.progress',
+        'cs.py.func',
+        'cs.py.modules',
+        'cs.queues',
+        'cs.resources',
+        'cs.seq',
+        'cs.threads',
+        'cs.upd',
+        'cs.urlutils',
+        'icontract',
+        'mitmproxy',
+        'requests',
+        'typeguard',
+    ],
+}
+
 # parallelism of jobs
 DEFAULT_JOBS = 4
 
