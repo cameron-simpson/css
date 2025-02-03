7004da2267cc248d178bb66df936ab94c387e509
Cameron Simpson <cs@cskk.id.au>
2025-02-04 09:14:18 +1100

    cs.queues: drop obsolete pretence of python 2 support, embrace the f-string, tweak a docstring
    --HG--
    branch : pilfer

--- lib/python/cs/queues.py
+++ lib/python/cs/queues.py
@@ -9,6 +9,7 @@
 
 from contextlib import contextmanager
 from functools import partial
+from queue import Queue, PriorityQueue, Queue_Empty
 import sys
 from threading import Timer, Lock, RLock, Thread
 import time
@@ -21,7 +22,6 @@ import cs.logutils
 from cs.logutils import exception, warning, debug
 from cs.obj import Sentinel
 from cs.pfx import Pfx, PfxCallInfo
-from cs.py3 import Queue, PriorityQueue, Queue_Empty
 from cs.resources import (
     MultiOpenMixin,
     RunState,
@@ -39,7 +39,6 @@ DISTINFO = {
     'keywords': ["python2", "python3"],
     'classifiers': [
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
     ],
     'install_requires': [
@@ -47,7 +46,6 @@ DISTINFO = {
         'cs.logutils',
         'cs.obj',
         'cs.pfx',
-        'cs.py3',
         'cs.resources',
         'cs.result',
         'cs.seq',
@@ -63,7 +61,7 @@ class QueueIterator(MultiOpenMixin, Iterable[Any]):
 
   def __init__(self, q, name=None):
     if name is None:
-      name = "QueueIterator-%d" % (seq(),)
+      name = f'{self.__class__.__name__}-{seq()}'
     self.q = q
     self.name = name
     self.finalise_later = True
@@ -75,7 +73,7 @@ class QueueIterator(MultiOpenMixin, Iterable[Any]):
     self.__lock = Lock()
 
   def __str__(self):
-    return "%s(%r:q=%s)" % (type(self).__name__, self.name, self.q)
+    return f'{self.__class__.__name__}({self.name!r},q={self.q})'
 
   @not_closed
   def put(self, item, *args, **kw):
@@ -124,13 +122,13 @@ class QueueIterator(MultiOpenMixin, Iterable[Any]):
       warning("%s: Queue_Empty: %s", self, e)
       self._put(self.sentinel)
       # pylint: disable=raise-missing-from
-      raise StopIteration("Queue_Empty: %s" % (e,))
+      raise StopIteration(f'{self}.get: queue.Empty: {e}') from e
     if item is self.sentinel:
       # sentinel consumed (clients won't see it, so we must)
       self.q.task_done()
       # put the sentinel back for other consumers
       self._put(self.sentinel)
-      raise StopIteration("SENTINEL")
+      raise StopIteration(f'{self}.get: SENTINEL')
     with self.__lock:
       self._item_count -= 1
     return item
@@ -144,7 +142,7 @@ class QueueIterator(MultiOpenMixin, Iterable[Any]):
       return next(self)
     except StopIteration as e:
       # pylint: disable=raise-missing-from
-      raise Queue_Empty("got %s from %s" % (e, self))
+      raise Queue_Empty(f'{self}._get: {e}') from e
 
   def empty(self):
     ''' Test if the queue is empty.
@@ -184,6 +182,7 @@ class QueueIterator(MultiOpenMixin, Iterable[Any]):
 
   def iter_batch(self, batch_size=1024):
     ''' A generator which yields batches of items from the queue.
+        The default `batch_size` is `1024`.
     '''
     while True:
       batch = self.next_batch(batch_size=batch_size, block_once=True)
