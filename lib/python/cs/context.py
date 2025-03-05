66b754c4aee34fbf3decfb6282027ac3c613664a
Cameron Simpson <cs@cskk.id.au>
2025-03-06 10:50:03 +1100

    cs.context: bump __version__ to 20250306-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/context.py
+++ lib/python/cs/context.py
@@ -21,7 +21,7 @@ from typing import Callable, Iterable
 from cs.deco import decorator
 from cs.gimmicks import error
 
-__version__ = '20250306'
+__version__ = '20250306-post'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
