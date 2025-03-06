3aa552122faa6f4b755d3c3004f80f76b7fe01ce
Cameron Simpson <cs@cskk.id.au>
2025-03-06 12:21:06 +1100

    Release information for cs.cmdutils-20250306. Summary: If a subcommand raises GetoptError, only show the usage for that subcommand.

--- lib/python/cs/cmdutils.py
+++ lib/python/cs/cmdutils.py
@@ -77,7 +77,7 @@ from cs.threads import HasThreadState, ThreadState
 from cs.typingutils import subtype
 from cs.upd import Upd, uses_upd, print  # pylint: disable=redefined-builtin
 
-__version__ = '20250103-post'
+__version__ = '20250306'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
