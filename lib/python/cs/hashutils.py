65ce2da4d4a6e310ea9f29e4bf10fc2de9ddf2d3
Cameron Simpson <cs@cskk.id.au>
2025-04-14 08:48:56 +1000

    cs.hashutils: bump __version__ to 20250414.1-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/hashutils.py
+++ lib/python/cs/hashutils.py
@@ -41,7 +41,7 @@ from cs.buffer import CornuCopyBuffer
 from cs.deco import promote
 from cs.lex import r
 
-__version__ = '20250414.1'
+__version__ = '20250414.1-post'
 
 DISTINFO = {
     'keywords': ["python3"],
