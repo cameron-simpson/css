dbec18bdac4dd88b31e919ca5bb92e5e976de69f
Cameron Simpson <cs@cskk.id.au>
2025-04-28 11:14:51 +1000

    cs.gimmicks: bump __version__ to 20250428-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/gimmicks.py
+++ lib/python/cs/gimmicks.py
@@ -6,7 +6,7 @@ Gimmicks and hacks to make some of my other modules more robust and
 less demanding of others.
 '''
 
-__version__ = '20250428'
+__version__ = '20250428-post'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
