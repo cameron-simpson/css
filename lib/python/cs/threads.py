24da1eab74fa6693b6cb495fe172181cbed0787c
Cameron Simpson <cs@cskk.id.au>
2025-05-28 20:14:55 +1000

    cs.threads: bump __version__ to 20250528-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/threads.py
+++ lib/python/cs/threads.py
@@ -34,7 +34,7 @@ from cs.py.func import funcname, prop
 from cs.py.stack import caller
 from cs.seq import Seq
 
-__version__ = '20250528'
+__version__ = '20250528-post'
 
 DISTINFO = {
     'description':
