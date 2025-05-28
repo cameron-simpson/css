bd1909cfe7757ad02c227de87a1f8c85bbd895c4
Cameron Simpson <cs@cskk.id.au>
2025-05-28 20:48:55 +1000

    Release information for cs.progress-20250528. Summary: progressbar: also run the progress bar if report_print is true in order to get the report (was optimised out when the Upd was disabled).

--- lib/python/cs/progress.py
+++ lib/python/cs/progress.py
@@ -49,7 +49,7 @@ from cs.units import (
 )
 from cs.upd import Upd, uses_upd, print  # pylint: disable=redefined-builtin
 
-__version__ = '20250412-post'
+__version__ = '20250528'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
