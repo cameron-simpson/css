e2cf9f3c34c5a9229204597c76d09384e0a503fd
Cameron Simpson <cs@cskk.id.au>
2025-05-31 10:44:59 +1000

    Release information for cs.cmdutils-20250531.1. Summary: Fix precedence of subcommand options over inherited common options.

--- lib/python/cs/cmdutils.py
+++ lib/python/cs/cmdutils.py
@@ -80,7 +80,7 @@ from cs.threads import HasThreadState, ThreadState
 from cs.typingutils import subtype
 from cs.upd import Upd, uses_upd, print  # pylint: disable=redefined-builtin
 
-__version__ = '20250531-post'
+__version__ = '20250531.1'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
