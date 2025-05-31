eb9d42720960f94c1688643f7c445ee8266d27b7
Cameron Simpson <cs@cskk.id.au>
2025-05-31 14:37:01 +1000

    Release information for cs.hashindex-20250531. Summary: hashindex rsync: bugfix remote rearrange, rearranges dstdir into dstdir using the source index.

--- lib/python/cs/hashindex.py
+++ lib/python/cs/hashindex.py
@@ -107,7 +107,7 @@ from cs.upd import (
     run_task,  # pylint: disable=redefined-builtin
 )
 
-__version__ = '20250528-post'
+__version__ = '20250531'
 
 DISTINFO = {
     'keywords': ["python3"],
