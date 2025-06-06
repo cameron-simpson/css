4ea057990d90c75add79d87475fd00f958b90281
Cameron Simpson <cs@cskk.id.au>
2025-06-06 15:19:24 +1000

    Release information for cs.djutils-20250606. Summary: model_batches_qs: new after=None parameter to indicate preiteration point. model_instances: new optional parameters prefetch_related and select_related to augument the querysets before iteration.

--- lib/python/cs/djutils.py
+++ lib/python/cs/djutils.py
@@ -29,7 +29,7 @@ from cs.cmdutils import BaseCommand as CSBaseCommand
 from cs.gimmicks import warning
 from cs.lex import cutprefix, stripped_dedent
 
-__version__ = '20250219-post'
+__version__ = '20250606'
 
 DISTINFO = {
     'keywords': ["python3"],
