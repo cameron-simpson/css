f8d9b92b580aba128442dbbdc4d6b418ee2890a1
Cameron Simpson <cs@cskk.id.au>
2025-07-24 11:31:17 +1000

    cs.py.func: bump __version__ to 20250724-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/py/func.py
+++ lib/python/cs/py/func.py
@@ -10,7 +10,7 @@ Convenience facilities related to Python functions.
 
 from functools import partial
 
-__version__ = '20250724'
+__version__ = '20250724-post'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
