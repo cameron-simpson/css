d76359049e2d792d06108b2c3fe96d2892323a27
Cameron Simpson <cs@cskk.id.au>
2025-12-30 08:54:50 +1100

    Release information for cs.deco-20251230. Summary: New @with_(func,obj) decorator to call func inside with obj:, primarily for Thread targets.

--- lib/python/cs/deco.py
+++ lib/python/cs/deco.py
@@ -18,7 +18,7 @@ import typing
 from cs.gimmicks import warning
 from cs.typingutils import is_optional
 
-__version__ = '20250914-post'
+__version__ = '20251230'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
