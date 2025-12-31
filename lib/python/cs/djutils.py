a9c1eced7b14804d3d1ed4ec1acaaaa4a184ea14
Cameron Simpson <cs@cskk.id.au>
2025-12-31 15:28:06 +1100

    Release information for cs.djutils-20251231. Summary: New IndexByPK mixin for Models, supporting Model[pk].

--- lib/python/cs/djutils.py
+++ lib/python/cs/djutils.py
@@ -29,7 +29,7 @@ from cs.cmdutils import BaseCommand as CSBaseCommand
 from cs.gimmicks import warning
 from cs.lex import cutprefix, stripped_dedent
 
-__version__ = '20250724-post'
+__version__ = '20251231'
 
 DISTINFO = {
     'keywords': ["python3"],
