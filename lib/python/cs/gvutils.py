84c5997b8d8dd75b959d4e9c027739b38d339a15
Cameron Simpson <cs@cskk.id.au>
2026-01-09 08:43:57 +1100

    cs.gvutils: new Node and Graph classes for constructing a graphviz style node graph

--- lib/python/cs/gvutils.py
+++ lib/python/cs/gvutils.py
@@ -8,14 +8,15 @@
 '''
 
 from base64 import b64encode
+from dataclasses import dataclass, field
 from os.path import exists as existspath
 from subprocess import Popen, PIPE
 import sys
 from threading import Thread
-from typing import Mapping
+from typing import Any, Mapping, Optional, Tuple
 from urllib.parse import quote as urlquote
 
-from cs.lex import cutprefix, cutsuffix, r
+from cs.lex import cutprefix, cutsuffix, indent as indent_text, r
 
 __version__ = '20230816-post'
 
@@ -326,3 +327,206 @@ class DOTNodeMixin:
         and a common implementation might return `self.name` or similar.
     '''
     return str(self)
+
+@dataclass
+class Node(DOTNodeMixin):
+  id: str
+  rankdir: str = "LR"
+  shape: str = "rect"
+  attrs: dict = field(default_factory=dict)
+
+  def __str__(self):
+    return self.as_dot()
+
+  def __hash__(self):
+    return id(self)
+
+  def __eq__(self, other):
+    return self is other
+
+  @property
+  def dot_node_id(self) -> str:
+    return self.id
+
+  def as_dot(self, *, no_attrs=False):
+    dot = []
+    node_id = self.dot_node_id
+    if node_id:
+      dot.append(quote(node_id))
+      if not no_attrs:
+        for k, v in sorted(self.attrs.items()):
+          dot.append(f'{k}={quote(v)}')
+    return " ".join(dot)
+
+@dataclass
+class Graph:
+  ''' A representation of a graphviz graph suitable for transcribing as DOT.
+  '''
+  id: Optional[str] = None
+  digraph: bool = False
+  strict: bool = False
+  attrs: dict = field(default_factory=dict)
+  node_attrs: dict = field(default_factory=dict)
+  edge_attrs: dict = field(default_factory=dict)
+  nodes: Mapping[str, Node] = field(default_factory=dict)
+  edges: list[Tuple[list, dict]] = field(default_factory=list)
+  subgraphs: Mapping[str, "Graph"] = field(default_factory=dict)
+
+  def __str__(self):
+    return self.as_dot()
+
+  def __hash__(self):
+    return id(self)
+
+  def __eq__(self, other):
+    return self is other
+
+  def __getitem__(self, node_id: str):
+    return self.nodes[node_id]
+
+  def add(self, *items):
+    ''' Add a `Node` id or a `Node`s or `Graph`s to `self.nodes`.
+    '''
+    for item in items:
+      if isinstance(item, str):
+        if item not in self.nodes:
+          node = Node(item)
+          self.nodes[item] = node
+      elif isinstance(item, Node):
+        if (node := self.nodes.get(item.id)) is None:
+          self.nodes[item.id] = item
+        elif node is not item:
+          raise ValueError(f'self.nodes[{item.id!r}] already exists')
+      elif isinstance(item, Graph):
+        if (graph := self.subgraphs.get(item.id)) is None:
+          self.subgraphs[item.id] = item
+        elif graph is not item:
+          raise ValueError(f'self.subgraphs[{item.id!r}] already exists')
+      else:
+        raise TypeError(f'not a Node or Graph: {r(item)}')
+
+  def join(self, *items, **attrs):
+    ''' Join the specified `Node`s, `Node` ids or `Graph`s in an edge.
+    '''
+    if len(items) < 2:
+      raise ValueError(
+          f'at least 2 items required to join, received {len(items)}'
+      )
+    edge_nodes = []
+    for item in items:
+      if isinstance(item, str):
+        if item in self.nodes:
+          item = self.nodes[item]
+        else:
+          node = Node(item)
+          self.nodes[item] = node
+          item = node
+      elif not isinstance(item, (Node, Graph)):
+        raise TypeError(f'not a Node or Graph or Node id: {r(item)}')
+      edge_nodes.append(item)
+    self.edges.append((edge_nodes, attrs))
+
+  @staticmethod
+  def mapping_as_dot(kv: Mapping[str, Any]):
+    ''' Transcribe a mapping as DOT i.e. an `a_list`.
+    '''
+    return ", ".join(f'{k}={quote(v)}' for k, v in sorted(kv.items()))
+
+  def as_dot(
+      self,
+      *,
+      fold=False,
+      indent="",
+      subindent="  ",
+      graphtype=None,
+  ) -> str:
+    ''' Return a DOT representation of this `Graph`.
+
+        Parameters:
+        * `fold`: default `False`; if true then produce indented multiline text
+        * `indent`: the prevailing indent if `fold`, default `""`
+        * `subindent`: incremental indent of nested items if `fold`, default `"  "`
+    '''
+    dot = [
+        " ".join(
+            filter(
+                None, (
+                    ('strict' if self.strict else ''),
+                    graphtype or ('digraph' if self.digraph else 'graph'),
+                    self.id and quote(self.id),
+                    '{',
+                )
+            )
+        )
+    ]
+    # graph wide object defaults
+    if self.attrs:
+      dot.append(f'graph [ {self.mapping_as_dot(self.attrs)} ]')
+    if self.node_attrs:
+      dot.append(f'node [ {self.mapping_as_dot(self.node_attrs)} ]')
+    if self.edge_attrs:
+      dot.append(f'edge [ {self.mapping_as_dot(self.edge_attrs)} ]')
+    # define nodes and their attributes
+    dot.extend(node.as_dot() for node in self.nodes.values())
+    # define subgraphs and their attributes
+    for graph in self.subgraphs.values():
+      dot.append(
+          graph.as_dot(fold=fold, subindent=subindent, graphtype='subgraph')
+      )
+    for edge_nodes, edge_attrs in self.edges:
+      edge_dot = []
+      first = True
+      for node in edge_nodes:
+        if first:
+          first = False
+        else:
+          edge_dot.append('->' if self.digraph else '--')
+        if isinstance(node, Node):
+          edge_dot.append(node.as_dot(no_attrs=True))
+        elif isinstance(node, Graph):
+          edge_dot.append(
+              node.as_dot(
+                  fold=fold,
+                  indent=indent + subindent,
+                  subindent=subindent,
+                  graphtype='subgraph'
+              )
+          )
+        else:
+          raise TypeError(f'unhandled egde node {r(node)}')
+      edge_dot.append(self.mapping_as_dot(edge_attrs))
+      dot.append(" ".join(edge_dot))
+    dot.append('}')
+    if not fold:
+      return " ".join(dot)
+    line0, *midlines, linez = dot
+    return indent + f'\n{indent}'.join(
+        (
+            line0,
+            *(indent_text(line, subindent) for line in midlines),
+            linez,
+        )
+    )
+
+  def print(self, **gvprint_kw):
+    dot_s = self.as_dot()
+    return gvprint(dot_s, **gvprint_kw)
+
+if __name__ == '__main__':
+  G = Graph(digraph=True)
+  G.attrs.update(rankdir="LR")
+  G.node_attrs.update(shape="rect")
+  G.node_attrs["b"] = "c"
+  g1 = Graph()
+  g1.add("ga", "gb")
+  g2 = Graph("cluster-g2")
+  g2.add("x", "y")
+  G.add("a", "b", g1, g2)
+  G["a"].attrs["x"] = 1
+  G.join("a", "b", "c")
+  G.join("b", g1)
+  G.join("gb", g2)
+  print(G.as_dot(fold=True))
+  for layout in 'neato fdp sfdp circo twopi osage patchwork dot'.split():
+    print("LAYOUT", layout)
+    G.print(layout=layout)
