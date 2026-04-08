d5d365fcd97a37d70e5c15840dbba58bfb4a6b62
Cameron Simpson <cs@cskk.id.au>
2026-04-09 09:49:45 +1000

    cs.debug: tabulate_obj: big cleanup of how objects are decomposed

--- lib/python/cs/debug.py
+++ lib/python/cs/debug.py
@@ -31,11 +31,13 @@ The allowed names are the list `cs.debug.__all__` and include:
 from __future__ import print_function
 import builtins
 from cmd import Cmd
+from collections import defaultdict
 from contextlib import redirect_stdout
 import inspect
 import logging
 import os
 from pprint import pformat, pprint  # pylint: disable=unused-import
+import re
 from subprocess import Popen, PIPE
 import sys
 from threading import (
@@ -49,7 +51,7 @@ import traceback
 from types import SimpleNamespace as NS
 from typing import Mapping, Sequence
 
-from cs.deco import ALL, decorator
+from cs.deco import ALL, attr, decorator
 from cs.fs import shortpath
 from cs.lex import (
     cropped_repr,
@@ -882,71 +884,131 @@ if builtin_names_s:
       setattr(builtins, builtin_name, vs[builtin_name])
 
 @ALL
+@attr(
+    # types whose values we check by value not id()
+    BASIC_TYPES=(int, float, str),
+    # a regexp to match name(x=1,y=2,...) repr()s
+    REPR_re=re.compile(r'[._\w]+\((([\w_]+=\S+, ?)*[\w_]+=\S+)\)$')
+)
 def tabulate_obj(obj, label=None, *, seen=None):
   ''' Tabulate the contents of an object for display via `cs.lex.printt()`.
   '''
-  if seen is None:
-    seen = set()
+  BASIC_TYPES = tabulate_obj.BASIC_TYPES
+  ##print(type(obj), repr(obj))
   if label is None:
     label = f'{obj.__class__.__name__}:{id(obj)}'
-  if isinstance(obj, type):
-    yield [label]
-    return
+  if seen is None:
+    seen = set()
   if id(obj) in seen:
-    yield [
-        label, "" if isinstance(obj, (int, float, str)) else "(already seen)"
-    ]
+    ##print("SEEN")
+    yield [label, "" if isinstance(obj, BASIC_TYPES) else "(already seen)"]
     return
   seen.add(id(obj))
-  try:
-    is_container = isinstance(obj, Mapping | Sequence)
-  except RecursionError as e:
-    ##warning(f'isinstance(obj:{type(obj)}, Mapping | Sequence): {e}')
-    yield [label, '(isinstance failure)']
-    return
-  if is_container:
-    yield [label]
-    subrows = []
-    if isinstance(obj, Mapping):
-      try:
-        ivs = sorted(obj.items())
-      except TypeError:
-        ivs = list(obj.items())
-    elif isinstance(obj, Sequence):
-      ivs = enumerate(obj)
-    else:
-      raise RuntimeError('unhandled object {r(obj)}')
-    for i, v in ivs:
-      if isinstance(v, Mapping | Sequence):
-        subrows.extend(tabulate_obj(v, str(i), seen=seen))
-      else:
-        subrows.append([i, v])
-    if subrows:
-      yield tuple(subrows)
-  else:
-    kvs = None
+  objcls = obj.__class__
+
+  # dotted attributes
+  attrmap = None
+  if not isinstance(obj, BASIC_TYPES):
     try:
-      objdict = obj.__dict__
+      attrmap = obj.__dict__
     except AttributeError:
-      objcls = obj.__class__
+      # TODO: unpack descriptors from dir(type(obj))?
       try:
-        objslots = objcls.__slots__
+        clsslots = objcls.__slots__
       except AttributeError:
-        yield [label, obj]
-        return
+        pass
       else:
-        kvs = [(f'{name}', getattr(obj, name)) for name in objslots]
+        if clsslots:
+          attrmap = {name: getattr(obj, name) for name in clsslots}
     else:
-      kvs = objdict.items()
-    if kvs is not None:
-      objattrs = {f'.{name}': value for name, value in kvs}
-      yield from tabulate_obj(objattrs, label=label, seen=seen)
+      attrmap = {
+          attr: value
+          for attr, value in attrmap.items()
+          if not attr.startswith('_')
+      }
+    if not attrmap:
+      # recognised namedtuples and their ilk via repr()
+      m = tabulate_obj.REPR_re.match(repr(obj))
+      if m:
+        attrmap = {}
+        for fv in re.split(', ?', m.group(1)):
+          f, v = fv.split('=', 1)
+          try:
+            v = int(v)
+          except ValueError:
+            try:
+              v = float(v)
+            except ValueError:
+              pass
+          attrmap[f] = v
+  assert attrmap is None or isinstance(attrmap, Mapping), (
+      f'attrmap should be a Mapping but is {type(attrmap)} {attrmap!r}'
+  )
+
+  # collection members
+  items = None
+  if isinstance(obj, Mapping):
+    try:
+      items = sorted(obj.items())
+    except TypeError:
+      items = list(obj.items())
+  # sequences, excluding strings which just make more strings
+  elif not isinstance(obj, str) and isinstance(obj, Sequence):
+    items = list(enumerate(obj))
+  assert items is None or isinstance(items, Sequence)
+
+  if not attrmap and not items:
+    yield [label, obj]
+  else:
+    yield [label, f'{obj.__class__.__name__}:{id(obj)}']
+
+    attrs_by_value_id = defaultdict(list)  # indexed rows to subsume
+    attrs_by_value = defaultdict(list)  # indexed rows to subsume
+    attr_indices = defaultdict(list)  # attrs for values
+    if attrmap is None:
+      attrmap = {}
+    else:
+      for attr, value in sorted(attrmap.items()):
+        if not attr.startswith('_'):
+          if isinstance(value, BASIC_TYPES):
+            attrs_by_value[value].append(attr)
+          else:
+            attrs_by_value_id[id(value)].append(attr)
+    if items is None:
+      items = ()
+    else:
+      # figure out which values have been seen
+      for index, value in items:
+        if isinstance(value, BASIC_TYPES):
+          attr_names = attrs_by_value.get(value, ())
+        else:
+          attr_names = attrs_by_value_id.get(id(value), ())
+        for attr in attr_names:
+          attr_indices[attr].append(index)
+    # generate the listing
+    subrows = []
+    # list the attributes
+    used_indices = set()
+    for attr, indices in sorted(attrmap.items()):
+      label = f'.{attr}'
+      indices = attr_indices.get(attr, ())
+      if indices:
+        label += f', also [{",".join(map(repr, indices))}]'
+        used_indices.update(indices)
+      subrows.extend(tabulate_obj(attrmap[attr], label, seen=seen))
+    # list the unmentioned indices
+    for index, value in items:
+      if index not in used_indices:
+        subrows.extend(tabulate_obj(value, f'[{index!r}]', seen=seen))
+    yield tuple(subrows)
 
 @ALL
 def print_obj(obj, label=None):
   ''' Call `tabulate_obj(obj,label=label)` and pass to `cs.lex.printt()`.
   '''
-  return printt(*tabulate_obj(obj, label=label))
+  table = list(tabulate_obj(obj, label=label))
+  ##pprint(table, indent=2)
+  return printt(*table)
 
 def selftest(module_name, defaultTest=None, argv=None):
   ''' Called by my unit tests.
