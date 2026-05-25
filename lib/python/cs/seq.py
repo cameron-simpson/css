13f733db8b1dcc4d64f5261552ff9018655df06a
Cameron Simpson <cs@cskk.id.au>
2026-05-25 11:32:04 +1000

    cs.seq: bump __version__ to 20260525-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/seq.py
+++ lib/python/cs/seq.py
@@ -25,7 +25,7 @@ from cs.deco import decorator
 from cs.gimmicks import warning
 from cs.typingutils import Sortable
 
-__version__ = '20260525'
+__version__ = '20260525-post'
 
 DISTINFO = {
     'description':
