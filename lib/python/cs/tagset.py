034f172bd8f0d378e0f93c7317df4cc891c24276
Cameron Simpson <cs@cskk.id.au>
2026-05-31 09:17:08 +1000

    Release information for cs.tagset-20260531. Summary: New TagSetTyping mixin for TagSets providing .type_name, .type_key, .type_zone and .type_subname properties based on partitioning .name into zone.type_subname.key; inherited by TagSet. New HasTags mixin for classes whose instances have a .tags attribute which is a TagSet, proxying several mapping methods .deref, and __getattr__ to the .tags. BaseTagSets: new deref(TagSet) method to dereference values in a TagSet's tags to their corresponding TagSets. New UsesTagSets, the bulk of UsesSQLTags, deref which promotes the result of BaseTagSets.deref and relies on subclasses to provide a .find(), such as SQLTags.find. TagSet: drop get_value() and get_format_attribute(). New TagSet.json() and HasTags.json() methods, usable as format conversions. New HasTags.printt() method calling cs.lex.printt(). TagSet: new printt() method. jsonable: support transcribe datetime.date. tagset,fstags,sqltags: make _id only special to SQLTagSet (the db row id), drop all mentions elsewhere. TagSet.dump: make as obsolete, prefer TagSet.printt. TagSet and HasTags:: make Refreshable, drop the .is_stale() method, superceded by .refresh_needed(). TagFile.save_tagset: have atomic_filename fall back to overwrite on PermissionError, uses new write_tagsets class method. HasTags: new .print() method for a nice summary, default calls .printt(). Many other small changes.

--- lib/python/cs/tagset.py
+++ lib/python/cs/tagset.py
@@ -337,7 +337,7 @@ from cs.py3 import date_fromisoformat, datetime_fromisoformat
 from cs.resources import MultiOpenMixin, openif
 from cs.threads import locked_property
 
-__version__ = '20250528-post'
+__version__ = '20260531'
 
 DISTINFO = {
     'keywords': ["python3"],
