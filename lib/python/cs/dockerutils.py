8f426804aa14408995f632c392cb058ce3c31d7e
Cameron Simpson <cs@cskk.id.au>
2026-05-31 15:49:38 +1000

    cs.dockerutils: bump __version__ to 20260531-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/dockerutils.py
+++ lib/python/cs/dockerutils.py
@@ -30,7 +30,7 @@ from cs.context import stackattrs
 from cs.pfx import Pfx, pfx, pfx_method
 from cs.psutils import run
 
-__version__ = '20260531'
+__version__ = '20260531-post'
 
 DISTINFO = {
     'keywords': ["python3"],
