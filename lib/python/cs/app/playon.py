a47dbb31a2ced04772738f5234bd1731070a0157
Cameron Simpson <cs@cskk.id.au>
2026-05-31 11:08:08 +1000

    Release information for cs.app.playon-20260531. Summary: DOwnloads: use cs.rfc2616.content_length to get the download content-length, open the download file with 'xb'. PlayOnAPI is now the source of the sqltags, do not expect to access it outside. New _PlayOnEntity(HasSQLTags) base class for Recording and LoginState. PlayOnAPI: new .login_userid property for external use. PlayOnAPI.__getitem__: an int index is promoted to ('recording,str(index)) for lookup. PlayOnAPI: new .recordings() finding playon.recording.*; use it for __iter__. PlayOnAPI: new .recording_ids_from_str() method for use by the CLI. PlayOnAPI: use the new UsesTagSets stuff. The PlayOn API has started reciting times with a microsecond field. PlayOnAPI.from_playon_date: accomodate time formats with and without microseconds. Recording.STALE_AGE -> Recording.refresh_lifespan for Refreshable. PlayOnCommand.cmd_dl: use pmap() to run the downloads in parallel. Assorted other internal changes.

--- lib/python/cs/app/playon.py
+++ lib/python/cs/app/playon.py
@@ -58,7 +58,7 @@ from cs.threads import monitor, bg as bg_thread, pmap
 from cs.units import BINARY_BYTES_SCALE
 from cs.upd import print, run_task  # pylint: disable=redefined-builtin
 
-__version__ = '20241007-post'
+__version__ = '20260531'
 
 DISTINFO = {
     'keywords': ["python3"],
