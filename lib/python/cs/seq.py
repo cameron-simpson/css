1acfb2dd73a6b460ce2ca46b68f9172a17937611
Cameron Simpson <cs@cskk.id.au>
2026-06-22 14:40:22 +1000

    Release information for cs.seq-20260622. Summary: Rename ClonedIteraor to ReIterable, sugestion by λambda @ python.discord. New consume(Iterable) function to "run" an iterable without storing the results. New consumed(Iterator) returning an Iterator with the last consumed item and the tail of the original iterator.

--- lib/python/cs/seq.py
+++ lib/python/cs/seq.py
@@ -25,7 +25,7 @@ from cs.deco import decorator, OBSOLETE
 from cs.gimmicks import warning
 from cs.typingutils import Sortable
 
-__version__ = '20260526-post'
+__version__ = '20260622'
 
 DISTINFO = {
     'description':
