6b0f76d1d6ec83a89256bbdae3e42c4b4b864f25
Cameron Simpson <cs@cskk.id.au>
2026-07-19 10:33:53 +1000

    cs.deco: bump __version__ to 20260719-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/deco.py
+++ lib/python/cs/deco.py
@@ -18,7 +18,7 @@ import typing
 from cs.gimmicks import warning
 from cs.typingutils import is_optional
 
-__version__ = '20260719'
+__version__ = '20260719-post'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
