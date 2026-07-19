c4fbd44910afce3a74ddeb06ad97bf578da99bd4
Cameron Simpson <cs@cskk.id.au>
2026-07-19 10:33:40 +1000

    Release information for cs.deco-20260719. Summary: OBSOLETE: also mark the obsolete function as deprecated, accomodate classes as well as functions.

--- lib/python/cs/deco.py
+++ lib/python/cs/deco.py
@@ -18,7 +18,7 @@ import typing
 from cs.gimmicks import warning
 from cs.typingutils import is_optional
 
-__version__ = '20260622-post'
+__version__ = '20260719'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
