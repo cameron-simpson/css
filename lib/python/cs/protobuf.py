308c110d4880a5b892a48247f7caa22b0562bb2a
Cameron Simpson <cs@cskk.id.au>
2026-08-24 08:38:49 +1000

    cs.protobuf: make a proper Protobuf binary class

--- lib/python/cs/protobuf.py
+++ lib/python/cs/protobuf.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-''' A simple minded protocol buffer decoder.
+''' A simple minded protocol buffer encoder/decoder.
 
     I'm basing this off:
     https://protobuf.dev/programming-guides/encoding/
@@ -9,7 +9,7 @@
 from enum import IntEnum
 from typing import Tuple
 
-from cs.binary import BinarySingleValue
+from cs.binary import AbstractBinary, BinarySingleValue
 from cs.buffer import CornuCopyBuffer
 from cs.deco import promote
 from cs.trace import Trace
@@ -21,34 +21,6 @@ class WireType(IntEnum):
   SGROUP = 3
   EGROUP = 4
   I32 = 5
-
-@Trace
-@promote
-def parse(bfr: CornuCopyBuffer, *, T: Trace):
-  record_hdr = VarInt.parse_value(bfr)
-  T("record_hdr", hex(record_hdr))
-  wire_type = record_hdr & 0x07
-  T("wire_type", wire_type)
-  field_number = record_hdr >> 3
-  T("field_number", field_number)
-  if wire_type == WireType.VARINT:
-    value = VarInt.parse_value(bfr)
-    T("VARINT", value)
-  elif wire_type == WireType.I64:
-    value = bfr.take(8)  # yet to decode into a number
-    T("I64", value)
-  elif wire_type == WireType.LEN:
-    length = VarInt.parse_value(bfr)
-    value = []
-    with Trace(f'LEN {length} records') as T2:
-      for i in range(length):
-        value.append(parse(bfr))
-  elif wire_type == WireType.I32:
-    value = bfr.take(4)  # yet to decode into a number
-    T("I32", value)
-  else:
-    raise ValueError(f'unsupported {wire_type=}')
-  return {"wire_type": wire_type, field_number: value}
 
 class VarInt(BinarySingleValue, value_type=int):
 
@@ -104,6 +76,66 @@ class VarInt(BinarySingleValue, value_type=int):
         break
     return bytes(bns)
 
+class Protobuf(AbstractBinary):
+  ''' A Google Protobuf.
+
+      See: https://protobuf.dev/programming-guides/encoding/
+  '''
+
+  def __init__(self, wire_type: int, field_number: int, value):
+    if field_number < 0:
+      raise ValueError(f'{field_number=} must be >= 0')
+    self.wire_type = WireType(wire_type)
+    self.field_number = field_number
+    self.value = value
+
+  @classmethod
+  @Trace
+  @promote
+  def parse(cls, bfr: CornuCopyBuffer, *, T: Trace):
+    record_hdr = VarInt.parse_value(bfr)
+    T("record_hdr", hex(record_hdr))
+    wire_type = record_hdr & 0x07
+    T("wire_type", wire_type)
+    field_number = record_hdr >> 3
+    T("field_number", field_number)
+    if wire_type == WireType.VARINT:
+      value = VarInt.parse_value(bfr)
+      T("VARINT", value)
+    elif wire_type == WireType.I64:
+      value = bfr.take(8)  # yet to decode into a number
+      T("I64", value)
+    elif wire_type == WireType.LEN:
+      length = VarInt.parse_value(bfr)
+      value = []
+      with Trace(f'LEN {length} records') as T2:
+        for i in range(length):
+          value.append(cls.parse(bfr))
+    elif wire_type == WireType.I32:
+      value = bfr.take(4)  # yet to decode into a number
+      T("I32", value)
+    else:
+      raise ValueError(f'unsupported {wire_type=}')
+    return cls(wire_type, field_number, value)
+
+  def transcribe(self):
+    wire_type = self.wire_type
+    record_hdr = (self.field_number << 3) | wire_type
+    yield VarInt.transcribe_value(record_hdr)
+    if wire_type == WireType.VARINT:
+      yield VarInt.transcribe_value(self.value)
+    elif wire_type == WireType.I64:
+      yield self.value  # should b 8 bytes
+    elif wire_type == WireType.LEN:
+      length = len(self.value)
+      yield VarInt.transcribe_value(length)
+      for subvalue in self.value:
+        yield subvalue.transcribe()
+    elif wire_type == WireType.I32:
+      yield self.value  # should be 4 bytes
+    else:
+      raise ValueError(f'unsupported {wire_type=}')
+
 if __name__ == '__main__':
   bs = b'\x08\x022\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
   ##import compression
@@ -114,7 +146,7 @@ if __name__ == '__main__':
   while not bfr.at_eof():
     try:
       with Trace("decode") as T:
-        value = parse(bfr)
+        pbuf = Protobuf.parse(bfr)
     finally:
       T.printt()
-    print(repr(value))
+    print(repr(pbuf))
