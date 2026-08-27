27ea2b971603faede55d584aa0445b35f3a15fe9
Cameron Simpson <cs@cskk.id.au>
2026-08-27 10:56:59 +1000

    cs.dateutils: new as_datetime() to convert various things into a timezone aware datetime, hauled in from cs.feeds

--- lib/python/cs/dateutils.py
+++ lib/python/cs/dateutils.py
@@ -9,7 +9,7 @@
     you're welcome to it, but it does not pretend to be large or complete.
 '''
 
-from datetime import date, datetime, tzinfo, timedelta
+from datetime import date, datetime, tzinfo, timedelta, timezone
 from time import localtime, mktime, strftime
 
 __version__ = '20250724-post'
@@ -92,6 +92,43 @@ def localdate2unixtime(d):
   ''' Convert a localtime `date` into a UNIX timestamp.
   '''
   return mktime(date(d.year, d.month, d.day).timetuple())
+
+def as_datetime(
+    dt: float | str | date | datetime,
+    *,
+    tz: tzinfo = timezone.utc
+) -> datetime:
+  ''' Turn a value into a timezone aware datetime.
+
+      Parameters:
+      * `dt`: the `datetime` specification, a `str` or `float` or `date` or `datetime`
+      * `tz`: optional `tzinfo` specifying the target timezone, default UTC
+
+      The conversion of `dt` is as follows:
+      * `datetime`: `dt.astimezone(tz=tz)`
+      * `date`: rather arbitrarily assume it is in the target timezone
+      * `float`: a UNIX timestamp (seconds since the epoch)
+      * `str`: try `datetime.fromisoformat` then `datetime.strptime("%a, %d %b %Y %H:%M:%S %z")`
+  '''
+  # turn dt into a UTC datetime
+  if isinstance(dt, datetime):
+    dt = dt.astimezone(tz=tz)
+  elif isinstance(dt, date):
+    # pretend the date is a UTC date
+    # TODO: I would to assume a localtime date (for no very good reason)
+    # but that seems... surprisingly hard to do.
+    dt = datetime(dt.year, dt.month, dt.day, tz=tz)
+  elif isinstance(dt, float):
+    dt = datetime.fromtimestamp(dt, tz=tz)
+  elif isinstance(dt, str):
+    try:
+      dt = datetime.fromisoformat(dt)
+    except ValueError:
+      dt = datetime.strptime("%a, %d %b %Y %H:%M:%S %z")
+    dt = dt.astimezone(tz=tz)
+  else:
+    raise TypeError(f'cannot convert {type(dt).__name__}:{dt!r} to a datetime')
+  return dt
 
 class UNIXTimeMixin:
   ''' A mixin for classes with a `.unixtime` attribute,
