e38430d8bed34bb9d50bbb54fc148d3276bd920a
Cameron Simpson <cs@cskk.id.au>
2026-09-12 12:26:32 +1000

    cs.tty: bump __version__ to 20260912-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/tty.py
+++ lib/python/cs/tty.py
@@ -20,7 +20,7 @@ import sys
 from termios import tcsetattr, tcgetattr, TCSANOW, TIOCGWINSZ
 from cs.gimmicks import warning
 
-__version__ = '20260912'
+__version__ = '20260912-post'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
