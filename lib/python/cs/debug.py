bd6589071a88f6d3f57a27809ccd11e986e76ea1
Cameron Simpson <cs@cskk.id.au>
2026-09-12 14:12:42 +1000

    Release information for cs.debug-20260912. Summary: New tabulate_class and print_class. Get Trace from cs.trace.

--- lib/python/cs/debug.py
+++ lib/python/cs/debug.py
@@ -82,7 +82,7 @@ from cs.trace import Trace
 from cs.upd import print  # pylint: disable=redefined-builtin
 from cs.x import X
 
-__version__ = '20260602-post'
+__version__ = '20260912'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
