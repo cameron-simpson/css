4a945f6a8fb75efcb4878d55b9e9870b547f40fb
Cameron Simpson <cs@cskk.id.au>
2026-09-12 12:33:31 +1000

    Release information for cs.upd-20260912. Summary: Move the ticker facility into UpdProxy (new UpdProxy.ticker() context manager), add new ticker_deferred=False parameter to run_task to allow users to allowate a slot an run the ticker just during the operation.

--- lib/python/cs/upd.py
+++ lib/python/cs/upd.py
@@ -92,7 +92,7 @@ except ImportError as curses_e:
   warning("cannot import curses: %s", curses_e)
   curses = None
 
-__version__ = '20260526-post'
+__version__ = '20260912'
 
 DISTINFO = {
     'keywords': ["python2", "python3"],
