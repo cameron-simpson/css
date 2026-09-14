0752d40a5a3d9e971af72504e0f334635e0e16c5
cs@cskk.id.au <cs@cskk.id.au>
2026-09-15 09:59:45 +1000

    Release information for cs.buffer-20260915.1. Summary: Doc fix.

--- lib/python/cs/buffer.py
+++ lib/python/cs/buffer.py
@@ -24,7 +24,7 @@ from typing import List
 from cs.deco import Promotable
 from cs.gimmicks import Buffer, r, warning
 
-__version__ = '20260915-post'
+__version__ = '20260915.1'
 
 DISTINFO = {
     'keywords': ["python3"],
