671a5e8f446436454ce6805b16faea8eceddef70
cs@cskk.id.au <cs@cskk.id.au>
2026-09-14 20:04:02 +1000

    cs.app.playon: bump __version__ to 20260914-post to avoid misleading value for future unreleased changes [IGNORE]

--- lib/python/cs/app/playon.py
+++ lib/python/cs/app/playon.py
@@ -56,7 +56,7 @@ from cs.threads import pmap
 from cs.units import BINARY_BYTES_SCALE
 from cs.upd import print, run_task  # pylint: disable=redefined-builtin
 
-__version__ = '20260914'
+__version__ = '20260914-post'
 
 DISTINFO = {
     'keywords': ["python3"],
